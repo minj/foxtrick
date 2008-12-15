989b7dea3eb6fe299db129da679494cfa68ff5dc
bummerland <bummerland@gmail.com>
2008-12-15 19:08:56 +0000

    Some layout enhancements on teampopuplinks.js.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@152 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/teampopuplinks.js
+++ content/shortcuts_and_tweaks/teampopuplinks.js
@@ -28,7 +28,7 @@ var FoxtrickTeamPopupLinks = {
         var head = doc.getElementsByTagName("head")[0];
 		var style = doc.createElement("style");
 		style.setAttribute("type", "text/css");
-		var zaw = 'span.myht1 {position: relative} div.myht2 {display: none} span.myht1:hover div.myht2 {display: block; width: 70px; position: absolute; left: 35px; top: -46px; background-color: #FFFFFF; border: solid 1px #267F30; padding: 2px}';
+		var zaw = 'span.myht1 {position: relative} div.myht2 {display: none} span.myht1:hover div.myht2 {display: inline; width: 80px; position: absolute; left: 20px; top: -54px; background-color: #FFFFFF; border: solid 1px #267F30; padding: 0px; z-index:999}';
 		style.appendChild(doc.createTextNode(zaw));
 		head.appendChild(style);
 		var aLinks = doc.getElementsByTagName('a'); //doc.links;
@@ -52,28 +52,45 @@ var FoxtrickTeamPopupLinks = {
 					continue;
 				}
 				
-					
+				var tbl = doc.createElement("table");
+				tbl.setAttribute("cell-padding", "2");
+				tbl.setAttribute("cell-spacing", "0");
+				var tr1 = doc.createElement("tr");
+				var td1 = doc.createElement("td");
 				var a1 = doc.createElement("a");
 				a1.setAttribute('href', '/Club/Matches/?TeamID=' + value);
 				a1.appendChild(doc.createTextNode(Foxtrickl10n.getString( 'Matches' )));
-				
+				td1.appendChild(a1);
+				tr1.appendChild(td1);
+				tbl.appendChild(tr1);
+				var tr2 = doc.createElement("tr");
+				var td2 = doc.createElement("td");
 				var a2 = doc.createElement("a");
 				a2.setAttribute('href', '/Club/Players/?TeamID=' + value);
 				a2.appendChild(doc.createTextNode(Foxtrickl10n.getString( 'Players' )));
+				td2.appendChild(a2);
+				tr2.appendChild(td2);
+				tbl.appendChild(tr2);
 				
+				var tr3 = doc.createElement("tr");
+				var td3 = doc.createElement("td");
 				var a3 = doc.createElement("a");
 				a3.setAttribute('href', '/Club/Manager/?teamId=' + value+'&ShowOldConnections=true');
 				a3.appendChild(doc.createTextNode(Foxtrickl10n.getString( 'last_5_ips' )));
+				td3.appendChild(a3);
+				tr3.appendChild(td3);
+				tbl.appendChild(tr3);
 				
 				var span = doc.createElement("span");
 				span.setAttribute('class', 'myht1');
 				var div = doc.createElement("div");
-				div.setAttribute('class', 'myht2');
-				div.appendChild(a1);
-				div.appendChild(doc.createElement("br"));
-				div.appendChild(a2);
-				div.appendChild(doc.createElement("br"));
-				div.appendChild(a3);
+				div.setAttribute('class', 'mainBox myht2');
+//				div.appendChild(a1);
+//				div.appendChild(doc.createElement("br"));
+//				div.appendChild(a2);
+//				div.appendChild(doc.createElement("br"));
+//				div.appendChild(a3);
+				div.appendChild(tbl);
 				par.insertBefore(span, aLinks[i]);
 				span.appendChild(sLink);
 				span.appendChild(div);
