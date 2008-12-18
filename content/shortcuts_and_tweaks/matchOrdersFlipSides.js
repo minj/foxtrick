e634ac824d4c8740bcc9b1ab83c512d1546916c7
larswortel <larswortel@gmail.com>
2008-12-18 20:40:18 +0000

    Bugfix, see #29
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@223 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/matchOrdersFlipSides.js
+++ content/shortcuts_and_tweaks/matchOrdersFlipSides.js
@@ -16,18 +16,24 @@ FoxtrickFlipSidesInMatchOrders = {
 
     run : function(page, doc) {
     
-        Foxtrick.addStyleSheet(doc, "chrome://foxtrick/content/resources/css/matchorders.css");
-        Foxtrick.addJavaScript(doc, "chrome://foxtrick/content/resources/js/matchorders.js");
+		var startLineupElement = doc.getElementById('startlineup');
+		if(startLineupElement) {
+			Foxtrick.addStyleSheet(doc, "chrome://foxtrick/content/resources/"
+				+ "css/matchorders.css");
+			Foxtrick.addJavaScript(doc, "chrome://foxtrick/content/resources/"
+				+ "js/matchorders.js");
     
-        var div = doc.createElement("div");
-        div.className = "foxtrick-flipsides";
+			var div = doc.createElement("div");
+			div.className = "foxtrick-flipsides";
           
-        var flipsides = doc.createElement("a");
-        flipsides.innerHTML = Foxtrickl10n.getString("foxtrick.matchorders.flipsides");
-        flipsides.href="javascript:FoxtrickMatchOrders.flipSides()";
-        div.appendChild(flipsides);
+			var flipsides = doc.createElement("a");
+			flipsides.innerHTML = Foxtrickl10n.getString("foxtrick.matchorders"
+				+ ".flipsides");
+			flipsides.href="javascript:FoxtrickMatchOrders.flipSides()";
+			div.appendChild(flipsides);
 
-        doc.getElementById('startlineup').appendChild(div);
+			startLineupElement.appendChild(div);
+		}
      }
 
 };
