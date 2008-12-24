7b8cd15a970ac7ca55238500c43de5d5dbd7c464
bummerland <bummerland@gmail.com>
2008-12-24 13:47:13 +0000

    Some enhancements to confirmplayerbid.js Changed height of preferences dialog to 450.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@410 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/confirmplayerbid.js
+++ content/shortcuts_and_tweaks/confirmplayerbid.js
@@ -25,9 +25,18 @@ var FoxtrickConfirmPlayerBid = {
             	var submitButton = doc.getElementById("ctl00_CPMain_btnBid");
             	if (submitButton){
             		var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
-            		sConfirmString = Foxtrickl10n.getString( "foxtrick.tweaks.Really_bid" ) + " (' + document.getElementById('ctl00_CPMain_txtBid').value + ')";
-            		sOnclick = "if (confirm('" + sConfirmString +"')){" + sOnclick + "} else {return false;}";
-            		submitButton.setAttribute("onClick", sOnclick);
+            		if (sOnclick.search(/confirm/) == -1){ // already added?
+	            		sConfirmString = Foxtrickl10n.getString( "foxtrick.bidconfirmation" );
+	            		sReplace = "document.getElementById('ctl00_CPMain_txtBid').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
+//	            		dump(doc.getElementById('ctl00_CPMain_txtBid').parentNode.innerHTML+"\n");
+//	            		sUnit = doc.getElementById('ctl00_CPMain_txtBid').nextSibling.data;
+//	            		sUnit = Foxtrick.trim(sUnit).replace(/\&nbsp\;/, "");
+//	            		dump(sUnit+"\n");
+//	            		sReplace  = sReplace + sUnit;
+	            		sStr = "var str = '"+sConfirmString+"';";
+	            		sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + "} else {return false;}";
+	            		submitButton.setAttribute("onClick", sOnclick);
+	            	}
             	}
               break;
         }
@@ -37,4 +46,3 @@ var FoxtrickConfirmPlayerBid = {
 	
 	}
 };
-
