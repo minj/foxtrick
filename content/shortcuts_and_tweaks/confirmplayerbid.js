2c244b2b3fe5ca7677282a631b90158bf3cd7fe3
larswortel <larswortel@gmail.com>
2008-12-27 00:18:36 +0000

    Added TL confirmation bid to confirmplayerbid.js
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@439 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/confirmplayerbid.js
+++ content/shortcuts_and_tweaks/confirmplayerbid.js
@@ -17,32 +17,54 @@ var FoxtrickConfirmPlayerBid = {
 
     run : function( page, doc ) {
 		
-        // var doc = Foxtrick.current_doc;
-
-        switch( page )
-        {
-            case 'playerdetail':
-            	var submitButton = doc.getElementById("ctl00_CPMain_btnBid");
-            	if (submitButton){
-            		var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
-            		if (sOnclick.search(/confirm/) == -1){ // already added?
-	            		sConfirmString = Foxtrickl10n.getString( "foxtrick.bidconfirmation" );
-	            		sReplace = "document.getElementById('ctl00_CPMain_txtBid').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
-//	            		dump(doc.getElementById('ctl00_CPMain_txtBid').parentNode.innerHTML+"\n");
-//	            		sUnit = doc.getElementById('ctl00_CPMain_txtBid').nextSibling.data;
-//	            		sUnit = Foxtrick.trim(sUnit).replace(/\&nbsp\;/, "");
-//	            		dump(sUnit+"\n");
-//	            		sReplace  = sReplace + sUnit;
-	            		sStr = "var str = '"+sConfirmString+"';";
-	            		sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + "} else {return false;}";
-	            		submitButton.setAttribute("onClick", sOnclick);
-	            	}
-            	}
-              break;
+        var submitButton = doc.getElementById("ctl00_CPMain_btnBid");
+        if (submitButton){
+        	var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
+        	if (sOnclick.search(/confirm/) == -1){ // already added?
+	       		sConfirmString = Foxtrickl10n.getString( "foxtrick.bidconfirmation" );
+	       		sReplace = "document.getElementById('ctl00_CPMain_txtBid').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
+	       		sStr = "var str = '"+sConfirmString+"';";
+	       		sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + "} else {return false;}";
+	       		submitButton.setAttribute("onClick", sOnclick);
+	       	}
         }
 	},
 	
 	change : function( page, doc ) {
 	
+	}
+};
+
+/**
+* Adds player transferlist confirm box
+* @author larsw84
+*/
+var FoxtrickConfirmTL = {
+	
+	MODULE_NAME : "ConfirmTL",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	DEFAULT_ENABLED : true,
+
+    init : function() {
+        Foxtrick.registerPageHandler( 'playerdetail',
+                                      FoxtrickConfirmTL );
+    },
+	
+	run : function( page, doc ) {
+		
+	},
+	
+	change : function( page, doc ) {
+		var submitButton = doc.getElementById("ctl00_CPSidebar_ucOwnerActions_btnSell");
+		if (submitButton){
+        	var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
+        	if (sOnclick.search(/confirm/) == -1){ // already added?
+	       		sConfirmString = Foxtrickl10n.getString( "foxtrick.tlconfirmation" );
+	       		sReplace = "document.getElementById('ctl00_CPSidebar_ucOwnerActions_txtPrice').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
+	       		sStr = "var str = \""+sConfirmString+"\";";
+				sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + ";} else {return false;}";
+				submitButton.setAttribute("onClick", sOnclick);
+	       	}
+        }
 	}
 };
