1bfdf04d38523186ee01d5ef81f2c6cc6e7d74b3
larswortel <larswortel@gmail.com>
2009-01-03 14:16:40 +0000

    * Two small bugfixes
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@492 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/largeflags.js
+++ content/shortcuts_and_tweaks/largeflags.js
@@ -20,26 +20,28 @@ var FoxtrickLargeFlags = {
 		for( var i = 0; i < allDivs.length; i++ ) {
 			if( allDivs[i].className == "byline" ) {
 				var link = allDivs[i].getElementsByTagName("a")[0];
-				var img = link.childNodes[0];
-				var oldStyle = img.style.background;
-				var newStyle = "transparent url(chrome://foxtrick/content/"
-				    + "resources/img/largeflags.png) no-repeat scroll ";
-				var firstPos = oldStyle.search("scroll ")+7;
-				var lastPos = oldStyle.search("px");
-				var pos = oldStyle.substr(firstPos, (lastPos-firstPos));
-				newPos = parseInt(pos) / 20 * 105;
-				newStyle = newStyle + newPos + "px 0pt";
-				img.style.background = newStyle;
-				img.style.width = "105px";
-				img.style.height = "70px";
-				// Move the link
-				var br1 = doc.createElement("br");
-				var br2 = doc.createElement("br");
-				allDivs[i].appendChild(br1);
-				allDivs[i].appendChild(br2);
-				allDivs[i].appendChild(link);
-				allDivs[i].setAttribute("style", "margin-left: 4px; "
+				if(link) {
+					var img = link.childNodes[0];
+					var oldStyle = img.style.background;
+					var newStyle = "transparent url(chrome://foxtrick/content/"
+						+ "resources/img/largeflags.png) no-repeat scroll ";
+					var firstPos = oldStyle.search("scroll ")+7;
+					var lastPos = oldStyle.search("px");
+					var pos = oldStyle.substr(firstPos, (lastPos-firstPos));
+					newPos = parseInt(pos) / 20 * 105;
+					newStyle = newStyle + newPos + "px 0pt";
+					img.style.background = newStyle;
+					img.style.width = "100px";
+					img.style.height = "70px";
+					// Move the link
+					var br1 = doc.createElement("br");
+					var br2 = doc.createElement("br");
+					allDivs[i].appendChild(br1);
+					allDivs[i].appendChild(br2);
+					allDivs[i].appendChild(link);
+					allDivs[i].setAttribute("style", "margin-left: 4px; "
 										+ "padding-bottom:0px");
+				}
 			}
 		}
 	},
