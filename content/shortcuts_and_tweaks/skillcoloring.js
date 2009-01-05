06655970e03e70b1be9b2a5247a3a80c12778594
daBux <dabux@gmx.de>
2009-01-05 17:17:45 +0000

    skillcoloring => sorry. Forgotten to add it. Shame on me. :(
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@543 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ content/shortcuts_and_tweaks/skillcoloring.js
@@ -0,0 +1,52 @@
+/**
+ * skillcoloring.js
+ * Script which add colorizes skills and shows numbers for the skills
+ * @author spambot, thx to baumanns
+ */
+
+var FoxtrickSkillColoring = {
+	
+    MODULE_NAME : "SkillColoring",
+    MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	DEFAULT_ENABLED : true,
+    OPTIONS : {},
+	
+    init : function() {
+            Foxtrick.registerPageHandler( 'all',
+                                          FoxtrickSkillColoring);
+            this.initOptions();                          
+    },
+
+    run : function( page, doc ) {
+        skillstyles = ["skill-color", "skill-number", "skill-number-selectoption"];
+        displaystyles = [ Foxtrick.isModuleFeatureEnabled( this, "skill_color"), 
+                    Foxtrick.isModuleFeatureEnabled( this, "skill_number"), 
+                    Foxtrick.isModuleFeatureEnabled( this, "skill_select") ];
+        for (var i = 0; i < skillstyles.length; i++) {
+            dump ('SKILLS: ' + i + ' - ' + displaystyles[i] + ' enabled.\n');
+            if (displaystyles[i]) {
+                
+                var css = "chrome://foxtrick/content/resources/skillcolors/" + skillstyles[i] + ".css";
+                var path = "head[1]";
+                var head = doc.evaluate(path,doc.documentElement,null,
+                    doc.DOCUMENT_NODE,null).singleNodeValue;
+
+                var link = doc.createElement("link");
+                link.setAttribute("rel", "stylesheet");
+                link.setAttribute("type", "text/css");
+                link.setAttribute("media", "all");
+                link.setAttribute("href", css);
+                head.appendChild(link);
+            }   
+        }
+	},
+	
+	change : function( page, doc ) {
+	},
+    
+    initOptions : function() {
+		this.OPTIONS = new Array( Foxtrickl10n.getString( "skill_color" ),
+								  Foxtrickl10n.getString( "skill_number" ),
+								  Foxtrickl10n.getString( "skill_select" ));
+	}
+};
