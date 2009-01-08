b0a96f82971a2c0fafcb900ec311421d43eefc22
daBux <dabux@gmx.de>
2009-01-08 12:18:02 +0000

    teamStats => My suggestion for injured Players. Not localized yet! - It's Beta! Feel free to commit a better version. :)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@591 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/teamStats.js
+++ content/shortcuts_and_tweaks/teamStats.js
@@ -55,6 +55,28 @@ var FTTeamStats= {
         for (var spec in specs) {
           specsTable += "<tr><th>" + spec.replace(/\[|\]/g,"") + "</th><td>" + specs[spec] + "</td></tr>";
         }
+        
+        var injuries = getElementsByClass( "injuryBruised", doc );
+        if (injuries.length > 0) {
+          specsTable += "<tr><th>Pflaster </th><td>" + injuries.length + "</td></tr>";
+        }
+
+        var injuries = getElementsByClass( "injuryInjured", doc );
+
+        if (injuries) {
+            var weeks = 0;
+            try {
+                for (var j = 0; j < injuries.length; j++) {
+                    var head = injuries[j].parentNode;
+                    weeks += parseInt(substr(head.innerHTML, strrpos( head.innerHTML, "<span>")+6, 1));              
+                }
+            } 
+            catch(e) {
+                dump(e);
+            }
+        }
+        
+        if (weeks > 0) specsTable += "<tr><th>Verletzt </th><td>" + injuries.length +  " (" + weeks + " w)" + "</td></tr>";
         if (specsTable != "") txt = '<table class="foxtrick-playerlist foxtrick-playerlistspecs">' + specsTable + "</table>";
 
 		
@@ -77,7 +99,6 @@ var FTTeamStats= {
 		NovaVar += '</div>';
 
 		boxrightt.innerHTML = NovaVar + contentDiv;
-
         },
         
         change : function( page, doc ) {
@@ -85,3 +106,37 @@ var FTTeamStats= {
         }
 };
 
+function strrpos( haystack, needle, offset){
+    var i = (haystack+'').lastIndexOf( needle, offset ); // returns -1
+    return i >= 0 ? i : false;
+}
+function substr( f_string, f_start, f_length ) {
+    // http://kevin.vanzonneveld.net
+    // +     original by: Martijn Wieringa
+    // +     bugfixed by: T.Wild
+    // +      tweaked by: Onno Marsman
+    // *       example 1: substr('abcdef', 0, -1);
+    // *       returns 1: 'abcde'
+    // *       example 2: substr(2, 0, -6);
+    // *       returns 2: ''
+ 
+    f_string += '';
+ 
+    if(f_start < 0) {
+        f_start += f_string.length;
+    }
+ 
+    if(f_length == undefined) {
+        f_length = f_string.length;
+    } else if(f_length < 0){
+        f_length += f_string.length;
+    } else {
+        f_length += f_start;
+    }
+ 
+    if(f_length < f_start) {
+        f_length = f_start;
+    }
+ 
+    return f_string.substring(f_start, f_length);
+}
