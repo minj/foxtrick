489d88e0074ceeb9edc85d62798297ce315c32a6
larswortel <larswortel@gmail.com>
2009-01-10 14:09:09 +0000

    Improved the look and placement of forumyouthicons.js
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@665 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumyouthicons.js
+++ content/forum/forumyouthicons.js
@@ -27,22 +27,47 @@ var FoxtrickForumYouthIcons = {
                 
                 var toolbar = getElementsByClass( "HTMLToolbar", doc );
                 toolbar = toolbar[0];
+				toolbar.setAttribute("style","float:left; margin-right:3px;");
+				
+				// Set styles of all buttons
+				var nextElement = toolbar.firstChild;
+				while (nextElement) {
+					try {
+						nextElement.setAttribute("style","margin:2px");
+						nextElement = nextElement.nextSibling;
+					} catch(e) { nextElement = nextElement.nextSibling; }
+				}
+				
+				var toolbar_label = doc.createElement( "div" );
+                toolbar_label.innerHTML = Foxtrickl10n.getString("ForumYouthIcons.labelToolbar");
+                toolbar_label.setAttribute( "style" , "background-color:#FEDCBA;margin-bottom:3px;text-align:center;");
+                toolbar.insertBefore( toolbar_label, toolbar.firstChild );
+							
+				// Set styles of next siblings
+				var nextElement = toolbar.nextSibling;
+				while (nextElement) {
+					try {
+						nextElement.setAttribute("style","clear:both;");
+						nextElement = nextElement.nextSibling;
+					} catch(e) { nextElement = nextElement.nextSibling; }
+				}
                 
                 FoxtrickForumYouthIcons._DOC = doc;
                 
                 var youthbar = doc.createElement( "div" );
                 youthbar.setAttribute( "class" , "HTMLToolbar");
-                youthbar.setAttribute( "style" , "width:130px;height:auto;background-color:#ABCDEF;margin-top:6px;text-align:center;");
+                youthbar.setAttribute( "style" , "float:left;");
 
                 var youthbar_label = doc.createElement( "div" );
                 youthbar_label.innerHTML = Foxtrickl10n.getString("ForumYouthIcons.label");
-                youthbar_label.setAttribute( "style" , "width:130px;height:auto;background-color:#FEDCBA;margin-top:6px;text-align:center;");
+                youthbar_label.setAttribute( "style" , "background-color:#FEDCBA;margin-bottom:3px;text-align:center;");
                 youthbar.appendChild( youthbar_label);                
                 
                 var newimage = doc.createElement( "img" );
                 newimage.src = "/Img/Icons/transparent.gif";
                 newimage.addEventListener( "click", this._youthplayer , false );
                 newimage.setAttribute( "class", "f_player");
+				newimage.setAttribute("style","margin:2px");
                 newimage.title = Foxtrickl10n.getString("ForumYouthIcons.youthplayerid");
                 youthbar.appendChild( newimage );
 
@@ -50,6 +75,7 @@ var FoxtrickForumYouthIcons = {
                 newimage.src = "/Img/Icons/transparent.gif";
                 newimage.addEventListener( "click", this._youthteam , false );
                 newimage.setAttribute( "class", "f_team");
+				newimage.setAttribute("style","margin:2px");
                 newimage.title = Foxtrickl10n.getString("ForumYouthIcons.youthteamid");
                 youthbar.appendChild( newimage );
                 
@@ -57,6 +83,7 @@ var FoxtrickForumYouthIcons = {
                 newimage.src = "/Img/Icons/transparent.gif";
                 newimage.addEventListener( "click", this._youthmatch , false );
                 newimage.setAttribute( "class", "f_match");
+				newimage.setAttribute("style","margin:2px");
                 newimage.title = Foxtrickl10n.getString("ForumYouthIcons.youthmatchid");
                 youthbar.appendChild( newimage );                
                 
@@ -64,14 +91,13 @@ var FoxtrickForumYouthIcons = {
                 newimage.src = "/Img/Icons/transparent.gif";
                 newimage.addEventListener( "click", this._youthseries , false );
                 newimage.setAttribute( "class", "f_series");
+				newimage.setAttribute("style","margin:2px");
                 newimage.title = Foxtrickl10n.getString("ForumYouthIcons.youthseries");
                 youthbar.appendChild( newimage );
-                var head = toolbar.parentNode;
-                				
-                head.insertBefore( youthbar, toolbar );                
 
-                
-                
+                var head = toolbar.parentNode;
+                head.insertBefore( youthbar, toolbar.nextSibling );
+
                 break;
         }
     },
