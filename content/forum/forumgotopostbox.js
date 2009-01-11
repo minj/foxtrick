9759fe3dee0c798cee42d7548c77dd3a3bc9cdd8
bummerland <bummerland@gmail.com>
2009-01-11 20:13:40 +0000

    Added functionality to enter a whole threadid (1234567.89) Added keyup eventlistener so that you can press enter to submit
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@731 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumgotopostbox.js
+++ content/forum/forumgotopostbox.js
@@ -25,20 +25,28 @@ var FoxtrickGoToPostBox = {
 			if (selectBoxBottom)
 				aSelectBoxes.push(selectBoxBottom);
 			for (i=0; i<aSelectBoxes.length;i++){
-				var boxId = 'foxtrick_forum_postbox_' + i;
+				var boxId = 'foxtrick_forum_postbox_postboxnum_' + i;
 				if (doc.getElementById(boxId))
 					continue;
 				var selectBox = aSelectBoxes[i];
 				var inputBoxTop = doc.createElement('input');
+				inputBoxTop.setAttribute('type', 'text');
+				inputBoxTop.setAttribute('size', '3');
 				var goButton = doc.createElement('input');
+				goButton.setAttribute('id', 'foxtrick_forum_postbox_okbutton_' + i);
+				inputBoxTop.setAttribute('id', boxId);
 				goButton.setAttribute('type', 'button');
 				var sTmp = selectBox.getAttribute('onChange');
 				var iTopicId = sTmp.match(/\d+/)[0];
 				goButton.setAttribute('value', 'OK');
-				goButton.setAttribute('onClick', 'location.href="/Forum/Read.aspx?t=' + iTopicId + '&n=" + document.getElementById("' + boxId + '").value + "&v=2#" + document.getElementById("' + boxId + '").value;');
-				inputBoxTop.setAttribute('type', 'text');
-				inputBoxTop.setAttribute('size', '3');
-				inputBoxTop.setAttribute('id', boxId);
+				goButton.setAttribute('onClick', 
+					'var val=document.getElementById("' + boxId + '").value; ' + 
+					'if (val.indexOf(".") > 0) {var aTemp = val.split("."); val = aTemp[0] + "&n=" + aTemp[1];} ' + 
+					'else {val = "' + iTopicId + '&n=" + val} ' + 
+					'location.href="/Forum/Read.aspx?t=" + val + "&v=2"'
+					);
+				
+
 
    				var inputBoxLabel = doc.createElement('span');
                 inputBoxLabel.innerHTML = '&nbsp;' + Foxtrickl10n.getString("foxtrick.GoToPostBox.label") + ':&nbsp;';
@@ -51,11 +59,24 @@ var FoxtrickGoToPostBox = {
                 selectBox.parentNode.appendChild(inputBoxLabel2);
                 
 				selectBox.parentNode.appendChild(goButton);
+				inputBoxTop.addEventListener("keyup" , FoxtrickGoToPostBox._submit, false); 
 			}
 			
 		},
 	
 	change : function( page, doc ) {
 	
-	}
+	},
+	
+	_submit : function(e){
+		 var doc = e.target.ownerDocument;
+		 var key = e.keyCode;
+		 if(key == 13){
+		 	var goButtonID = e.target.getAttribute("id").replace(/postboxnum/, "okbutton");
+		 	var goButton = doc.getElementById(goButtonID);
+		 	if (goButton)
+		  	goButton.click(); 
+		  return false; 
+		 };
+		}
 };
