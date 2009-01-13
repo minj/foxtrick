31810f54a6476f575245018e34cf4f7aeb897bd9
daBux <dabux@gmx.de>
2009-01-13 01:44:49 +0000

    Some improvements for transferListDeadline => now shown on playerdetails.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@772 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/teamStats.js
+++ content/shortcuts_and_tweaks/teamStats.js
@@ -145,38 +145,3 @@ var FTTeamStats= {
         
         }
 };
-
-function strrpos( haystack, needle, offset){
-    var i = (haystack+'').lastIndexOf( needle, offset ); // returns -1
-    return i >= 0 ? i : false;
-}
-function substr( f_string, f_start, f_length ) {
-    // http://kevin.vanzonneveld.net
-    // +     original by: Martijn Wieringa
-    // +     bugfixed by: T.Wild
-    // +      tweaked by: Onno Marsman
-    // *       example 1: substr('abcdef', 0, -1);
-    // *       returns 1: 'abcde'
-    // *       example 2: substr(2, 0, -6);
-    // *       returns 2: ''
- 
-    f_string += '';
- 
-    if(f_start < 0) {
-        f_start += f_string.length;
-    }
- 
-    if(f_length == undefined) {
-        f_length = f_string.length;
-    } else if(f_length < 0){
-        f_length += f_string.length;
-    } else {
-        f_length += f_start;
-    }
- 
-    if(f_length < f_start) {
-        f_length = f_start;
-    }
- 
-    return f_string.substring(f_start, f_length);
-}
--- content/shortcuts_and_tweaks/transferListDeadline.js
+++ content/shortcuts_and_tweaks/transferListDeadline.js
@@ -11,72 +11,104 @@ FoxtrickTransferListDeadline = {
     
     init : function() {
         Foxtrick.registerPageHandler('transferListSearchResult', this);
+        Foxtrick.registerPageHandler('players', this);
     },
 
     run : function(page, doc) {
     
         var httime = doc.getElementById( "time" ).innerHTML;
-        var HY = httime.substr(6,4);
-        var HM = httime.substr(3,2);
-        var HD = httime.substr(0,2);
-        var HH = httime.substr(11,2);
-        var HMn = httime.substr(14,2);
-        var HS = '00';
-        // dump('\nHTTIME:' + httime + ':' + HY + '-' + (HM) + '-' + HD + '-' + HH + '-' + HMn + '-' + HH + '!\n');
-        var HT_date = new Date(HY, HM-1, HD, HH, HMn, HS);
 
-        var spans = doc.getElementsByTagName('span');
-        var j = 0;
-        for (var i=0; i<spans.length; i++) {
-            cell = "ctl00_CPMain_dl_ctrl" + j + "_TransferPlayer_lblDeadline";
-            var selltime_elm = doc.getElementById( cell );
-            if  (selltime_elm == null ) break;
+        HT_date = this._getDatefromCellHTML( httime );
+        if (!HT_date) return;
+        
+        switch ( page ) {
+            case 'transferListSearchResult' :
+                var spans = doc.getElementsByTagName('span');
+                var j = 0;
+                for (var i=0; i<spans.length; i++) {
+                    cell = "ctl00_CPMain_dl_ctrl" + j + "_TransferPlayer_lblDeadline";
+                    var selltime_elm = doc.getElementById( cell );
+                    if  (selltime_elm == null ) return;
 
-            var selltime = selltime_elm.innerHTML;
-            
-            var SY = selltime.substr(6,4);
-            var SM = selltime.substr(3,2);
-            var SD = selltime.substr(0,2);
-            var SH = selltime.substr(11,2);
-            var SMn = selltime.substr(14,2);
-            var SS = '00';
-            // dump('\nSELLTIME:' + selltime + ':' + SY + '-' + (SM) + '-' + SD + '-' + SH + '-' + SMn + '-' + SS + '!\n');
-            var ST_date = new Date(SY, SM-1, SD, SH, SMn, SS);
-
-            var deadline_s = Math.floor( (ST_date.getTime()-HT_date.getTime()) / 1000); //Sec
-            //dump ('\n>>>>>' + deadline_s + '<<<<<\n');
-            var DeadlineText = "<br>";
-            if(deadline_s >= 86400)
-            {
-                var Days = Math.floor(deadline_s/86400);
-                deadline_s = deadline_s-Days*86400;
-
-                if(Days > 1 )
-                {
-                    DeadlineText += Days + "d ";
+                    var selltime = selltime_elm.innerHTML;
+                    
+                    ST_date = this._getDatefromCellHTML( selltime );
+                    if (!ST_date) return;
+                    
+                    var deadline_s = Math.floor( (ST_date.getTime()-HT_date.getTime()) / 1000); //Sec
+                    
+                    var DeadlineText = this._DeadlineToText (deadline_s);
+                    
+                    //dump ('\n>>>>>' + DeadlineText + '<<<<<\n');
+                    selltime_elm.innerHTML +=  '<br><span style="font-weight:bold; color:#800000">' + DeadlineText + '</span>';
+                    j++;
                 }
-            }
-
-            if(deadline_s >= 3600)
-            {
-                var Hours = Math.floor(deadline_s/3600);
-                deadline_s = deadline_s-Hours*3600;
-                DeadlineText += Hours + "h ";
+                break;
+            case 'players' :
                 
-            }
-            if(deadline_s >= 60)
-            {
-                var Minutes = Math.floor(deadline_s/60);
-                deadline_s = deadline_s-Minutes*60;
-                DeadlineText += Minutes + "m ";
-            }
-            //dump ('\n>>>>>' + DeadlineText + '<<<<<\n');
-            selltime_elm.innerHTML +=  '<span style="margin-left:10px; font-weight:bold; color:#800000">' + DeadlineText + '</span>';
-            j++;
+                var spans = getElementsByClass( "alert", doc );
+                if (spans == null) return;
+                
+                var selltime_elm = spans[1].getElementsByTagName( "p" );
+                if (selltime_elm == null) return;
+                
+                selltime = Foxtrick.trim(selltime_elm[0].innerHTML);
+                selltime = substr(selltime, strrpos( selltime, ";")+1, selltime.length);
+                // dump('ST: ' + selltime + '\n');
+
+                ST_date = this._getDatefromCellHTML( selltime );
+                    if (!ST_date) return;
+                    
+                    var deadline_s = Math.floor( (ST_date.getTime()-HT_date.getTime()) / 1000); //Sec
+                    
+                    var DeadlineText = this._DeadlineToText (deadline_s);
+                    
+                    // dump ('\n>>>>>' + DeadlineText + '<<<<<\n');
+                    selltime_elm[0].innerHTML +=  '<span style="margin-left:10px; font-weight:bold; color:#800000">(' + DeadlineText + ')</span>';
+
+                break;
         }
     },
 	
 	change : function( page, doc ) {
 	
-	}
+	},
+    
+    _getDatefromCellHTML : function( cell ) {
+        if (cell == '') return false;
+            var SY = cell.substr(6,4);
+            var SM = cell.substr(3,2);
+            var SD = cell.substr(0,2);
+            var SH = cell.substr(11,2);
+            var SMn = cell.substr(14,2);
+            var SS = '00';
+            // dump('\nSELLTIME:' + cell + ':' + SY + '-' + (SM) + '-' + SD + '-' + SH + '-' + SMn + '-' + SS + '!\n');
+            var CellDate = new Date(SY, SM-1, SD, SH, SMn, SS);
+        return CellDate;
+    },
+        
+    _DeadlineToText : function( deadline_s ) {        
+        var DeadlineText = "";
+        var Days = 0; var Minutes = 0; var Hours = 0;
+        
+        if(deadline_s >= 86400)
+        {
+            Days = Math.floor(deadline_s/86400);
+            deadline_s = deadline_s-Days*86400;
+            DeadlineText += Days + "d ";
+        }
+        
+        if (( deadline_s >= 3600 ) || ( Days > 0 ))
+        {
+            Hours = Math.floor(deadline_s/3600);
+            deadline_s = deadline_s-Hours*3600;
+            DeadlineText += Hours + "h ";
+        }    
+            
+        Minutes = Math.floor(deadline_s/60);
+        deadline_s = deadline_s-Minutes*60;
+        DeadlineText += Minutes + "m";
+        
+        return DeadlineText;
+    }
 };
