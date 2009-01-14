7d8b65988c9e779e77f44669bef3b64bf5800ca0
larswortel <larswortel@gmail.com>
2009-01-14 14:00:46 +0000

    Implementation of the suggestion in #202
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@822 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/largeflags.js
+++ content/shortcuts_and_tweaks/largeflags.js
@@ -33,14 +33,28 @@ var FoxtrickLargeFlags = {
 					img.style.background = newStyle;
 					img.style.width = "105px";
 					img.style.height = "70px";
-					// Move the link
-					var br1 = doc.createElement("br");
-					var br2 = doc.createElement("br");
-					allDivs[i].appendChild(br1);
-					allDivs[i].appendChild(br2);
-					allDivs[i].appendChild(link);
-					allDivs[i].setAttribute("style", "margin-left: 4px; "
+					// Move the link; either to the faceCard div or if that doesn't exist, to the bottom of the current div
+					var faceCardDiv = doc.getElementById('ctl00_CPMain_ucPlayerFace_pnlAvatar');
+					if( faceCardDiv ) {
+						var parentNode = faceCardDiv.parentNode;
+						var nextSibling = faceCardDiv.nextSibling;
+						var ownDiv = doc.createElement("div");
+						ownDiv.setAttribute("style","float:left;");
+						ownDiv.style.width = "110px";
+						ownDiv.style.height = "250px";
+						ownDiv.style.padding = "10px 0px 0px 0px";
+						ownDiv.appendChild( link );
+						ownDiv.appendChild( faceCardDiv );
+						parentNode.insertBefore( ownDiv, nextSibling );
+					} else {
+						var br1 = doc.createElement("br");
+						var br2 = doc.createElement("br");
+						allDivs[i].appendChild(br1);
+						allDivs[i].appendChild(br2);
+						allDivs[i].appendChild(link);
+						allDivs[i].setAttribute("style", "margin-left: 4px; "
 										+ "padding-bottom:0px");
+					}
 				}
 			}
 		}
