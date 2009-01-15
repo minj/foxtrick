674f27f379a12221e8e7ca61d705609eb3cc905e
daBux <dabux@gmx.de>
2009-01-15 00:15:08 +0000

    fixcssproblems => layout switch
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@841 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/fixcssproblems.js
+++ content/shortcuts_and_tweaks/fixcssproblems.js
@@ -17,33 +17,26 @@ FoxtrickFixcssProblems = {
     },
 
     run : function(page, doc) {
-                    
+        var LAYOUTSWITCH = new Array (
+            "standard"
+        );
         for (var i = 0; i < this.OPTIONS.length; i++) {
             
-            
-            if (Foxtrick.isModuleFeatureEnabled( this, this.OPTIONS[i]  ) )
-                dump ('  FIXES: ' + i + ' - ' + this.OPTIONS[i] + ' enabled.\n')
-            else 
-                dump ('  FIXES: ' + i + ' - ' + this.OPTIONS[i] + ' disabled.\n')
-            
             if (Foxtrick.isModuleFeatureEnabled( this, this.OPTIONS[i]  ) ) {
-                
                 var css = "chrome://foxtrick/content/resources/css/fixes/" + this.OPTIONS[i] + ".css";
-
-                var path = "head[1]";
-                var head = doc.evaluate(path,doc.documentElement,null,
-                    doc.DOCUMENT_NODE,null).singleNodeValue;
-
-                var link = doc.createElement("link");
-                link.setAttribute("rel", "stylesheet");
-                link.setAttribute("type", "text/css");
-                link.setAttribute("media", "all");
-                link.setAttribute("href", css);
-                head.appendChild(link);
+                if ( ( (LAYOUTSWITCH[i] == 'standard' ) || (LAYOUTSWITCH[i] == 'all') ) && Foxtrick.isStandardLayout ) {
+                    dump ('  FIXES: (standard) ' + i + ' - ' + this.OPTIONS[i] + ' enabled.\n');
+                    Foxtrick.addStyleSheet( doc, css );
+                } 
+                else if ( ((LAYOUTSWITCH[i] == 'simple' ) || (LAYOUTSWITCH[i] == 'all')) && !Foxtrick.isStandardLayout ) {
+                    dump ('  FIXES: (simple) ' + i + ' - ' + this.OPTIONS[i] + ' enabled.\n');
+                    Foxtrick.addStyleSheet ( doc, css );
+                } 
+                else {
+                    dump ('  FIXES: ' + i + ' - ' + this.OPTIONS[i] + ' disabled.\n');
+                }
             }
-            
         }    
-                    
     },
 	
 	change : function( page, doc ) {
