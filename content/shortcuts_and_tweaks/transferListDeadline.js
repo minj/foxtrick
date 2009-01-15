f2ed3db24192b4a0c5da0aeca466788d8c40bd24
daBux <dabux@gmx.de>
2009-01-15 17:08:31 +0000

    transferListDeadline => Damned leading zeros!
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@871 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/transferListDeadline.js
+++ content/shortcuts_and_tweaks/transferListDeadline.js
@@ -214,7 +214,7 @@ FoxtrickTransferListDeadline = {
         if (cell == '') return false;
             cell +=' ';
             
-            dump ('  CELL :[' + cell + ']\n');
+            // dump ('  CELL :[' + cell + ']\n');
 
             var reg = /(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)/i;
             var ar = reg.exec(cell);
@@ -224,7 +224,7 @@ FoxtrickTransferListDeadline = {
             var SH = ar[7];
 			var SMn = ar[9];
 			var SS = '00';
-             dump('  TIME:' + cell + ' = ' + SY + '-' + SM + '-' + SD + ' ' + SH + ':' + SMn + ':' + SS + '!\n');
+            // dump('  TIME:' + cell + ' = ' + SY + '-' + SM + '-' + SD + ' ' + SH + ':' + SMn + ':' + SS + '!\n');
             var CellDate = new Date(SY, SM-1, SD, SH, SMn, SS);
         return CellDate;
     },
@@ -266,62 +266,67 @@ FoxtrickTransferListDeadline = {
     
     _gregorianToHT : function ( date ) {
         
-       if (date == '') return false;
-       date +=' ';
-            
-       // dump ('  DATE :[' + date + ']\n');
+        if (date == '') return false;
+        date +=' ';
 
-       var reg = /(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)/i;
-       var ar = reg.exec(date);
-       
-       var months = [];
-       var years = [];
+        // dump ('  DATE => :[' + date + ']\n');
 
-       months[1] = 0;
-       months[2] = 31;
-       months[3] = 59;
-       months[4] = 90;
-       months[5] = 120;
-       months[6] = 151;
-       months[7] = 181;
-       months[8] = 212;
-       months[9] = 243;
-       months[10] = 273;
-       months[11] = 304;
-       months[12] = 334;
+        var reg = /(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)(\d+)(.*?)/i;
+        var ar = reg.exec(date);
+        // dump ('  ARRAY => :[' + ar + ']\n');
+        var months = [];
+        var years = [];
 
-       years[0] = 833;         // From 2000
-       years[1] = 1199;
-       years[2] = 1564;
-       years[3] = 1929;
-       years[4] = 2294;
-       years[5] = 2660;
-       years[6] = 3025;
-       years[7] = 3390;
-       years[8] = 3755;
-       years[9] = 4121;
-       years[10] = 4486;       // = 2010
+        months[1] = 0;
+        months[2] = 31;
+        months[3] = 59;
+        months[4] = 90;
+        months[5] = 120;
+        months[6] = 151;
+        months[7] = 181;
+        months[8] = 212;
+        months[9] = 243;
+        months[10] = 273;
+        months[11] = 304;
+        months[12] = 334;
 
-       var day = parseInt(ar[1]);
-       var month = parseInt(ar[3]);
-       var year = parseInt(ar[5]);
-       
-       // dump ('  DATE :[' + year + '-' + month + '-' + day + ']\n');
-       
-       var dayCount = years[year-2000] + months[month] + (day-1);
+        years[0] = 833;         // From 2000
+        years[1] = 1199;
+        years[2] = 1564;
+        years[3] = 1929;
+        years[4] = 2294;
+        years[5] = 2660;
+        years[6] = 3025;
+        years[7] = 3390;
+        years[8] = 3755;
+        years[9] = 4121;
+        years[10] = 4486;       // = 2010
 
-       // leap day
-       if (year % 4 == 0 && month > 2)
-               ++dayCount;
+        for (i = 0; i < ar.length; i++) {
+            ar[i] = ar[i].replace( /^(0+)/g, '' );
+            // dump ('  ARRAY ==> :[' + ar[i] + ']\n');    
+        }
+        
+        var day = parseInt(ar[1]);
+        var month = parseInt(ar[3]);
+        var year = parseInt(ar[5]);
 
-       // This function wont work for dates before season 11
-       if (dayCount < 1120)
-               return this._htDatePrintFormat(date, -1, -1, -1);
+        // dump ('  DATE => :[' + year + '-' + month + '-' + day + ']\n');
 
-       var htDate = this._htDatePrintFormat(date, (Math.floor(dayCount/(16*7)) + 1),
-                      (Math.floor((dayCount%(16*7))/7) + 1), dayCount%7 + 1);
+        var dayCount = years[year-2000] + months[month] + (day-1);
 
-       return htDate;
+        // leap day
+        if (year % 4 == 0 && month > 2)
+            ++dayCount;
+
+        // This function wont work for dates before season 11
+        if (dayCount < 1120)
+           return this._htDatePrintFormat(date, -1, -1, -1);
+
+        var htDate = this._htDatePrintFormat(date, (Math.floor(dayCount/(16*7)) + 1),
+                  (Math.floor((dayCount%(16*7))/7) + 1), dayCount%7 + 1);
+
+        return htDate;
     },
         
     _htDatePrintFormat : function (originalDate, season, week, day) {
