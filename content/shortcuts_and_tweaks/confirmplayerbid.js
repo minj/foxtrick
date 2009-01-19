d7936d2fe4ff18eac2286f254e1a1718951e7863
spambot.de <spambot.de@gmail.com>
2009-01-19 16:11:34 +0000

    confirmplayerbid => added some dumps. Can't confirm #270. It works fine now, after adding try{} ;)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1004 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/confirmplayerbid.js
+++ content/shortcuts_and_tweaks/confirmplayerbid.js
@@ -16,14 +16,18 @@ var FoxtrickConfirmPlayerBid = {
     },
 
     run : function( page, doc ) {
-		var submitButton = doc.getElementById("ctl00_CPMain_btnBid");
-        if (submitButton){
-        	var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
-        	sConfirmString = Foxtrickl10n.getString( "foxtrick.bidconfirmation" );
-	       	sReplace = "document.getElementById('ctl00_CPMain_txtBid').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
-	       	sStr = "var str = '"+sConfirmString+"';";
-	       	sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + "} else {return false;}";
-	       	submitButton.setAttribute("onClick", sOnclick);
+        try {
+            var submitButton = doc.getElementById("ctl00_CPMain_btnBid");
+            if (submitButton){
+                var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
+                sConfirmString = Foxtrickl10n.getString( "foxtrick.bidconfirmation" );
+                sReplace = "document.getElementById('ctl00_CPMain_txtBid').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
+                sStr = "var str = '"+sConfirmString+"';";
+                sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + "} else {return false;}";
+                submitButton.setAttribute("onClick", sOnclick);
+            }
+        } catch( e) {
+            dump('ConfirmPlayerBid ' + e + '\n'); 
         }
 	},
 	
@@ -62,16 +66,20 @@ var FoxtrickConfirmTL = {
 	},
 	
 	change : function( page, doc ) {
-		var submitButton = doc.getElementById("ctl00_CPSidebar_ucOwnerActions_btnSell");
-		if (submitButton){
-        	var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
-        	if (sOnclick.search(/confirm/) == -1){ // already added?
-	       		sConfirmString = Foxtrickl10n.getString( "foxtrick.tlconfirmation" );
-	       		sReplace = "document.getElementById('ctl00_CPSidebar_ucOwnerActions_txtPrice').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
-	       		sStr = "var str = \""+sConfirmString+"\";";
-				sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + ";} else {return false;}";
-				submitButton.setAttribute("onClick", sOnclick);
-	       	}
+		try {
+            var submitButton = doc.getElementById("ctl00_CPSidebar_ucOwnerActions_btnSell");
+            if (submitButton){
+                var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
+                if (sOnclick.search(/confirm/) == -1){ // already added?
+                    sConfirmString = Foxtrickl10n.getString( "foxtrick.tlconfirmation" );
+                    sReplace = "document.getElementById('ctl00_CPSidebar_ucOwnerActions_txtPrice').value.split( '' ).reverse().join( '' ).replace( new RegExp( '(.{' + 3 + '})(?!$)', 'g' ), '$1' + ' ' ).split( '' ).reverse().join( '' )";
+                    sStr = "var str = \""+sConfirmString+"\";";
+                    sOnclick = sStr + " if (confirm(str.replace(/\%s/, " + sReplace + "))){" + sOnclick + ";} else {return false;}";
+                    submitButton.setAttribute("onClick", sOnclick);
+                }
+            }
+        } catch(e) { 
+            dump('ConfirmTL ' + e + '\n'); 
         }
 	}
 };
