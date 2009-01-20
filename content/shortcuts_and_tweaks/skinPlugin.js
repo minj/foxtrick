3ba766f9eca32bbe71cd9ccfb6acf97f5ba99d1f
smates.90 <smates.90@gmail.com>
2009-01-20 11:45:42 +0000

    skinPlugin => tiny changes
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1026 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/skinPlugin.js
+++ content/shortcuts_and_tweaks/skinPlugin.js
@@ -17,59 +17,21 @@ var FoxtrickSkinPlugin = {
     },
 
     run : function( page, doc ) {  
-		var prefObj = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-		var PrefsBranch = prefObj.getBranch("foxtrick.skin.");
-    
-
-		
 		
 		
 var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
 var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
 
+var uri = ios.newURI(FoxtrickPrefs.getString("cssSkin"), null, null);
 
-
-
-var uri = ios.newURI(PrefsBranch.getCharPref("cssSkin"), null, null);
-
-       //chrome://foxtrick/content/resources/skins/
-       //if (PrefsBranch.getBoolPref("useHattrickSkin") == false){}
-       if (!FoxtrickPrefs.getBool("module.SkinPlugin.enabled")){sss.unregisterSheet(uri, sss.USER_SHEET);}
-      // if (!Foxtrick.isModuleFeatureEnabled(this, "ActivatedSkin")){sss.unregisterSheet(uri, sss.USER_SHEET);}
        
-       if (PrefsBranch.getBoolPref("useHattrickSkin")){
+       if (!FoxtrickPrefs.getBool("useSkin")){sss.unregisterSheet(uri, sss.USER_SHEET);}
+       if (FoxtrickPrefs.getBool("module.SkinPlugin.enabled")){
        
 		   sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
 
-
-		
-		
-		
-  			/*var CSSSKIN = PrefsBranch.getCharPref("cssSkin");
-			var css =  CSSSKIN;  
-      
-       //var cssSkin = encodeURIComponent(PrefsBranch.getCharPref("cssSkin"));
-      var css = "chrome://foxtrick/content/skins/retro/Retro.css";
-         
-			var path = "head[1]";
-			var head = doc.evaluate(path,doc.documentElement,null,doc.DOCUMENT_NODE,null).singleNodeValue;
-
-			var link = doc.createElement("link");
-				link.setAttribute("rel", "stylesheet");
-				link.setAttribute("type", "text/css");
-				link.setAttribute("media", "all");
-				link.setAttribute("href", css);
-				head.appendChild(link);
-               
-			try {
-				var headID_s = doc.getElementsByTagName("link")[1];
-					headID_s.parentNode.removeChild(headID_s); 
-            } catch(e) {alert(e);}     */   
-		
-		
-		
 		/*OLD MEDALS SCRIPT*/
-		/*var sidebar = doc.getElementById('sidebar');
+		var sidebar = doc.getElementById('sidebar');
 		if( sidebar ) {
 			var images = sidebar.getElementsByTagName('img');
 			for(var i = 0; i < images.length; i++) {
@@ -86,9 +48,10 @@ var uri = ios.newURI(PrefsBranch.getCharPref("cssSkin"), null, null);
 					img.src = newString + imgSrc;
 				}
 			}
-		}*/
+		}
 		/*END*/
 		}
+		
 	},
 
 		
