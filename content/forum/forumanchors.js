79c7dab6903530ca47b237563c21053d226f617f
spambot.de <spambot.de@gmail.com>
2009-01-21 20:47:10 +0000

    Default disabled, removed dumps
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1096 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumanchors.js
+++ content/forum/forumanchors.js
@@ -8,37 +8,38 @@ var FoxtrickForumAnchors = {
 	
     MODULE_NAME : "ForumAnchors",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
-	DEFAULT_ENABLED : true,
+	DEFAULT_ENABLED : false,
 
     init : function() {
             Foxtrick.registerPageHandler( 'forumViewThread', this );
     },
 
     run : function( page, doc ) {
-		var elems = getElementsByClass("float_right", doc);
-        for (var i = 0; i < elems.length; i++) {
-            try {
-                var time = elems[i].innerHTML.match(/(\d{2})\:(\d{2})/);
-                if ( time[1] != null && time[2] != null ) {
-                    dump(time + '\n');
-                    dump ( elems[i].innerHTML + '\n');
-                    var anchor = doc.createElement( "a" );
-                    anchor.href = "#logo";
-                    anchor.title = "TOP";
-                    
-                    var img = doc.createElement("img");
-                    img.style.padding = "0px 2px 2px 2px;";
-                    img.className = "actionIcon";
-                    img.alt = "anchor_top";
-                    img.title = "Goto Top";
-                    img.src = "chrome://foxtrick/content/resources/linkicons/anchor_top.png";
-                    anchor.appendChild(img);                        
+        var body = doc.getElementById("mainBody");
+        if (body != null) {
+            var elems = getElementsByClass("float_right", body);
+            for (var i = 0; i < elems.length; i++) {
+                try {
+                    var time = elems[i].innerHTML.match(/(\d{2})\:(\d{2})/);
+                    if ( time[1] != null && time[2] != null ) {
+                        var anchor = doc.createElement( "a" );
+                        anchor.href = "#logo";
+                        anchor.title = "TOP";
                         
-                    elems[i].appendChild( anchor );
+                        var img = doc.createElement("img");
+                        img.style.padding = "0px 2px 2px 2px;";
+                        img.className = "actionIcon";
+                        img.alt = "anchor_top";
+                        img.title = "Goto Top";
+                        img.src = "chrome://foxtrick/content/resources/linkicons/anchor_top.png";
+                        anchor.appendChild(img);                        
+                            
+                        elems[i].appendChild( anchor );
+                    }
                 }
-            }
-            catch(e) {
-                dump(e);
+                catch(e) {
+                    // dump(e);
+                }
             }
         }
 	},
