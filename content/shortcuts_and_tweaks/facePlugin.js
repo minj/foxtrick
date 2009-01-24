dd0e035670958811a6001209dbfaaf60a669d3ac
spambot.de <spambot.de@gmail.com>
2009-01-24 15:19:53 +0000

    added strings for TickerColoring and MovePlayerStatement. MovePlayerStatement now deafault DISABLED
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1167 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/facePlugin.js
+++ content/shortcuts_and_tweaks/facePlugin.js
@@ -121,39 +121,40 @@ var FoxtrickMovePlayerStatement = {
 	
     MODULE_NAME : "MovePlayerStatement",
     MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	  DEFAULT_ENABLED : true,
+    DEFAULT_ENABLED : false,
 	
     init : function() {
             Foxtrick.registerPageHandler('playerdetail',this);
-            
     },
 
     run : function( page, doc ) {
-    try {
-		var contentSpeak = "";
-		var elems = doc.getElementsByTagName("em");
-		for(var i=0; i < elems.length; i++) {
-			if( elems[i].getAttribute("class","shy") ) {
-       contentSpeak = elems[i].innerHTML;
-       
-				elems[i].parentNode.removeChild(elems[i]);
-         }
-       }
-   
-         var newImg = doc.createElement("img");
+
+        try {
+            var contentSpeak = "";
+            var elems = doc.getElementsByTagName("em");
+            for(var i=0; i < elems.length; i++) {
+                if( elems[i].getAttribute("class","shy") ) {
+                    contentSpeak = elems[i].innerHTML;
+                    elems[i].parentNode.removeChild(elems[i]);
+                }
+            }
+
+            var newImg = doc.createElement("img");
             newImg.setAttribute("src","chrome://foxtrick/content/resources/img/speak.PNG");
             newImg.setAttribute("title",contentSpeak+"");
             newImg.setAttribute("style","left: 65px; top: 134px;");
-        var elemsa = doc.getElementsByTagName("img");
-	     	for(var b=0; b < elemsa.length; b++) {
-		    	if( elemsa[b].src.match(/\/Avatar\/numbers/i) && contentSpeak != "") {
-			
-		    		elemsa[b].parentNode.appendChild(newImg);
-		    		
-           }
-         }
-		
-		}catch (e){dump('  PlrStatement>' + e + '\n');}
+            var elemsa = doc.getElementsByTagName("img");
+            for ( var b=0; b < elemsa.length; b++) {
+                if( elemsa[b].src.match(/\/Avatar\/numbers/i) && contentSpeak != "") {
+                    elemsa[b].parentNode.appendChild(newImg);
+                }
+            }
+
+        }
+        catch (e) {
+            dump('  PlrStatement >' + e + '\n');
+        }
+        
 	},
 	
 	change : function( page, doc ) {
