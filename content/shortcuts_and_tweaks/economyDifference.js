e4a37a006d99c97610298e33d060eb913911d176
spambot.de <spambot.de@gmail.com>
2009-01-30 14:33:13 +0000

    economyDifference => Added option for +/-
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1303 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/economyDifference.js
+++ content/shortcuts_and_tweaks/economyDifference.js
@@ -9,17 +9,20 @@ var FoxtrickEconomyDifference = {
     MODULE_NAME : "TwoWeekBalance",
     MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
 	DEFAULT_ENABLED : true,
-	
+	RADIO_OPTIONS : new Array("plus", "minus"),
+    
     init : function() {
-            Foxtrick.registerPageHandler( 'finances',
-                                          FoxtrickEconomyDifference);
+            Foxtrick.registerPageHandler( 'finances', this);
     },
 
     run : function( page, doc ) {
 		// localize this
-		const STR_S_TABLE_NAME = Foxtrickl10n.getReturnFormatedValue(
-			"foxtrick.tweaks.twoweekbalance");
-		var cs= FoxtrickPrefs.getReturnFormatedValue("oldCurrencySymbol");//currencysymbol which in the your country
+        
+        var DIFF_OPTION = FoxtrickPrefs.getInt("module." + this.MODULE_NAME + ".value");
+        if (DIFF_OPTION == null) DIFF_OPTION = 0;
+		const STR_S_TABLE_NAME = Foxtrickl10n.getString(
+			"foxtrick.tweaks.twoweekbalance") + '&nbsp;(' + this.RADIO_OPTIONS[DIFF_OPTION] + ')';
+		var cs= FoxtrickPrefs.getString("oldCurrencySymbol");//currencysymbol which in the your country
             
 		/*var tableLast = doc.getElementsByTagName("table")[2];
 		var tableCurr = doc.getElementsByTagName("table")[1];
@@ -52,101 +55,138 @@ var FoxtrickEconomyDifference = {
 		
 		var newRowsDiff = newTabDiff.tBodies[0].rows;
 		
-		var prijmy_ted_vstupne = FoxtrickEconomyDifference.extractAmount(
-			tableCurr.rows[1].cells[1]) + FoxtrickEconomyDifference.
-			extractAmount(tableLast.rows[1].cells[1]);
-		var prijmy_ted_sponzori = FoxtrickEconomyDifference.extractAmount(
-			tableCurr.rows[2].cells[1]) + FoxtrickEconomyDifference.
-			extractAmount(tableLast.rows[2].cells[1]);
-		var prijmy_ted_investice = FoxtrickEconomyDifference.extractAmount(
-			tableCurr.rows[3].cells[1]) + FoxtrickEconomyDifference.
-			extractAmount(tableLast.rows[3].cells[1]);
-		var prijmy_ted_m = FoxtrickEconomyDifference.extractAmount(
-			tableCurr.rows[4].cells[1]) + FoxtrickEconomyDifference.
-			extractAmount(tableLast.rows[4].cells[1]);
-    	var prijmy_all = prijmy_ted_m+prijmy_ted_investice + 
-			prijmy_ted_sponzori+prijmy_ted_vstupne;    
-		
+        if (DIFF_OPTION == 0) {
+            var econ_ted_crowd = this.extractAmount(
+                tableCurr.rows[1].cells[1]) + this.
+                extractAmount(tableLast.rows[1].cells[1]);
+            var econ_ted_sponsors = this.extractAmount(
+                tableCurr.rows[2].cells[1]) + this.
+                extractAmount(tableLast.rows[2].cells[1]);
+            var econ_ted_financial  = this.extractAmount(
+                tableCurr.rows[3].cells[1]) + this.
+                extractAmount(tableLast.rows[3].cells[1]);
+            var econ_ted_temporary = this.extractAmount(
+                tableCurr.rows[4].cells[1]) + this.
+                extractAmount(tableLast.rows[4].cells[1]);
+            var econ_all = econ_ted_temporary+econ_ted_financial  + 
+                econ_ted_sponsors+econ_ted_crowd;    
+        } else {
+            var econ_ted_crowd = this.extractAmount(
+                tableCurr.rows[1].cells[1]) - this.
+                extractAmount(tableLast.rows[1].cells[1]);
+            var econ_ted_sponsors = this.extractAmount(
+                tableCurr.rows[2].cells[1]) - this.
+                extractAmount(tableLast.rows[2].cells[1]);
+            var econ_ted_financial  = this.extractAmount(
+                tableCurr.rows[3].cells[1]) - this.
+                extractAmount(tableLast.rows[3].cells[1]);
+            var econ_ted_temporary = this.extractAmount(
+                tableCurr.rows[4].cells[1]) - this.
+                extractAmount(tableLast.rows[4].cells[1]);
+            var econ_all = econ_ted_temporary+econ_ted_financial  + 
+                econ_ted_sponsors+econ_ted_crowd;    
+        }
 		newTabDiff.rows[1].cells[1].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(prijmy_ted_vstupne) + '>'
-			+ ReturnFormatedValue(prijmy_ted_vstupne,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(econ_ted_crowd) + '>'+ 
+			ReturnFormatedValue(econ_ted_crowd,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[2].cells[1].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(prijmy_ted_sponzori) + '>'
-			+ ReturnFormatedValue(prijmy_ted_sponzori,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(econ_ted_sponsors) + '>' +
+			ReturnFormatedValue(econ_ted_sponsors,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[3].cells[1].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(prijmy_ted_investice) + '>'
-			+ ReturnFormatedValue(prijmy_ted_investice,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(econ_ted_financial ) + '>' + 
+            ReturnFormatedValue(econ_ted_financial ,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[4].cells[1].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(prijmy_ted_m) + '>' + 
-			ReturnFormatedValue(prijmy_ted_m,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(econ_ted_temporary) + '>' + 
+			ReturnFormatedValue(econ_ted_temporary,'&nbsp;') + '&nbsp;' +  cs+'</span>';
         newTabDiff.rows[8].cells[1].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(prijmy_all) + '>' + 
-			ReturnFormatedValue(prijmy_all,'&nbsp;') + '&nbsp;' +  cs+'</span>'; 
+			this.getColorStyle(econ_all) + '>' + 
+			ReturnFormatedValue(econ_all,'&nbsp;') + '&nbsp;' +  cs+'</span>'; 
 
-		var vydaje_arena = FoxtrickEconomyDifference.extractAmount(tableCurr.
-			rows[1].cells[3])+FoxtrickEconomyDifference.extractAmount(
-			tableLast.rows[1].cells[3]);
-		var vydaje_wages = FoxtrickEconomyDifference.extractAmount(tableCurr.
-			rows[2].cells[3])+FoxtrickEconomyDifference.extractAmount(
-			tableLast.rows[2].cells[3]);
-		var vydaje_u = FoxtrickEconomyDifference.extractAmount(tableCurr.
-			rows[3].cells[3])+FoxtrickEconomyDifference.extractAmount(
-			tableLast.rows[3].cells[3]);
-		var vydaje_m = FoxtrickEconomyDifference.extractAmount(tableCurr.
-			rows[4].cells[3])+FoxtrickEconomyDifference.extractAmount(
-			tableLast.rows[4].cells[3]);
-		var vydaje_z = FoxtrickEconomyDifference.extractAmount(tableCurr.
-			rows[5].cells[3])+FoxtrickEconomyDifference.extractAmount(
-			tableLast.rows[5].cells[3]);
-		var vydaje_j = FoxtrickEconomyDifference.extractAmount(tableCurr.
-			rows[6].cells[3])+FoxtrickEconomyDifference.extractAmount(
-			tableLast.rows[6].cells[3]);
-		var vydaje_all = vydaje_arena+vydaje_wages+vydaje_u+vydaje_m+vydaje_z+vydaje_j;
+        if (DIFF_OPTION == 0) {
+            var diff_arena = this.extractAmount(tableCurr.
+                rows[1].cells[3])+this.extractAmount(
+                tableLast.rows[1].cells[3]);
+            var diff_wages = this.extractAmount(tableCurr.
+                rows[2].cells[3])+this.extractAmount(
+                tableLast.rows[2].cells[3]);
+            var diff_u = this.extractAmount(tableCurr.
+                rows[3].cells[3])+this.extractAmount(
+                tableLast.rows[3].cells[3]);
+            var diff_m = this.extractAmount(tableCurr.
+                rows[4].cells[3])+this.extractAmount(
+                tableLast.rows[4].cells[3]);
+            var diff_z = this.extractAmount(tableCurr.
+                rows[5].cells[3])+this.extractAmount(
+                tableLast.rows[5].cells[3]);
+            var diff_j = this.extractAmount(tableCurr.
+                rows[6].cells[3])+this.extractAmount(
+                tableLast.rows[6].cells[3]);
+            var diff_all = diff_arena+diff_wages+diff_u+diff_m+diff_z+diff_j;
+        } else {
+            var diff_arena = this.extractAmount(tableCurr.
+                rows[1].cells[3])-this.extractAmount(
+                tableLast.rows[1].cells[3]);
+            var diff_wages = this.extractAmount(tableCurr.
+                rows[2].cells[3])-this.extractAmount(
+                tableLast.rows[2].cells[3]);
+            var diff_u = this.extractAmount(tableCurr.
+                rows[3].cells[3])-this.extractAmount(
+                tableLast.rows[3].cells[3]);
+            var diff_m = this.extractAmount(tableCurr.
+                rows[4].cells[3])-this.extractAmount(
+                tableLast.rows[4].cells[3]);
+            var diff_z = this.extractAmount(tableCurr.
+                rows[5].cells[3])-this.extractAmount(
+                tableLast.rows[5].cells[3]);
+            var diff_j = this.extractAmount(tableCurr.
+                rows[6].cells[3])-this.extractAmount(
+                tableLast.rows[6].cells[3]);
+            var diff_all = diff_arena+diff_wages+diff_u+diff_m+diff_z+diff_j;
+        }
 
 		newTabDiff.rows[1].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_arena) + '>' + 
-			ReturnFormatedValue(vydaje_arena,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(-diff_arena) + '>' + 
+			ReturnFormatedValue(diff_arena,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[2].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_wages) + '>' + 
-			ReturnFormatedValue(vydaje_wages,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(-diff_wages) + '>' + 
+			ReturnFormatedValue(diff_wages,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[3].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_u) + '>' + 
-			ReturnFormatedValue(vydaje_u,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(-diff_u) + '>' + 
+			ReturnFormatedValue(diff_u,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[4].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_m) + '>' + 
-			ReturnFormatedValue(vydaje_m,'&nbsp;') + '&nbsp;' +  cs+'</span>';
+			this.getColorStyle(-diff_m) + '>' + 
+			ReturnFormatedValue(diff_m,'&nbsp;') + '&nbsp;' +  cs+'</span>';
 		newTabDiff.rows[5].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_z) + '>' + 
-			ReturnFormatedValue(vydaje_z,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
+			this.getColorStyle(-diff_z) + '>' + 
+			ReturnFormatedValue(diff_z,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
 		newTabDiff.rows[6].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_j) + '>' + 
-			ReturnFormatedValue(vydaje_j,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
+			this.getColorStyle(-diff_j) + '>' + 
+			ReturnFormatedValue(diff_j,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
 		newTabDiff.rows[8].cells[3].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(-vydaje_all) + '>' + 
-			ReturnFormatedValue(vydaje_all,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
+			this.getColorStyle(-diff_all) + '>' + 
+			ReturnFormatedValue(diff_all,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
 
-		var twoWeekBalance = prijmy_all - vydaje_all;
+		var twoWeekBalance = econ_all - diff_all;
+
 		newTabDiff.rows[10].cells[1].innerHTML = '<span ' + 
-			FoxtrickEconomyDifference.getColorStyle(twoWeekBalance) + '>' + 
+			this.getColorStyle(twoWeekBalance) + '>' + 
 			ReturnFormatedValue(twoWeekBalance,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
-		
 	},
 	
 	change : function( page, doc ) {
 	
-	}
+	},
+    
+    extractAmount : function( cell ) {
+        return parseInt(cell.textContent.replace(/\s*/g, ""));
+    },
+    
+    getColorStyle : function( val ) {
+        if (val < 0) {
+            return 'style="color: #aa0000;font-weight: bold;"';
+        } else if (val > 0) {
+            return 'style="color: #377f31;font-weight: bold;"';
+        }
+        return '';
+    }        
 };
-
-FoxtrickEconomyDifference.extractAmount = function( cell ) {
-	return parseInt(cell.textContent.replace(/\s*/g, ""));
-}
-
-FoxtrickEconomyDifference.getColorStyle = function( val ) {
-    if (val < 0) {
-		return 'style="color: #aa0000;font-weight: bold;"';
-    } else if (val > 0) {
-        return 'style="color: #377f31;font-weight: bold;"';
-    }
-    return '';
-}
-
