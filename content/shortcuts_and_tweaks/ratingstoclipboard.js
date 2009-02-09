508cc823929fc5bda758bd1163c6c4ce0ab485cd
spambot.de <spambot.de@gmail.com>
2009-02-09 17:54:28 +0000

    ratingstoclipboard => dropped pic/mots/normal
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1527 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/ratingstoclipboard.js
+++ content/shortcuts_and_tweaks/ratingstoclipboard.js
@@ -54,15 +54,19 @@ var FoxtrickCopyRatingsToClipboard = {
         var ad = '[table]';
         var table = doc.getElementsByTagName('table')[0];
         for (var row = 0; row < table.rows.length; row ++) {
-            if (row != table.rows.length-2 && table.rows[row].cells[1] &&  table.rows[row].cells[1].innerHTML.indexOf( '<br>') == -1 )  {
-                //no hatstats detailes and no pic/mots/normal, i hope :)
-                ad += '[tr][td]';
-                if (table.rows[row].cells[0]) ad += table.rows[row].cells[0].textContent;
-                ad += '[/td][td]';
-                if (table.rows[row].cells[1]) ad += table.rows[row].cells[1].textContent;
-                ad += '[/td][td]';
-                if (table.rows[row].cells[2]) ad += table.rows[row].cells[2].textContent;            
-                ad += '[/td][/tr]';
+            if (row != table.rows.length-3 )  {
+                try {
+                    if ( table.rows[row].cells[1] && table.rows[row].cells[1].innerHTML.indexOf( '<br>' ) != -1 ) {} else {
+                    //no hatstats detailes and no pic/mots/normal, i hope :)
+                    ad += '[tr][td]';
+                    if (table.rows[row].cells[0]) ad += table.rows[row].cells[0].textContent;
+                    ad += '[/td][td]';
+                    if (table.rows[row].cells[1]) ad += table.rows[row].cells[1].textContent;
+                    ad += '[/td][td]';
+                    if (table.rows[row].cells[2]) ad += table.rows[row].cells[2].textContent;            
+                    ad += '[/td][/tr]';
+                    }
+                } catch (e) {}
             }
         }
         ad += '[/table]';        
