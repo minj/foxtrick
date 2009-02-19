8e8868bd2af66c8020ec61c1794f66f7a0051359
warheart_8 <warheart_8@hotmail.com>
2009-02-19 07:58:30 +0000

    Changed dialog height 100
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1683 1e3807c3-0949-0410-b78e-337b627472c1

--- content/preferences-dialog.js
+++ content/preferences-dialog.js
@@ -675,7 +675,7 @@ getConverterCurrValue: function (itemToSearch, options, xmlDoc) {
 				}
 				var spacer = doc.createElement('spacer');
 				spacer.setAttribute('flex',0);
-				spacer.setAttribute('height',20);
+				spacer.setAttribute('height',100);
 				
 				modules_list.appendChild( spacer );
     },
