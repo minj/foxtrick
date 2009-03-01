7fc5854db56add472bd0e054058d5fd890b1bf75
convincedd <convincedd@gmail.com>
2009-03-01 19:41:03 +0000

    forgot button for mails
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@1867 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumpreview.js
+++ content/forum/forumpreview.js
@@ -43,17 +43,19 @@ var FoxtrickForumPreview = {
         preview_ctrl_div.style.marginTop = "1em";
 
         var button_ok=doc.getElementById("ctl00_CPMain_btnOK");
-		button_ok.setAttribute( "tabindex",  "11" );
+		var index =11;
+		if (!button_ok) {button_ok=doc.getElementById("ctl00_CPMain_btnSendNew"); index=5;}
+		button_ok.setAttribute( "tabindex",  index);
 		button_ok.setAttribute( "value",  Foxtrickl10n.getString( 'sendmessage'));
 		var button_cancel=doc.getElementById("ctl00_CPMain_btnCancel");
-		button_cancel.setAttribute( "tabindex",  "12" );
+		if (button_cancel) button_cancel.setAttribute( "tabindex",  "12" );
      	
 		var new_button = doc.createElement( "input" );
         new_button.setAttribute( "value", Foxtrickl10n.getString( 'preview' ));
         new_button.setAttribute( "title",  Foxtrickl10n.getString( 'show_preview_from_post' ) );
         new_button.setAttribute( "id",  "idFTPreview" );
         new_button.setAttribute( "type",  "button" );
-        new_button.setAttribute( "tabindex",  "10" );
+        new_button.setAttribute( "tabindex",  index-1 );
      	new_button.addEventListener( "click", FoxtrickForumPreview._toggleListener, false );
         button_ok.parentNode.insertBefore(new_button,button_ok);
 		
