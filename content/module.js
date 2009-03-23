364e8a6024775cccba5c0d385c17ba0816f8e7bc
convincedd <convincedd@gmail.com>
2009-03-23 01:07:58 +0000

    added missing playerspage
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@2085 1e3807c3-0949-0410-b78e-337b627472c1

--- content/module.js
+++ content/module.js
@@ -51,7 +51,7 @@ Foxtrick.ht_pages = {
     'transferListSearchResult'  : '\/World\/Transfers\/TransfersSearchResult.aspx',
     'teamPage'                  : '\/Club\/.TeamID=',
     'teamPageGeneral'           : '\/Club\/|\/World\/Series\/',
-    'players'                   : '\/Club\/Players\/.TeamID=|\/Club\/NationalTeam\/NTPlayers\.aspx|\/Club\/Players\/Oldies\.aspx|\/Club\/Players\/Coaches\.aspx',
+    'players'                   : '\/Club\/Players\/.TeamID=|\/Club\/Players\/default\.aspx.TeamID=|\/Club\/NationalTeam\/NTPlayers\.aspx|\/Club\/Players\/Oldies\.aspx|\/Club\/Players\/Coaches\.aspx',
     'YouthPlayers'              : 'YouthPlayers\.aspx',
     'YouthPlayer'               : 'YouthPlayer\.aspx',
     'training'                  : '\/Club\/Training\/',
