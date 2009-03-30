76fb61eef569fbb2a2e7c242d8f5cdfff468c729
convincedd <convincedd@gmail.com>
2009-03-30 14:17:11 +0000

    option excl. temp to economyDifference.js. adding some comfort
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@2175 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/economyDifference.js
+++ content/shortcuts_and_tweaks/economyDifference.js
@@ -9,20 +9,29 @@ var FoxtrickEconomyDifference = {
     MODULE_NAME : "TwoWeekBalance",
     MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
 	DEFAULT_ENABLED : true,
-	RADIO_OPTIONS : new Array("plus", "minus"),
+	RADIO_OPTIONS : new Array("plus", "minus","plus_exclude_temp", "minus_exclude_temp"),
     
     init : function() {
             Foxtrick.registerPageHandler( 'finances', this);
     },
 
     run : function( page, doc ) {
+			
 		// localize this
         
         var DIFF_OPTION = FoxtrickPrefs.getInt("module." + this.MODULE_NAME + ".value");
+		var exclude_temp=false;
+		var exclude_temp_desc="";
+		if (DIFF_OPTION>1) {
+			DIFF_OPTION-=2;
+			exclude_temp=true;
+			exclude_temp_desc=' '+Foxtrickl10n.getString("foxtrick.TwoWeekBalance.exclude_temp.desc");
+		}
         var DIFF_OPTION_STR = new Array(Foxtrickl10n.getString("foxtrick.TwoWeekBalance.plus.desc"),Foxtrickl10n.getString("foxtrick.TwoWeekBalance.minus.desc"));
-        if (DIFF_OPTION == null) DIFF_OPTION = 0;
+		if (DIFF_OPTION == null) DIFF_OPTION = 0;
+		
 		const STR_S_TABLE_NAME = Foxtrickl10n.getString(
-			"foxtrick.tweaks.twoweekbalance") + '&nbsp;(' + DIFF_OPTION_STR[DIFF_OPTION] + ')';
+			"foxtrick.tweaks.twoweekbalance") + '&nbsp;(' + DIFF_OPTION_STR[DIFF_OPTION] + exclude_temp_desc+')';
 		var cs= FoxtrickPrefs.getString("oldCurrencySymbol");//currencysymbol which in the your country
             
 		/*var tableLast = doc.getElementsByTagName("table")[2];
@@ -30,29 +39,51 @@ var FoxtrickEconomyDifference = {
                 var path = "body/table[0]/tbody/tr[1]/td[2]";*/
 
 
-                var mainBody = doc.getElementById( "mainBody" );
-                var tables = mainBody.getElementsByTagName( "table" );
-                var tableLast = tables[ 2 ];
-                var tableCurr = tables[ 1 ];
+        var mainBody = doc.getElementById( "mainBody" );
+        var tables = mainBody.getElementsByTagName( "table" );
+        var tableLast = tables[ 2 ];
+        var tableCurr = tables[ 1 ];
 
        
-		var heading = doc.createElement("h2");
-		var headingId = "foxtrick_eD_heading";
-		heading.setAttribute("class","tblBox");
-		heading.setAttribute("id",headingId);
-		heading.innerHTML =  STR_S_TABLE_NAME;
-		if( !doc.getElementById( headingId ) ) {
-			tableCurr.parentNode.parentNode.appendChild(heading);
+		var mainbox = doc.getElementById('mainBoxTwoWeekBalance');
+		if (mainbox==null) {
+			mainbox=doc.createElement("div");
+			mainbox.setAttribute("class","mainBox");
+			mainbox.setAttribute("id","mainBoxTwoWeekBalance");
+			tableCurr.parentNode.parentNode.appendChild(mainbox);
+			var heading = doc.createElement("h2");
+			var headingId = "foxtrick_eD_heading";
+			heading.setAttribute("class","tblBox");
+			heading.setAttribute("id",headingId);
+			heading.innerHTML =  STR_S_TABLE_NAME;
+			
+			// add headerclick
+			heading.setAttribute('title',Foxtrickl10n.getString("foxtrick.TwoWeekBalanceToogle"));
+			var div = doc.createElement("div");
+            div.appendChild(heading);
+            div.setAttribute("style","cursor:pointer;");
+            div.setAttribute("id", "div"+headingId);
+            div.addEventListener( "click", this.HeaderClick, false );
+            FoxtrickEconomyDifference.HeaderClick.doc=doc;
+			mainbox.appendChild(div);
+		}
+		else { // update header
+			var heading=doc.getElementById('foxtrick_eD_heading');
+			heading.innerHTML =  STR_S_TABLE_NAME;			
 		}
 		
-		var newTabDiff = tableCurr.cloneNode(true);
-		//var newTabDiff = tableCurr.parentNode.parentNode.appendChild(
-		//	tableCurr.cloneNode(true) );
-		var newTabDiffId = "foxtrick_eD_newTabDiff";
-		newTabDiff.setAttribute("id", newTabDiffId);
-		newTabDiff.setAttribute("style", "margin-bottom: 10px");
-		newTabDiff.setAttribute("class","indent");
-		tableCurr.parentNode.parentNode.appendChild(newTabDiff);
+		// get or copy table
+		var newTabDiff = doc.getElementById('foxtrick_eD_newTabDiff');
+		if (newTabDiff==null) {
+			newTabDiff=tableCurr.cloneNode(true);
+			//var newTabDiff = tableCurr.parentNode.parentNode.appendChild(
+			//	tableCurr.cloneNode(true) );
+			var newTabDiffId = "foxtrick_eD_newTabDiff";
+			newTabDiff.setAttribute("id", newTabDiffId);
+			newTabDiff.setAttribute("style", "margin-bottom: 10px");
+			newTabDiff.setAttribute("class","indent");
+			mainbox.appendChild(newTabDiff);
+		}
 		
 		var newRowsDiff = newTabDiff.tBodies[0].rows;
 		
@@ -69,6 +100,7 @@ var FoxtrickEconomyDifference = {
             var econ_ted_temporary = this.extractAmount(
                 tableCurr.rows[4].cells[1]) + this.
                 extractAmount(tableLast.rows[4].cells[1]);
+			if (exclude_temp) econ_ted_temporary=0;
             var econ_all = econ_ted_temporary+econ_ted_financial  + 
                 econ_ted_sponsors+econ_ted_crowd;    
         } else {
@@ -84,6 +116,7 @@ var FoxtrickEconomyDifference = {
             var econ_ted_temporary = this.extractAmount(
                 tableCurr.rows[4].cells[1]) - this.
                 extractAmount(tableLast.rows[4].cells[1]);
+            if (exclude_temp) econ_ted_temporary=0;
             var econ_all = econ_ted_temporary+econ_ted_financial  + 
                 econ_ted_sponsors+econ_ted_crowd;    
         }
@@ -171,7 +204,8 @@ var FoxtrickEconomyDifference = {
 
 		newTabDiff.rows[10].cells[1].innerHTML = '<span " ' + 
 			this.getColorStyle(twoWeekBalance) + '>' + 
-			ReturnFormatedValue(twoWeekBalance,'&nbsp;') + '&nbsp;' +  cs+'</span>';            
+			ReturnFormatedValue(twoWeekBalance,'&nbsp;') + '&nbsp;' +  cs+'</span>';            						
+			
 	},
 	
 	change : function( page, doc ) {
@@ -189,5 +223,19 @@ var FoxtrickEconomyDifference = {
             return 'style="direction:ltr !important; color: #377f31;font-weight: bold;"';
         }
         return 'style="direction:ltr !important;"';
-    }        
+    } ,  
+
+	HeaderClick : function(evt) {
+		try { 
+			var doc=FoxtrickEconomyDifference.HeaderClick.doc;
+			FoxtrickPrefs.setInt("module." + FoxtrickEconomyDifference.MODULE_NAME + ".value", FoxtrickPrefs.getInt("module." + FoxtrickEconomyDifference.MODULE_NAME + ".value")+1);
+			if (FoxtrickPrefs.getInt("module." + FoxtrickEconomyDifference.MODULE_NAME + ".value")==FoxtrickEconomyDifference.RADIO_OPTIONS.length) 
+				FoxtrickPrefs.setInt("module." + FoxtrickEconomyDifference.MODULE_NAME + ".value",0);
+			
+			//doc.location.reload();
+			FoxtrickEconomyDifference.run("",doc);
+		} 
+		catch (e) {dump("SelectBox->HeaderClick: "+e+'\n');}
+	},
+	
 };
