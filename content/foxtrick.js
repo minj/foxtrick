ae79647fff2d267f9663fc19917ff375c870812e
spambot.de <spambot.de@gmail.com>
2009-06-23 23:49:37 +0000

    added module MatchTables for smaler tables
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@2982 1e3807c3-0949-0410-b78e-337b627472c1

--- content/foxtrick.js
+++ content/foxtrick.js
@@ -1579,3 +1579,16 @@ Foxtrick.getSelectBoxFromXML = function (doc,xmlfile, basenodestr, labelstr, val
 	
 	return selectbox;
 }
+
+Foxtrick.get_url_param = function (url, name){
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( url );
+
+	if ( results == null )
+		return null;
+	else
+		return results[1];
+}
