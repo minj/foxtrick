bd495899b896382904433688f9b9f34616327b7d
spambot.de <spambot.de@gmail.com>
2009-06-25 23:39:55 +0000

    matchtables => some more picels for your eyes :)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3015 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/tables.js
+++ content/shortcuts_and_tweaks/tables.js
@@ -59,3 +59,77 @@ var FoxtrickTables = {
         }
     }
 };
+<<<<<<< .mine
+
+var FoxtrickMatchTables = {
+
+    MODULE_NAME : "MatchTables",
+    MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	PAGES : new Array('matchesarchiv', 'matches'), 
+    DEFAULT_ENABLED : false,
+    NEW_AFTER_VERSION: "0.4.8.3",	
+    LASTEST_CHANGE:"removes white space on match tables",    
+
+    init : function() {
+    },
+    
+    run : function( page, doc ) {
+        var id = "ft_matchtable";
+        if (doc.getElementById(id)) return;
+        
+        try {
+            var div = doc.getElementById('mainBody');
+            if (!div) return;
+            var tbl = div.getElementsByTagName('TABLE')[0];
+            if (!tbl) return;
+            tbl.id = 'ft_matchtable';
+            tbl.setAttribute('class', '');
+            tbl.setAttribute('style', 'margin-left:-6px; margin-right:-6px; padding:0px;width:440px;');
+            
+            var tblBodyObj = tbl.tBodies[0];
+            for (var i=0; i<tblBodyObj.rows.length; i++) {
+                if (tblBodyObj.rows[i].cells[1]) {
+                    var cell = tblBodyObj.rows[i].cells[0];
+                    var content = Foxtrick.trim(cell.innerHTML);
+                    var reg = /(\d{1,4})(.*?)(\d{1,2})(.*?)(\d{1,4})(.*?)/i;
+                    if (content.search(':') > -1) reg = /(\d{1,4})(.*?)(\d{1,2})(.*?)(\d{1,4})(.*?)(\d{2})(.*?)(\d{2})(.*?)/i;
+                    var ar = reg.exec(content);
+                    var DATEFORMAT = FoxtrickPrefs.getString("htDateformat");
+                    if  (DATEFORMAT == null ) DATEFORMAT = 'ddmmyyyy';
+
+                    switch ( DATEFORMAT ) {
+                        case 'ddmmyyyy': 
+                            if (content.search(/\(/) > -1) cell.innerHTML = ar[1] + '.' + ar[3] + '.' + ar[5].substring(2,4) + '&nbsp;<span id="ft_HTDateFormat">(' + content.split('(')[1] + '</span>'; 
+                            else cell.innerHTML = ar[1] + '.' + ar[3] + '.' + ar[5].substring(2,4);
+                            break;
+                        case 'mmddyyyy':
+                            if (content.search(/\(/) > -1) cell.innerHTML = ar[1] + '.' + ar[3] + '.' + ar[5].substring(2,4) + '&nbsp;<span id="ft_HTDateFormat">(' + content.split('(')[1] + '</span>'; 
+                            else cell.innerHTML = ar[1] + '.' + ar[3] + '.' + ar[5].substring(2,4);
+                            break;
+                        case 'yyyymmdd':
+                            if (content.search(/\(/) > -1) cell.innerHTML = ar[1] + '-' + ar[3] + '-' + ar[5].substring(2,4) + '&nbsp;<span id="ft_HTDateFormat">(' + content.split('(')[1] + '</span>'; 
+                            else cell.innerHTML = ar[1] + '-' + ar[3] + '-' + ar[5].substring(2,4);
+                            break;
+                    }
+                    cell.setAttribute('style', "font-size:9px; text-align:center;padding:2px 0px 2px 0px;vertical-align:middle;");
+                    cell.setAttribute('class', "date");
+                    
+                    for(var j = 1; j < 7; j++) {
+                        var cell = tblBodyObj.rows[i].cells[j];
+                        if (cell) {
+                            cell.setAttribute('style', "padding:1px; margin:0px;font-size:10px;vertical-align:middle;text-align:center");
+                            cell.innerHTML = cell.innerHTML.replace(/\&nbsp\;/gi, '').replace(' - ', ':');
+                        }
+                    }
+                }
+            }        
+        } catch(e) {dump(this.MODULE_NAME + ':' + e + '\n');}
+    },
+    
+    change : function( page, doc ) {
+        var id = "ft_matchtable";
+        if(!doc.getElementById(id)) {
+            this.run( page, doc );
+        }
+    }
+};=======>>>>>>> .theirs
