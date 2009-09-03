1e3053bf1cedb1faf9ad338747caa1d2d1a5f372
spambot.de <spambot.de@gmail.com>
2009-09-03 16:47:13 +0000

    Version 0.4.8.6 No solution for hidesignatures so far!
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3218 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumhidesignatures.js
+++ content/forum/forumhidesignatures.js
@@ -10,6 +10,8 @@ var FoxtrickHideSignatures = {
     MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
 	PAGES : new Array('forumViewThread'), 
 	DEFAULT_ENABLED : false,
+	NEW_AFTER_VERSION: "0.4.8.6",
+	LASTEST_CHANGE:"Catched up some errors (Still no link for showing the sig). Use the HT option for shorted signatures until Module is fixed",
 	
     init : function() {
     },
@@ -22,32 +24,34 @@ var FoxtrickHideSignatures = {
 				elems[i].className == "signature-trunc") {
 				p++;
 				if( !doc.getElementById( "foxtrick-st-link"+p ) ) {
-					elems[i].style.display="none";
-					var sigId = elems[i].id;
-					if( !sigId ) {
-						sigId = "foxtrick-signature-"+p;
-						elems[i].id = sigId;
-					}
-			    
-					var showSig = [];
-					showSig[p] = doc.createElement("a");
-					showSig[p].setAttribute("id","foxtrick-st-link"+p);
-					showSig[p].className="foxtrick-signaturetoggle";
-					showSig[p].innerHTML = Foxtrickl10n.getString('foxtrick.conferences.signaturetoggle');
-					showSig[p].href = "javascript:showHide('" + sigId + "');";
-					// append the show sig link to the right footer
-					var cfMessage = elems[i].parentNode;
-					var cfFooter = cfMessage.nextSibling;
-					while( cfFooter.className != "cfFooter" ) {
-						cfFooter = cfFooter.nextSibling;
-					}
-					var divsInFooter = cfFooter.getElementsByTagName("div");
-					for(var j = 0; j < divsInFooter.length; j++) {
-						if( divsInFooter[j].className == "float_right" ) {
-							divsInFooter[j].appendChild(showSig[p]);
-						}
-					}
-				}
+                    try {
+                        elems[i].style.display="none";
+                        var sigId = elems[i].id;
+                        if( !sigId ) {
+                            sigId = "foxtrick-signature-"+p;
+                            elems[i].id = sigId;
+                        }
+                    
+                        var showSig = [];
+                        showSig[p] = doc.createElement("a");
+                        showSig[p].setAttribute("id","foxtrick-st-link"+p);
+                        showSig[p].className="foxtrick-signaturetoggle";
+                        showSig[p].innerHTML = Foxtrickl10n.getString('foxtrick.conferences.signaturetoggle');
+                        showSig[p].href = "javascript:showHide('" + sigId + "');";
+                        // append the show sig link to the right footer
+                        var cfMessage = elems[i].parentNode;
+                        var cfFooter = cfMessage.nextSibling;
+                        while( cfFooter.className != "cfFooter" ) {
+                            cfFooter = cfFooter.nextSibling;
+                        }
+                        var divsInFooter = cfFooter.getElementsByTagName("div");
+                        for(var j = 0; j < divsInFooter.length; j++) {
+                            if( divsInFooter[j].className == "float_right" ) {
+                                divsInFooter[j].appendChild(showSig[p]);
+                            }
+                        }
+                    } catch(e) {dump('HideSignatures ERROR ' + e + '\n');}
+                } 
             }
 		}
 	},
