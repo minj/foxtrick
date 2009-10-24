0c83cb68310b549fa59d42f1006e9f138aa514e0
warheart_8 <warheart_8@hotmail.com>
2009-10-24 22:55:05 +0000

    trying to make pixels right
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3382 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/staffmarker.js
+++ content/forum/staffmarker.js
@@ -20,8 +20,8 @@ var FoxtrickStaffMarker = {
 											"background-color:white; color:green;", //LA
 											"background-color:green; color:white;", //CHPP
 											"background-color:green;", //editor
-											"background-image: url('chrome://foxtrick/content/resources/linkicons/foxtrick_2.png'); background-repeat: no-repeat; padding: 1px 0px 3px 18px;", //foxtrick-dev
-											"background-image: url('chrome://foxtrick/content/resources/linkicons/chpp.png'); background-repeat: no-repeat; padding: 1px 0px 3px 40px;", //chpps
+											"background-image: url('chrome://foxtrick/content/resources/linkicons/foxtrick_2.png'); background-repeat: no-repeat; padding: 2px 1px 2px 18px;", //foxtrick-dev
+											"background-image: url('chrome://foxtrick/content/resources/linkicons/chpp.png'); background-repeat: no-repeat; padding: 1px 1px 1px 40px;", //chpps
 											"background-image: url('chrome://foxtrick/content/resources/linkicons/hyouthclub_small.png'); background-repeat: no-repeat; padding: 1px 0px 3px 18px;", //hty_staff
 											"background-color:white; color:black;", //flag
 											"userId=1000 userId=1001 style='color:yellow;' userId=1002 style='background-color:yellow;'" //own
