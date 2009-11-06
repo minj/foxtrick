e883859eaaa28970d53dacbea106a235eed10a5f
spambot.de <spambot.de@gmail.com>
2009-11-06 12:42:56 +0000

    matchReport => ITALIAN fix
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3475 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/matchReport.js
+++ content/matches/matchReport.js
@@ -164,6 +164,8 @@ FoxtrickMatchReportFormat = {
             );
         part[0] = part[0].replace(/(.{1,2})\-(.{1,2})\-(.{1,2})\ /g,"<span class='ft_mR_format' style='font-weight:bold;color:black'>$1</span>-<span class='ft_mR_format' style='font-weight:bold;color:black'>$2</span>-<span class='ft_mR_format' style='font-weight:bold;color:black'>$3</span> ");
         part[0] = part[0].replace(/(.{1,2})\-(.{1,2})\-(.{1,2})\-/g,"<span class='ft_mR_format' style='font-weight:bold;color:black'>$1</span>-<span class='ft_mR_format' style='font-weight:bold;color:black'>$2</span>-<span class='ft_mR_format' style='font-weight:bold;color:black'>$3</span>-");
+        
+        part[1] = part[1].replace(/(\d{1,2})\ a\ (\d{1,2})/g,"$1 - $2"); //ITALIAN LA's work...
         for (var i = 0; i<search.length; i++) {
             part[1] = part[1].replace(search[i],replace[i]);
         }
