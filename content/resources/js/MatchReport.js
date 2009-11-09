5fd824df4363f66d5e99941fc9a0af982441bae9
spambot.de <spambot.de@gmail.com>
2009-11-09 20:48:05 +0000

    MatchReport => Javascript can be that easy. :D Fixed Scrolling thing...
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3524 1e3807c3-0949-0410-b78e-337b627472c1

--- content/resources/js/MatchReport.js
+++ content/resources/js/MatchReport.js
@@ -3,9 +3,6 @@ function gotoElmentID(elementID) {
         scroll(0, 0);
     } else {
         var element = document.getElementById(elementID);
-        alert('before: ' + element.scrollTop +"|"+ element.offsetTop);
-        element.scrollTop = element.offsetTop;
-        //alert(element.textContent);
-        alert('after: ' + element.scrollTop +"|"+ element.offsetTop);
+        window.scrollBy(0, element.offsetTop);
     }
 }
