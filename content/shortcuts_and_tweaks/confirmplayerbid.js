69c20d7f21191bb18eedf0188a8cd465b3e8544c
convincedd <convincedd@gmail.com>
2009-11-13 12:35:22 +0000

    added confirmstaffhire
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3558 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/confirmplayerbid.js
+++ content/shortcuts_and_tweaks/confirmplayerbid.js
@@ -1,7 +1,7 @@
 /**
  * confirmplayerbid.js
  * Adds a confirmation box for bidding
- * @author bummerland
+ * @author bummerland 
  */
 ////////////////////////////////////////////////////////////////////////////////
 var FoxtrickConfirmPlayerBid = {
@@ -139,6 +139,66 @@ var FoxtrickNTConfirmAddRemove = {
             }
         } catch(e) { 
             Foxtrick.dump('FoxtrickNTConfirmRemove CHG ' + e + '\n'); 
+        }
+	}
+};
+
+
+/**
+ * confirmplayerbid.js
+ * Adds a confirmation box for staff changes
+ * @author convindedd 
+ */
+////////////////////////////////////////////////////////////////////////////////
+var FoxtrickConfirmStaffChange = {
+    
+    MODULE_NAME : "ConfirmStaffChange",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	PAGES : new Array('staff'), 
+	DEFAULT_ENABLED : true,
+
+    init : function() {
+    },
+
+	run : function( page, doc ) {
+		try {
+            var submitButton = doc.getElementById("ctl00_CPMain_btnStaffAction");
+            if (submitButton){
+                var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
+                if (sOnclick.search(/confirm/) == -1){ // already added?
+                    //var sConfirmString =  ); 
+                    var sConfirmString = "var cstr=new Array('"+Foxtrickl10n.getString('foxtrick.staffconfirmationhire')+"','"+Foxtrickl10n.getString('foxtrick.staffconfirmationsack')+"');";
+					var numstr = "var num = document.getElementById('ctl00_CPMain_txtAmount').value;";
+					var kindindex = "var kindindex = document.getElementById('ctl00_CPMain_ddlStaffRole').selectedIndex;";
+					var kindstr = "var kind = document.getElementById('ctl00_CPMain_ddlStaffRole').options[document.getElementById('ctl00_CPMain_ddlStaffRole').selectedIndex].innerHTML;";
+					var actionstr = "var action = document.getElementById('ctl00_CPMain_ddlStaffAction').selectedIndex;";
+                    sOnclick = sConfirmString + numstr + kindstr +kindindex + actionstr + "if(num=='' || kindindex==0) return "+sOnclick+";  if (confirm(cstr[action].replace(/\%num/, num).replace(/\%kind/, kind))){" + sOnclick + ";} else {document.getElementById('ctl00_CPMain_btnStaffAction').removeAttribute('disabled');  return false;}";
+                    submitButton.setAttribute("onClick", sOnclick);
+                }
+            }
+        } catch(e) { 
+            Foxtrick.dump('ConfirmStaff ' + e + '\n'); 
+        }		
+	},
+	
+	change : function( page, doc ) {
+		try {
+            var submitButton = doc.getElementById("ctl00_CPMain_btnStaffAction");
+            if (submitButton){
+                var sOnclick = submitButton.getAttribute("onClick").replace(/javascript\:/, "");
+                if (sOnclick.search(/confirm/) == -1){ // already added?
+                    //var sConfirmString =  ); 
+                    var sConfirmString = "var cstr=new Array('"+Foxtrickl10n.getString('foxtrick.staffconfirmationhire')+"','"+Foxtrickl10n.getString('foxtrick.staffconfirmationsack')+"');";
+					var numstr = "var num = document.getElementById('ctl00_CPMain_txtAmount').value;";
+					var kindindex = "var kindindex = document.getElementById('ctl00_CPMain_ddlStaffRole').selectedIndex;";
+					var kindstr = "var kind = document.getElementById('ctl00_CPMain_ddlStaffRole').options[document.getElementById('ctl00_CPMain_ddlStaffRole').selectedIndex].innerHTML;";
+					var actionstr = "var action = document.getElementById('ctl00_CPMain_ddlStaffAction').selectedIndex;";
+                    sOnclick = sConfirmString + numstr + kindstr +kindindex + actionstr + "if(num=='' || kindindex==0) return "+sOnclick+";  if (confirm(cstr[action].replace(/\%num/, num).replace(/\%kind/, kind))){" + sOnclick + ";} else {document.getElementById('ctl00_CPMain_btnStaffAction').removeAttribute('disabled');  return false;}";
+                    submitButton.setAttribute("onClick", sOnclick);
+                }
+            }
+        } catch(e) { 
+            Foxtrick.dump('ConfirmStaff CHG ' + e + '\n'); 
         }
 	}
 };
