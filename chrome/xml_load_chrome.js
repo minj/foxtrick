50f4020cedaf610b1e0bc492c34a1994ce9b90b7
convincedd <convincedd@gmail.com>
2009-12-28 01:55:43 +0000

    chrome: removed moz-document statement where not needed
    git-svn-id: https://foxtrick.googlecode.com/svn/branches@3923 1e3807c3-0949-0410-b78e-337b627472c1

--- chrome/xml_load_chrome.js
+++ chrome/xml_load_chrome.js
@@ -26,52 +26,9 @@ Foxtrick.XMLData = {
 	matchxmls: new Array(),
 	
 	init : function() {
-	try{
-	} catch(e){Foxtrick.dump('Foxtrick.XMLData.init: '+e+'\n');}
 	},
 	
 	run : function(page,doc) {
-	
-		/*try {
-			if (FoxtrickStaffMarker.hty_staff==null){
-				FoxtrickStaffMarker.hty_staff = new Array();
-				var req = new XMLHttpRequest();
-				req.open('GET', 'http://www.hattrick-youthclub.org/_admin/foxtrick/team.xml', false); 
-				req.send(null);
-				if (req.status == 200) {
-					//Foxtrick.dump(req.responseText+'\n');
-					var frag = doc.createElement('dummy');
-					frag.innerHTML = req.responseText;
-					var htyusers = frag.getElementsByTagName('user');
-					for (var i=0;i<htyusers.length;++i) {
-						FoxtrickStaffMarker.hty_staff.push(htyusers[i].getElementsByTagName('alias')[0].innerHTML);
-						//Foxtrick.dump(FoxtrickStaffMarker.hty_staff[i]+' ')
-					}
-					Foxtrick.dump('hty_staff loaded\n')
-				}
-				else {Foxtrick.dump('no connection to hty\n'); }				
-			}
-		}catch(e) {Foxtrick.dump('hty.xml: '+e+'\n'); }
-
-	
-	/*try{
-		var matchid = FoxtrickHelper.getMatchIdFromUrl(doc.location.href); 
-		var isarchivedmatch = (doc.getElementById("ctl00_CPMain_lblMatchInfo")==null);
-		var isprematch = (doc.getElementById("ctl00_CPMain_pnlPreMatch")!=null);
-		if (isprematch) return;
-		
-		if (isarchivedmatch && typeof(this.matchxmls[matchid]) == 'undefined') {
-			var req = new XMLHttpRequest();
-			req.open('GET', 'http://'+doc.location.hostname+'/Community/CHPP/Matches/chppxml.axd?file=matchdetails&matchID='+matchid+'&matchEvents=true', false); 
-			req.send(null);
-			if (req.status == 200) {
-				this.matchxmls[matchid] = req.responseXML;
-				dump('matches.js: get new xml\n');
-			}
-			else Foxtrick.dump('matches.js: xml request failed\n');
-		}
-	} catch(e){Foxtrick.dump('matches.js run: '+e+'\n');}*/
-
 	},
 
 	change : function(page,doc) {
@@ -82,7 +39,7 @@ Foxtrick.XMLData = {
 // get settings
 var portgetsettings = chrome.extension.connect({name: "ftpref-query"});
 portgetsettings.onMessage.addListener(function(msg) { 
-	if (msg.changed) {
+	if (msg.is_settings) {
 		FoxtrickPrefs.pref = msg.pref; 
 		FoxtrickPrefs.pref_default = msg.pref_default; 
 		Foxtrickl10n.properties = msg.properties; 
@@ -97,8 +54,14 @@ portgetsettings.onMessage.addListener(function(msg) {
 		Foxtrick.XMLData.aboutXML = msg.about;
 		Foxtrick.XMLData.League = msg.League; 
 		Foxtrick.XMLData.countryid_to_leagueid = msg.countryid_to_leagueid;
+//		FoxtrickStaffMarker.hty_staff = msg.hty_staff;
 		console.log('got pref');
+	}
+	else if (msg.is_hty_staff) {
+		FoxtrickStaffMarker.hty_staff = msg.hty_staff;
+		console.log('got hty_staff');
 	}
 });
 
 portgetsettings.postMessage({reqtype: "get_settings"})
+portgetsettings.postMessage({reqtype: "get_hty_staff"})
