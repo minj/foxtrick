9e1645b1bc1a2f6d4114ed568de8e564ac9673c1
ryan <ryan@ryanium.com>
2009-12-31 14:49:08 +0000

    Added birthday today in playerbirthday
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@3942 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/playerbirthday.js
+++ content/shortcuts_and_tweaks/playerbirthday.js
@@ -9,137 +9,116 @@ var FoxtrickPlayerBirthday = {
 
     MODULE_NAME : "PlayerBirthday",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	PAGES : new Array('players','YouthPlayers'), 
+	PAGES : new Array('players','YouthPlayers'),
 	DEFAULT_ENABLED : true,
 	NEW_AFTER_VERSION: "0.4.8.9",
 	LATEST_CHANGE:"Birthdaybox fixed for russian lang",
 
-    init : function() {
-   },
-    
-    run : function( page, doc ) { 
-	try {
-		var ArrayPlayers = new Array();      // arrays
-		var ArrayPlayersLate = new Array();
+	init : function() {
+	},
 
-		var a = 0;      // indexes
-		var b = 0;
+    run : function( page, doc ) {
+	try {
+		// array of players
+		var birthdayToday = new Array();
+		var birthdayFuture = new Array();
+		var birthdayPast = new Array();
 
 		var ClassPlayer = doc.getElementsByClassName('playerInfo');
 
 		for (var i = 0; i < ClassPlayer.length; i++) {
-			var PlayerName = Array();
-      var Player = ClassPlayer[i].getElementsByTagName('a')[0];
-      if (Player.getElementsByTagName('img').length > 0) {
-        PlayerName[i] = ClassPlayer[i].getElementsByTagName('a')[1];    // player name - if the player is national team player, the first <a> tag is a link to national team containing <img> tags
-      } else {
-        PlayerName[i] = ClassPlayer[i].getElementsByTagName('a')[0];    // player name
-      }
-			var PlayerInfo = ClassPlayer[i].getElementsByTagName('p')[0].innerHTML;
-			if (PlayerInfo.search(/\<br\>/) !=-1)       // player info (PlayerAge, form and stamina)
-				PlayerInfo=PlayerInfo.substring(0, PlayerInfo.search(/\<br\>/)); // we need the first occurence
-			
-			if (PlayerInfo.search(/^\s*TSI/) != -1) PlayerInfo = PlayerInfo.replace(/.+,/,''); // In the language Vlaams, TSI and age are switched. This is a fix for that
-			
-			var reg=/(\d+)\D+(\d+).+/; // regular expression for getting the PlayerAge, works with Czech format, for example "18 let a 11 dní"
-			
-			var PlayerAge = PlayerInfo.match(reg);
-			
+			var player = {};
+			// player name - if the player is national team player, the first <a> tag is a link to national team containing <img> tags
+			var playerTag = ClassPlayer[i].getElementsByTagName('a')[0];
+			if (playerTag.getElementsByTagName('img').length > 0) {
+				player.playerName = ClassPlayer[i].getElementsByTagName('a')[1];    
+			}
+			else {
+				player.playerName = ClassPlayer[i].getElementsByTagName('a')[0];
+			}
+			var playerInfo = ClassPlayer[i].getElementsByTagName('p')[0].innerHTML;
+			if (playerInfo.search(/\<br\>/) != -1) { // player info (PlayerAge, form and stamina)
+				playerInfo = playerInfo.substring(0, playerInfo.search(/\<br\>/)); // we need the first occurence
+			}
+			if (playerInfo.search(/^\s*TSI/) != -1) {
+				playerInfo = playerInfo.replace(/.+,/,''); // In the language Vlaams, TSI and age are switched. This is a fix for that
+			}
+			var reg = /(\d+)\D+(\d+).+/; // regular expression for getting the PlayerAge, works with Czech format, for example "18 let a 11 dnÃ­"
+
+			player.years = playerInfo.match(reg)[1];
+			player.days = playerInfo.match(reg)[2];
+
 			var regText=/(\d+\D+\d+\s\S+)/; // regular expression for getting the whole PlayerAge string
 			var regTextRus=/(\D+\d+\D+\d+)/; // regular expression for getting the whole PlayerAge string, for rusian
-			
-			var TextAgeTmp=''
-			try {TextAgeTmp=String(PlayerInfo.match(regText)[1]).replace(/,/,'');}  // try to get full age string
-			catch(e){TextAgeTmp=String(PlayerInfo.match(regTextRus)[1]).replace(/,/,'');} // if not get russian version ie strings before the number
 
-			if (PlayerAge[2]>105 || PlayerAge[2] == 0) {     // player who will have birthday in 7 days or who have birthday today
-				ArrayPlayers[a] = new Array(5); //Foxtrick.dump(PlayerName[i]+' '+PlayerAge[2]+'\n');
-				ArrayPlayers[a][1] = PlayerName[i];            // player's name
-				ArrayPlayers[a][2] = PlayerAge[1];             // player's PlayerAge (years)
-				ArrayPlayers[a][3] = PlayerAge[2];             // player's PlayerAge (days)
-				ArrayPlayers[a][4] = TextAgeTmp             // player's PlayerAge (days)
-				a++;
+			try {
+				player.age = String(playerInfo.match(regText)[1]).replace(/,/,''); // try to get full age string
 			}
-			if (PlayerAge[2]<7 && PlayerAge[2] > 0) {        // player who had birthday 7 or fewer days ago
-				ArrayPlayersLate[b] = new Array(5);
-				ArrayPlayersLate[b][1] = PlayerName[i];
-				ArrayPlayersLate[b][2] = PlayerAge[1];
-				ArrayPlayersLate[b][3] = PlayerAge[2];
-				ArrayPlayersLate[b][4] = TextAgeTmp;
-				b++;
+			catch(e) {
+				TextAgeTmp=String(playerInfo.match(regTextRus)[1]).replace(/,/,''); // if not get russian version ie strings before the number
+			}
+
+			if (player.days >= 0) {
+				if (player.days == 0) { // players who have birthday today
+					birthdayToday.push(player);
+				}
+				else if (player.days > 105) { // player who will have birthday in 7 days
+					birthdayFuture.push(player);
+				}
+				else if (player.days < 7) { // player who had birthday 7 or fewer days ago
+					birthdayPast.push(player);
+				}
 			}
 		}
 
-		ArrayPlayers.sort(this.sortMultiDimensional);     // sorting of arrays according to days
-		ArrayPlayersLate.sort(this.sortMultiDimensional);
+		// sorting of arrays according to days
+		birthdayFuture.sort(this.sortByDays);
+		birthdayPast.sort(this.sortByDays);
 
 		var parentDiv = doc.createElement("div");
 		parentDiv.id = "foxtrick_addactionsbox_parentDiv";
 
-		var newBR = doc.createElement('br');
+		FoxtrickPlayerBirthday.addType(parentDiv, Foxtrickl10n.getString('foxtrick.tweaks.BirthdayToday'), birthdayToday, doc);
+		FoxtrickPlayerBirthday.addType(parentDiv, Foxtrickl10n.getString('foxtrick.tweaks.BirthdayNextWeek'), birthdayFuture, doc);
+		FoxtrickPlayerBirthday.addType(parentDiv, Foxtrickl10n.getString('foxtrick.tweaks.BirthdayLastWeek'), birthdayPast, doc);
 
-		var newDiv = doc.createElement("div");   // DIV with players who WILL HAVE birthday
-		var newStrong = doc.createElement("strong");
-		var newStrongText = doc.createTextNode(Foxtrickl10n.getString( 'foxtrick.tweaks.BirthdayNextWeek' ));
-		newStrong.appendChild(newStrongText);
-		newDiv.appendChild(newStrong);
-		newDiv.appendChild(newBR);
-
-		for (var i = 0; i < ArrayPlayers.length; i++) {
-			var player =ArrayPlayers[i][1].cloneNode(true);
-			player.setAttribute('style','padding: 2px 0 0 0;');
-			newDiv.appendChild(player);
-			var newT = doc.createTextNode(ArrayPlayers[i][4]);
-			var newSpan = doc.createElement('span');
-			newSpan.setAttribute('class',"smallText");
-			newSpan.appendChild(newT);
-			newDiv.appendChild(newSpan);
-			var newBR = doc.createElement('br');
-			newDiv.appendChild(newBR);
-		}
-		if (ArrayPlayers.length>0) parentDiv.appendChild(newDiv);
-
-
-		var newDivLate = doc.createElement("div");   // DIV with players who HAD birthday
-		var newStrongLate = doc.createElement("strong");
-		var newStrongTextLate = doc.createTextNode(Foxtrickl10n.getString( 'foxtrick.tweaks.BirthdayLastWeek' ));
-		newStrongLate.appendChild(newStrongTextLate);
-		var newBR = doc.createElement('br');
-		newDivLate.appendChild(newBR);
-		newDivLate.appendChild(newStrongLate);
-		var newBR = doc.createElement('br');
-		newDivLate.appendChild(newBR);
-
-		for (var i = 0; i < ArrayPlayersLate.length; i++) {
-			var player =ArrayPlayersLate[i][1].cloneNode(true);
-			player.setAttribute('style','padding: 2px 0 0 0;');
-			newDivLate.appendChild(player);
-			var newT = doc.createTextNode(ArrayPlayersLate[i][4]);
-			var newSpan = doc.createElement('span');
-			newSpan.setAttribute('class',"smallText");
-			newSpan.appendChild(newT);
-			newDivLate.appendChild(newSpan);
-			var newBR = doc.createElement('br');
-			newDivLate.appendChild(newBR);
-		}
-		
-		if (ArrayPlayersLate.length>0) parentDiv.appendChild(newDivLate);
-		
 		// Append the box to the sidebar
 		var newBoxId = "foxtrick_birthday_box";
-		if (ArrayPlayers.length+ArrayPlayersLate.length>0) Foxtrick.addBoxToSidebar( doc, Foxtrickl10n.getString( 
-			"foxtrick.tweaks.Birthdays" ), parentDiv, newBoxId, "last", "");
-	}catch(e) {Foxtrick.dump('PlayerBirthday: '+e+'\n');}
+		if (birthdayToday.length + birthdayFuture.length + birthdayPast.length > 0) {
+			Foxtrick.addBoxToSidebar(doc, Foxtrickl10n.getString("foxtrick.tweaks.Birthdays"), parentDiv, newBoxId, "last", "");
+		}
+	}
+	catch(e) {
+		Foxtrick.dump('PlayerBirthday: ' + e + '\n');}
 	},
-	
+
 	change : function( page, doc ) {
-	},	
-	
-	sortMultiDimensional : function (a,b)
-	{
-		// this should sort the array of players according to days
-		return ((a[3] < b[3]) ? -1 : ((a[3] > b[3]) ? 1 : 0));
 	},
 
-};
+	sortByDays : function (a, b) {
+		// this should sort the array of players according to days
+		return ((a.days < b.days) ? -1 : ((a.days > b.days) ? 1 : 0));
+	},
 
+	addType : function(parent, header, players, doc) {
+		if (!players.length)
+			return;
+		var div = doc.createElement("div");
+		var caption = doc.createElement("h5");
+		var captionText = doc.createTextNode(header);
+		var list = doc.createElement("ul");
+		parent.appendChild(div);
+		div.appendChild(caption);
+		div.appendChild(list);
+		caption.appendChild(captionText);
+
+		for (var i in players) {
+			var item = doc.createElement("li");
+			var player = players[i].playerName.cloneNode(true);
+			var age = doc.createTextNode(players[i].age);
+			list.appendChild(item);
+			item.appendChild(player);
+			item.appendChild(age);
+		}
+	}
+};
