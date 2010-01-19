fe6c817326855f36f645415082b567acb2913161
warheart_8 <warheart_8@hotmail.com>
2010-01-19 18:30:36 +0000

    fi locale update. updated also devs, also made some little change on defaults
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4030 1e3807c3-0949-0410-b78e-337b627472c1

--- defaults/preferences/foxtrick.js
+++ defaults/preferences/foxtrick.js
@@ -43,7 +43,7 @@ pref( "extensions.foxtrick.prefs.module.SeniorTeamShortCuts.value", 0 );
 pref( "extensions.foxtrick.prefs.cssSkin.value", "chrome://foxtrick/content/resources/css/mainr.css");
 pref( "extensions.foxtrick.prefs.module.SkillColoring.no_skill_links.enabled", false );
 pref( "extensions.foxtrick.prefs.module.SkillColoring.skill_number_translated.enabled", false );
-pref( "extensions.foxtrick.prefs.module.ForumAlterHeaderLine.TruncateLongNick.enabled", true );
+pref( "extensions.foxtrick.prefs.module.ForumAlterHeaderLine.TruncateLongNick.enabled", false );
 pref( "extensions.foxtrick.prefs.module.ForumAlterHeaderLine.CheckDesign.enabled", false );
 pref( "extensions.foxtrick.prefs.module.ForumAlterHeaderLine.HideOldTime.enabled", true );
 pref( "extensions.foxtrick.prefs.module.ForumAlterHeaderLine.SingleHeaderLine.enabled", true );
