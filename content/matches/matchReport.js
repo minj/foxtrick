9aec64a01e913776ee7509c88f2c95f1ca431033
convincedd <convincedd@gmail.com>
2010-01-29 03:07:32 +0000

    matchReport: youthfix
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4139 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/matchReport.js
+++ content/matches/matchReport.js
@@ -104,21 +104,22 @@ FoxtrickMatchReportFormat = {
          var text_normal = this._getPrefs( 12 );
          var text_dark = this._getPrefs( 13 );
 
+		 var youthstr='';
+		 if (isyouth) youthstr='&isYouth=True';
         //Replace myTeam colour
 		if (HomeTeamId == myTeamId) {
             bg_col_hm = bg_col_my;
             border_color_hm = border_color_my;
             txt_col_hm = txt_col_my;
-            table.rows[0].cells[1].innerHTML = '<a style="color:' + txt_col_my + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + HomeTeamId + '">' + table.rows[0].cells[1].textContent +'</a>';
-            table.rows[0].cells[2].innerHTML = '<a style="color:' + txt_col_aw + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + AwayTeamId + '">' + table.rows[0].cells[2].textContent +'</a>';
-
+            table.rows[0].cells[1].innerHTML = '<a style="color:' + txt_col_my + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + HomeTeamId + youthstr + '">' + table.rows[0].cells[1].textContent +'</a>';
+            table.rows[0].cells[2].innerHTML = '<a style="color:' + txt_col_aw + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + AwayTeamId + youthstr + '">' + table.rows[0].cells[2].textContent +'</a>';
         }
 		else if (AwayTeamId == myTeamId) {
             bg_col_aw = bg_col_my;
             border_color_aw = border_color_my;
             txt_col_aw = txt_col_my;
-            table.rows[0].cells[1].innerHTML = '<a style="color:' + txt_col_hm + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + HomeTeamId + '">' + table.rows[0].cells[1].textContent +'</a>';
-            table.rows[0].cells[2].innerHTML = '<a style="color:' + txt_col_my + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + AwayTeamId + '">' + table.rows[0].cells[2].textContent +'</a>';
+            table.rows[0].cells[1].innerHTML = '<a style="color:' + txt_col_hm + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + HomeTeamId + youthstr + '">' + table.rows[0].cells[1].textContent +'</a>';
+            table.rows[0].cells[2].innerHTML = '<a style="color:' + txt_col_my + ';" href="/Club/Matches/MatchLineup.aspx?MatchID=' + gameid + '&TeamID=' + AwayTeamId + youthstr + '">' + table.rows[0].cells[2].textContent +'</a>';
         }
 
 		// color ratings
