719793e9c654ff5e1a79e1c5f0c7f8828d5b8795
ljushaff <ljushaff@gmail.com>
2010-02-07 12:57:41 +0000

    staffmarker dump typo corrected
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4210 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/staffmarker.js
+++ content/forum/staffmarker.js
@@ -374,7 +374,7 @@ var FoxtrickStaffMarker = {
             break;
 			
             case 'teamPageAny':
-                // Foxtrick.dump(''teamPageAny\n');
+                // Foxtrick.dump('teamPageAny\n');
                 FoxtrickStaffMarker._MarkAliases_thread(doc);
             break;
         }
