b3ca9d845b91a535281b636d99ebcccf775a4b9c
ljushaff <ljushaff@gmail.com>
2010-02-07 18:49:13 +0000

    Serbian Translation update 2
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4223 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumstriphattricklinks.js
+++ content/forum/forumstriphattricklinks.js
@@ -23,9 +23,9 @@ var FoxtrickForumStripHattrickLinks = {
 		var a = ev.target; 
 		if (a.nodeName=='A') {
 			var hostname = ev.target.ownerDocument.location.hostname;
-			if ( a.href.search(/http:\/\/stage|www\d+\.hattrick\.org.*?/i)!=-1 && a.href.search(hostname)==-1) {
-				if (Foxtrick.isModuleFeatureEnabled( FoxtrickForumStripHattrickLinks, "NoConfirmStripping" )) a.href = a.href.replace(/http:\/\/stage|www\d+\.hattrick\.org(.*?)/i,hostname+'$1');
-				else if (Foxtrick.confirmDialog('Replace server with '+hostname +'?')) a.href = a.href.replace(/http:\/\/stage|www\d+\.hattrick\.org(.*?)/i,hostname+'$1');
+			if ( a.href.search(/http:\/\/stage|wwwxx|www\d+\.hattrick\.org.*?/i)!=-1 && a.href.search(hostname)==-1) {
+				if (Foxtrick.isModuleFeatureEnabled( FoxtrickForumStripHattrickLinks, "NoConfirmStripping" )) a.href = a.href.replace(/http:\/\/stage|wwwxx|www\d+\.hattrick\.org(.*?)/i,hostname+'$1');
+				else if (Foxtrick.confirmDialog('Replace server with '+hostname +'?')) a.href = a.href.replace(/http:\/\/stage|wwwxx|www\d+\.hattrick\.org(.*?)/i,hostname+'$1');
 			}
 			//else a.target=+'_blank';
 		}
@@ -46,9 +46,9 @@ var FoxtrickForumStripHattrickLinks = {
 		Foxtrick.dump(button_ok.getAttribute('id')+'\n');
 		Foxtrick.dump(button_ok.getAttribute('onclick')+'\n');
 		if (Foxtrick.isModuleFeatureEnabled( this, "NoConfirmStripping" )) 
-				button_ok.setAttribute('onclick', "var textarea = document.getElementById('mainBody').getElementsByTagName('textarea')[0]; if (textarea && textarea.value.search(/\\[link=http:\\/\\/(stage|www(\\d+))\\.hattrick\\.org(.*?)\\]/) > -1)  { textarea.value = textarea.value.replace(/\\[link=http:\\/\\/(stage|www(\\d+))\\.hattrick\\.org(.*?)\\]/gi,'[link=$3]');} "+button_ok.getAttribute('onclick'));
+				button_ok.setAttribute('onclick', "var textarea = document.getElementById('mainBody').getElementsByTagName('textarea')[0]; if (textarea && textarea.value.search(/\\[link=http:\\/\\/(stage|wwwxx|www(\\d+))\\.hattrick\\.org(.*?)\\]/) > -1)  { textarea.value = textarea.value.replace(/\\[link=http:\\/\\/(stage|wwwxx|www(\\d+))\\.hattrick\\.org(.*?)\\]/gi,'[link=$3]');} "+button_ok.getAttribute('onclick'));
 		else
-				button_ok.setAttribute('onclick', "var textarea = document.getElementById('mainBody').getElementsByTagName('textarea')[0]; if (textarea && textarea.value.search(/\\[link=http:\\/\\/(stage|www(\\d+))\\.hattrick\\.org(.*?)\\]/) > -1)  { if(confirm('"+Foxtrickl10n.getString('foxtrick.confirmstripserver')+"')) {textarea.value = textarea.value.replace(/\\[link=http:\\/\\/(stage|www(\\d+))\\.hattrick\\.org(.*?)\\]/gi,'[link=$3]');} else;} "+button_ok.getAttribute('onclick'));
+				button_ok.setAttribute('onclick', "var textarea = document.getElementById('mainBody').getElementsByTagName('textarea')[0]; if (textarea && textarea.value.search(/\\[link=http:\\/\\/(stage|wwwxx|www(\\d+))\\.hattrick\\.org(.*?)\\]/) > -1)  { if(confirm('"+Foxtrickl10n.getString('foxtrick.confirmstripserver')+"')) {textarea.value = textarea.value.replace(/\\[link=http:\\/\\/(stage|wwwxx|www(\\d+))\\.hattrick\\.org(.*?)\\]/gi,'[link=$3]');} else;} "+button_ok.getAttribute('onclick'));
 		Foxtrick.dump(button_ok.getAttribute('onclick')+'\n');
 		
 	} catch(e) {Foxtrick.dump('FoxtrickForumStripHattrickLinks '+e+'\n');}
