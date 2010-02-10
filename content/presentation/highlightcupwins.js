0a7bee74950dc3db7e4c9aab0c937040d55acb3f
ryan <ryan@ryanium.com>
2010-02-10 08:36:32 +0000

    highlightcupwins.js: fixed for draws and for new cup match display method.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4277 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/highlightcupwins.js
+++ content/presentation/highlightcupwins.js
@@ -10,40 +10,49 @@ var FoxtrickHighlightCupwins = {
     MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
 	PAGES : new Array('cupmatches'), 
 	DEFAULT_ENABLED : true,
-	NEW_AFTER_VERSION: "0.4.8.1",
-	LATEST_CHANGE:"Highlight winning teams on CupMatches page",
+	NEW_AFTER_VERSION: "0.5.0.2",
+	LATEST_CHANGE : "Fixed for draws and for new cup match display method.",
+	LATEST_CHANGE_CATEGORY: Foxtrick.latestChangeCategories.FIX,
 	OPTIONS : new Array("highlight_outsider"),
 	
     init : function() {
     },
 
     run : function( page, doc ) {	
-	try{
-	var highlight_outsider = Foxtrick.isModuleFeatureEnabled( this, "highlight_outsider");
+		try {
+			var highlight_outsider = Foxtrick.isModuleFeatureEnabled( this, "highlight_outsider");
 
-	var mainBody=doc.getElementById('mainBody');
-	var table= mainBody.getElementsByTagName('table')[0]; 
-	for (var i=1;i<table.rows.length;++i) {
-			var goals = table.rows[i].cells[3].innerHTML.match(/\d+/g); 
-			var goals0=parseInt(goals[0]);
-			var goals1=parseInt(goals[1]);
-			
-			var homewon = (goals0>goals1)
-			var matchlink = table.rows[i].cells[2].getElementsByTagName('a')[0];
-			var firstsep=matchlink.innerHTML.indexOf(' - ' );
-			var hometeam = matchlink.innerHTML.substring(0,firstsep);
-			// if there are names with ' - ' take the last part for away only
-			var lastsep=matchlink.innerHTML.lastIndexOf(' - ' )+3;
-			var awayteam = matchlink.innerHTML.substring(lastsep);
-			if (homewon) {
-				if (highlight_outsider) {table.rows[i].cells[3].innerHTML = '<strong>'+table.rows[i].cells[3].innerHTML+'</strong>';}
-				matchlink.innerHTML = '<strong>'+hometeam+'</strong> - '+awayteam;
+			var mainBody=doc.getElementById('mainBody');
+			var table= mainBody.getElementsByTagName('table')[0]; 
+			for (var i=1;i<table.rows.length;++i) {
+				var goals = table.rows[i].cells[3].innerHTML.match(/\d+/g);
+				if (!goals) {
+					// perhaps some results aren't shown
+					continue;
+				}
+				var goalsHome=parseInt(goals[0]);
+				var goalsAway=parseInt(goals[1]);
+
+				// win, draw, lose from the aspect of home team
+				var win = (goalsHome > goalsAway);
+				var draw = (goalsHome === goalsAway); // 0 - 0 is match isn't played yet
+				var lose = (goalsHome < goalsAway);
+				var matchlink = table.rows[i].cells[2].getElementsByTagName('a')[0];
+				var firstsep=matchlink.innerHTML.indexOf(' - ' );
+				var hometeam = matchlink.innerHTML.substring(0,firstsep);
+				// if there are names with ' - ' take the last part for away only
+				var lastsep=matchlink.innerHTML.lastIndexOf(' - ' )+3;
+				var awayteam = matchlink.innerHTML.substring(lastsep);
+				if (win) {
+					if (highlight_outsider) {table.rows[i].cells[3].innerHTML = '<strong>'+table.rows[i].cells[3].innerHTML+'</strong>';}
+					matchlink.innerHTML = '<strong>'+hometeam+'</strong> - '+awayteam;
+				}
+				else if (lose) {
+					matchlink.innerHTML = hometeam+' - <strong>'+awayteam+'</strong>';
+				}			
 			}
-			else {
-				matchlink.innerHTML = hometeam+' - <strong>'+awayteam+'</strong>';
-			}			
 		}
-	}catch(e){Foxtrick.dump(e)}	
+		catch(e){Foxtrick.dump(e);}
  	},
 	
 	change : function( page, doc ) {	
