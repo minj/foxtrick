303f83cc6c04ed3f6c47eb21cd32ea90af11db4f
convincedd <convincedd@gmail.com>
2010-02-18 20:16:23 +0000

    stage warning v2
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4418 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum_stage.js
+++ content/forum_stage.js
@@ -25,14 +25,30 @@ var FoxtrickForumStage = {
 	try{		
 		var forum = doc.getElementById('mainWrapper').getElementsByTagName("h2")[0].getElementsByTagName("a")[1].innerHTML; 
 		if (forum == 'Stage') {		
-			var button_ok = doc.getElementById('ctl00_ctl00_CPContent_CPMain_btnOK');			
-			button_ok.addEventListener('click',this.onclick,true);
+//			var button_ok = doc.getElementById('ctl00_ctl00_CPContent_CPMain_btnOK');			
+//			button_ok.addEventListener('click',this.onclick,true);
 
 			var textarea = doc.getElementById('mainBody').getElementsByTagName('textarea')[0]; 
 			var div = doc.createElement('div');
 			div.className = 'alert';
-			div.innerHTML = "<b>Disable FoxTrick</b> and other extensions before you report any kind of bug. Repeated ignorance = Stage kick." 
+			div.innerHTML = "<b>Disable FoxTrick</b> and other extensions before you report any kind of bug. Repeated ignorance = Stage kick.<br>"
 			textarea.parentNode.insertBefore(div, textarea);
+	
+			var datenow = new Date();
+			var dayofmonth = datenow.getDate();
+			Foxtrick.dump(dayofmonth+' '+ FoxtrickPrefs.getInt('LastStageWarningDay')+'\n');
+			
+			if (dayofmonth != FoxtrickPrefs.getInt('LastStageWarningDay')) {
+				if (Foxtrick.confirmDialog('You are posting on a stage forum. If you think you discovered a bug: Have you disabled FoxTrick and tried to reproduce the bug without FoxTrick?')) {
+				}
+				else {
+					var t=doc.location.href.match(/t=\d+/);
+					var v=doc.location.href.match(/v=\d+/);
+					var n=doc.location.href.match(/n=\d+/);
+					doc.location.href = doc.location.href.replace(/Write.+/i,'Read.aspx?'+t+'&'+n+'&'+v);
+				}
+			}
+			FoxtrickPrefs.setInt('LastStageWarningDay', dayofmonth);
 		}
 	} catch(e) {Foxtrick.dump('FoxtrickForumStage '+e+'\n');}
 	},
