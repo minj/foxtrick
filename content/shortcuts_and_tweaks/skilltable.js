085996a6a0fa94a20a5da8590f107c8531240b76
ryan <ryan@ryanium.com>
2010-03-04 03:53:38 +0000

    skilltable.js: fixed nationality link.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4491 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/skilltable.js
+++ content/shortcuts_and_tweaks/skilltable.js
@@ -337,7 +337,7 @@ var FoxtrickSkillTable = {
 						var td = doc.createElement("td");
 						td.title = Foxtrick.XMLData.League[LeagueID].LeagueName;
 						var a=doc.createElement("a");
-						a.href="";
+						a.href="/World/Leagues/League.aspx?LeagueID=" + LeagueID;
 						a.className ="flag inner";
 						var img=doc.createElement("img");
 						var style="vertical-align:top; margin-top:1px; background: transparent url(/Img/Flags/flags.gif) no-repeat scroll "+ (-20)*LeagueID+"px 0pt; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial;";
