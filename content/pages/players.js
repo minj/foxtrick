45a1120405672de15fb47cd1c7c4f9cc4c61e5e6
ryan <ryan@ryanium.com>
2010-03-20 07:43:21 +0000

    pages/players.js: using Gecko's cache control instead of private one.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4620 1e3807c3-0949-0410-b78e-337b627472c1

--- content/pages/players.js
+++ content/pages/players.js
@@ -4,10 +4,6 @@
  */
 
 Foxtrick.Pages.Players = {
-	lastSuccessfulXML : "", // last successful XML retrieve location
-	lastSuccessfulTime : 0, // last successful XML retrieve time (in milliseconds)
-	cachedXML : null, // use this if lastSuccessfulPage matches
-	XMLExpiry : 60000, // reload XML after this period of time (in milliseconds)
 	getXML : function(doc) {
 		if (doc.location.href.search(/\/Club\/Players\/\?TeamID=/i) == -1
 			&& doc.location.href.search(/\/Club\/Players\/$/) == -1
@@ -35,44 +31,37 @@ Foxtrick.Pages.Players = {
 
 			var location = "http://" + doc.location.hostname + "/Community/CHPP/Players/chppxml.axd?" + file + team + selection;
 
-			var cacheAge = (new Date()).getTime() - this.lastSuccessfulTime;
-			if (this.lastSuccessfulXML === location && cacheAge < this.XMLExpiry) {
-				// use cached XML
-				Foxtrick.dump("Foxtrick.Pages.Players has cached " + location + ", reusing cache.\n");
-				Foxtrick.dump("Cache age: " + cacheAge + "ms; will be reloaded after " + (this.XMLExpiry - cacheAge) + "ms.\n");
-				return this.cachedXML;
-			}
-			else {
-				Foxtrick.dump("Foxtrick.Pages.Players getting: " + location + "\n");
-				// get players.xml
-				try {
-					var req = new XMLHttpRequest();
-					req.open("GET", location, false);
-					req.send(null);
-					if (req.status == 200) {
-						var error = req.responseXML.getElementsByTagName("Error");
-						if (error.length == 0) {
-							Foxtrick.dump("FileName: " + req.responseXML.getElementsByTagName("FileName")[0].textContent + "\n");
-							Foxtrick.dump("Version: " + req.responseXML.getElementsByTagName("Version")[0].textContent + "\n");
-							Foxtrick.dump("UserID: " + req.responseXML.getElementsByTagName("UserID")[0].textContent + "\n");
-							Foxtrick.dump("ActionType: " + req.responseXML.getElementsByTagName("ActionType")[0].textContent + "\n");
-							this.lastSuccessfulXML = location;
-							this.lastSuccessfulTime = (new Date()).getTime();
-							this.cachedXML = req.responseXML;
-							return this.cachedXML;
-						}
-						else {
-							Foxtrick.dump("Error: " + error[0].textContent+"\n");
-							Foxtrick.dump("Server: " + req.responseXML.getElementsByTagName("Server")[0].textContent + "\n");
-						}
+			Foxtrick.dump("Foxtrick.Pages.Players getting: " + location + "\n");
+			// get players.xml
+			try {
+				var startTime = (new Date()).getTime();
+				var req = new XMLHttpRequest();
+				req.open("GET", location, false);
+				req.send(null);
+				if (req.status == 200) {
+					var endTime = (new Date()).getTime();
+					Foxtrick.dump("Time used: " + (endTime - startTime) + "ms. "
+						+ "(This estimation is inaccurate, please use Tamper Data or other tools for better estimation)\n");
+
+					var error = req.responseXML.getElementsByTagName("Error");
+					if (error.length == 0) {
+						Foxtrick.dump("FileName: " + req.responseXML.getElementsByTagName("FileName")[0].textContent + "\n");
+						Foxtrick.dump("Version: " + req.responseXML.getElementsByTagName("Version")[0].textContent + "\n");
+						Foxtrick.dump("UserID: " + req.responseXML.getElementsByTagName("UserID")[0].textContent + "\n");
+						Foxtrick.dump("ActionType: " + req.responseXML.getElementsByTagName("ActionType")[0].textContent + "\n");
+						return req.responseXML;
 					}
 					else {
-						Foxtrick.dump("Failure getting " + location + ", request status: " + req.status + ".\n");
+						Foxtrick.dump("Error: " + error[0].textContent+"\n");
+						Foxtrick.dump("Server: " + req.responseXML.getElementsByTagName("Server")[0].textContent + "\n");
 					}
 				}
-				catch (e) {
-					Foxtrick.dump("Failure getting " + location + ": " + e + "\n");
+				else {
+					Foxtrick.dump("Failure getting " + location + ", request status: " + req.status + ".\n");
 				}
+			}
+			catch (e) {
+				Foxtrick.dump("Failure getting " + location + ": " + e + "\n");
 			}
 		}
 		// In case of errors or ExtraPlayerslistInfo disabled, return null
