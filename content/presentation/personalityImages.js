11d82640b0a7cebc0df4c05687df4053ee6fe58f
ryan <ryan@ryanium.com>
2010-03-25 05:56:56 +0000

    *: separated honesty/gentleness/aggressiveness images, new images pending.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4639 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/personalityImages.js
+++ content/presentation/personalityImages.js
@@ -6,33 +6,38 @@
 
 var FoxtrickPersonalityImages = {
 
-    MODULE_NAME : "PersonalityImages",
-    MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
+	MODULE_NAME : "PersonalityImages",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
 	PAGES : new Array('playerdetail'),
 	DEFAULT_ENABLED : true,
 	LATEST_CHANGE : "Show aggressiveness value as HT denomination",
 	LATEST_CHANGE_CATEGORY : Foxtrick.latestChangeCategories.FIX,
 
-    init : function() {
-    },
+	init : function() {
+	},
 
-    run : function( page, doc ) {
+	run : function( page, doc ) {
 		try {
-			var displaystyles = ["red_1.jpg","orange_2.jpg","yellow_3.jpg","lightgreen_4.jpg","darkgreen_5.jpg","blue_6.jpg"];
 			var outerdiv = doc.getElementById('ctl00_CPMain_pnlplayerInfo');
 			var linksArray = outerdiv.getElementsByTagName('a');
-				for (var j=0; j<linksArray.length; j++) {
-					var link = linksArray[j];
-					if (link.href.search(/lt\=honesty/i) > -1
-						|| link.href.search(/lt\=gentleness/i) > -1
-						|| link.href.search(/lt\=aggressiveness/i) > -1 ) {
-                            var level = link.href.match(/ll\=(\d)/)[1];
-                            var img = doc.createElement("img");
-                            img.className = "ft-personality-img";
-                            img.src = Foxtrick.ResourcePath + "resources/personality/" + displaystyles[level];
-                            link.appendChild(img);
-					}
+			for (var j=0; j<linksArray.length; j++) {
+				var link = linksArray[j];
+				if (link.href.search(/lt\=honesty/i) > -1
+					|| link.href.search(/lt\=gentleness/i) > -1) {
+					var level = link.href.match(/ll\=(\d)/)[1];
+					var img = doc.createElement("img");
+					img.className = "ft-personality-img";
+					img.src = Foxtrick.ResourcePath + "resources/personality/red2blue/" + level + ".jpg";
+					link.appendChild(img);
 				}
+				else if (link.href.search(/lt\=aggressiveness/i) > -1) {
+					var level = link.href.match(/ll\=(\d)/)[1];
+					var img = doc.createElement("img");
+					img.className = "ft-personality-img";
+					img.src = Foxtrick.ResourcePath + "resources/personality/blue2red/" + level + ".jpg";
+					link.appendChild(img);
+				}
+			}
 		}
 		catch (e) {Foxtrick.dump('PersonalityImages->'+e+'\n');}
 	},
