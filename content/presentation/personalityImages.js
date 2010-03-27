0270567aa4141f05197f80757da2d80ed17aea13
ryan <ryan@ryanium.com>
2010-03-27 08:43:42 +0000

    personalityImages.js: now working on training page.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4662 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/personalityImages.js
+++ content/presentation/personalityImages.js
@@ -8,38 +8,43 @@ var FoxtrickPersonalityImages = {
 
 	MODULE_NAME : "PersonalityImages",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
-	PAGES : new Array('playerdetail'),
+	PAGES : new Array("playerdetail", "training"),
 	DEFAULT_ENABLED : true,
-	LATEST_CHANGE : "Show aggressiveness value as HT denomination",
+	LATEST_CHANGE : "Show aggressiveness value as HT denomination, and working on training page.",
 	LATEST_CHANGE_CATEGORY : Foxtrick.latestChangeCategories.FIX,
 
 	init : function() {
 	},
 
-	run : function( page, doc ) {
+	run : function(page, doc) {
 		try {
-			var outerdiv = doc.getElementById('ctl00_CPMain_pnlplayerInfo');
-			var linksArray = outerdiv.getElementsByTagName('a');
-			for (var j=0; j<linksArray.length; j++) {
-				var link = linksArray[j];
-				if (link.href.search(/lt\=honesty/i) > -1
-					|| link.href.search(/lt\=gentleness/i) > -1) {
-					var level = link.href.match(/ll\=(\d)/)[1];
+			var skills = doc.getElementsByClassName("skill");
+			for (var j = 0; j < skills.length; ++j) {
+				var skill = skills[j];
+				if (!skill.href) {
+					// it should be an <a> element
+					continue;
+				}
+				if (skill.href.search(/lt\=honesty/i) > -1
+					|| skill.href.search(/lt\=gentleness/i) > -1) {
+					var level = skill.href.match(/ll\=(\d)/)[1];
 					var img = doc.createElement("img");
 					img.className = "ft-personality-img";
 					img.src = Foxtrick.ResourcePath + "resources/personality/red2blue/" + level + ".jpg";
-					link.appendChild(img);
+					skill.appendChild(img);
 				}
-				else if (link.href.search(/lt\=aggressiveness/i) > -1) {
-					var level = link.href.match(/ll\=(\d)/)[1];
+				else if (skill.href.search(/lt\=aggressiveness/i) > -1) {
+					var level = skill.href.match(/ll\=(\d)/)[1];
 					var img = doc.createElement("img");
 					img.className = "ft-personality-img";
 					img.src = Foxtrick.ResourcePath + "resources/personality/blue2red/" + level + ".jpg";
-					link.appendChild(img);
+					skill.appendChild(img);
 				}
 			}
 		}
-		catch (e) {Foxtrick.dump('PersonalityImages->'+e+'\n');}
+		catch (e) {
+			Foxtrick.dumpError(e);
+		}
 	},
 
 	change : function( page, doc ) {
