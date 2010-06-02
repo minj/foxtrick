03242fa31d8d0a8d220e14ee5a16024c4cd44d05
ryan <ryan@ryanium.com>
2010-06-02 14:56:28 +0000

    matchincome.js: fixed insert position and code refinement.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@4944 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/matchincome.js
+++ content/matches/matchincome.js
@@ -27,6 +27,7 @@ var FoxtrickMatchIncome = {
 			if (alldivs[j].className=="sidebarBox" 
 			&& alldivs[j].getElementsByTagName('h2')[0].innerHTML.search(Foxtrickl10n.getString('foxtrick.matches.soldseats'))!=-1 ) {
 				var table=alldivs[j].getElementsByTagName('table')[0];
+				var tbody = table.getElementsByTagName('tbody')[0];
 				var sum=Foxtrick.trimnum(table.rows[0].cells[1].textContent)*6.5
 					+ Foxtrick.trimnum(table.rows[1].cells[1].textContent)*9.5
 					+ Foxtrick.trimnum(table.rows[2].cells[1].textContent)*18
@@ -44,13 +45,14 @@ var FoxtrickMatchIncome = {
 				var tr2 = doc.createElement('tr');
 				var td2a = doc.createElement('td');
 				var td2b = doc.createElement('td');
-				table.appendChild(tr2);
+				tbody.appendChild(tr2);
 				tr2.appendChild(td2a);
 				tr2.appendChild(td2b);
-				td2a.innerHTML=Foxtrickl10n.getString('foxtrick.matches.income');
-				td2a.setAttribute('class','ch');
                 td2a.id = 'ft_matchincome';
-				td2b.innerHTML=Foxtrick.ReturnFormatedValue (Math.floor(sum), '&nbsp;')+'&nbsp;'+FoxtrickPrefs.getString("oldCurrencySymbol");
+                td2a.className = "ch";
+                td2a.textContent = Foxtrickl10n.getString('foxtrick.matches.income');
+                td2b.className = "nowrap";
+				td2b.textContent = Foxtrick.ReturnFormatedValue (Math.floor(sum), ' ')+' '+FoxtrickPrefs.getString("oldCurrencySymbol");
 			}
         }    
     },
