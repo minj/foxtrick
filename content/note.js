57ed87aa7dec29c9d4e9d6057e2ddc0cfea87738
ryan <ryan@ryanium.com>
2010-07-02 07:21:05 +0000

    note.js: added hasClose parameter for close links.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5096 1e3807c3-0949-0410-b78e-337b627472c1

--- content/note.js
+++ content/note.js
@@ -25,49 +25,74 @@ Foxtrick.Note = {
 	 *         },
 	 *         ...
 	 *     ]
+	 * hasClose - whether to add a link labelled "Close" for closing the note
 	 */
-	create : function(doc, id, msg, buttons) {
+	create : function(doc, id, msg, buttons, hasClose) {
 		try {
 			var container = doc.createElement("div");
 			container.id = id;
 			container.className = "ft-note";
 
 			var par = doc.createElement("p");
-			par.appendChild(doc.createTextNode(msg));
+			par.textContent = msg;
+			container.appendChild(par);
 
-			var buttonContainer = doc.createElement("div");
-			for (var i in buttons) {
-				var button = doc.createElement("input");
-				button.type = "button";
-				button.value = Foxtrickl10n.getString(buttons[i].type.name);
-				if (buttons[i].listener) {
-					button.addEventListener("click", buttons[i].listener, false);
-				}
-				else if (buttons[i].onClick) {
-					button.setAttribute("onclick", buttons[i].onClick);
-				}
-				else if (buttons[i].type == this.BUTTON_CANCEL) {
-					button.setAttribute("container", id);
-					// default event listener for BUTTON_CANCEL
-					button.addEventListener("click", function(ev) {
-							try {
-								var doc = ev.target.ownerDocument;
-								var containerId = ev.currentTarget.getAttribute("container");
-								var container = doc.getElementById(containerId);
-								if (container) {
-									container.parentNode.removeChild(container);
+			if (buttons && buttons.length) {
+				var buttonContainer = doc.createElement("div");
+				for (var i in buttons) {
+					var button = doc.createElement("input");
+					button.type = "button";
+					button.value = Foxtrickl10n.getString(buttons[i].type.name);
+					if (buttons[i].listener) {
+						button.addEventListener("click", buttons[i].listener, false);
+					}
+					else if (buttons[i].onClick) {
+						button.setAttribute("onclick", buttons[i].onClick);
+					}
+					else if (buttons[i].type == this.BUTTON_CANCEL) {
+						button.setAttribute("container", id);
+						// default event listener for BUTTON_CANCEL
+						button.addEventListener("click", function(ev) {
+								try {
+									var doc = ev.target.ownerDocument;
+									var containerId = ev.currentTarget.getAttribute("container");
+									var container = doc.getElementById(containerId);
+									if (container) {
+										container.parentNode.removeChild(container);
+									}
 								}
-							}
-							catch (e) {
-								Foxtrick.dump(e);
-							}
-						}, false);
+								catch (e) {
+									Foxtrick.dumpError(e);
+								}
+							}, false);
+					}
+					buttonContainer.appendChild(button);
 				}
-				buttonContainer.appendChild(button);
+				container.appendChild(buttonContainer);
 			}
 
-			container.appendChild(par);
-			container.appendChild(buttonContainer);
+			if (hasClose === true) {
+				var close = doc.createElement("a");
+				close.className = "close";
+				close.textContent = Foxtrickl10n.getString("button.close");
+				close.addEventListener("click", function(ev) {
+						try {
+							var container = ev.target.parentNode;
+							if (container) {
+								container.parentNode.removeChild(container);
+							}
+						}
+						catch (e) {
+							Foxtrick.dumpError(e);
+						}
+					}, false);
+				container.appendChild(close);
+				// we need to add clear since close is floated to the right
+				var clear = doc.createElement("div");
+				clear.className = "clear";
+				container.appendChild(clear);
+			}
+
 			return container;
 		}
 		catch (e) {
