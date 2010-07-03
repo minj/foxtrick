111c0f4d21c2d11807d4eaa7cd753e4efa270c4f
ryan <ryan@ryanium.com>
2010-07-03 11:35:34 +0000

    staffmarker.js: use CSS file for styling.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5107 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/staffmarker.js
+++ content/forum/staffmarker.js
@@ -1,7 +1,7 @@
 /**
  * staffmarker.js
  * Foxtrick forum staff (HT, GM, Mod, CHPP, LA) marker
- * @author bummerland
+ * @author bummerland, ryanli
  */
 
 var FoxtrickStaffMarker = {
@@ -10,27 +10,18 @@ var FoxtrickStaffMarker = {
 	MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
 	PAGES : new Array('forumViewThread','forumWritePost','teamPage'),
     DEFAULT_ENABLED : true,
-	NEW_AFTER_VERSION: "0.5.0.5",
-	LATEST_CHANGE:"Marks staff (HT, GM, Mod, CHPP, LA, CHPP licence owners, Hattrick-Youthclub, FoxTrick) on team pages as well. SelectBox coloring fixed",
+	NEW_AFTER_VERSION : "0.5.2.1",
+	LATEST_CHANGE : "Use CSS file for styling.",
 	LATEST_CHANGE_CATEGORY : Foxtrick.latestChangeCategories.FIX,
-	OPTIONS : new Array("HT", "GM", "MOD", "LA", "CHPP", "editor", "foxtrick-dev", "chpp-holder", "hty_staff", "flag", "own", "manager"),
+	OPTIONS : ["flag", "own", "manager"],
 	OPTION_TEXTS : true,
-	OPTION_TEXTS_DEFAULT_VALUES : new Array("background-color:red;", //HT
-											"background-color:orange; color:black;", //GM
-                                            "background-color:yellow; color:black;", //MOD
-											"background-color:white; color:green;", //LA
-											"background-color:green; color:white;", //CHPP
-											"background-color:#7BB300; color:white;", //editor
-											"background-image: url('"+Foxtrick.ResourcePath+"resources/linkicons/foxtrick_2.png'); background-repeat: no-repeat; padding: 1px 1px 1px 18px;", //foxtrick-dev
-											"background-image: url('"+Foxtrick.ResourcePath+"resources/linkicons/chpp.png'); background-repeat: no-repeat; padding: 1px 1px 1px 24px;", //chpps
-											"background-image: url('"+Foxtrick.ResourcePath+"resources/linkicons/hyouthclub_small.png'); background-repeat: no-repeat; padding: 1px 1px 1px 18px;", //hty_staff
-											"background-color:white; color:black;", //flag
-											"userId=1000 userId=1001 style='color:yellow;' userId=1002 style='background-color:yellow;'", //own
-                                            ""
-                                        	),
-    OPTION_TEXTS_DISABLED_LIST : new Array(false,false,false,false,false,false,false,false,false,false,false, true),
-    // OPTIONS : new Array("HT", "GM", "MOD", "LA", "CHPP", "editor", "foxtrick-dev","own"),
-    
+	OPTION_TEXTS_DEFAULT_VALUES : ["",
+		"userId=1000 userId=1001 style='color:yellow;' userId=1002 style='background-color:yellow;'", //own
+		""],
+    OPTION_TEXTS_DISABLED_LIST : [true, false, true],
+
+	CSS : Foxtrick.ResourcePath + "resources/css/staff-marker.css",
+
 	htreg : /^HT-/i,
 	gmreg : /^GM-/i,
 	modreg : /^MOD-/i,
@@ -50,54 +41,14 @@ var FoxtrickStaffMarker = {
                                   "ctl00_CPMain_ddlRecipient"
     ),
 
-		stl_HT :'',
-		stl_GM : '',
-		stl_MOD : '',
-		stl_CHPP : '',
-		stl_hty_staff : '',
-		stl_LA : '',
-		stl_editor : '',
-		//editorsArray_joined : '',
-        chpps_joined : '',
-		//hty_staff_joined : '',
-		stl_foxtrick_dev : '',
-        stl_flag : '',
-		//foxtrickersArray_joined :'',
-
     init : function() {
-	try {
-		this.stl_HT = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "HT_text");
-          if (!this.stl_HT) this.stl_HT = this.OPTION_TEXTS_DEFAULT_VALUES[0];
-        this.stl_GM = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "GM_text");
-          if (!this.stl_GM) this.stl_GM = this.OPTION_TEXTS_DEFAULT_VALUES[1];
-        this.stl_MOD = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "MOD_text");
-          if (!this.stl_MOD) this.stl_MOD = this.OPTION_TEXTS_DEFAULT_VALUES[2];
-        this.stl_CHPP = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "CHPP_text");
-          if (!this.stl_CHPP) this.stl_CHPP = this.OPTION_TEXTS_DEFAULT_VALUES[3];
-        this.stl_LA = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "LA_text");
-          if (!this.stl_LA) this.stl_LA = this.OPTION_TEXTS_DEFAULT_VALUES[4];
-        this.stl_editor = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "editor_text");
-          if (!this.stl_editor) this.stl_editor = this.OPTION_TEXTS_DEFAULT_VALUES[5];
-		//this.editorsArray_joined = this.editorsArray.join();
-        this.stl_foxtrick_dev = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "foxtrick-dev_text");
-          if (!this.stl_foxtrick_dev) this.stl_foxtrick_dev = this.OPTION_TEXTS_DEFAULT_VALUES[6];
-		//this.foxtrickersArray_joined = this.foxtrickersArray.join();
-		this.stl_chpps = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "chpp-holder_text");
-          if (!this.stl_chpps) this.stl_chpps = this.OPTION_TEXTS_DEFAULT_VALUES[7];
-		//this.chpps_joined = this.chppholder.join();
-		this.stl_hty_staff = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "hty_staff_text");
-          if (!this.stl_hty_staff) this.stl_hty_staff = this.OPTION_TEXTS_DEFAULT_VALUES[8];
-		//this.hty_staff_joined = this.hty_staff.join();
-		this.stl_flag = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "flag_text");
-          if (!this.stl_flag) this.stl_flag = this.OPTION_TEXTS_DEFAULT_VALUES[9];
-      } catch(e){Foxtrick.dump('staffmarker '+e+'\n');}
     },
 
-    run : function( page, doc ) {
-    try {    
+    run : function(page, doc) {
+    	try {    
 			// not on open new thread
 			if (doc.location.href.search(/\/Forum\/Write\.aspx\?v=/)!=-1) return;
-						
+
 			// getting userids and colors
 			var utext = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "own_text");
             if (!utext)
@@ -106,53 +57,43 @@ var FoxtrickStaffMarker = {
 			if (Foxtrick.isModuleFeatureEnabled( this, "own")) {
 				users = utext.match(/userid=(\d+)/ig);
 				var ii=0,user;
-				while ( user = users[ii++] ) {
+				while (user = users[ii++]) {
 					try {
 						var ustyle = utext.substring(utext.search(user)).match(/style=\'(.+)\'/)[1];
 						this.ulist[user.replace(/userid=/i,'')] = ustyle;
-					} catch (e) {Foxtrick.alert('StaffMarker: Error in Style for: '+ user);}
+					}
+					catch (e) {
+						Foxtrick.dumpError(e);
+					}
 				}
 			}
-        switch( page )
-        {
-            case 'forumViewThread':
-                // Foxtrick.dump('forumViewThread\n');
-                FoxtrickStaffMarker._MarkAliases_thread(doc);
-                FoxtrickStaffMarker._MarkAliases_select(doc);
-            break;
+		    switch( page ) {
+		        case 'forumViewThread':
+		            // Foxtrick.dump('forumViewThread\n');
+		            FoxtrickStaffMarker._MarkAliases_thread(doc);
+		            FoxtrickStaffMarker._MarkAliases_select(doc);
+		        break;
 
-            case 'forumWritePost':
-                // Foxtrick.dump('forumWritePost\n');
-                FoxtrickStaffMarker._MarkAliases_select(doc);
-            break;
-			
-        }
-        
-		if (Foxtrick.isModuleFeatureEnabled( this, "manager")) {
-			switch( page )
-			{
+		        case 'forumWritePost':
+		            // Foxtrick.dump('forumWritePost\n');
+		            FoxtrickStaffMarker._MarkAliases_select(doc);
+		        break;
+		        
 				case 'teamPage':
-						// Foxtrick.dump('teamPage\n');
+					if (Foxtrick.isModuleFeatureEnabled( this, "manager")) {
 						FoxtrickStaffMarker._MarkAliases_thread(doc);
+					}
 				break;
-			}
+		    }
 		}
-	} catch(e){Foxtrick.dump('staffmarker: '+e+'\n');}
+		catch (e) {
+			Foxtrick.dumpError(e);
+		}
     },
-
 
     //Alias - Staff-Color
     _MarkAliases_thread : function (doc) {
         try {
-			var do_HT = Foxtrick.isModuleFeatureEnabled( this, "HT") ;
-			var do_GM = Foxtrick.isModuleFeatureEnabled( this, "GM") ;
-			var do_MOD = Foxtrick.isModuleFeatureEnabled( this, "MOD") ;
-			var do_CHPP = Foxtrick.isModuleFeatureEnabled( this, "CHPP") ;
-			var do_LA = Foxtrick.isModuleFeatureEnabled( this, "LA") ;
-			var do_editor = Foxtrick.isModuleFeatureEnabled( this, "editor") ;
-			var do_foxtrick_dev = Foxtrick.isModuleFeatureEnabled( this, "foxtrick-dev") ;
-			var do_chpps = Foxtrick.isModuleFeatureEnabled( this, "chpp-holder") ;
-			var do_hty_staff = Foxtrick.isModuleFeatureEnabled( this, "hty_staff") ;
 			var do_own = Foxtrick.isModuleFeatureEnabled( this, "own");
 
 			var userDivs = Foxtrick.getElementsByClass("float_left", doc.getElementById('mainWrapper'));
@@ -174,117 +115,110 @@ var FoxtrickStaffMarker = {
 
 					// earlier overwrites later. 
 					if (do_own && this.ulist[uid]!=null) {
-                        a.setAttribute("style", this.ulist[uid]);
+                        a.style = this.ulist[uid];
                     }
-                    if (do_HT && this.htreg.test(uname)) {
-                        a.setAttribute("style", this.stl_HT);
-                    } else if (do_GM && this.gmreg.test(uname)) {
-                         a.setAttribute("style", this.stl_GM);
-                    } else if (do_MOD && this.modreg.test(uname)) {
-                        a.setAttribute("style", this.stl_MOD);
-                    } else if (do_CHPP && this.chppreg.test(uname)) {
-                        a.setAttribute("style", this.stl_CHPP);
-                    } else if (do_LA && this.lareg.test(uname)) {
-                        a.setAttribute("style", this.stl_LA);
-                    } else if (do_editor && this.editorsArray[uid]!=null) {
-                        a.setAttribute("style", this.stl_editor);
-                    } 
-					// adding background images. lowest priority first
-					var cur_style= a.getAttribute("style");
-					if (!cur_style) cur_style='';
-					if (do_hty_staff && this.hty_staff != null && Foxtrick.in_array(this.hty_staff,uname)) {
-                        a.setAttribute("style", cur_style+this.stl_hty_staff);
-                    } 				
-					if (do_foxtrick_dev && this.foxtrickersArray[uid]!=null) {
-						 a.setAttribute("style", cur_style+this.stl_foxtrick_dev);						
+
+					// exclusive categories
+                    if (this.htreg.test(uname)) {
+                    	Foxtrick.addClass(a, "ft-staff-ht");
                     }
-					if (do_chpps && this.chppholder[uid]!=null) {
-                        a.setAttribute("style", cur_style+this.stl_chpps );
+                    else if (this.gmreg.test(uname)) {
+                    	Foxtrick.addClass(a, "ft-staff-gm");
+                    }
+                    else if (this.modreg.test(uname)) {
+                        Foxtrick.addClass(a, "ft-staff-mod");
+                    }
+                    else if (this.chppreg.test(uname)) {
+                        Foxtrick.addClass(a, "ft-staff-chpp");
+                    }
+                    else if (this.lareg.test(uname)) {
+                        Foxtrick.addClass(a, "ft-staff-la");
+                    }
+
+                    if (this.editorsArray[uid] != null) {
+                        Foxtrick.addClass(a, "ft-staff-editor");
+                    }
+					if (this.hty_staff != null && Foxtrick.in_array(this.hty_staff, uname)) {
+                        Foxtrick.addClass(a, "ft-staff-hty");
+                    }
+					if (this.foxtrickersArray[uid] != null) {
+						Foxtrick.addClass(a, "ft-staff-foxtrick");
+                    }
+					if (this.chppholder[uid]!=null) {
+                        Foxtrick.addClass(a, "ft-staff-chpp-holder");
                     } 
-					
                 }
             }
         }
-        catch(e) {
-            Foxtrick.dump('_MarkAliases_thread: '+e+'\n');
+        catch (e) {
+            Foxtrick.dumpError(e);
         }
     },
 
     //SelectBox - Staff-Color
     _MarkAliases_select : function (doc) {
         try {
-			var do_HT = Foxtrick.isModuleFeatureEnabled( this, "HT") ;
-			var do_GM = Foxtrick.isModuleFeatureEnabled( this, "GM") ;
-			var do_MOD = Foxtrick.isModuleFeatureEnabled( this, "MOD") ;
-			var do_CHPP = Foxtrick.isModuleFeatureEnabled( this, "CHPP") ;
-			var do_LA = Foxtrick.isModuleFeatureEnabled( this, "LA") ;
-			var do_editor = Foxtrick.isModuleFeatureEnabled( this, "editor") ;
-			var do_foxtrick_dev = Foxtrick.isModuleFeatureEnabled( this, "foxtrick-dev") ;
-            var do_chpps = Foxtrick.isModuleFeatureEnabled( this, "chpp-holder") ;
-			var do_hty_staff = Foxtrick.isModuleFeatureEnabled( this, "hty_staff") ;
-			var do_own = Foxtrick.isModuleFeatureEnabled( this, "own");
-            var do_flag = Foxtrick.isModuleFeatureEnabled( this, "flag");
+        	var do_flag = Foxtrick.isModuleFeatureEnabled(this, "flag"); 
+			var do_own = Foxtrick.isModuleFeatureEnabled(this, "own");
 
-            var new_style = '';
-            // Foxtrick.dump('forumSELECT => select\n');
-            var selectBoxTop = null;//doc.getElementById('ctl00_CPMain_ucThread_ucPagerTop_filterUser');
-			var selectBoxBottom = null;//doc.getElementById('ctl00_CPMain_ucThread_ucPagerBottom_filterUser');
-			
 			var selects = doc.getElementById('mainWrapper').getElementsByTagName('select');
-//			Foxtrick.dump('forumSELECT => select '+selects.length+'\n');
             for (var k=0;k<selects.length;++k) {
 				el_Select = selects[k];
 				if (el_Select.id.search(/filter/i)!=-1) {
                     //Foxtrick.dump('forumSELECT => select box:'+ el_Select.id + '.\n');
 					var i = 1, option;
                     while ( option = el_Select.options[i++] ) {
-                        new_style = '';
+                        var style = option.style;
                         //Foxtrick.dump('forumSELECT => select i:'+ i + '.\n');
-						var uname = Foxtrick.trim( option.text );
+						var uname = Foxtrick.trim(option.textContent);
                         uname = uname.substring(0, uname.indexOf(' '));
                         if (uname == '') uname = Foxtrick.trim( option.text );
                         if  (uname == '') break;
 						var uid = option.value.replace(/by_|to_/gi,'');
 
+						// exclusive categories
+						if (this.htreg.test(uname)) {
+							Foxtrick.addClass(option, "ft-staff-ht");
+						}
+						else if (this.gmreg.test(uname)) {
+							Foxtrick.addClass(option, "ft-staff-gm");
+						}
+						else if (this.modreg.test(uname)) {
+							Foxtrick.addClass(option, "ft-staff-mod");
+						}
+						else if (this.chppreg.test(uname)) {
+							Foxtrick.addClass(option, "ft-staff-chpp");
+						}
+						else if (this.lareg.test(uname)) {
+							Foxtrick.addClass(option, "ft-staff-la");
+                        }
+
+                        if (this.editorsArray[uid] != null) {
+                        	Foxtrick.addClass(option, "ft-staff-editor");
+                        }
+						if (this.hty_staff != null && Foxtrick.in_array(this.hty_staff,uname)) {
+							Foxtrick.addClass(option, "ft-staff-hty");
+                        } 					
+						if (this.foxtrickersArray[uid] != null) {
+							Foxtrick.addClass(option, "ft-staff-foxtrick");
+                        } 
+						if (this.chppholder[uid] != null) {
+                            Foxtrick.addClass(option, "ft-staff-chpp-holder");
+                        }
+
 						if (do_own && this.ulist[uid]!=null) {
 							new_style = this.ulist[uid];
 						}
-						if (do_HT && this.htreg.test(uname)) {
-							new_style += this.stl_HT;
-						} else if (do_GM && this.gmreg.test(uname)) {
-							new_style += this.stl_GM;
-						} else if (do_MOD && this.modreg.test(uname)) {
-							new_style += this.stl_MOD;
-						} else if (do_CHPP && this.chppreg.test(uname)) {
-							new_style += this.stl_CHPP;
-						} else if (do_LA && this.lareg.test(uname)) {
-							new_style += this.stl_LA;
-                        } else if (do_editor && this.editorsArray[uid]!=null) {
-                            new_style +=  this.stl_editor;
-                        } 
-						
-						// adding background images. lowest priority fisrt
-						if (do_hty_staff && this.hty_staff != null && Foxtrick.in_array(this.hty_staff,uname)) {
-                            new_style += this.stl_hty_staff;
-                        } 					
-						if (do_foxtrick_dev && this.foxtrickersArray[uid]!=null) {
-                            new_style +=  this.stl_foxtrick_dev;
-                        } 
-
-						if (do_chpps && this.chppholder[uid]!=null) {
-                            new_style += this.stl_chpps;
-                        } 
-
 						if (do_flag) {
-                            new_style += ';background-image: url("http://flags.alltidhattrick.org/userflags/' + option.value.replace(/by_|to_/,'') + '.gif");  background-repeat:no-repeat; padding-left:2px; background-position:180px 50%; width:195px;border-bottom:dotted thin #ddd';
+                            style += ';background-image: url("http://flags.alltidhattrick.org/userflags/' + option.value.replace(/by_|to_/,'') + '.gif");  background-repeat:no-repeat; padding-left:2px; background-position:180px 50%; width:195px;border-bottom:dotted thin #ddd';
                         }
-                        option.setAttribute("style", new_style);
+                        option.setAttribute("style", style);
                     }
                 }
             }
         }
-        catch(e) {
-            Foxtrick.dump('FoxtrickStaffMarker'+e);
+        catch (e) {
+            Foxtrick.dumpError(e);
         }
-    },
+    }
 };
