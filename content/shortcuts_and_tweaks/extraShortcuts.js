8397f4ca0fec4ca8a048b77245f7bef87062f1a8
ryan <ryan@ryanium.com>
2010-07-05 02:58:57 +0000

    Categorized the images under resources/img/ directory.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5122 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/extraShortcuts.js
+++ content/shortcuts_and_tweaks/extraShortcuts.js
@@ -57,7 +57,7 @@ var FoxtrickExtraShortcuts = {
                     var img1 = doc.createElement('img');
                     img1.setAttribute( "class", "ftSuppStats");
                     img1.src = Foxtrick.ResourcePath+"resources/img/transparent.gif";
-                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/stats.png') !important;");
+                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/stats.png') !important;");
                     img1.title = Foxtrickl10n.getString("foxtrick.ExtraShortcuts.statistics");
 
                     link.appendChild(img1);
@@ -81,7 +81,7 @@ var FoxtrickExtraShortcuts = {
                     var img1 = doc.createElement('img');
                     img1.setAttribute( "class", "ftMyTransfers");
                     img1.src = Foxtrick.ResourcePath+"resources/img/transparent.gif";
-                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/transfers.png') !important;");
+                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/transfers.png') !important;");
                     img1.title = Foxtrickl10n.getString("foxtrick.ExtraShortcuts.transfers");
 
                     link.appendChild(img1);
@@ -105,7 +105,7 @@ var FoxtrickExtraShortcuts = {
                     var img1 = doc.createElement('img');
                     img1.setAttribute( "class", "ftSCPrefs");
                     img1.src = Foxtrick.ResourcePath+"resources/img/transparent.gif";
-                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/options.png') !important;");
+                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/options.png') !important;");
                     img1.title = Foxtrickl10n.getString("foxtrick.ExtraShortcuts.prefs");
 
                     link.appendChild(img1);
@@ -129,7 +129,7 @@ var FoxtrickExtraShortcuts = {
                     var img1 = doc.createElement('img');
                     img1.setAttribute( "class", "ftSCPrefs");
                     img1.src = Foxtrick.ResourcePath+"resources/img/transparent.gif";
-                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/FT_icon.png') !important;");
+                    img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/foxtrick.png') !important;");
                     img1.title = Foxtrickl10n.getString("foxtrick.ExtraShortcuts.ftprefs");
 
                     link.appendChild(img1);
@@ -157,7 +157,7 @@ var FoxtrickExtraShortcuts = {
                     img1.setAttribute( "class", "ftSCPrefs");
                     img1.src = Foxtrick.ResourcePath+"resources/img/transparent.gif";
                     img1.id = 'ht_radio_icon';					
-					img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/radio-icon-offline.png') !important;");                    
+					img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/radio-icon-offline.png') !important;");                    
 					img1.title = "Hattrick Radio offline";
                     link.appendChild(img1);
                     
@@ -185,7 +185,7 @@ var FoxtrickExtraShortcuts = {
 											var link = doc.getElementById('ht_radio_link');
 											if (radio_xml.getElementsByTagName('status')[0].textContent==='online') {
 												var img1 = doc.getElementById('ht_radio_icon');
-												img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/radio-icon.png') !important;");                    
+												img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/radio-icon.png') !important;");                    
 												img1.title = "Hattrick Radio now playing: "+radio_xml.getElementsByTagName('song')[0].textContent;
 												if (Foxtrick.isModuleFeatureEnabled( this, "HtRadioWinamp")) 
 													link.href = radio_xml.getElementsByTagName('winamp')[0].textContent;
@@ -243,13 +243,13 @@ var FoxtrickExtraShortcuts = {
 						{ online=true; title='cool down die Ligaspiele Deutschland / Schweiz';}
 					if (online) {
 						//doc.getElementById('ft_extra-shortcuts_ht_radio_icon').
-						img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/dtradio.png') !important;");                    
+						img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/dtradio.png') !important;");                    
 						img1.title = "Radio downtime now playing: "+title;
 						link.href = 'http://www.down-times.de:2500/listen.pls';
 					}
 					//doc.getElementById('ft_extra-shortcuts_ht_radio_icon').
 					if (!online) {
-						img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/dtradio_offline.png') !important;");                    
+						img1.setAttribute("style","margin-left:2px; background-image: url('"+Foxtrick.ResourcePath+"resources/img/shortcuts/dtradio_offline.png') !important;");                    
 						img1.title = "Radio downtime offline";
 					}				
 				}
