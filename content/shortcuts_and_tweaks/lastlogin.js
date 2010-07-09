993fa4e708e01200c6aaf6bc23aa222fa0ff451f
ryan <ryan@ryanium.com>
2010-07-09 05:48:41 +0000

    lastlogin.js: unified indention.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5180 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/lastlogin.js
+++ content/shortcuts_and_tweaks/lastlogin.js
@@ -4,58 +4,57 @@
  */
 
 FoxtrickLastLogin = {
-
-    MODULE_NAME : "LastLogin",
-    MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	PAGES : new Array('managerPage'), 
-    DEFAULT_ENABLED : true,
+	MODULE_NAME : "LastLogin",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	PAGES : new Array('managerPage'),
+	DEFAULT_ENABLED : true,
 	NEW_AFTER_VERSION : "0.5.2.1",
 	LATEST_CHANGE : "Prettier layout for pages with faces disabled.",
 	LATEST_CHANGE_CATEGORY : Foxtrick.latestChangeCategories.FIX,
-	
-	change : function( page, doc ) {
+
+	change : function(page, doc) {
 		// FIXME - what does this mean?
 		if (doc.getElementById('ctl00_CPMain_upGB') != null) return;
 		FoxtrickLastLogin._Show(doc);
 	},
 
-    _Show : function(doc) {
-        try {
-            var div = doc.getElementById("pnlLogin");
-		    if (!div || div.hasAttribute("processed")) {
-		    	// if the last login div isn't present or has been processed,
-		    	// return.
-		        return;
-		    }
+	_Show : function(doc) {
+		try {
+			var div = doc.getElementById("pnlLogin");
+			if (!div || div.hasAttribute("processed")) {
+				// if the last login div isn't present or has been processed,
+				// return.
+				return;
+			}
 			var httime = doc.getElementById("time").textContent;
-            var HT_date = Foxtrick.getDateFromText(httime);
-            if (!HT_date) return;
+			var HT_date = Foxtrick.getDateFromText(httime);
+			if (!HT_date) return;
 
-            var login_elm = div.innerHTML.split('<br>');
-            var newInner = "";
-            for (var i=0; i<login_elm.length;i++){
-                login_elm[i] = Foxtrick.trim(login_elm[i]);
-                var last = '';
-                if (login_elm[i].search(/\*\*\*\.\*\*\*/) != -1) {
-                    var ST_date = Foxtrick.getDateFromText( login_elm[i] );
+			var login_elm = div.innerHTML.split('<br>');
+			var newInner = "";
+			for (var i=0; i<login_elm.length;i++){
+				login_elm[i] = Foxtrick.trim(login_elm[i]);
+				var last = '';
+				if (login_elm[i].search(/\*\*\*\.\*\*\*/) != -1) {
+					var ST_date = Foxtrick.getDateFromText(login_elm[i]);
 
-                    var _s = Math.floor( (HT_date.getTime() - ST_date.getTime()) / 1000); //Sec
-                    var DiffText = TimeDifferenceToText (_s);
-                    if (DiffText.search("NaN") == -1)
-                        last +=  '<span class="date smallText ft-last-login-diff">(' + DiffText + ')</span>';
-                    else Foxtrick.dump('  Could not create timediff (NaN)\n');
-                }
-                newInner += login_elm[i] + last;
-                if (i !== login_elm.length - 1) {
-                    // if not last row, add a break
-                    newInner += "<br/>\n";
-                }
-            }
+					var _s = Math.floor((HT_date.getTime() - ST_date.getTime()) / 1000); //Sec
+					var DiffText = TimeDifferenceToText(_s);
+					if (DiffText.search("NaN") == -1)
+						last += '<span class="date smallText ft-last-login-diff">(' + DiffText + ')</span>';
+					else Foxtrick.dump('Could not create timediff (NaN)\n');
+				}
+				newInner += login_elm[i] + last;
+				if (i !== login_elm.length - 1) {
+					// if not last row, add a break
+					newInner += "<br/>\n";
+				}
+			}
 			div.innerHTML = newInner;
 			div.setAttribute("processed", "processed");
-        }
-        catch (e) {
-            Foxtrick.dumpError(e);
-        }
-    }
+		}
+		catch (e) {
+			Foxtrick.dumpError(e);
+		}
+	}
 };
