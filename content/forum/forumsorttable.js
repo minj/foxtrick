c2360537cf69620e8dce5501d2ae096e9c3e1013
convincedd <convincedd@gmail.com>
2010-07-24 13:29:02 +0000

    forumsorttable.js: proper youthskill and age sort added
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5232 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forumsorttable.js
+++ content/forum/forumsorttable.js
@@ -14,8 +14,11 @@ var FoxtrickForumSortTable = {
 	LATEST_CHANGE:"Sort option for forum tables",
 	LATEST_CHANGE_CATEGORY : Foxtrick.latestChangeCategories.NEW,
 	DEFAULT_ENABLED : true,
-
-	sortString : true,
+	CSS: Foxtrick.ResourcePath + "resources/css/forumsorttable.css",
+	
+	sortNum : false,
+	sortYouthSkill : false,
+	sortAge : false,
 	sortIndex : -1,			
 				
 	run : function( page, doc ) {
@@ -43,13 +46,17 @@ var FoxtrickForumSortTable = {
 			var index = j;	
 			Foxtrick.dump('index ' + index + '\n');
 			
-			var is_num = true;
+			var is_num = true, is_age=true, is_youthskill = true;;
 			for (var i = 1; i < table.rows.length; ++i) {
 		    	var inner = Foxtrick.trim(Foxtrick.stripHTML(table.rows[i].cells[index].innerHTML));
 				//Foxtrick.dump( (inner!='')+' '+isNaN(parseFloat(inner))+' '+ parseInt(inner)+'\n');	
-				if (isNaN(parseFloat(inner)) && inner!='') {is_num=false; break;} 
+				if (isNaN(parseFloat(inner)) && inner!='') {is_num=false;} 
+		    	if (inner.search(/^(-|\d)\/(-|\d)$/)==-1 && inner!='') {is_youthskill=false;} 
+		    	if (inner.search(/^\d+\.\d+$/)==-1 && inner!='') {is_age=false;} 
 		    }
 			Foxtrick.dump('is_num '+is_num+'\n');
+			Foxtrick.dump('is_youthskill '+is_youthskill+'\n');
+			Foxtrick.dump('is_age '+is_age+'\n');
 			
 			// old rows to array
 			var table_old = table.cloneNode(true);
@@ -57,11 +64,16 @@ var FoxtrickForumSortTable = {
 			for (var i = 1; i < table.rows.length; ++i) {
 				rows.push(table_old.rows[i].cloneNode(true));
 			}
+			
 			// sort options
-			if (!is_num) FoxtrickForumSortTable.sortString = true;
+			FoxtrickForumSortTable.sortYouthSkill = is_youthskill;
+			FoxtrickForumSortTable.sortAge = is_age;
+			FoxtrickForumSortTable.sortNum = is_num;
 			FoxtrickForumSortTable.sortIndex = index;			
+			
 			// sort them
 			rows.sort(FoxtrickForumSortTable.sortCompare);
+			
 			// put them back
 			for (var i = 1; i < table.rows.length; ++i) {
 				table_old.rows[i].innerHTML = rows[i-1].innerHTML;
@@ -82,12 +94,16 @@ var FoxtrickForumSortTable = {
 	sortCompare : function(a, b) {
 		var aContent, bContent;
 		
-		aContent = Foxtrick.stripHTML(a.cells[FoxtrickForumSortTable.sortIndex].textContent);
-		bContent = Foxtrick.stripHTML(b.cells[FoxtrickForumSortTable.sortIndex].textContent);
+		aContent = a.cells[FoxtrickForumSortTable.sortIndex].innerHTML;
+		bContent = b.cells[FoxtrickForumSortTable.sortIndex].innerHTML;
 	
 		if (aContent === bContent) {
 			return 0;
 		}
+
+		aContent = Foxtrick.stripHTML(aContent);
+		bContent = Foxtrick.stripHTML(bContent);
+
 		// place empty cells at the bottom
 		if (aContent === "" || aContent === null || aContent === undefined) {
 			return 1;
@@ -95,25 +111,37 @@ var FoxtrickForumSortTable = {
 		if (bContent === "" || bContent === null || bContent === undefined) {
 			return -1;
 		}
-		if (FoxtrickForumSortTable.sortString) {
-			// always sort by ascending order
-			return aContent.localeCompare(bContent);
+		
+		if (FoxtrickForumSortTable.sortYouthSkill) {
+			aContent = aContent.replace('-','0').match(/^(\d)\/(\d)$/);
+			aContent = aContent[1] * 18 + aContent[2] * 2 + (a.cells[FoxtrickForumSortTable.sortIndex].getElementsByTagName('strong').length==0?0:1); 			
+			bContent = bContent.replace('-','0').match(/^(\d)\/(\d)$/);
+			bContent = bContent[1] * 18 + bContent[2] * 2 + (b.cells[FoxtrickForumSortTable.sortIndex].getElementsByTagName('strong').length==0?0:1); 
+			return bContent - aContent;
 		}
-		else {
+		else if (FoxtrickForumSortTable.sortAge) {
+			aContent = aContent.match(/^(\d+)\.(\d+)$/);
+			aContent = parseInt(aContent[1]) * 1000 + parseInt(aContent[2]) ;			
+			bContent = bContent.match(/^(\d+)\.(\d+)$/);
+			bContent = parseInt(bContent[1]) * 1000 + parseInt(bContent[2]);
+			return aContent - bContent;
+		}
+		else if (FoxtrickForumSortTable.sortNum) {
 			aContent = parseFloat(aContent);
 			bContent = parseFloat(bContent);
 			aContent = isNaN(aContent) ? 0 : aContent;
 			bContent = isNaN(bContent) ? 0 : bContent;
 			if (aContent === bContent) {
 				return 0;
-			}
-			/*if (FoxtrickForumSortTable.sortAsc) {
-				return aContent - bContent;
-			}
-			else*/ {
+			}			
+			else {
 				return bContent - aContent;
 			}
 		}
+		else { // sort string
+			// always sort by ascending order
+			return aContent.localeCompare(bContent);
+		}		
 	},
 
 };
