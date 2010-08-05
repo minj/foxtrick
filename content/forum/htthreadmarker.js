68db73820135095fe0856c5f3634811b0ae348dc
ryan <ryan@ryanium.com>
2010-08-05 09:26:58 +0000

    htthreadmarker.js: used CSS file for styling.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5292 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/htthreadmarker.js
+++ content/forum/htthreadmarker.js
@@ -10,70 +10,45 @@
     MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
 	PAGES : new Array('forum'), 
     DEFAULT_ENABLED : true,
-	NEW_AFTER_VERSION: "0.5.0.5",
-	LATEST_CHANGE:"Fix for threads with masked chars",
+    CSS : Foxtrick.ResourcePath + "resources/css/ht-thread.css",
+	NEW_AFTER_VERSION : "0.5.2.1",
+	LATEST_CHANGE : "Use CSS file for styling.",
 	LATEST_CHANGE_CATEGORY : Foxtrick.latestChangeCategories.FIX,
 
 	run : function( page, doc ) {
-	
-	try {
-		
-		this.ColorLatest (doc, "threadContent", "folderitem");
-		
-//		if (doc.getElementById( 'HT_thread0' )) return;
-//		Foxtrick.dump(this.MODULE_NAME+' change\n')
-    
+		this.ColorLatest(doc, "threadContent", "folderitem");
+
 		var myForums = doc.getElementById("content").getElementsByTagName('div')[0];
 		var divs = myForums.getElementsByTagName( "div" );
 		var cname;
-		//var htthreadnum=0;
-		
-        // Foxtrick.dump( "found " + links.length + "\n" );
-		var i = 0, div;
-		while ( div = divs[++i] ) {
-			cname = div.getAttribute( "class" ); 
-			if ( cname ==  "url" )
-			{	
-				//if (htthreadnum==0) div.setAttribute( "id", "HT_thread"+htthreadnum );
-				//htthreadnum++;
 
+		var i = 0, div;
+		while (div = divs[++i]) {
+			cname = div.getAttribute( "class" ); 
+			if (cname == "url" ) {	
 				var inner =div.childNodes[0];
-				var strong=div.getElementsByTagName('strong');
-				if (strong!=null && strong[0]!=null) {inner=strong[0];}
-				inner=inner.firstChild.data;
-				//Foxtrick.dump (inner+' '+div.childNodes[0].getAttribute( "title" )+'\n');
-				var title = div.childNodes[0].getAttribute( "title" ).replace(inner,'');
-								//var title = div.childNodes[0].getAttribute( "title" );
-				var poster = title;//.substring(title.lastIndexOf(' '));
-				//Foxtrick.dump (title+' '+poster+'\n');
-				
-				
-				if ( poster.match(/ HT-\S+/))		//old:  (/.* HT-[^\s]*$/i ) )
-				{
-					//Foxtrick.dump (inner.nodeType+' : '+ inner.nodeValue+' : '+inner.data+' : '+inner.innerHTML+' : '+div.childNodes[0].getAttribute( "title" )+'\n');
-					var curr_class = div.getAttribute( "class" );
-					if (!curr_class) curr_class='';
-					if (curr_class.search('HT_thread')==-1) {
-						div.setAttribute( "class", curr_class + " HT_thread" );
-						//Foxtrick.dump( "matched: " + title + " : "+curr_class+ "\n" );
-					}
+				var strong = div.getElementsByTagName('strong');
+				if (strong != null && strong[0] != null) {
+					inner=strong[0];
+				}
+				inner = inner.firstChild.data;
+				var title = div.childNodes[0].getAttribute("title").replace(inner,'');
+				var poster = title;
+
+				if (poster.match(/ HT-\S+/)) {
+					Foxtrick.addClass(div, "ft-ht-thread");
 				}
 			}	
 		}
-	} catch(e) {Foxtrick.dump('HTThreadMarker: '+e+'\n');}
 	},
 
     change : function( page, doc ) {        
-        this.run( page, doc );
+        this.run(page, doc);
 	},
-	
+
 	ColorLatest : function (doc,id,classname) {
-		//if (doc.getElementById( 'HT_threadLatest0' )) return;
-    
 		var myForums = doc.getElementById( id );
 		if (myForums) {
-			//Foxtrick.dump('ColorLatest change\n')
-			//var htthreadnum=0;
 			var links = myForums.getElementsByTagName( "table" );
 
 			for (var j=0;j<links.length;j++) {
@@ -83,22 +58,15 @@
 					|| links[j].rows[0].cells.length==0 
 					|| links[j].rows[0].cells[0].className.search('date')==-1) continue;
 				
-				for ( var i = 0; i < links[j].rows.length; ++i )
-				{	if (links[j].rows[i].cells.length<=1) continue;
+				for ( var i = 0; i < links[j].rows.length; ++i ) {
+					if (links[j].rows[i].cells.length<=1) continue;
 					var node = links[j].rows[i].cells[1].childNodes[1];
 					var title = node.getElementsByTagName( "a" )[0].title;					
-					if ( title.match( /.* HT-[^\s]*$/i ) )
-					{
-						var curr_class = node.getAttribute( "class" );
-						if (curr_class.search('HT_thread')==-1) {
-							node.setAttribute( "class", curr_class + " HT_thread" );
-							//node.setAttribute( "id", "HT_threadLatest"+htthreadnum );
-							//htthreadnum++;
-							//Foxtrick.dump( "matched: " + title + " : "+curr_class+ "\n" );
-						}
+					if (title.match( /.* HT-[^\s]*$/i )) {
+						Foxtrick.addClass(node, "ft-ht-thread");
 					}
 				}
 			}
 		}
-	},
+	}
 };
