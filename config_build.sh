12932a3577d8c5203a38e5bb7731dbe331688b5d
ryan <ryan@ryanium.com>
2010-08-28 14:23:02 +0000

    Added bash build script and config script.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5380 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ config_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build config for the build script, build.sh. Look there for more info.
+
+APP_NAME=foxtrick
+CHROME_PROVIDERS="content skin"
+CLEAN_UP=1
+ROOT_FILES="HACKING"
+ROOT_DIRS="defaults platform"
+BEFORE_BUILD=
+AFTER_BUILD=
