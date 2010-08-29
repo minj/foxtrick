de8f59bd3ca733c175d8dc6503d0daee21511a9a
ryan <ryan@ryanium.com>
2010-08-29 13:03:25 +0000

    mediantransferprice.js: some clean up.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5385 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts_and_tweaks/mediantransferprice.js
+++ content/shortcuts_and_tweaks/mediantransferprice.js
@@ -5,17 +5,15 @@
  */
 ////////////////////////////////////////////////////////////////////////////////
 var FoxtrickMedianTransferPrice = {
-
-    MODULE_NAME : "MedianTransferPrice",
+	MODULE_NAME : "MedianTransferPrice",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
 	PAGES : new Array('transferCompare'),
 	DEFAULT_ENABLED : true,
 
-    run : function( page, doc ) {
-
-        var table = doc.getElementsByTagName("table")[0];
-        if (!table) return;
-        if (table.rows[0].cells.length < 5) return;
+	run : function( page, doc ) {
+		var table = doc.getElementsByTagName("table")[0];
+		if (!table) return;
+		if (table.rows[0].cells.length < 5) return;
 		var count = table.rows.length;
 		var priceArray = [];
 		for (var i = 5; i<count; i++) {
@@ -28,47 +26,42 @@ var FoxtrickMedianTransferPrice = {
 		priceArray.sort(function(a,b){return a-b;});
 		var median = 0;
 		var avg = 0;
-		var lengte = priceArray.length;
-		for (var i in priceArray){
+		var len = priceArray.length;
+		for (var i in priceArray) {
 			avg = avg + priceArray[i];
 		}
-		avg = Math.round(avg / lengte)+"";
+		avg = Math.round(avg / len);
 
-		if(lengte % 2 ==1){
-			median = Math.round(priceArray[(lengte-1)/2])+"";
-		} else {
-			median = Math.round((priceArray[(lengte/2)-1]+priceArray[lengte/2])/2)+"";
+		if (len % 2 ==1) {
+			median = Math.round(priceArray[(len-1)/2]);
+		}
+		else {
+			median = Math.round((priceArray[(len/2)-1]+priceArray[len/2])/2);
 		}
 
 		if (count>0) {
-		    var currency = Foxtrick.trim(table.rows[5].cells[3].textContent.match(/\D+$/)[0]);
-		    var row = table.insertRow(table.rows.length);
-		    var cell = row.insertCell(0);
-		    cell.setAttribute("style", "text-align: left; font-weight: bold");
-		    cell.colSpan = 2;
-		    cell.innerHTML = Foxtrickl10n.getString( 'transfercompare_medianprice' );;
-		    cell = row.insertCell(1);
-		    cell.setAttribute("style", "text-align: right; font-weight: bold");
-		    cell.colSpan = 2;
-		    cell.innerHTML = this.group(median," ") + " " + currency;
+			var currency = Foxtrick.trim(table.rows[5].cells[3].textContent.match(/\D+$/)[0]);
+			var row = table.insertRow(table.rows.length);
+			var cell = row.insertCell(0);
+			cell.className = "left bold";
+			cell.colSpan = 3;
+			cell.textContent = Foxtrickl10n.getString( 'transfercompare_medianprice' );;
+			cell = row.insertCell(1);
+			cell.className = "right bold";
+			cell.textContent = Foxtrick.formatNumber(median) + " " + currency;
+			cell = row.insertCell(2);
+			cell.colSpan = 2;
 
-		    row = table.insertRow(table.rows.length);
-		    cell = row.insertCell(0);
-		    cell.setAttribute("style", "text-align: left; font-weight: bold");
-		    cell.colSpan = 2;
-		    cell.innerHTML = Foxtrickl10n.getString( 'transfercompare_avgprice' );;
-		    cell = row.insertCell(1);
-		    cell.setAttribute("style", "text-align: right; font-weight: bold");
-		    cell.colSpan = 2;
-		    cell.innerHTML = this.group(avg," ") + " " + currency;
-	    }
-	},
-
-
-	group : function (astring, chr, size ) {
-		if ( typeof chr == 'undefined' ) chr = ",";
-		if ( typeof size == 'undefined' ) size = 3;
-		return astring.split( '' ).reverse().join( '' ).replace( new RegExp( "(.{" + size + "})(?!$)", "g" ), "$1" + chr ).split( '' ).reverse().join( '' );
+			row = table.insertRow(table.rows.length);
+			cell = row.insertCell(0);
+			cell.className = "left bold";
+			cell.colSpan = 3;
+			cell.textContent = Foxtrickl10n.getString( 'transfercompare_avgprice' );;
+			cell = row.insertCell(1);
+			cell.className = "right bold";
+			cell.textContent = Foxtrick.formatNumber(avg) + " " + currency;
+			cell = row.insertCell(2);
+			cell.colSpan = 2;
+		}
 	}
 };
-
