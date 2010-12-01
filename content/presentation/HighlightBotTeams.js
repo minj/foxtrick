fa125e0845ed934c7ce58f2bfbaecdcbdd258655
ryan <ryan@ryanium.com>
2010-12-01 14:20:43 +0000

    HighlightBotTeams.js: fix for latest Hattrick changes.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5613 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/HighlightBotTeams.js
+++ content/presentation/HighlightBotTeams.js
@@ -14,7 +14,7 @@ FoxtrickHighlightBotTeams = {
 	CSS : Foxtrick.ResourcePath + "resources/css/highlight-bots.css",
 
 	run : function(page, doc) {
-		var span = doc.getElementById("ctl00_CPMain_repLeagueTable");
+		var span = doc.getElementById("ctl00_ctl00_CPContent_CPMain_repLeagueTable");
 		var table = span.getElementsByTagName("table")[0];
 		var bots = table.getElementsByClassName("shy");
 		for (var i = 0; i < bots.length; ++i) {
