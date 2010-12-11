6dbdabbb855b1472896965d630dc59ac702e516f
ryan <ryan@ryanium.com>
2010-12-11 02:20:09 +0000

    linksnational.js: add links even if no special links are present.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5738 1e3807c3-0949-0410-b78e-337b627472c1

--- content/links/linksnational.js
+++ content/links/linksnational.js
@@ -31,33 +31,29 @@ var FoxtrickLinksNational = {
         var links = Foxtrick.LinkCollection.getLinks("nationalteamlink", { "countryid": countryid,"ntteamid":ntteamid,"LeagueOfficeTypeID":LeagueOfficeTypeID }, doc, this);
 
 		var added=0;
-		if (links.length > 0) {
-			ownBoxBody = doc.createElement("div");
-			var header = Foxtrickl10n.getString(
-						"foxtrick.links.boxheader" );
-			var ownBoxId = "foxtrick_links_box";
-			var ownBoxBodyId = "foxtrick_links_content";
-			ownBoxBody.setAttribute( "id", ownBoxBodyId );
+		ownBoxBody = doc.createElement("div");
+		var header = Foxtrickl10n.getString(
+					"foxtrick.links.boxheader" );
+		var ownBoxId = "foxtrick_links_box";
+		var ownBoxBodyId = "foxtrick_links_content";
+		ownBoxBody.setAttribute( "id", ownBoxBodyId );
 
-			for (var k = 0; k < links.length; k++) {
-				links[k].link.className ="inner";
-				ownBoxBody.appendChild(links[k].link);
-				++added;
-			}
-
+		for (var k = 0; k < links.length; k++) {
+			links[k].link.className ="inner";
+			ownBoxBody.appendChild(links[k].link);
+			++added;
 		}
+
 		if (Foxtrick.isModuleEnabled(FoxtrickLinksTracker)) {
 			var links2 = Foxtrick.LinkCollection.getLinks("trackernationalteamlink", { "countryid": countryid,"ntteamid":ntteamid,"LeagueOfficeTypeID":LeagueOfficeTypeID }, doc, FoxtrickLinksTracker);
-   			if (links2.length > 0) {
-				for (var k = 0; k < links2.length; k++) {
-					links2[k].link.className ="flag inner";
-					var img=links2[k].link.getElementsByTagName('img')[0];
-					var style="vertical-align:top; margin-top:1px; background: transparent url(/Img/Flags/flags.gif) no-repeat scroll "+ (-20)*countryid+"px 0pt; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial;";
-					img.setAttribute('style',style);
-					img.src="/Img/Icons/transparent.gif";
-					ownBoxBody.appendChild(links2[k].link);
-					++added;
-				}
+			for (var k = 0; k < links2.length; k++) {
+				links2[k].link.className ="flag inner";
+				var img=links2[k].link.getElementsByTagName('img')[0];
+				var style="vertical-align:top; margin-top:1px; background: transparent url(/Img/Flags/flags.gif) no-repeat scroll "+ (-20)*countryid+"px 0pt; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial;";
+				img.setAttribute('style',style);
+				img.src="/Img/Icons/transparent.gif";
+				ownBoxBody.appendChild(links2[k].link);
+				++added;
 			}
 		}
 		if (added) Foxtrick.addBoxToSidebar( doc, header, ownBoxBody, ownBoxId, "first", "");
