ae085c633100a91651bf19abd048b23bbd287c1a
ryan <ryan@ryanium.com>
2010-12-17 06:03:10 +0000

    extendedplayerdetails.js: unified indention.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5789 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/extendedplayerdetails.js
+++ content/presentation/extendedplayerdetails.js
@@ -5,94 +5,94 @@
  */
 
 FoxtrickExtendedPlayerDetails = {
-    MODULE_NAME : "ExtendedPlayerDetails",
-    MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	MODULE_NAME : "ExtendedPlayerDetails",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
 	PAGES : new Array('playerdetail'),
-    RADIO_OPTIONS : new Array( "SWD", "SW", "SD", "WD", "D" ),
+	RADIO_OPTIONS : new Array( "SWD", "SW", "SD", "WD", "D" ),
 
-    run : function(page, doc) {
-        this._Player_Joined ( doc );
-    },
+	run : function(page, doc) {
+		this._Player_Joined(doc);
+	},
 
-    _Player_Joined  : function ( doc ) {
-        // Player in team since...
-        var processed = doc.getElementsByClassName("ft_since");
-        if (processed.length > 0)
-        	return;
+	_Player_Joined : function(doc) {
+		// Player in team since...
+		var processed = doc.getElementsByClassName("ft_since");
+		if (processed.length > 0)
+			return;
 
-        var div = doc.getElementsByClassName("playerInfo")[0];
-        var joined_elm = div.getElementsByClassName("shy")[0];
+		var div = doc.getElementsByClassName("playerInfo")[0];
+		var joined_elm = div.getElementsByClassName("shy")[0];
 
-        var dateObj = Foxtrick.util.time.getDateFromText(joined_elm.textContent);
-        var season_week = Foxtrick.util.time.gregorianToHT(dateObj);
+		var dateObj = Foxtrick.util.time.getDateFromText(joined_elm.textContent);
+		var season_week = Foxtrick.util.time.gregorianToHT(dateObj);
 
 		var htDate = Foxtrick.util.time.getHtDate(doc)
 
-        var joined_s = Math.floor((htDate.getTime() - dateObj.getTime()) / 1000); //Sec
+		var joined_s = Math.floor((htDate.getTime() - dateObj.getTime()) / 1000); //Sec
 
-        var JoinedText = 'NaN';
-        try {
-            JoinedText = Foxtrick.util.time.timeDifferenceToText (joined_s , true);
-        }
-        catch(ee) {
-            Foxtrick.dump('  JoinedText >' + ee + '\n');
-        }
+		var JoinedText = 'NaN';
+		try {
+			JoinedText = Foxtrick.util.time.timeDifferenceToText (joined_s , true);
+		}
+		catch(ee) {
+			Foxtrick.dump('  JoinedText >' + ee + '\n');
+		}
 
-        if (JoinedText.search("NaN") == -1) {
-            var part1 = Foxtrick.substr(joined_elm.innerHTML, 0, Foxtrick.strrpos( joined_elm.innerHTML, ")"));
-            part1 = part1.replace('(', '<span class="date smallText ft_since"><br>(');
-            joined_elm.innerHTML = part1 + ' <span>('+ season_week.week + '/' + season_week.season + ')</span>, ' + JoinedText + ')</span>';
-        }
-        else Foxtrick.dump('  Could not create jointime (NaN)\n');
-    }
+		if (JoinedText.search("NaN") == -1) {
+			var part1 = Foxtrick.substr(joined_elm.innerHTML, 0, Foxtrick.strrpos( joined_elm.innerHTML, ")"));
+			part1 = part1.replace('(', '<span class="date smallText ft_since"><br>(');
+			joined_elm.innerHTML = part1 + ' <span>('+ season_week.week + '/' + season_week.season + ')</span>, ' + JoinedText + ')</span>';
+		}
+		else Foxtrick.dump('  Could not create jointime (NaN)\n');
+	}
 };
 
 
 FoxtrickExtendedPlayerDetailsWage = {
 
-    MODULE_NAME : "ExtendedPlayerDetailsWage",
-    MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	MODULE_NAME : "ExtendedPlayerDetailsWage",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
 	PAGES : new Array('playerdetail'),
-    OPTIONS : new Array( "WageWithoutBonus", "SeasonWage"),
+	OPTIONS : new Array( "WageWithoutBonus", "SeasonWage"),
 
-    run : function(page, doc) {
+	run : function(page, doc) {
 		var div = doc.getElementById("ft_bonuswage");
-        if (div != null) return;
+		if (div != null) return;
 
-        try {
-        	var div = doc.getElementsByClassName("playerInfo")[0];
-            var wageElm = div.getElementsByTagName("table")[0].rows[2].cells[1];
-        }
-        catch (e) {
-        	// no such thing, return
-        	return;
-        }
+		try {
+			var div = doc.getElementsByClassName("playerInfo")[0];
+			var wageElm = div.getElementsByTagName("table")[0].rows[2].cells[1];
+		}
+		catch (e) {
+			// no such thing, return
+			return;
+		}
 
 		var wageText = wageElm.textContent;
-        var hasBonus = wageText.indexOf("%") > 0;
+		var hasBonus = wageText.indexOf("%") > 0;
 
-        var currency = Foxtrick.util.currency.getSymbol();
+		var currency = Foxtrick.util.currency.getSymbol();
 		var currencyLen = currency.length;
 		var splitPos = wageText.indexOf(currency) + currencyLen;
-        var part1 = wageText.substr(0, splitPos);
-        var part2 = wageText.substr(splitPos);
+		var part1 = wageText.substr(0, splitPos);
+		var part2 = wageText.substr(splitPos);
 
 		var wage = parseInt(part1.replace(/\s/g, ""));
 		if (isNaN(wage))
 			return;
 
-        if (!hasBonus)
-	        var formattedWage = Foxtrick.formatNumber(wage, "&nbsp;");
-	    else {
-	    	var reducedWage =  Math.floor(wage / 1.2);
-	    	var formattedWage = Foxtrick.formatNumber(reducedWage, "&nbsp;");
-	    }
+		if (!hasBonus)
+			var formattedWage = Foxtrick.formatNumber(wage, "&nbsp;");
+		else {
+			var reducedWage = Math.floor(wage / 1.2);
+			var formattedWage = Foxtrick.formatNumber(reducedWage, "&nbsp;");
+		}
 
 		// get space before currency symbol
 		part1 = part1.replace(" " + currency, currency);
 		part1 = part1.replace(currency, "&nbsp;" + currency);
 
-	    if (hasBonus && Foxtrick.isModuleFeatureEnabled(this, "WageWithoutBonus")) {
+		if (hasBonus && Foxtrick.isModuleFeatureEnabled(this, "WageWithoutBonus")) {
 			wageElm.innerHTML = part1
 			+ '&nbsp;<span id="ft_bonuswage" style="direction: ltr !important; color:#666666; ">('
 			+ formattedWage + '&nbsp;' + currency + ')</span> '
@@ -103,5 +103,5 @@ FoxtrickExtendedPlayerDetailsWage = {
 				+ Foxtrick.formatNumber(wage * 16, "&nbsp;") + "&nbsp;"
 				+ currency
 				+ Foxtrickl10n.getString("foxtrick.ExtendedPlayerDetails.perseason");
-    }
+	}
 };
