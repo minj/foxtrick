0cae72f28ba0a146417e620ce269caa0f461778b
ryan <ryan@ryanium.com>
2010-12-31 06:57:20 +0000

    maintainer/upload-nightly.sh: fix version name.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@5891 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/upload-nightly.sh
+++ maintainer/upload-nightly.sh
@@ -21,7 +21,7 @@ REVISION=`cd ${SOURCE_DIR} && git svn find-rev master`
 cp update-template.rdf update.rdf
 GECKO_MAJOR_VERSION=`cd ${SOURCE_DIR} && grep '<em:version>' install.rdf | \
 	sed -r -e 's|^.+<em:version>(.+)</em:version>|\1|'`
-GECKO_VERSION="${GECKO_MAJOR_VERSION}.r${REVISION}"
+GECKO_VERSION="${GECKO_MAJOR_VERSION}.${REVISION}"
 GECKO_SHA1SUM=`sha1sum "${SOURCE_DIR}/foxtrick.xpi" | sed -r 's/\s+.+$//g'`
 sed -i "s|{UPDATE_LINK}|http://foxtrick.foundationhorizont.org/nightly/foxtrick-r${REVISION}.xpi|g" update.rdf
 sed -i "s|{UPDATE_HASH}|sha1:${GECKO_SHA1SUM}|g" update.rdf
@@ -30,8 +30,8 @@ sed -i "s|{VERSION}|${GECKO_VERSION}|g" update.rdf
 # modify update.xml for Google Chrome
 cp update-template.xml update.xml
 CHROME_MAJOR_VERSION=`cd ${SOURCE_DIR} && grep '"version"' manifest.json | \
-	sed -r -e 's|^.*"version" : "(([0-9]+\.)*)[0-9]+".*$|\1|'`
-CHROME_VERSION="${CHROME_MAJOR_VERSION}${REVISION}"
+	sed -r -e 's|^.*"version" : "(.+)".*$|\1|'`
+CHROME_VERSION="${CHROME_MAJOR_VERSION}.${REVISION}"
 sed -i "s|{CODEBASE}|http://foxtrick.foundationhorizont.org/nightly/chrome/foxtrick-r${REVISION}.crx|g" update.xml
 sed -i "s|{VERSION}|${CHROME_VERSION}|g" update.xml
 
