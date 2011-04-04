d9487f8cb6fb49f313a456119571a98fe6b89af2
ryan <ryan@ryanium.com>
2011-04-04 12:36:44 +0000

    new-mail.js: use CSS for styling.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6312 1e3807c3-0949-0410-b78e-337b627472c1

--- content/alert/new-mail.js
+++ content/alert/new-mail.js
@@ -1,55 +1,43 @@
 /**
  * new-mail.js
  * Script which makes the new mails more visible
- * @author htbaumanns
+ * @author htbaumanns, ryanli
  */
 
 var FoxtrickNewMail = {
 	MODULE_NAME : "NewMail",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.ALERT,
-	PAGES : new Array ( 'all' ),
-	CSS:  "",
-	OLD_CSS:"",
-	OPTIONS : new Array("CustomColor","HighlightNewMailIcon","HighlightNewForum"),
-	OPTION_TEXTS : true,
-	OPTION_TEXTS_DISABLED_LIST : new Array(false,true,true),
-	NewMailColor:"red",
+	PAGES : ["all"],
 
-	init : function() {
-		FoxtrickNewMail.NewMailColor = "red";
-		if (Foxtrick.isModuleFeatureEnabled( this, "CustomColor")) {
-			FoxtrickNewMail.NewMailColor = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "CustomColor_text");
+	CSS : Foxtrick.ResourcePath + "resources/css/new-mail.css",
+
+	run : function(page, doc) {
+		var menu = doc.getElementById("menu");
+		// mail count within My Hattrick link
+		var myHt = menu.getElementsByTagName("a")[0];
+		if (myHt.getElementsByTagName("span").length) {
+			var mailCountSpan = myHt.getElementsByTagName("span")[0];
+			mailCountSpan.className = "ft-new-mail";
 		}
-		var HighlightNewMailIconCss="img.scNewMail {background: url('"+Foxtrick.ResourcePath+"resources/img/new_mail.png') !important;}";
-
-		if (!Foxtrick.isModuleFeatureEnabled( this, "HighlightNewMailIcon")) HighlightNewMailIconCss="";
-
-		var zaw = '/*NEWMAILCOLOR*/ @-moz-document domain(hattrick.org), domain(hattrick.interia.pl), domain(hattrick.ws) {'+
-			'div.subMenuBox>div.boxBody>ul>li>span,'+
-			'div#folders.sidebarBox>div.boxBody>p>span,'+
-			'div#folders.sidebarBox>p>span,'+
-			'div#header>div#menu a>span,'+
-			'div#ctl00_pnlSubMenu.subMenu>div.subMenuBox>ul>li>span'+
-			'{'+
-			'color:'+FoxtrickNewMail.NewMailColor +'!important;'+
-			'font-weight:bold !important;'+
-			'}'+
-			HighlightNewMailIconCss +
-			'}';
-
-		this.OLD_CSS = this.CSS;
-		if (Foxtrick.BuildFor=='Chrome') Foxtrick.load_css_permanent(zaw);
-		else this.CSS=Foxtrick.GetDataURIText(zaw);
-	},
-
-	run : function( page, doc ) {
-		if (Foxtrick.isModuleFeatureEnabled( this, "HighlightNewForum")) {
-			var  menu=doc.getElementById('menu');
-			if (menu) {
-				var forum = menu.getElementsByTagName('a')[3];
-				var parts = forum.innerHTML.split('(');
-				if (parts.length>1) forum.innerHTML = parts[0]+'<span>('+parts[1]+'</span>';
-			}
+		// mail count in left menu
+		var subMenu = doc.getElementsByClassName("subMenu")[0];
+		var subMenuBox = subMenu.getElementsByClassName("subMenuBox")[0];
+		var listItems = subMenuBox.getElementsByTagName("li");
+		var mailCountItems = Foxtrick.filter(listItems, function(n) {
+			return n.getElementsByTagName("span").length > 0;
+		});
+		if (mailCountItems.length) {
+			var mailCount = mailCountItems[0].getElementsByTagName("span")[0];
+			mailCount.className = "ft-new-mail";
+		}
+		// new forum message
+		var forum = menu.getElementsByTagName("a")[3];
+		if (forum.textContent.indexOf("(") > -1) {
+			// has new message, no span this time, we need to add it
+			forum.innerHTML = forum.innerHTML.replace(
+				/^(.+)(\(\d+\))(.+)$/,
+				"$1<span class=\"ft-new-forum-msg\">$2</span>$3"
+			);
 		}
  	}
 };
