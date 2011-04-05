ee8446102edfa158d0492daf40bf549bd1d1f902
ryan <ryan@ryanium.com>
2011-04-05 12:25:53 +0000

    ticker-alert.js: add custom preferences for each ticker and sound.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6323 1e3807c3-0949-0410-b78e-337b627472c1

--- content/alert/ticker-alert.js
+++ content/alert/ticker-alert.js
@@ -3,14 +3,70 @@
  * display a notification when a new ticker has arrived
  * @author taised, convincedd, ryanli
  */
-////////////////////////////////////////////////////////////////////////////////
 
 var FoxtrickTickerAlert = {
 	MODULE_NAME : "TickerAlert",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.ALERT,
 	PAGES : ["all"],
 
+	OPTION_FUNC : function(doc) {
+		var table = doc.createElement("table");
+		table.className = "bordered";
+
+		// header
+		var header = doc.createElement("tr");
+		table.appendChild(header);
+		var holder = doc.createElement("th");
+		header.appendChild(holder);
+		var enableh = doc.createElement("th");
+		enableh.setAttribute("text-key", "TickerAlert.enable");
+		header.appendChild(enableh);
+		var soundh = doc.createElement("th");
+		soundh.setAttribute("text-key", "TickerAlert.sound");
+		header.appendChild(soundh);
+
+		for (var type in FoxtrickTickerAlert.TYPES) {
+			var row = doc.createElement("tr");
+			table.appendChild(row);
+			var rhead = doc.createElement("th");
+			rhead.setAttribute("text-key", "ticker.type." + type);
+			row.appendChild(rhead);
+			var enablec = doc.createElement("td");
+			row.appendChild(enablec);
+			var enable = doc.createElement("input");
+			enable.type = "checkbox";
+			enable.setAttribute("pref", "module.TickerAlert." + type + ".enabled");
+			enablec.appendChild(enable);
+			var soundc = doc.createElement("td");
+			row.appendChild(soundc);
+			var sound = doc.createElement("input");
+			sound.setAttribute("pref", "module.TickerAlert." + type + ".sound");
+			soundc.appendChild(sound);
+		}
+
+		return table;
+	},
+
+	TYPES : {
+		"welcome" : /\/MyHattrick/i,
+		"supporter" : /\/Club\/Manager\/\?teamId=/i,
+		"forum" : /\/Forum/i,
+		"transfer" : /\/Players/i,
+		"challenge" : /\/Challenges/i,
+		"guestbook" : /\/Club\/Manager\/Guestbook\.aspx\?teamid=/i,
+		"mail" : /\/Inbox/i,
+		"myht" : /\/Myhattrick\/\?actionType/i
+	},
+
 	run : function(page, doc) {
+		var getType = function(url) {
+			for (var type in FoxtrickTickerAlert.TYPES) {
+				var regexp = FoxtrickTickerAlert.TYPES[type];
+				if (url.match(regexp))
+					return type;
+			}
+			return null;
+		};
 		// add watch to ticker
 		var ticker = doc.getElementById("ticker");
 		var tickerArrival = function() {
@@ -34,7 +90,13 @@ var FoxtrickTickerAlert = {
 					return true;
 				});
 				Foxtrick.map(newTickers, function(n) {
-					Foxtrick.util.notify.create(n.text, n.href);
+					var type = getType(n.href);
+					if (FoxtrickPrefs.getBool("module.TickerAlert." + type + ".enabled")) {
+						Foxtrick.util.notify.create(n.text, n.href);
+						var sound = FoxtrickPrefs.getString("module.TickerAlert." + type + ".sound");
+						if (sound)
+							Foxtrick.playSound(sound);
+					}
 				});
 			});
 		};
