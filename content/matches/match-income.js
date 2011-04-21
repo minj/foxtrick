eaf718501432bc4dd25b1005756b1c8e7349a177
ryan <ryan@ryanium.com>
2011-04-21 16:16:18 +0000

    match-income.js: use heuristic method to identify sold seats box.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6376 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/match-income.js
+++ content/matches/match-income.js
@@ -1,47 +1,51 @@
 /**
  * match-income.js
  * Foxtrick add links to played matches pages
- * @author convinced
+ * @author convinced, ryanli
  */
 
 ////////////////////////////////////////////////////////////////////////////////
 var FoxtrickMatchIncome = {
 	MODULE_NAME : "MatchIncome",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.MATCHES,
-	PAGES : new Array('match'),
+	PAGES : ['match'],
 
-	run : function( page, doc ) {
-		var ft_match = doc.getElementById ('ft_matchincome');
-		if (ft_match != null) return;
-		var youthmatch = FoxtrickHelper.findIsYouthMatch(doc.location.href);
+	run : function(page, doc) {
+		var sidebar = doc.getElementById('sidebar');
+		var sidebarBoxes = sidebar.getElementsByClassName('sidebarBox');
+		var isSoldSeats = function(n) {
+			// returns whether a sidebarBox is sold seats box
+			if (n.getElementsByTagName('table').length != 1)
+				return false;
+			if (n.getElementsByTagName('tr').length != 4)
+				return false;
+			if (n.getElementsByTagName('td').length != 4 * 2)
+				return false;
+			return true;
+		};
+		var soldSeatBox = Foxtrick.filter(sidebarBoxes, isSoldSeats)[0];
+		if (!soldSeatBox) // cannot find sold seat boxes
+			return;
 
-		var isarchivedmatch=doc.location.href.search(/useArchive=true/i)!=-1;
-		var ownBoxBody=null;
-		var alldivs = doc.getElementsByTagName('div');
-		for (var j = 0; j < alldivs.length; j++) {
-			if (alldivs[j].className=="sidebarBox"
-			&& alldivs[j].getElementsByTagName('h2')[0].innerHTML.search(Foxtrickl10n.getString('foxtrick.matches.soldseats'))!=-1 ) {
-				var table=alldivs[j].getElementsByTagName('table')[0];
-				var tbody = table.getElementsByTagName('tbody')[0];
-				var sum=Foxtrick.trimnum(table.rows[0].cells[1].textContent)*6.5
-					+ Foxtrick.trimnum(table.rows[1].cells[1].textContent)*9.5
-					+ Foxtrick.trimnum(table.rows[2].cells[1].textContent)*18
-					+ Foxtrick.trimnum(table.rows[3].cells[1].textContent)*32.5;
+		var table = soldSeatBox.getElementsByTagName('table')[0];
+		var tbody = table.getElementsByTagName('tbody')[0];
+		var sum = Foxtrick.trimnum(table.rows[0].cells[1].textContent)*6.5
+			+ Foxtrick.trimnum(table.rows[1].cells[1].textContent)*9.5
+			+ Foxtrick.trimnum(table.rows[2].cells[1].textContent)*18
+			+ Foxtrick.trimnum(table.rows[3].cells[1].textContent)*32.5;
 
-				sum/=parseFloat(Foxtrick.util.currency.getRate());
+		// convert to local currency
+		sum /= Foxtrick.util.currency.getRate();
 
-				var tr2 = doc.createElement('tr');
-				var td2a = doc.createElement('td');
-				var td2b = doc.createElement('td');
-				tbody.appendChild(tr2);
-				tr2.appendChild(td2a);
-				tr2.appendChild(td2b);
-				td2a.id = 'ft_matchincome';
-				td2a.className = "ch";
-				td2a.textContent = Foxtrickl10n.getString('foxtrick.matches.income');
-				td2b.className = "nowrap";
-				td2b.textContent = Foxtrick.formatNumber(Math.floor(sum), ' ')+' '+Foxtrick.util.currency.getSymbol();
-			}
-		}
+		var tr2 = doc.createElement('tr');
+		var td2a = doc.createElement('td');
+		var td2b = doc.createElement('td');
+		tbody.appendChild(tr2);
+		tr2.appendChild(td2a);
+		tr2.appendChild(td2b);
+		td2a.className = "ch";
+		td2a.textContent = Foxtrickl10n.getString('foxtrick.matches.income');
+		td2b.className = "nowrap";
+		td2b.textContent = Foxtrick.formatNumber(Math.floor(sum), ' ')+' '+Foxtrick.util.currency.getSymbol();
 	}
 };
