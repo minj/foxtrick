7998364cf7833c66d1457da779263252c25f6c05
ryan <ryan@ryanium.com>
2011-05-21 11:45:17 +0000

    default/preferences: mark version as 0.7.9 in repository.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6500 1e3807c3-0949-0410-b78e-337b627472c1

--- defaults/preferences/foxtrick.js
+++ defaults/preferences/foxtrick.js
@@ -1,6 +1,6 @@
 pref("extensions.foxtrick.prefs.CopyPostingStyle", "ht-ml");
 pref("extensions.foxtrick.prefs.copyratings.teams", "both");
-pref("extensions.foxtrick.prefs.version", "0.7.1");
+pref("extensions.foxtrick.prefs.version", "0.7.9");
 pref("extensions.foxtrick.prefs.disableOnStage", false);
 pref("extensions.foxtrick.prefs.disableTemporary", false);
 pref("extensions.foxtrick.prefs.DisplayHTMLDebugOutput", false);
