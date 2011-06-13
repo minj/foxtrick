2a7ac24b8e9c229c206c97ca0c5450b2cc07fc45
ryan <ryan@ryanium.com>
2011-06-13 15:49:24 +0000

    ticker-coloring.js: use DOMSubtreeModified only on #ticker.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6569 1e3807c3-0949-0410-b78e-337b627472c1

--- content/alert/ticker-coloring.js
+++ content/alert/ticker-coloring.js
@@ -7,43 +7,44 @@
 var FoxtrickTickerColoring = {
 	MODULE_NAME : "TickerColoring",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.ALERT,
-	PAGES : new Array('all'),
+	PAGES : ["all"],
 	CSS : Foxtrick.ResourcePath + "resources/css/ticker-coloring.css",
 
-	addClass : function(doc) {
-		var ticker = doc.getElementById("ticker");
-		var links = ticker.getElementsByTagName("a");
-		for (var i = 0; i < links.length; ++i) {
-			var link = links[i];
-			if (!Foxtrick.hasClass(link, "ft-ticker-link")) {
-				Foxtrick.addClass(link, "ft-ticker-link");
-				// need to use getAttribute to get relative path
-				var href = link.getAttribute("href");
-				if (href === "/MyHattrick/Dashboard.aspx")
-					Foxtrick.addClass(link, "ft-ticker-welcome");
-				else if (href.indexOf("/Club/Manager/?teamId=") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-supporter");
-				else if (href.indexOf("/Forum/") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-forum");
-				else if (href.indexOf("/Players/") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-transfer");
-				else if (href.indexOf("/Challenges/") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-challenge");
-				else if (href.indexOf("/Club/Manager/Guestbook.aspx?teamid=") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-guestbook");
-				else if (href.indexOf("/Inbox/") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-mail");
-				else if (href.indexOf("/Myhattrick/?actionType") !== -1)
-					Foxtrick.addClass(link, "ft-ticker-myht");
-			}
-		}
-	},
-
 	run : function(page, doc) {
-		this.addClass(doc);
-	},
+		var ticker = doc.getElementById("ticker");
 
-	change : function(page, doc) {
-		this.addClass(doc);
+		var update = function() {
+			// prevent self-triggered calls
+			ticker.removeEventListener("DOMSubtreeModified", update, false);
+
+			var links = ticker.getElementsByTagName("a");
+			for (var i = 0; i < links.length; ++i) {
+				var link = links[i];
+				if (!Foxtrick.hasClass(link, "ft-ticker-link")) {
+					Foxtrick.addClass(link, "ft-ticker-link");
+					// need to use getAttribute to get relative path
+					var href = link.getAttribute("href");
+					if (href === "/MyHattrick/Dashboard.aspx")
+						Foxtrick.addClass(link, "ft-ticker-welcome");
+					else if (href.indexOf("/Club/Manager/?teamId=") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-supporter");
+					else if (href.indexOf("/Forum/") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-forum");
+					else if (href.indexOf("/Players/") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-transfer");
+					else if (href.indexOf("/Challenges/") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-challenge");
+					else if (href.indexOf("/Club/Manager/Guestbook.aspx?teamid=") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-guestbook");
+					else if (href.indexOf("/Inbox/") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-mail");
+					else if (href.indexOf("/Myhattrick/?actionType") !== -1)
+						Foxtrick.addClass(link, "ft-ticker-myht");
+				}
+			}
+			ticker.removeEventListener("DOMSubtreeModified", update, false);
+		}
+
+		update();
 	}
 };
