db7067f8da05d29f8219d3ac1e754f12b5cd9ce1
convincedd <convincedd@gmail.com>
2011-06-29 14:29:25 +0000

    add-class for youthleague search
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6622 1e3807c3-0949-0410-b78e-337b627472c1

--- content/add-class.js
+++ content/add-class.js
@@ -8,18 +8,35 @@ if (!Foxtrick) var Foxtrick = {};
 Foxtrick.AddClass = {
 	MODULE_NAME : "AddClass",
 	CORE_MODULE : true,
-	PAGES : ["playerdetail"],
+	PAGES : ["playerdetail", "search"],
 
 	run : function(page, doc) {
 		if (page == "playerdetail")
 			this.addDateForTl(doc);
+		else if (page == "search")
+			this.addDateForYouthLeagueSearch(doc);
 	},
 
 	change : function(page, doc) {
-		if (page == "playerdetail")
-			this.addDateForTl(doc);
+		this.run(page, doc);
 	},
 
+	// add date class for youth league search
+	addDateForYouthLeagueSearch : function(doc) {
+		var table = doc.getElementById("ctl00_ctl00_CPContent_CPMain_grdYouthSeries_ctl00");
+		if (!table)
+			return;
+
+		const timeRe = /(\d{1,4}\D\d{1,2}\D\d{1,4}\s+\d{1,2}\D\d{1,2})/;
+
+		// start time
+		var cells = table.getElementsByTagName("td");
+		Foxtrick.map(cells, function(cell) {
+			if (cell.getElementsByClassName("date").length == 0)
+			cell.innerHTML = cell.innerHTML.replace(timeRe, "<span class=\"date\">$1</span>");
+		});
+	},
+	
 	// add date class for transfer-list
 	addDateForTl : function(doc) {
 		var transferList = doc.getElementById("ctl00_ctl00_CPContent_CPMain_updBid");
