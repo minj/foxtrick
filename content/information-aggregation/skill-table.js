c4dee267b15c3cb44da66439b019ce7e882a2ad5
ryan <ryan@ryanium.com>
2011-06-29 05:28:26 +0000

    skill-table.js: option to show coloured youth skills. (See #701)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6611 1e3807c3-0949-0410-b78e-337b627472c1

--- content/information-aggregation/skill-table.js
+++ content/information-aggregation/skill-table.js
@@ -9,8 +9,8 @@ var FoxtrickSkillTable = {
 
 	MODULE_NAME : "SkillTable",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.INFORMATION_AGGREGATION,
-	PAGES : new Array("players", "oldcoaches","YouthPlayers", "transferSearchResult"),
-	OPTIONS : new Array("OtherTeams"),
+	PAGES : ["players", "oldcoaches","YouthPlayers", "transferSearchResult"],
+	OPTIONS : ["OtherTeams", "ColouredYouth"],
 	CSS : Foxtrick.ResourcePath + "resources/css/skilltable.css",
 	CSS_RTL : Foxtrick.ResourcePath + "resources/css/skilltable_rtl.css",
 
@@ -334,6 +334,37 @@ var FoxtrickSkillTable = {
 						var current = skill.current ? skill.current : "-";
 						var max = skill.max ? skill.max : "-";
 						cell.appendChild(doc.createTextNode(current + "/" + max));
+						// and we deal with colours
+						if (Foxtrick.isModuleFeatureEnabled(FoxtrickSkillTable, "ColouredYouth")) {
+							if (skill.max > 3) {
+								// normalized values for convenience in further calculations
+								var skillBase = {}; // skills below 4 are not regarded as interesting
+								skillBase.max = skill.max>3 ? skill.max-3 : 0;
+								skillBase.current = skill.current>3 ? skill.current-3 : 0;
+
+								// calculate color for capability of improvement
+								var r = 0;
+								if (skillBase.max > skillBase.current)
+									r = ((skillBase.max - skillBase.current) / skillBase.max)*255+51;
+								var g = 255;
+								var b = 0;
+
+								// apply alpha, indicating max skill
+								var a = 1 - skillBase.max / 5; // assuming max skill will never exceed 8...
+								a = (a < 0) ? 0 : a; //... but just to be sure
+								if (a != 1) {
+									r = Math.round(r+(255-r)*a);
+									g = Math.round(g+(255-g)*a);
+									b = Math.round(b+(255-b)*a);
+								}
+								cell.style.backgroundColor='rgb('+r+','+g+','+b+')';
+							}
+							else if (skill.max != 0) {
+								// display unimportant skills/low capabilities in gray
+								cell.style.backgroundColor = "rgb(204,204,204)";
+								cell.style.color = "rgb(102,102,102)";
+							}
+						}
 					}
 				}
 				else {
--- defaults/preferences/foxtrick.js
+++ defaults/preferences/foxtrick.js
@@ -432,6 +432,7 @@ pref("extensions.foxtrick.prefs.module.SkillColoring.skill_number.enabled", true
 pref("extensions.foxtrick.prefs.module.SkillColoring.skill_number_translated.enabled", false);
 pref("extensions.foxtrick.prefs.module.SkillColoring.skill_select.enabled", true);
 pref("extensions.foxtrick.prefs.module.SkillTable.enabled", true);
+pref("extensions.foxtrick.prefs.module.SkillTable.ColouredYouth.enabled", false);
 pref("extensions.foxtrick.prefs.module.SkillTable.OtherTeams.enabled", true);
 pref("extensions.foxtrick.prefs.module.SkillTable.senior.nt.Age", true);
 pref("extensions.foxtrick.prefs.module.SkillTable.senior.nt.Current_club", true);
