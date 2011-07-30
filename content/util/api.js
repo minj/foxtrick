9d92a59879f6391bebe64631a92b7eebefbd281e
convincedd <convincedd@gmail.com>
2011-07-30 17:26:02 +0000

    opera: oauth hotfix. xmlhttprequest status not correct. returns 0 for some reason if requesting with outdated key
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6889 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/api.js
+++ content/util/api.js
@@ -253,7 +253,7 @@ Foxtrick.util.api = {
 												cache_lifetime:cache_lifetime });
 							callback(x);
 						}
-						else if (status == 401) {
+						else if (status == 401 || (typeof(opera)=='object' && status==0) ) { // opera hotfix. returned status not correct
 							Foxtrick.log("ApiProxy: error 401, unauthorized. Arguments: ", parameters);
 							Foxtrick.util.api.invalidateAccessToken(doc);
 							Foxtrick.util.api.authorize(doc);
