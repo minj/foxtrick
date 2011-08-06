456ea441731f0b105920d3ffe7ebaf8718068d5c
convincedd <convincedd@gmail.com>
2011-08-06 18:58:28 +0000

    sandboxed: jscript injection by background for all (so machtreportformat click now works also for chrome)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@6993 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/inject.js
+++ content/util/inject.js
@@ -39,7 +39,7 @@ Foxtrick.util.inject.css = function(doc, css, id) {
 
 // attaches a JavaScript file to the page
 Foxtrick.util.inject.jsLink = function(doc, url) {
-	if (typeof(opera)=='object') {
+	if (Foxtrick.BuildFor == "Sandboxed") {
 		Foxtrick.load(url, function(text) {
 			Foxtrick.util.inject.js(doc, text);
 		}, true);
