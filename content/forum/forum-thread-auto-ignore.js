478c3f70519cb4ab9ef072c689301aad8a3963ab
ryan <ryan@ryanium.com>
2011-09-18 13:31:54 +0000

    forum-thread-auto-ignore.js: remove module object references by name.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@7397 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/forum-thread-auto-ignore.js
+++ content/forum/forum-thread-auto-ignore.js
@@ -4,8 +4,7 @@
  * @author convincedd
  */
 
-var FoxtrickForumThreadAutoIgnore = {
-
+Foxtrick.util.module.register({
 	MODULE_NAME : "ForumThreadAutoIgnore",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
 	PAGES : new Array('forum'),
@@ -16,87 +15,78 @@ var FoxtrickForumThreadAutoIgnore = {
 	whitelist : null,
 
 	run : function(doc) {
-		this.checkthreads(doc);
-		Foxtrick.listen(doc.getElementById('ctl00_ctl00_CPContent_ucLeftMenu_pnlLeftMenuScrollContent'),'DOMSubtreeModified',FoxtrickForumThreadAutoIgnore.myforum_change,true);
-	},
+		var deletingThreadId = -1;
+		var checkThreads = function() {
+			if (!FoxtrickPrefs.isModuleOptionEnabled("ForumThreadAutoIgnore",'Tags')) return;
+			var tags_string = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "Tags_text");
+			if (!tags_string) return;
 
+			// get tags. comma seperated in the prefs
+			this.tags = tags_string.split(',');
+			for (var i=0; i<this.tags.length; ++i) {
+				this.tags[i] = this.tags[i].replace(/^\s+/,''); // leading space removed
+				this.tags[i] = this.tags[i].replace(/\s+$/,''); // trailing space removed
+			}
 
-	deleting_thread_id: -1, // stores  thread is is currently trying to delete.
-
-	checkthreads : function( doc ) {
-
-		if (!FoxtrickPrefs.isModuleOptionEnabled("ForumThreadAutoIgnore",'Tags')) return;
-		var tags_string = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "Tags_text");
-		if (!tags_string) return;
-
-		// get tags. comma seperated in the prefs
-		this.tags = tags_string.split(',');
-		for (var i=0; i<this.tags.length; ++i) {
-			this.tags[i] = this.tags[i].replace(/^\s+/,''); // leading space removed
-			this.tags[i] = this.tags[i].replace(/\s+$/,''); // trailing space removed
-		}
-
-		// get whitelisted threadIDs. comma seperated in the prefs
-		if (FoxtrickPrefs.isModuleOptionEnabled("ForumThreadAutoIgnore",'Whitelist_ThreadIDs')) {
-			var whitelist_string = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "Whitelist_ThreadIDs_text");
-			if (whitelist_string) {
-				this.whitelist = whitelist_string.split(',');
-				for (var i=0; i<this.whitelist.length; ++i) {
-					this.whitelist[i] = this.whitelist[i].replace(/^\s+/,''); // leading space removed
-					this.whitelist[i] = this.whitelist[i].replace(/\s+$/,''); // trailing space removed
-					 //Foxtrick.dump(this.whitelist[i]+'\n');
+			// get whitelisted threadIDs. comma seperated in the prefs
+			if (FoxtrickPrefs.isModuleOptionEnabled("ForumThreadAutoIgnore",'Whitelist_ThreadIDs')) {
+				var whitelist_string = FoxtrickPrefs.getString("module." + this.MODULE_NAME + "." + "Whitelist_ThreadIDs_text");
+				if (whitelist_string) {
+					this.whitelist = whitelist_string.split(',');
+					for (var i=0; i<this.whitelist.length; ++i) {
+						this.whitelist[i] = this.whitelist[i].replace(/^\s+/,''); // leading space removed
+						this.whitelist[i] = this.whitelist[i].replace(/\s+$/,''); // trailing space removed
+						 //Foxtrick.dump(this.whitelist[i]+'\n');
+					}
 				}
 			}
-		}
 
-		var myForums = doc.getElementById('myForums');
-		var threadItems = myForums.getElementsByClassName('threadItem');
-		for (var i=0; i<threadItems.length; ++i) {
-			var url =  threadItems[i].getElementsByClassName('url')[0];
+			var myForums = doc.getElementById('myForums');
+			var threadItems = myForums.getElementsByClassName('threadItem');
+			for (var i=0; i<threadItems.length; ++i) {
+				var url =  threadItems[i].getElementsByClassName('url')[0];
 
-			if (url== null) continue;
+				if (url== null) continue;
 
-			var a = url.getElementsByTagName('a')[0];
-			for (var j=0; j<this.tagmarkers.length; ++j){
-				for (var k=0; k<this.tags.length; ++k){
-					var reg = new RegExp(this.tagmarkers[j][0] + this.tags[k] + this.tagmarkers[j][1], "i");
-					if (a.innerHTML.search(reg)!=-1) {
-						// only autoignore if there is ht's ignore option
-						var ignore = threadItems[i].getElementsByClassName('ignore')[0];
-						if ( ignore ) {
-							var thread_id = a.href.match(/\/Forum\/Read.aspx\?t=(\d+)/)[1];
-							// check whitelist
-							var whitelisted = false;
-							if (whitelist_string) {
-								for (var l=0; l<this.whitelist.length; ++l){
-									if (this.whitelist[l]==thread_id) {
-										whitelisted = true;
-										continue;
+				var a = url.getElementsByTagName('a')[0];
+				for (var j=0; j<this.tagmarkers.length; ++j){
+					for (var k=0; k<this.tags.length; ++k){
+						var reg = new RegExp(this.tagmarkers[j][0] + this.tags[k] + this.tagmarkers[j][1], "i");
+						if (a.innerHTML.search(reg)!=-1) {
+							// only autoignore if there is ht's ignore option
+							var ignore = threadItems[i].getElementsByClassName('ignore')[0];
+							if ( ignore ) {
+								var thread_id = a.href.match(/\/Forum\/Read.aspx\?t=(\d+)/)[1];
+								// check whitelist
+								var whitelisted = false;
+								if (whitelist_string) {
+									for (var l=0; l<this.whitelist.length; ++l){
+										if (this.whitelist[l]==thread_id) {
+											whitelisted = true;
+											continue;
+										}
 									}
 								}
+								if (whitelisted) continue;
+
+								// check if finished deleting the last one. if ids match, the last delet order isn't finished. come back with next onchange
+								if (thread_id == FoxtrickForumThreadAutoIgnore.deleting_thread_id) return;
+								FoxtrickForumThreadAutoIgnore.deleting_thread_id = thread_id;
+
+								// ignore thread using ht's javascript link
+								var func = ignore.getAttribute('onclick');
+								doc.location.href = func;
+								Foxtrick.dump('autoignore '+this.tags[k]+': '+a.innerHTML+'\n');
+
+								// only one at a time. recheck after page has changed
+								return;
 							}
-							if (whitelisted) continue;
-
-							// check if finished deleting the last one. if ids match, the last delet order isn't finished. come back with next onchange
-							if (thread_id == FoxtrickForumThreadAutoIgnore.deleting_thread_id) return;
-							FoxtrickForumThreadAutoIgnore.deleting_thread_id = thread_id;
-
-							// ignore thread using ht's javascript link
-							var func = ignore.getAttribute('onclick');
-							doc.location.href = func;
-							Foxtrick.dump('autoignore '+this.tags[k]+': '+a.innerHTML+'\n');
-
-							// only one at a time. recheck after page has changed
-							return;
 						}
 					}
 				}
 			}
-		}
-	},
-
-	myforum_change : function(ev) {
-		FoxtrickForumThreadAutoIgnore.checkthreads(ev.target.ownerDocument);
+		};
+		checkThreads();
+		Foxtrick.listen(doc.getElementById('ctl00_ctl00_CPContent_ucLeftMenu_pnlLeftMenuScrollContent'), 'DOMSubtreeModified', checkThreads, true);
 	}
-};
-Foxtrick.util.module.register(FoxtrickForumThreadAutoIgnore);
+});
