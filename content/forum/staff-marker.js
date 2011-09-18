8aba90e9d31f4e0e91eb563df71a404d66c855bd
ryan <ryan@ryanium.com>
2011-09-18 12:59:52 +0000

    staff-marker.js: remove module object reference by name.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@7389 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/staff-marker.js
+++ content/forum/staff-marker.js
@@ -4,7 +4,7 @@
  * @author bummerland, ryanli
  */
 
-var FoxtrickStaffMarker = {
+Foxtrick.util.module.register({
 	MODULE_NAME : "StaffMarker",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
 	PAGES : ["forumViewThread", "forumWritePost", "teamPage"],
@@ -67,7 +67,7 @@ var FoxtrickStaffMarker = {
 			// getting user-defined IDs and colors
 			var customMarker = {};
 			if (FoxtrickPrefs.isModuleOptionEnabled("StaffMarker", "own")) {
-				var customText = FoxtrickPrefs.getString("module." + FoxtrickStaffMarker.MODULE_NAME + "." + "own_text");
+				var customText = FoxtrickPrefs.getString("module.StaffMarker.own_text");
 				try {
 					customMarker = JSON.parse(customText);
 				}
@@ -109,91 +109,92 @@ var FoxtrickStaffMarker = {
 						Foxtrick.addClass(object, "ft-staff-" + type);
 				}
 			};
+
+			// mark staffs in thread
+			var markThread = function() {
+				var userDivs = doc.getElementById("mainWrapper").getElementsByClassName("float_left");
+				Foxtrick.map(function(user) {
+					var links = user.getElementsByTagName("a");
+					Foxtrick.map(function(a) {
+						if (!a.href) {Foxtrick.log('error ',a.parentNode.innerHTML);return;}
+						if (a.getAttribute("href").search(/\/Club\/Manager\/\?userId\=/i) == -1
+							|| a.getAttribute("href").search(/redir_to_league=true/i) != -1)
+							return;
+						var uname = Foxtrick.trim(a.title);
+						// Work-around for supporter star
+						if (uname.lastIndexOf("*") == uname.length-1)
+							uname = uname.substring(0,uname.length-1);
+						var uid = a.href.replace(/.+userId=/i, "").match(/^\d+/);
+
+						modifier(uid, uname, a);
+					}, links);
+				}, userDivs);
+			};
+			// mark staffs in select box
+			var markSelect = function() {
+				var do_flag = FoxtrickPrefs.isModuleOptionEnabled("StaffMarker", "flag");
+
+				var selects = doc.getElementById("mainWrapper").getElementsByClassName("threadPagingFilter");
+				var select = selects[0];
+				if ( select ) {
+					if (select.id.search(/filter/i) == -1
+						&& select.id.search(/recipient/i) == -1)
+						return;
+
+					var css = '';
+					var i = 1;
+					var user_hasClass = {};
+					var option;
+					while (option = select.options[i++]) {
+						var uname = Foxtrick.trim(option.textContent);
+						uname = uname.substring(0, uname.indexOf(" "));
+						if (uname == "")
+							uname = Foxtrick.trim(option.text);
+						if (uname == "")
+							break;
+						var uid = option.value.replace(/by_|to_/gi, "");
+
+						modifier(uid, uname, option); // no background image in chrome for select. background-colors only
+
+						if (option.value==-3) {
+							Foxtrick.addClass(option, "ft-staff-seperator");
+						}
+						else if (option.value=="by_-1") {
+							Foxtrick.addClass(option, "ft-staff-official");
+						}
+						else if ( do_flag && Foxtrick.arch === "Gecko") { // no background image in chrome for select
+							Foxtrick.addClass(option, "ft-userid-"+uid);
+							if (user_hasClass[uid])
+								continue;
+							css += ".ft-userid-" + uid + " {"
+								+ "background-image: url('http://flags.alltidhattrick.org/userflags/" + uid + ".gif');"
+								+ "background-position: 180px 50%;"
+								+ "background-repeat: no-repeat;"
+								+ "padding: 1px 1px 1px 1px;"
+								+ "width: 198px;"
+								+ "border-bottom: dotted thin #ddd;}\n";
+							user_hasClass[uid] = true; // css for that users addes
+						}
+					}
+					if (do_flag)
+						Foxtrick.util.inject.css(doc, css);
+					if (selects[0] && selects[1])
+						selects[1].innerHTML = selects[0].innerHTML;
+				}
+			};
+
 			if (Foxtrick.isPage("forumViewThread", doc)) {
-				FoxtrickStaffMarker._MarkAliases_thread(doc, modifier);
-				FoxtrickStaffMarker._MarkAliases_select(doc, modifier);
+				markThread(doc, modifier);
+				markSelect(doc, modifier);
 			}
 			else if (Foxtrick.isPage("forumWritePost", doc)) {
-				FoxtrickStaffMarker._MarkAliases_select(doc, modifier);
+				markSelect(doc, modifier);
 			}
 			else if (Foxtrick.isPage("teamPage", doc)) {
 				if (FoxtrickPrefs.isModuleOptionEnabled("StaffMarker", "manager")) {
-					FoxtrickStaffMarker._MarkAliases_thread(doc, modifier);
+					markThread(doc, modifier);
 				}
 			}
 		});
-	},
-
-	_MarkAliases_thread : function(doc, modifier) {
-		var userDivs = doc.getElementById("mainWrapper").getElementsByClassName("float_left");
-		Foxtrick.map(function(user) {
-			var links = user.getElementsByTagName("a");
-			Foxtrick.map(function(a) {
-				if (!a.href) {Foxtrick.log('error ',a.parentNode.innerHTML);return;}
-				if (a.getAttribute("href").search(/\/Club\/Manager\/\?userId\=/i) == -1
-					|| a.getAttribute("href").search(/redir_to_league=true/i) != -1)
-					return;
-				var uname = Foxtrick.trim(a.title);
-				// Work-around for supporter star
-				if (uname.lastIndexOf("*") == uname.length-1)
-					uname = uname.substring(0,uname.length-1);
-				var uid = a.href.replace(/.+userId=/i, "").match(/^\d+/);
-
-				modifier(uid, uname, a);
-			}, links);
-		}, userDivs);
-	},
-
-	_MarkAliases_select : function(doc, modifier) {
-		var do_flag = FoxtrickPrefs.isModuleOptionEnabled("StaffMarker", "flag");
-
-		var selects = doc.getElementById("mainWrapper").getElementsByClassName("threadPagingFilter");
-		var select = selects[0];
-		if ( select ) {
-			if (select.id.search(/filter/i) == -1
-				&& select.id.search(/recipient/i) == -1)
-				return;
-
-			var css = '';
-			var i = 1;
-			var user_hasClass = {};
-			var option;
-			while (option = select.options[i++]) {
-				var uname = Foxtrick.trim(option.textContent);
-				uname = uname.substring(0, uname.indexOf(" "));
-				if (uname == "")
-					uname = Foxtrick.trim(option.text);
-				if (uname == "")
-					break;
-				var uid = option.value.replace(/by_|to_/gi, "");
-
-				modifier(uid, uname, option); // no background image in chrome for select. background-colors only
-
-				if (option.value==-3) {
-					Foxtrick.addClass(option, "ft-staff-seperator");
-				}
-				else if (option.value=="by_-1") {
-					Foxtrick.addClass(option, "ft-staff-official");
-				}
-				else if ( do_flag && Foxtrick.arch === "Gecko") { // no background image in chrome for select
-					Foxtrick.addClass(option, "ft-userid-"+uid);
-					if (user_hasClass[uid])
-						continue;
-					css += ".ft-userid-" + uid + " {"
-						+ "background-image: url('http://flags.alltidhattrick.org/userflags/" + uid + ".gif');"
-						+ "background-position: 180px 50%;"
-						+ "background-repeat: no-repeat;"
-						+ "padding: 1px 1px 1px 1px;"
-						+ "width: 198px;"
-						+ "border-bottom: dotted thin #ddd;}\n";
-					user_hasClass[uid] = true; // css for that users addes
-				}
-			}
-			if (do_flag)
-				Foxtrick.util.inject.css(doc, css);
-			if (selects[0] && selects[1])
-				selects[1].innerHTML = selects[0].innerHTML;
-		}
 	}
-};
-Foxtrick.util.module.register(FoxtrickStaffMarker);
+});
