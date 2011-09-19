13ba33fc782698a726b9a01e476b1286c77683fc
ryan <ryan@ryanium.com>
2011-09-19 08:12:13 +0000

    mobile-enhancements.js: remove module object references by name.
    Not registered as it was.
    
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@7402 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/mobile-enhancements.js
+++ content/presentation/mobile-enhancements.js
@@ -3,14 +3,14 @@
  * Collection of enhancements using mobile device/fennec
  * @author convinced
  */
-////////////////////////////////////////////////////////////////////////////////
-var FoxtrickMobileEnhancements = {
+
+({
 	MODULE_NAME : "MobileEnhancements",
 	MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
 	PAGES : ["all"],
 	CSS : Foxtrick.InternalPath + "resources/css/mobile-enhancements.css",
 
-	ft_pageactions : {
+	pageActions : {
 		'rightSidebar':		{ match:{pattern:"Forum",result:false},	size:{'simple':190, standard:222},	css:'mobile-enhancements-right.css'},
 		'mainContent':		{ match:{pattern:"."	,result:true},	size:{'simple':440, standard:580},	css:'mobile-enhancements-main.css'},
 		'leftSidebar':		{ match:{pattern:"."	,result:true},	size:{'simple':125, standard:180},	css:'mobile-enhancements-left.css'},
@@ -19,8 +19,9 @@ var FoxtrickMobileEnhancements = {
 
 	onLoad : function() {
 		var pageactionsContainer = document.getElementById('pageactions-container');
+		var pageActions = this.pageActions;
 
-		for ( var i in FoxtrickMobileEnhancements.ft_pageactions ) {
+		for (var i in pageActions) {
 			var action = document.createElement('pageaction');
 			action.id = 'foxtrick-'+i;
 			action.setAttribute("title", Foxtrickl10n.getString('pageAction.'+i));
@@ -30,9 +31,9 @@ var FoxtrickMobileEnhancements = {
 			// page action click
 			action.addEventListener('click',function(ev){
 				var type = ev.target.getAttribute('type');
-				FoxtrickPrefs.setString('FoxtrickMobileEnhancements.selection', type)
-				var css = FoxtrickMobileEnhancements.getStyle(type);
-				sandboxed.extension.sendRequest({ req : "replace_css", css:css ,id:'foxtrick-pageaction-css', size:FoxtrickMobileEnhancements.ft_pageactions[type].size});
+				FoxtrickPrefs.setString('module.MobileEnhancements.selection', type)
+				var css = this.getStyle(type);
+				sandboxed.extension.sendRequest({ req : "replace_css", css:css ,id:'foxtrick-pageaction-css', size: pageActions[type].size});
 			},false);
 			pageactionsContainer.insertBefore(action, pageactionsContainer.firstChild);
 
@@ -42,30 +43,35 @@ var FoxtrickMobileEnhancements = {
 					var url = getBrowser().currentURI.spec;
 					var type = el.getAttribute('type');
 					return Foxtrick.isHtUrl(url) &&
-						((url.search(new RegExp(FoxtrickMobileEnhancements.ft_pageactions[type].match.pattern,'i'))!==-1)
-								===	FoxtrickMobileEnhancements.ft_pageactions[type].match.result);
+						((url.search(new RegExp(pageActions[type].match.pattern,'i'))!==-1)
+								===	pageActions[type].match.result);
 				} catch(e){Foxtrick.log(e)}
 			});
 		}
 	},
 
 	run : function(doc) {
+		var pageActions = this.pageActions;
+		var getStyle = this.getStyle;
+		var setStyle = this.setStyle;
+		var setMetaViewport = this.setMetaViewport;
+
 		// rescale to use all space
 		if (doc.getElementById('ctl00_ctl00_CPContent_ucSubMenu_ucLogin_txtUserName')!==null) {
-			var css = this.getStyle('all');
-			this.setStyle(content.document,'foxtrick-pageaction-css', css, this.ft_pageactions['all'].size);
+			var css = getStyle('all');
+			setStyle(content.document,'foxtrick-pageaction-css', css, pageActions['all'].size);
 		}
-		else if (FoxtrickPrefs.getString('FoxtrickMobileEnhancements.selection')!=='all') {
-			var css = this.getStyle('mainContent');
-			this.setStyle(content.document,'foxtrick-pageaction-css', css, this.ft_pageactions['mainContent'].size);
+		else if (FoxtrickPrefs.getString('module.MobileEnhancements.selection')!=='all') {
+			var css = getStyle('mainContent');
+			setStyle(content.document,'foxtrick-pageaction-css', css, pageActions['mainContent'].size);
 		}
 		else
-			this.setMetaViewport(doc, "device-width");
+			setMetaViewport(doc, "device-width");
 
 		// listener to viewport resizing
 		sandboxed.extension.onRequest.addListener(function(request, sender, sendResponse) {
 			if (request.req=='replace_css') {
-				FoxtrickMobileEnhancements.setStyle(content.document, request.id, request.css, request.size);
+				setStyle(content.document, request.id, request.css, request.size);
 			}
 		});
 	},
@@ -84,20 +90,18 @@ var FoxtrickMobileEnhancements = {
 	},
 
 	setStyle : function(doc, id, css, size) {
-		for (var i in FoxtrickMobileEnhancements.ft_pageactions) {
-			Foxtrick.unload_css_permanent(Foxtrick.InternalPath + "resources/css/" + FoxtrickMobileEnhancements.ft_pageactions[i].css);
-		Foxtrick.log('unload: ',Foxtrick.InternalPath + "resources/css/" + FoxtrickMobileEnhancements.ft_pageactions[i].css)
+		var pageActions = this.pageActions;
+		for (var i in pageActions) {
+			Foxtrick.unload_css_permanent(Foxtrick.InternalPath + "resources/css/" + pageActions[i].css);
+			Foxtrick.log('unload: ',Foxtrick.InternalPath + "resources/css/" + pageActions[i].css);
 		}
 		Foxtrick.load_css_permanent(Foxtrick.InternalPath + "resources/css/" + css);
-		Foxtrick.log('load: ',Foxtrick.InternalPath + "resources/css/" + FoxtrickMobileEnhancements.ft_pageactions[i].css)
+		Foxtrick.log('load: ',Foxtrick.InternalPath + "resources/css/" + pageActions[i].css);
 		var size = Foxtrick.util.layout.isRtl(doc) ? size.standard : size.simple;
-		FoxtrickMobileEnhancements.setMetaViewport(doc, size+'px');
+		this.setMetaViewport(doc, size+'px');
 	},
 
 	getStyle : function(title) {
-		return FoxtrickMobileEnhancements.ft_pageactions[title].css;
-	},
-
-};
-//if (Foxtrick.platform == "Fennec")
-//Foxtrick.util.module.register(FoxtrickMobileEnhancements);
+		return this.pageActions[title].css;
+	}
+});
