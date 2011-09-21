e3f2c0e429e6f8ec4e2d9aaf39377123f1fb1781
ryan <ryan@ryanium.com>
2011-09-21 11:21:47 +0000

    ticker-alert.js: move the anonymous function inside a level.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@7436 1e3807c3-0949-0410-b78e-337b627472c1

--- content/alert/ticker-alert.js
+++ content/alert/ticker-alert.js
@@ -3,9 +3,8 @@
  * display a notification when a new ticker has arrived
  * @author taised, convincedd, ryanli
  */
-(function() {
-	
-	var types =  {
+Foxtrick.util.module.register((function() {
+	var types = {
 		"welcome" : /\/MyHattrick\/Dashboard\.aspx$/i,
 		"supporter" : /\/Club\/Manager\/\?teamId=/i,
 		"forum" : /\/Forum/i,
@@ -16,140 +15,136 @@
 		"myht" : /\/Myhattrick\/Dashboard\.aspx\?actionType=/i,
 		"others" : /.*/
 	};
+	return {
+		MODULE_NAME : "TickerAlert",
+		MODULE_CATEGORY : Foxtrick.moduleCategories.ALERT,
+		PAGES : ["all"],
+		NICE : 20, // after all other modules that make changes to #ticker
 
-Foxtrick.util.module.register({
-	MODULE_NAME : "TickerAlert",
-	MODULE_CATEGORY : Foxtrick.moduleCategories.ALERT,
-	PAGES : ["all"],
-	NICE : 20, // after all other modules that make changes to #ticker
+		OPTION_FUNC : function(doc) {
+			var table = doc.createElement("table");
+			table.className = "bordered center maxed";
 
-	OPTION_FUNC : function(doc) {
-		var table = doc.createElement("table");
-		table.className = "bordered center maxed";
+			// header
+			var header = doc.createElement("tr");
+			table.appendChild(header);
+			var holder = doc.createElement("th");
+			header.appendChild(holder);
+			var enableh = doc.createElement("th");
+			enableh.setAttribute("data-text", "TickerAlert.enable");
+			enableh.className='col_checkbox';
+			header.appendChild(enableh);
+			var soundh = doc.createElement("th");
+			soundh.setAttribute("data-text", "TickerAlert.sound");
+			soundh.className='col_textfield';
+			header.appendChild(soundh);
+			var fileh = doc.createElement("th");
+			fileh.className='col_filepicker';
+			header.appendChild(fileh);
 
-		// header
-		var header = doc.createElement("tr");
-		table.appendChild(header);
-		var holder = doc.createElement("th");
-		header.appendChild(holder);
-		var enableh = doc.createElement("th");
-		enableh.setAttribute("data-text", "TickerAlert.enable");
-		enableh.className='col_checkbox';
-		header.appendChild(enableh);
-		var soundh = doc.createElement("th");
-		soundh.setAttribute("data-text", "TickerAlert.sound");
-		soundh.className='col_textfield';
-		header.appendChild(soundh);
-		var fileh = doc.createElement("th");
-		fileh.className='col_filepicker';
-		header.appendChild(fileh);
-
-		for (var type in this.TYPES) {
-			var row = doc.createElement("tr");
-			table.appendChild(row);
-			var rhead = doc.createElement("th");
-			rhead.setAttribute("data-text", "ticker.type." + type);
-			row.appendChild(rhead);
-			var enablec = doc.createElement("td");
-			row.appendChild(enablec);
-			var enable = doc.createElement("input");
-			enable.type = "checkbox";
-			enable.setAttribute("pref", "module.TickerAlert." + type + ".enabled");
-			enablec.appendChild(enable);
-			var soundc = doc.createElement("td");
-			row.appendChild(soundc);
-			var sound = doc.createElement("input");
-			sound.setAttribute("pref", "module.TickerAlert." + type + ".sound");
-			soundc.appendChild(sound);
-			var filec = doc.createElement("td");
-			row.appendChild(filec);
-			var input = Foxtrick.filePickerForDataUrl(doc, (function(sound) {
-					return function(url) { sound.value = url; };
-				})(sound));
-			filec.appendChild(input);
-		}
-
-		return table;
-	},
-
-	TYPES : types,
-
-	run : function(doc) {
-		// type of change to listen to. opera doesn't support DOMSubtreeModified
-		var DOMMutationEventType = (Foxtrick.platform == "Opera") ? "DOMNodeInserted" : "DOMSubtreeModified";
-
-		var getType = function(url) {
 			for (var type in types) {
-				var regexp = types[type];
-				if (url.match(regexp))
-					return type;
-			}
-			return null;
-		};
-		// add watch to ticker
-		var ticker = doc.getElementById("ticker");
-		var getTickers = function() {
-			const divs = ticker.getElementsByTagName("div");
-			const tickers = Foxtrick.map(function(n) {
-				return {
-					text : n.textContent,
-					link : n.getElementsByTagName("a")[0].href,
-					isNew : (n.getElementsByTagName("strong").length > 0)
-				};
-			}, divs);
-			return tickers;
-		};
-		var tickerCheck = function(ev) {
-			if (ev.target) var doc = ev.target.ownerDocument;
-			else var doc = ev; // called directly
-
-			// prevent from multiple tickerCheck() instances running at the
-			// same time
-			ticker.removeEventListener(DOMMutationEventType, tickerCheck, false);
-			var tickers = Foxtrick.sessionGet("tickers");
-			if (tickers == undefined)
-				tickers = [];
-
-			const tickersNow = getTickers();
-			if (tickersNow.length < tickers.length) {
-				// Hattrick.org clears all tickers before adding a new one,
-				// so to not alert when the tickers are being cleared, we
-				// return when ticker count now is less than old ticker
-				// count
-				return;
+				var row = doc.createElement("tr");
+				table.appendChild(row);
+				var rhead = doc.createElement("th");
+				rhead.setAttribute("data-text", "ticker.type." + type);
+				row.appendChild(rhead);
+				var enablec = doc.createElement("td");
+				row.appendChild(enablec);
+				var enable = doc.createElement("input");
+				enable.type = "checkbox";
+				enable.setAttribute("pref", "module.TickerAlert." + type + ".enabled");
+				enablec.appendChild(enable);
+				var soundc = doc.createElement("td");
+				row.appendChild(soundc);
+				var sound = doc.createElement("input");
+				sound.setAttribute("pref", "module.TickerAlert." + type + ".sound");
+				soundc.appendChild(sound);
+				var filec = doc.createElement("td");
+				row.appendChild(filec);
+				var input = Foxtrick.filePickerForDataUrl(doc, (function(sound) {
+						return function(url) { sound.value = url; };
+					})(sound));
+				filec.appendChild(input);
 			}
 
-			Foxtrick.sessionSet("tickers", tickersNow);
+			return table;
+		},
 
-			const newTickers = Foxtrick.filter(function(n) {
-				if (!n.isNew)
-					return false;
-				for (var i = 0; i < tickers.length; ++i) {
-					var old = tickers[i]; 
-					if (old.text == n.text && old.link.replace(/http:\/\/.+\//,'/') == n.link.replace(/http:\/\/.+\//,'/'))
+		run : function(doc) {
+			// type of change to listen to. opera doesn't support DOMSubtreeModified
+			var DOMMutationEventType = (Foxtrick.platform == "Opera") ? "DOMNodeInserted" : "DOMSubtreeModified";
+
+			var getType = function(url) {
+				for (var type in types) {
+					var regexp = types[type];
+					if (url.match(regexp))
+						return type;
+				}
+				return null;
+			};
+			// add watch to ticker
+			var ticker = doc.getElementById("ticker");
+			var getTickers = function() {
+				const divs = ticker.getElementsByTagName("div");
+				const tickers = Foxtrick.map(function(n) {
+					return {
+						text : n.textContent,
+						link : n.getElementsByTagName("a")[0].href,
+						isNew : (n.getElementsByTagName("strong").length > 0)
+					};
+				}, divs);
+				return tickers;
+			};
+			var tickerCheck = function(ev) {
+				if (ev.target) var doc = ev.target.ownerDocument;
+				else var doc = ev; // called directly
+
+				// prevent from multiple tickerCheck() instances running at the
+				// same time
+				ticker.removeEventListener(DOMMutationEventType, tickerCheck, false);
+				var tickers = Foxtrick.sessionGet("tickers");
+				if (tickers == undefined)
+					tickers = [];
+
+				const tickersNow = getTickers();
+				if (tickersNow.length < tickers.length) {
+					// Hattrick.org clears all tickers before adding a new one,
+					// so to not alert when the tickers are being cleared, we
+					// return when ticker count now is less than old ticker
+					// count
+					return;
+				}
+
+				Foxtrick.sessionSet("tickers", tickersNow);
+
+				const newTickers = Foxtrick.filter(function(n) {
+					if (!n.isNew)
 						return false;
-				}
-				return true;
-			}, tickersNow);
-
-			Foxtrick.map(function(n) {
-				var type = getType(n.link);
-				if (FoxtrickPrefs.getBool("module.TickerAlert." + type + ".enabled")) {
-					Foxtrick.util.notify.create(n.text, n.link);
-					var sound = FoxtrickPrefs.getString("module.TickerAlert." + type + ".sound");
-					if (sound) {
-						// use foxtrick:// for Foxtrick.ResourcePath
-						// for cross-platform compatibility
-						sound = sound.replace(/^foxtrick:\/\//, Foxtrick.ResourcePath);
-						Foxtrick.playSound(sound, doc);
+					for (var i = 0; i < tickers.length; ++i) {
+						var old = tickers[i]; 
+						if (old.text == n.text && old.link.replace(/http:\/\/.+\//,'/') == n.link.replace(/http:\/\/.+\//,'/'))
+							return false;
 					}
-				}
-			}, newTickers);
-			ticker.addEventListener(DOMMutationEventType, tickerCheck, false);
-		};
-		if (Foxtrick.util.layout.isSupporter(doc))
-			tickerCheck(doc);
-	}
-});
+					return true;
+				}, tickersNow);
 
-})()
+				Foxtrick.map(function(n) {
+					var type = getType(n.link);
+					if (FoxtrickPrefs.getBool("module.TickerAlert." + type + ".enabled")) {
+						Foxtrick.util.notify.create(n.text, n.link);
+						var sound = FoxtrickPrefs.getString("module.TickerAlert." + type + ".sound");
+						if (sound) {
+							// use foxtrick:// for Foxtrick.ResourcePath
+							// for cross-platform compatibility
+							sound = sound.replace(/^foxtrick:\/\//, Foxtrick.ResourcePath);
+							Foxtrick.playSound(sound, doc);
+						}
+					}
+				}, newTickers);
+				ticker.addEventListener(DOMMutationEventType, tickerCheck, false);
+			};
+			if (Foxtrick.util.layout.isSupporter(doc))
+				tickerCheck(doc);
+		}
+	};
+})());
