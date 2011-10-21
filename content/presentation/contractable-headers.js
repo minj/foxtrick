50320a21b7124e6b23c73361e52699b90fe47a8a
convincedd <convincedd@gmail.com>
2011-10-21 22:04:17 +0000

    contractable-headers.js: displayed unread forumpreview and fixed teampopups in headers
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@7701 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/contractable-headers.js
+++ content/presentation/contractable-headers.js
@@ -13,11 +13,27 @@ Foxtrick.util.module.register({
 
 	run : function(doc) {
 		
+		var getH2TextContent = function(h2) {
+			// remove some of our content to get original textContent of h2
+			var h2Clone = h2.cloneNode(true);
+			var highlight = h2Clone.getElementsByClassName('highlight')[0];
+			if (highlight)
+				highlight.parentNode.removeChild(highlight);
+			var ftpopuplist = h2Clone.getElementsByClassName('ft-popup-list')[0];
+			if (ftpopuplist)
+				ftpopuplist.parentNode.removeChild(ftpopuplist);
+			return h2Clone.textContent;
+		};
+		
 		var toggle = function(h2) {
 			Foxtrick.toggleClass(h2, 'ft-expander-expanded');
 			Foxtrick.toggleClass(h2, 'ft-expander-unexpanded');
-			FoxtrickPrefs.setBool('ContractableHeaders.'+doc.location.pathname+'.'+h2.textContent+'.folded', Foxtrick.hasClass(h2,'ft-expander-unexpanded'));
-
+			
+			var key  = ('ContractableHeaders.'+doc.location.pathname+'.'+getH2TextContent(h2)+'.folded').replace(/\s+/g, '');
+			if (Foxtrick.hasClass(h2, 'ft-expander-expanded'))
+				FoxtrickPrefs.deleteValue(key);
+			else
+				FoxtrickPrefs.setBool(key, true);
 			// go though all children and toggle 'hidden'. in tables use table rows (eg current matches)
 			if (h2.parentNode.nodeName=='TD')
 				var parent = h2.parentNode.parentNode.parentNode;
@@ -25,6 +41,7 @@ Foxtrick.util.module.register({
 				var parent = h2.parentNode;
 			}
 			var el = parent.firstChild, elementBeforeHeader = true;
+			var forumThreads = {}, numUnread = 0;
 			while ( el ) {
 				// start after h2
 				if (elementBeforeHeader) {
@@ -50,9 +67,33 @@ Foxtrick.util.module.register({
 						break
 					}
 					Foxtrick.toggleClass(el, 'hidden');
+					
+					// count new forum postings
+					if ( Foxtrick.hasClass(el,'hidden') && el.getElementsByClassName('fplThreadInfo')[0] != undefined ) {
+						var rows = el.getElementsByClassName('fplThreadInfo');
+						Foxtrick.map(function(n) {
+							var unread = n.getElementsByClassName('highlight')[0];
+							if (unread !== undefined) {
+								var tid = unread.getAttribute('onclick').match(/'read\|(\d+)'/)[1];
+								if (!forumThreads[tid])
+									numUnread += Number(unread.textContent);
+								forumThreads[tid] = true;
+							}
+						}, rows);
+					}
 				}
 				el = el.nextSibling;
 			}
+			
+			// show new forum postings
+			if (numUnread && h2.getElementsByClassName('highlight')[0] == undefined) {
+				h2.appendChild(doc.createTextNode(' '));
+				var span = doc.createElement('span');
+				span.className = 'highlight';
+				span.textContent = '('+numUnread+')';
+				h2.appendChild(span);
+			}
+
 		};
 		
 		// add listener to all h2s in mainBody
@@ -66,7 +107,8 @@ Foxtrick.util.module.register({
 			}
 			Foxtrick.listen(n, "click", function(ev){toggle(ev.target);}, false);
 			Foxtrick.addClass(n, 'ft-expander-expanded');
-			if (FoxtrickPrefs.getBool('ContractableHeaders.'+doc.location.pathname+'.'+n.textContent+'.folded'))
+			var key  = ('ContractableHeaders.'+doc.location.pathname+'.'+getH2TextContent(n)+'.folded').replace(/\s+/g, '');
+			if (FoxtrickPrefs.getBool(key))
 				toggle(n);
 		}, h2s);
 	}
