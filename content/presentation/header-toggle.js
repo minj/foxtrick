b6c1b88881df5155c32404b5f56a4eaf84a4ef33
convincedd <convincedd@gmail.com>
2011-10-23 20:49:41 +0000

    header-toggle.js: better pathname detection for settings saving
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@7726 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/header-toggle.js
+++ content/presentation/header-toggle.js
@@ -13,6 +13,10 @@ Foxtrick.util.module.register({
 
 	run : function(doc) {
 		
+		var pathname = doc.location.pathname;
+		if (pathname.search(/\/$/)!=-1)
+			pathname += 'Default.aspx';
+		
 		var getH2TextContent = function(h2) {
 			// remove some of our content to get original textContent of h2
 			var h2Clone = h2.cloneNode(true);
@@ -25,84 +29,77 @@ Foxtrick.util.module.register({
 			return h2Clone.textContent;
 		};
 		
-		var toggle = function(h2) {
+		var toggle = function(h2) { 
 			Foxtrick.toggleClass(h2, 'ft-expander-expanded');
 			Foxtrick.toggleClass(h2, 'ft-expander-unexpanded');
 			
-			var key  = ('HeaderToggle.'+doc.location.pathname+'.'+getH2TextContent(h2)+'.folded').replace(/\s+/g, '');
+			var key  = ('HeaderToggle.'+pathname+'.'+getH2TextContent(h2)+'.folded').replace(/\s+/g, '');
 			if (Foxtrick.hasClass(h2, 'ft-expander-expanded'))
 				FoxtrickPrefs.deleteValue(key);
 			else
 				FoxtrickPrefs.setBool(key, true);
-			// go though all children and toggle 'hidden'. in tables use table rows (eg current matches)
-			if (h2.parentNode.nodeName=='TD')
-				var parent = h2.parentNode.parentNode.parentNode;
-			else {
-				var parent = h2.parentNode;
-			}
-			var el = parent.firstChild, elementBeforeHeader = true;
-			var forumThreads = {}, numUnread = 0;
-			while ( el ) {
-				// start after h2
-				if (elementBeforeHeader) {
-					if (el == h2
-						|| (el.nodeType ==  Node.ELEMENT_NODE && el.getElementsByTagName('h2')[0] == h2)) {
-						elementBeforeHeader = false;
+			
+			var toggleSiblings = function(el) {
+				var parent = el.parentNode;
+				el = el.nextSibling;
+				var forumThreads = {}, numUnread = 0;
+				while ( el ) { 
+					// if text node, wrap in span on first encounter
+					if (el.nodeType ==  Node.TEXT_NODE) {
+						var target = el.nextSibling;
+						var span =  doc.createElement('span');
+						span.appendChild(el);
+						el = parent.insertBefore(span, target);
+						
+					}
+
+					// stop with next header or dedicated parentNode mainBox
+					if ( (el.className == 'mainBox' && el.getElementsByTagName('h2')[0] != undefined)
+						|| el.nodeName == 'H1'
+						|| (el.nodeName == 'H2' && !Foxtrick.hasClass(el, 'info'))
+						|| (el.getElementsByTagName('h2')[0] !== undefined && !Foxtrick.hasClass(el.getElementsByTagName('h2')[0],'info') )) {
+						break
+					}
+
+					// don't show which is hidden originally, eg ft-forum-preview-area 
+					if (el.id == 'ft-forum-preview-area' && Foxtrick.hasClass(el, 'hidden')) {
+						el = el.nextSibling;
+						continue;
+					}
+
+					Foxtrick.toggleClass(el, 'hidden');
+
+					// count new forum postings
+					if ( Foxtrick.hasClass(el,'hidden') && el.getElementsByClassName('fplThreadInfo')[0] != undefined ) {
+						var rows = el.getElementsByClassName('fplThreadInfo');
+						Foxtrick.map(function(n) {
+							var unread = n.getElementsByClassName('highlight')[0];
+							if (unread !== undefined) {
+								var tid = unread.getAttribute('onclick').match(/'read\|(\d+)'/)[1];
+								if (!forumThreads[tid])
+									numUnread += Number(unread.textContent);
+								forumThreads[tid] = true;
+							}
+						}, rows);
 					}
 					el = el.nextSibling;
-					continue;
 				}
-
-				// if text node, wrap in span on first encounter
-				if (el.nodeType ==  Node.TEXT_NODE) {
-					var target = el.nextSibling;
-					var span =  doc.createElement('span');
-					span.appendChild(el);
-					el = parent.insertBefore(span, target);
-					
+				
+				// show new forum postings
+				if (numUnread && h2.getElementsByClassName('highlight')[0] == undefined) {
+					h2.appendChild(doc.createTextNode(' '));
+					var span = doc.createElement('span');
+					span.className = 'highlight';
+					span.textContent = '('+numUnread+')';
+					h2.appendChild(span);
 				}
-
-				// stop with next header or dedicated parentNode mainBox
-				if ( (el.className == 'mainBox' && el.getElementsByTagName('h2')[0] != undefined)
-					|| el.nodeName == 'H1'
-					|| (el.nodeName == 'H2' && !Foxtrick.hasClass(el, 'info'))
-					|| (el.getElementsByTagName('h2')[0] !== undefined && !Foxtrick.hasClass(el.getElementsByTagName('h2')[0],'info') )) {
-					break
-				}
-
-				// don't show which is hidden originally, eg ft-forum-preview-area 
-				if (el.id == 'ft-forum-preview-area' && Foxtrick.hasClass(el, 'hidden')) {
-					el = el.nextSibling;
-					continue;
-				}
-
-				Foxtrick.toggleClass(el, 'hidden');
-
-				// count new forum postings
-				if ( Foxtrick.hasClass(el,'hidden') && el.getElementsByClassName('fplThreadInfo')[0] != undefined ) {
-					var rows = el.getElementsByClassName('fplThreadInfo');
-					Foxtrick.map(function(n) {
-						var unread = n.getElementsByClassName('highlight')[0];
-						if (unread !== undefined) {
-							var tid = unread.getAttribute('onclick').match(/'read\|(\d+)'/)[1];
-							if (!forumThreads[tid])
-								numUnread += Number(unread.textContent);
-							forumThreads[tid] = true;
-						}
-					}, rows);
-				}
-				el = el.nextSibling;
-			}
+			};
 			
-			// show new forum postings
-			if (numUnread && h2.getElementsByClassName('highlight')[0] == undefined) {
-				h2.appendChild(doc.createTextNode(' '));
-				var span = doc.createElement('span');
-				span.className = 'highlight';
-				span.textContent = '('+numUnread+')';
-				h2.appendChild(span);
+			toggleSiblings(h2);
+			if (h2.parentNode.nodeName=='TD') {
+				// in tables we also toggle sibling rows
+				toggleSiblings(h2.parentNode.parentNode);
 			}
-
 		};
 		
 		// add listener to all h2s in mainBody
@@ -117,7 +114,7 @@ Foxtrick.util.module.register({
 			Foxtrick.listen(n, "click", function(ev){toggle(ev.target);}, false);
 			
 			Foxtrick.addClass(n, 'ft-expander-expanded ft-contractable-header');
-			var key  = ('HeaderToggle.'+doc.location.pathname+'.'+getH2TextContent(n)+'.folded').replace(/\s+/g, '');
+			var key  = ('HeaderToggle.'+pathname+'.'+getH2TextContent(n)+'.folded').replace(/\s+/g, '');
 			if (FoxtrickPrefs.getBool(key))
 				toggle(n);
 		}, h2s);
