af9c49a78a993d3fcba9ec879ba8bb20313eb8da
ryan <ryan@ryanium.com>
2011-12-01 14:16:16 +0000

    @0.8: merge upload-nightly.sh from master.
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.8@7913 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/upload-nightly.sh
+++ maintainer/upload-nightly.sh
@@ -12,58 +12,60 @@
 # 2) Failed to make
 # 3) Failed to upload
 
+# FTP settings
 USER=
 PASSWORD=
-HOST='web306.ixwebhosting.com'
-URL_BASE='http://foxtrick.foundationhorizont.org/nightly'
+HOST='www.foxtrick.org'
 DEST=.
 
-SOURCE_DIR='..'
+# update manifest settings
+URL_BASE='http://foxtrick.foundationhorizont.org/nightly'
+DIST=nightly
 
+# source setting
+SRC_DIR=..
+
+# load configuration, probably overwriting defaults above
 . ./upload.conf.sh
 
-if [[ -z "${USER}" || -z "${PASSWORD}" ]]; then
+# see if values are set
+if [[ -z "$USER" || -z "$PASSWORD" ]]; then
 	echo 'Please specify USER and PASSWORD in upload.conf.sh' >&2
 	exit 1
 fi
 
-(cd ${SOURCE_DIR} && make DIST_TYPE=nightly "$@") || exit 2
+# version settings
+MAJOR_VERSION=$(cd ${SRC_DIR} && ./version.sh)
+if [ "$DIST" == "nightly" ]; then
+	REVISION=$(cd "$SRC_DIR" && git svn find-rev HEAD)
+	VERSION="$MAJOR_VERSION.$REVISION"
+else
+	VERSION="$MAJOR_VERSION"
+fi
 
-REVISION=`cd ${SOURCE_DIR} && git svn find-rev HEAD`
+(cd "$SRC_DIR" && make DIST_TYPE="$DIST" "$@") || exit 2
 
 # modify update-firefox.rdf for Gecko
 cp update-tmpl-firefox.rdf update-firefox.rdf
-GECKO_MAJOR_VERSION=`cd ${SOURCE_DIR} && grep '<em:version>' install.rdf | \
-	sed -r -e 's|^.+<em:version>(.+)</em:version>|\1|'`
-GECKO_VERSION="${GECKO_MAJOR_VERSION}.${REVISION}"
-GECKO_SHA1SUM=`sha1sum "${SOURCE_DIR}/foxtrick.xpi" | sed -r 's/\s+.+$//g'`
-sed -i "s|{UPDATE_LINK}|${URL_BASE}/foxtrick-r${REVISION}.xpi|g" update-firefox.rdf
+GECKO_SHA1SUM=`sha1sum "${SRC_DIR}/foxtrick.xpi" | sed -r 's/\s+.+$//g'`
+sed -i "s|{UPDATE_LINK}|${URL_BASE}/foxtrick-${VERSION}.xpi|g" update-firefox.rdf
 sed -i "s|{UPDATE_HASH}|sha1:${GECKO_SHA1SUM}|g" update-firefox.rdf
-sed -i "s|{VERSION}|${GECKO_VERSION}|g" update-firefox.rdf
+sed -i "s|{VERSION}|${VERSION}|g" update-firefox.rdf
 
 # modify update-chrome.xml for Google Chrome
 cp update-tmpl-chrome.xml update-chrome.xml
-CHROME_MAJOR_VERSION=`cd ${SOURCE_DIR} && grep '"version"' manifest.json | \
-	sed -r -e 's|^.*"version" : "(.+)".*$|\1|'`
-CHROME_VERSION="${CHROME_MAJOR_VERSION}.${REVISION}"
-sed -i "s|{UPDATE_LINK}|${URL_BASE}/chrome/foxtrick-r${REVISION}.crx|g" update-chrome.xml
-sed -i "s|{VERSION}|${CHROME_VERSION}|g" update-chrome.xml
+sed -i "s|{UPDATE_LINK}|${URL_BASE}/chrome/foxtrick-${VERSION}.crx|g" update-chrome.xml
+sed -i "s|{VERSION}|${VERSION}|g" update-chrome.xml
 
 # modify update-opera.xml for Opera
 cp update-tmpl-opera.xml update-opera.xml
-OPERA_MAJOR_VERSION=`cd ${SOURCE_DIR} && grep -E 'version=.+network=.+' config.xml | \
-	sed -r -e 's|^.*version="([^"]+)".*$|\1|'`
-OPERA_VERSION="${OPERA_MAJOR_VERSION}.${REVISION}"
-sed -i "s|{UPDATE_LINK}|${URL_BASE}/opera/foxtrick-r${REVISION}.oex|g" update-opera.xml
-sed -i "s|{VERSION}|${OPERA_VERSION}|g" update-opera.xml
+sed -i "s|{UPDATE_LINK}|${URL_BASE}/opera/foxtrick-${VERSION}.oex|g" update-opera.xml
+sed -i "s|{VERSION}|${VERSION}|g" update-opera.xml
 
 # modify update-safari.plist for Safari
 cp update-tmpl-safari.plist update-safari.plist
-SAFARI_MAJOR_VERSION=`cd ${SOURCE_DIR} && grep -m 1 -E '<string>.+</string><!--version-->' Info.plist | \
-	sed -r -e 's|.+<string>(.+)</string><!--version-->|\1|'`
-SAFARI_VERSION="${SAFARI_MAJOR_VERSION}.${REVISION}"
-sed -i "s|{UPDATE_LINK}|${URL_BASE}/safari/foxtrick-r${REVISION}.safariextz|g" update-safari.plist
-sed -i "s|{VERSION}|${SAFARI_VERSION}|g" update-safari.plist
+sed -i "s|{UPDATE_LINK}|${URL_BASE}/safari/foxtrick-${VERSION}.safariextz|g" update-safari.plist
+sed -i "s|{VERSION}|${VERSION}|g" update-safari.plist
 
 cp ftp-tmpl ftp
 sed -i \
@@ -71,8 +73,8 @@ sed -i \
 	-e "s|{PASSWORD}|${PASSWORD}|g" \
 	-e "s|{HOST}|${HOST}|g" \
 	-e "s|{DEST}|${DEST}|g" \
-	-e "s|{PATH}|${SOURCE_DIR}|g" \
-	-e "s|{REVISION}|${REVISION}|g" ftp
+	-e "s|{PATH}|${SRC_DIR}|g" \
+	-e "s|{VERSION}|${VERSION}|g" ftp
 
 lftp -f ftp || exit 3
 
