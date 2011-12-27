8a04aab76cbb95121690b358c32e2b7f46bb9775
convincedd <convincedd@gmail.com>
2011-12-27 18:19:10 +0000

    avg ratings in hatstats. renamed ratings-range.js into ratings-display.js
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8091 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ content/presentation/ratings-display.js
@@ -0,0 +1,95 @@
+"use strict";
+/**
+ * Changes ratings to the 0-20 range
+ * @author convincedd
+ */
+
+Foxtrick.util.module.register({
+	MODULE_NAME : "RatingsDisplay",
+	MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
+	PAGES : ["statsSeries", "match"],
+
+	RADIO_OPTIONS : ["DenominationsRange", "HatStats" ],
+	
+	run : function(doc) {
+	
+		var do_matchreport = function() {
+			var isprematch = (doc.getElementById("ctl00_ctl00_CPContent_CPMain_pnlPreMatch")!=null);
+			if (isprematch) return;
+
+			var table = Foxtrick.Pages.Match.getRatingsTable(doc);
+			if (table == null) return;
+			if (Foxtrick.Pages.Match.isWalkOver(table)) return;
+
+			var mean_home=0, mean_away=0;
+			for (var row = 1; row < table.rows.length; ++row) {
+				if (table.rows[row].cells[3]) {
+					if (table.rows[row].cells[1].getElementsByTagName('a').length !=0
+					&& table.rows[row].cells[1].textContent != table.rows[row].cells[1].getElementsByTagName('a')[0].textContent) {
+						var val = Number(table.rows[row].cells[3].textContent.replace(',','.'));
+						if (FoxtrickPrefs.getInt("module.RatingsDisplay.value") == 0) 
+							table.rows[row].cells[3].textContent = (val/4+0.75).toFixed(2);
+						else {
+							if (row == table.rows.length-1)  {// total average
+								table.rows[row].cells[3].textContent = mean_home;
+							}
+							else if (row >= table.rows.length-4)  {// averages
+								mean_home += val*3;
+								table.rows[row].cells[3].textContent = val * 3;
+							}
+						}
+					}
+				}
+				if (table.rows[row].cells[4]) {
+					if (table.rows[row].cells[2].getElementsByTagName('a').length !=0
+					&& table.rows[row].cells[2].textContent != table.rows[row].cells[2].getElementsByTagName('a')[0].textContent) {
+						var val = Number(table.rows[row].cells[4].textContent.replace(',','.'));
+						if (FoxtrickPrefs.getInt("module.RatingsDisplay.value") == 0) 
+							table.rows[row].cells[4].textContent = (val/4+0.75).toFixed(2);
+						else {
+							if (row == table.rows.length-1)  {// total average
+								table.rows[row].cells[4].textContent = mean_away;
+							}
+							else if (row >= table.rows.length-4) { // averages
+								mean_away += val*3;
+								table.rows[row].cells[4].textContent = val * 3;
+							}
+						}
+					}
+				}
+			}
+		};
+		
+		var do_seriesstats = function() {
+			var table = doc.getElementById('mainBody').getElementsByTagName('table')[0];
+			for (var row = 1; row < table.rows.length; ++row) {
+				var mean_avg = 0, mean_max = 0;
+				for (var cells = table.rows[row].cells.length-1; cells >1 ; --cells) {
+					var val_max = Number(table.rows[row].cells[cells].getElementsByTagName('a')[0].textContent.replace(',','.'));
+					var val_avg = Number(table.rows[row].cells[cells].getElementsByTagName('span')[1].textContent.replace(',','.'));
+					if (FoxtrickPrefs.getInt("module.RatingsDisplay.value") == 0) {
+						table.rows[row].cells[cells].getElementsByTagName('a')[0].textContent = (val_max/4+0.75).toFixed(2);
+						table.rows[row].cells[cells].getElementsByTagName('span')[1].textContent = (val_avg/4+0.75).toFixed(2);
+					}
+					else {
+						if (cells == 2) {
+							table.rows[row].cells[cells].getElementsByTagName('a')[0].textContent = mean_max;
+							table.rows[row].cells[cells].getElementsByTagName('span')[1].textContent = mean_avg;
+						}
+						else {
+							mean_max += val_max*3;
+							mean_avg += val_avg*3;
+							table.rows[row].cells[cells].getElementsByTagName('a')[0].textContent = val_max * 3;
+							table.rows[row].cells[cells].getElementsByTagName('span')[1].textContent = val_avg * 3;
+						}
+					}
+				}
+			}
+		};
+		
+		if (Foxtrick.isPage("match", doc))
+			do_matchreport();
+		else if (Foxtrick.isPage("statsSeries", doc))
+			do_seriesstats();
+	}
+});
