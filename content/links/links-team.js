22028eeceadee2be637396d5e411aec70a054149
CatzHoek <catzhoek@gmail.com>
2012-01-06 02:40:46 +0000

    links-team.js : Wasn't that all complete overkill?
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8214 1e3807c3-0949-0410-b78e-337b627472c1

--- content/links/links-team.js
+++ content/links/links-team.js
@@ -25,28 +25,27 @@ Foxtrick.util.module.register({
 
 	AddLinksRight : function(doc) {
 		if (!this.isTeamPage(doc)) {return;}
-		var alldivs = doc.getElementsByTagName('div');
+		var div = doc.getElementById('ctl00_ctl00_CPContent_divStartMain');
 		var ownBoxBody=null;
-		for (var j = 0; j < alldivs.length; j++) {
-			if (alldivs[j].className=="main mainRegular") {
-				var teaminfo = this.gatherLinks( alldivs[j], doc );
+		if(!div)
+			return;
 
-				var links = Foxtrick.util.module.get("Links").getLinks("teamlink", teaminfo, doc, this );
-				if (links.length > 0) {
-					ownBoxBody = doc.createElement("div");
-					var header = Foxtrickl10n.getString("foxtrick.links.boxheader" );
-					var ownBoxBodyId = "foxtrick_links_content";
-					ownBoxBody.id = ownBoxBodyId;
+		var teaminfo = this.gatherLinks( div, doc );
 
-					for (var k = 0; k < links.length; k++) {
-						links[k].link.className ="inner";
-						ownBoxBody.appendChild(links[k].link);
-					}
-					var box = Foxtrick.addBoxToSidebar(doc, header, ownBoxBody, -20);
-					box.id = "ft-links-box";
-				}
-				break;
+		var links = Foxtrick.util.module.get("Links").getLinks("teamlink", teaminfo, doc, this );
+		if (links.length > 0) {
+			ownBoxBody = doc.createElement("div");
+			var header = Foxtrickl10n.getString("foxtrick.links.boxheader" );
+			var ownBoxBodyId = "foxtrick_links_content";
+			ownBoxBody.id = ownBoxBodyId;
+
+			for (var k = 0; k < links.length; k++) {
+				links[k].link.className ="inner";
+				ownBoxBody.appendChild(links[k].link);
 			}
+			var box = Foxtrick.addBoxToSidebar(doc, header, ownBoxBody, -20);
+			box.id = "ft-links-box";
+
 		}
 		Foxtrick.util.links.add(doc,ownBoxBody,this.MODULE_NAME,teaminfo);
 	},
