608827a5f9b947334833282878f884ea56d3b270
CatzHoek <catzhoek@gmail.com>
2012-01-13 17:26:45 +0000

    Crowin Test
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8339 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ maintainer/locale/Hattrick/CrowdIn.py
@@ -0,0 +1,19 @@
+import httplib, urllib
+
+class Crowdin:
+	def __init__(self, project_identfier, project_key):
+		self.masterFileName = "foxtrick.properties"
+		self.identifier = project_identfier
+		self.key = project_key
+		self.baseUrl = "api.crowdin.net"
+
+	def getStatus(self):
+		params = urllib.urlencode({'key': self.key})
+		print params
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn = httplib.HTTPConnection(self.baseUrl)
+		conn.request("POST", "/api/project/" + self.identifier + "/status", params, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		print data
