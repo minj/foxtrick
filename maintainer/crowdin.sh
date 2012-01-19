0c83a6180aacfb0d8d7e054c20dd878b158b8092
convincedd <convincedd@gmail.com>
2012-01-19 17:47:15 +0000

    added first crowdin script
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8394 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ maintainer/crowdin.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CROWDIN_KEY=
+CROWDIN_URL=http://api.crowdin.net/api/project/foxtrick
+
+# load configuration, probably overwriting defaults above
+. ./upload.conf.sh
+
+
+#export, pack latest translations
+#curl \
+# "$CROWDIN_URL"/export?key="$CROWDIN_KEY"
+
+
+#Download all translations as a single ZIP archive.
+wget -nv -O langs.zip "$CROWDIN_URL"/download/all.zip?key="$CROWDIN_KEY"
+  if [ $? -ne 0 ]; then
+    echo "Link not up"
+    exit -1
+  else
+    echo "OK"
+  fi
+
+mkdir crowdin/locale/
+unzip -q -o -a langs.zip -d crowdin/locale/
+  if [ $? -ne 0 ]; then
+    echo "not unzipped"
+    exit -1
+  else
+    echo "OK unzip"
+  fi
+
+#Download French translations.
+#wget http://api.crowdin.net/api/project/{project-identifier}/download/fr.zip?key={project-key}
+
+#cleanup
+rm -rf langs.zip
+
