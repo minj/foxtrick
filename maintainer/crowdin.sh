0ec4a3af79dba739fd38ef5b4ee6757232078db0
convincedd <convincedd@gmail.com>
2012-01-20 13:20:06 +0000

    crowdin.sh: comments
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8414 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/crowdin.sh
+++ maintainer/crowdin.sh
@@ -8,30 +8,33 @@ CROWDIN_URL=http://api.crowdin.net/api/project/foxtrick
 
 
 #export master
+echo "upload new master to crowdin"
 curl \
--F "files[foxtrick.properties]=@../content/foxtrick.properties" "$CROWDIN_URL"/update-file?key="$CROWDIN_KEY"
+  -F "files[foxtrick.properties]=@../content/foxtrick.properties" "$CROWDIN_URL"/update-file?key="$CROWDIN_KEY"
 
 #export, pack latest translations
-curl "$CROWDIN_URL"/export?key="$CROWDIN_KEY"
+echo "order crowdin to repack"
+curl \
+  "$CROWDIN_URL"/export?key="$CROWDIN_KEY"
 
 
 #Download all translations as a single ZIP archive.
 wget -nv -O langs.zip "$CROWDIN_URL"/download/all.zip?key="$CROWDIN_KEY"
   if [ $? -ne 0 ]; then
-    echo "Link not up"
+    echo "Translations download failed"
     exit -1
   else
-    echo "OK"
+    echo "Translations downloaded"
   fi
 
 mkdir crowdin/
 mkdir crowdin/locale/
 unzip -q -o -a langs.zip -d crowdin/locale/
   if [ $? -ne 0 ]; then
-    echo "not unzipped"
+    echo "Unzipped failed"
     exit -1
   else
-    echo "OK unzip"
+    echo "Unzipped"
   fi
 
 #Download French translations.
