970521715612f18e773869b7fbc77b2899b2589d
convincedd <convincedd@gmail.com>
2012-02-01 02:16:34 +0000

    crowdin.sh: log beautification
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8517 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/crowdin.sh
+++ maintainer/crowdin.sh
@@ -3,15 +3,22 @@
 CROWDIN_KEY=
 CROWDIN_URL=http://api.crowdin.net/api/project/foxtrick
 SVN_FILES=../content/locale/*
+CROWDIN_FOLDER=crowdin/locale
 
 # load configuration, probably overwriting defaults above
 . ./upload.conf.sh
 
 
 #export master
-echo "upload new master to crowdin"
-curl \
-  -F "files[foxtrick.properties]=@../content/foxtrick.properties" "$CROWDIN_URL"/update-file?key="$CROWDIN_KEY"
+echo "upload to crowdin"
+re="$(curl -s \
+  -F "files[foxtrick.properties]=@../content/foxtrick.properties" \
+  "$CROWDIN_URL"/update-file?key="$CROWDIN_KEY"  | grep -c success)"
+  if [ $re -ne 1 ]; then
+    echo "master failed"
+  else
+    echo "master ok"
+  fi
 
 for LOC in $SVN_FILES
 do
@@ -22,43 +29,45 @@ do
     -F "import_eq_suggestions=1" \
     "$CROWDIN_URL"/upload-translation?key="$CROWDIN_KEY" | grep -c success)"
   if [ $re -ne 1 ]; then
-    echo "upload ${LOC##*/} to crowdin failed"
+    echo "${LOC##*/} failed"
   else
-    echo "uploaded ${LOC##*/} to crowdin"
+    echo "${LOC##*/} ok"
   fi
 done
   
 #export/pack latest translations
 echo "order crowdin to repack.."
-re="$(curl -s "$CROWDIN_URL"/export?key="$CROWDIN_KEY" | grep -c success)"
+re="$(curl -s \
+  "$CROWDIN_URL"/export?key="$CROWDIN_KEY" | grep -c success)"
   if [ $re -ne 1 ]; then
-    echo "crowdin repack failed. don't get outdated zip"
+    echo "export failed"
     exit -1
   else
-    echo "crowdin repacked. get new zip"
+    echo "ok"
   fi
   
 #Download all translations as a single ZIP archive.
+echo "download zip"
 wget -nv -O langs.zip "$CROWDIN_URL"/download/all.zip?key="$CROWDIN_KEY"
   if [ $? -ne 0 ]; then
-	echo "Translations download failed"
+	echo "failed"
 	exit -1
   else
-	echo "Translations downloaded"
+	echo "ok"
   fi
 
-mkdir crowdin/
-mkdir crowdin/locale/
-unzip -q -o -a langs.zip -d crowdin/locale/
+mkdir -p "$CROWDIN_FOLDER"
+unzip -q -o -a langs.zip -d "$CROWDIN_FOLDER"
   if [ $? -ne 0 ]; then
-	echo "Unzipped failed"
+	echo "Unzip failed"
 	exit -1
   else
-	echo "Unzipped"
+	echo "Unzipped to $CROWDIN_FOLDER"
   fi
 
 #get translation status  
-curl -s "$CROWDIN_URL"/status?key="$CROWDIN_KEY" > crowdin/locale/status.xml
+curl -s \
+  "$CROWDIN_URL"/status?key="$CROWDIN_KEY" > "$CROWDIN_FOLDER"/status.xml
  
 #cleanup
 rm -rf langs.zip
