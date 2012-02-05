661c74c5d44305718c58def1700b642fdc1d9404
convincedd <convincedd@gmail.com>
2012-02-05 13:19:32 +0000

    Foxtrick.NodeTypes: use our own instead of browsers. the other version just looks to obscure
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8558 1e3807c3-0949-0410-b78e-337b627472c1

--- content/env.js
+++ content/env.js
@@ -622,29 +622,21 @@ else {
 }
 
 
-// Node not available for all browsers. make one if needed
-if (typeof(Node)=='undefined') {
-	var Node = {
-		ELEMENT_NODE : 1,
-		ATTRIBUTE_NODE : 2,
-		TEXT_NODE : 3,
-		CDATA_SECTION_NODE : 4,
-		ENTITY_REFERENCE_NODE : 5,
-		ENTITY_NODE : 6,
-		PROCESSING_INSTRUCTION_NODE : 7, 
-		COMMENT_NODE : 8,
-		DOCUMENT_NODE : 9,
-		DOCUMENT_TYPE_NODE : 10,
-		DOCUMENT_FRAGMENT_NODE : 11,
-		NOTATION_NODE: 12
-	};
-	// try override with browser defined
-	if (typeof( window ) !=='undefined' && typeof( window.Node ) !=='undefined') 
-		Node = window.Node;
-	// fennec. in content context window is called content
-	else if (typeof( content ) !=='undefined' && typeof( content.Node ) !=='undefined') 
-		Node = content.Node;
-}
+// Node not available for all browsers. let's just use our own
+Foxtrick.NodeTypes = {
+	ELEMENT_NODE : 1,
+	ATTRIBUTE_NODE : 2,
+	TEXT_NODE : 3,
+	CDATA_SECTION_NODE : 4,
+	ENTITY_REFERENCE_NODE : 5,
+	ENTITY_NODE : 6,
+	PROCESSING_INSTRUCTION_NODE : 7, 
+	COMMENT_NODE : 8,
+	DOCUMENT_NODE : 9,
+	DOCUMENT_TYPE_NODE : 10,
+	DOCUMENT_FRAGMENT_NODE : 11,
+	NOTATION_NODE: 12
+};
 
 // bind seem to miss in safari extensions
 /** vice-versa Function
