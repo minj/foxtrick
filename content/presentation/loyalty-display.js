f5ef5f5cb9345675fdffbc227a96fe02247ec9f1
CatzHoek <catzhoek@gmail.com>
2012-02-10 22:04:59 +0000

    loyalty-display.js : Removed unbreakable spaces
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8623 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/loyalty-display.js
+++ content/presentation/loyalty-display.js
@@ -68,8 +68,8 @@ Foxtrick.modules["LoyaltyDisplay"]={
 					//bars is updated on the fly when removing class "percentageImage", so iterate like this
 					while(bars.length){
 					//	Foxtrick.makeFeaturedElement(bars[0], this);
-						bars[0].setAttribute("title", bars[0].getAttribute("title") + '\u00a0' + '+' + String(skillUp).substring(0, 4));
-						bars[0].setAttribute("alt", bars[0].getAttribute("alt") + '\u00a0' + '+' + String(skillUp).substring(0, 4));
+						bars[0].setAttribute("title", bars[0].getAttribute("title") + ' +' + String(skillUp).substring(0, 4));
+						bars[0].setAttribute("alt", bars[0].getAttribute("alt") + ' +' + String(skillUp).substring(0, 4));
 						Foxtrick.addClass(bars[0], "ft-percentImage-loyalty-" + appendix);
 						Foxtrick.addClass(bars[0], "ft-percentImage");
 						Foxtrick.removeClass(bars[0], "percentImage");
