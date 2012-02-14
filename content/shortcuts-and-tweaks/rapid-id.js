92ef7f961a3d8b737ae100b3a51f246ab0cbb1ff
CatzHoek <catzhoek@gmail.com>
2012-02-14 14:30:49 +0000

    rapid-id.js : isYouth=true -> &SourceSystem=Youth
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8645 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/rapid-id.js
+++ content/shortcuts-and-tweaks/rapid-id.js
@@ -22,7 +22,7 @@ Foxtrick.modules["RapidId"]={
 			{ value: "youth-team", text: "Team", url: "Club/Youth/Default.aspx?YouthTeamID=%n" },
 			{ value: "youth-series", text: "Series", url: "World/Series/YouthSeries.aspx?YouthLeagueId=%n" },
 			{ value: "youth-player", text: "Player", url: "Club/Players/YouthPlayer.aspx?YouthPlayerID=%n" },
-			{ value: "youth-match", text: "Match", url: "Club/Matches/Match.aspx?matchID=%n&isYouth=true" }
+			{ value: "youth-match", text: "Match", url: "Club/Matches/Match.aspx?matchID=%n&SourceSystem=Youth" }
 		];
 		var setSelected = function(val) {
 			FoxtrickPrefs.setString("module.RapidId.selected", val);
