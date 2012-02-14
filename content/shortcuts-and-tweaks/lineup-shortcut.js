62e926966f3942cb57995c1007bb38c9da51b832
CatzHoek <catzhoek@gmail.com>
2012-02-14 14:22:17 +0000

    lineup-shortcut.js : SourceSystem changes are on production already
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8644 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/lineup-shortcut.js
+++ content/shortcuts-and-tweaks/lineup-shortcut.js
@@ -127,15 +127,9 @@ Foxtrick.modules["LineupShortcut"]={
 		cell.className = "ft-lineup-cell";
 		var link = Foxtrick.createFeaturedElement(doc, this, "a");
 		if (type == "youth"){
-			if(Foxtrick.isStage(doc))
 				link.href = "/Club/Matches/MatchLineup.aspx?matchID=" + matchid + "&YouthTeamID=" + teamid + "&HighlightPlayerID=" + playerid + "&SourceSystem=Youth";
-			else
-				link.href = "/Club/Matches/MatchLineup.aspx?MatchID=" + matchid + "&YouthTeamID=" + teamid + "&isYouth=true&HighlightPlayerID=" + playerid;
 		} else {
-			if(Foxtrick.isStage(doc))
 				link.href = "/Club/Matches/MatchLineup.aspx?matchID=" + matchid + "&TeamId=" + teamid + "&HighlightPlayerID=" + playerid + "&SourceSystem=Hattrick";
-			else
-				link.href = "/Club/Matches/MatchLineup.aspx?MatchID=" + matchid + "&TeamID=" + teamid + "&HighlightPlayerID=" + playerid;
 		}		
 		var src = '';
 		if (type == "NT")
