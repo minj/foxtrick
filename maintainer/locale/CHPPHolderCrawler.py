288fd89d6be157c4b8e936aa83097eb4afce24d0
CatzHoek <catzhoek@gmail.com>
2012-02-23 22:37:29 +0000

    CHPPHolderCrawler.py : Cleaned the thing up a bit
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8704 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/locale/CHPPHolderCrawler.py
+++ maintainer/locale/CHPPHolderCrawler.py
@@ -1,9 +1,6 @@
 from Hattrick.Web import HattrickWeb
 from Hattrick.Parsers import CHPPHolderParser
 
-from xml.dom.minidom import Document
-import json
-
 def login(username, password):
 	#use stage for now
 	ht = HattrickWeb(username, password, stage=True)
@@ -34,23 +31,14 @@ if __name__ == "__main__":
 	ht.open("/Community/CHPP/ChppPrograms.aspx")
 	chppHolderParser = CHPPHolderParser.CHPPHolderParser()
 	chppHolderParser.feed(ht.body)
-	res = {}
-	res['type'] = "chpp-holder"
-	holders = chppHolderParser.get()
-	res['list'] =sorted(holders, key=lambda x: x["name"])
+	sorted_users = sorted(chppHolderParser.get(), key=lambda x: x["name"])
 	
 	print 'writing', outfile, '...'
 	file = open( outfile, "w")
-
 	file.write('{\n')
 	file.write('\t"type": "%s",\n' % "chpp-holder")
 	file.write('\t"list": [\n')
-	file.write('\t\t' + ',\n\t\t'.join(map(lambda a: '{ "id": %d, "name": "%s" }' % (a["id"], a["name"]), res['list'])))
+	file.write('\t\t' + ',\n\t\t'.join(map(lambda a: '{ "id": %d, "name": "%s" }' % (a["id"], a["name"]), sorted_users)))
 	file.write('\n\t]\n}')
-	# for entry in res['list']:
-	# 	json.dump(entry, file);
-	# 	file.write('\n')
-	#json.dump(res, file, indent=4, sort_keys=False)
-	#json.dump(res, file);
 	file.close()
 	print outfile, 'written'
