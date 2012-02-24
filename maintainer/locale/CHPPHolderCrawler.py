45bc9e0af72a17d48ee5c707580fa0eafaa25cf8
CatzHoek <catzhoek@gmail.com>
2012-02-24 13:00:00 +0000

    CHPPHolderCrawler.py: Updated to crawl application Name too
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@8709 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/locale/CHPPHolderCrawler.py
+++ maintainer/locale/CHPPHolderCrawler.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 		pw = raw_input("Password:");
 		success, ht = login( user, pw )
 
-	outfile = raw_input("Outfile (*.xml): ");
+	outfile = raw_input("Outfile (ext json): ");
 
 	ht.open("/Community/CHPP/ChppPrograms.aspx")
 	chppHolderParser = CHPPHolderParser.CHPPHolderParser()
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 	file.write('{\n')
 	file.write('\t"type": "%s",\n' % "chpp-holder")
 	file.write('\t"list": [\n')
-	file.write('\t\t' + ',\n\t\t'.join(map(lambda a: '{ "id": %d, "name": "%s" }' % (a["id"], a["name"]), sorted_users)))
+	file.write('\t\t' + ',\n\t\t'.join(map(lambda a: '{ "id": %d, "name": "%s", "appName" = "%s" }' % (a["id"], a["name"].encode('utf-8'),a["appName"].encode('utf-8')), sorted_users)))
 	file.write('\n\t]\n}')
 	file.close()
 	print outfile, 'written'
