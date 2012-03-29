7f72c5fba18ec4f1ac5f902b200d6f9da26e9b6a
CatzHoek <catzhoek@gmail.com>
2012-03-29 18:14:07 +0000

    player-stats-experience.js: Cleaned up a lot, fixed stuff. Still needs proper formating and will probably still fail on tournament games. Also first game after/at skillup needs to be subtracted from minimum calculation. Decent presentation and so still to come.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9223 1e3807c3-0949-0410-b78e-337b627472c1

--- content/information-aggregation/player-stats-experience.js
+++ content/information-aggregation/player-stats-experience.js
@@ -38,79 +38,116 @@ Foxtrick.modules["PlayerStatsExperience"]={
 
 		//headers
 		var stats_head = stats.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0];
-		//var matches_head = matches.getElementsByTagName("thead")[0].getElementsByTagName("tr")[0];
 
 		var ts_xp = doc.createElement("th");
 		Foxtrick.addClass(ts_xp,"stats");
 		Foxtrick.addClass(ts_xp,"ft-dummy");
-		ts_xp.textContent = "XP Pts."
-		stats_head.appendChild(ts_xp);
+		ts_xp.textContent = "XP+"
+		//stats_head.appendChild(ts_xp);
+		stats_head.insertBefore(ts_xp, stats_head.getElementsByTagName("th")[7]);
 
 		//and their entries
-		var stats_entries = stats.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-		//var matches_entries = matches.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-
-		var intRE = RegExp("\\d+", "g");
+		var stats_entries = stats.getElementsByTagName("tbody")[0].getElementsByTagName("tr");		
 
 		var lastGameXP = null;
 		var xp_sub_min = 0.0;
 		var xp_sub_max = 0.0;
 		var friendly_count_since_skillup = 0;
-		var firstEntry = true;
 		var hadXpSkillUp = false;
+		var xp_sub_min_before = 0.0;
+
+		var matchcounter = {}
+		var minutecounter = {}
 
 		Foxtrick.map( function( entry ){
-			var gametypeParent = entry.getElementsByClassName("keyColumn")[0];
-			var gameTypeImage = gametypeParent.getElementsByClassName("iconMatchtype")[0].getElementsByTagName("img")[0];
-			var gameType = gameTypeImage.className;
-			var playtime = entry.getElementsByClassName("endColumn1")[0];
+			// var gametypeParent = entry.getElementsByClassName("keyColumn")[0];
+			// var gameTypeImage = gametypeParent.getElementsByClassName("iconMatchtype")[0].getElementsByTagName("img")[0];
+			// var gameType = gameTypeImage.className;
+			// var isNTmatch = gameTypeImage.parentNode.getAttribute("style") != null;
+			//var playtimes = entry.getElementsByClassName("endColumn1")[0];
+			var	xp_entry = parseInt(entry.getElementsByTagName("td")[xp_column].textContent);
 			
-			var	xp_entry = parseInt(entry.getElementsByTagName("td")[6].textContent);
 			
-			
-			if(lastGameXP === null){
+			if(lastGameXP === null)
 				lastGameXP = xp_entry;
-				firstEntry = false;
-			}			
-			var playMinutes = playtime.textContent.match(intRE);
-			var minutes = 0;
-			if(playMinutes != null)
-				for (var i = 0; i < playMinutes.length; i++)
-					if (!isNaN(playMinutes[i]))
-						minutes += parseInt(playMinutes[i]);
 
-			var minutes = Math.min(90, minutes);	
-			var ts_xp = doc.createElement("td");
+			var isNTmatch = function(node){
+				var gametypeParent = node.getElementsByClassName("keyColumn")[0];
+				var gameTypeImage = gametypeParent.getElementsByClassName("iconMatchtype")[0].getElementsByTagName("img")[0];
+				var gameType = gameTypeImage.className;
+				return gameTypeImage.parentNode.getAttribute("style") != null;	
+			}
+			var getPlayedMinutes = function(node){
+				var playtimes = entry.getElementsByClassName("endColumn1")[0];
+				//sum up the diff positions		
+				var intRE = RegExp("\\d+", "g");
+				var playMinutes = playtimes.textContent.match(intRE);
+				var minutes = 0;
+				if(playMinutes != null)
+					for (var i = 0; i < playMinutes.length; i++)
+						if (!isNaN(playMinutes[i]))
+							minutes += parseInt(playMinutes[i]);
+				//max 90'
+				return Math.min(90, minutes);
+			}
 
-			var isNTmatch = gameTypeImage.parentNode.getAttribute("style") != null;
-			var bonus = xp[gameType];
-			if(isNTmatch){
-				if(gameType == "matchFriendly")
-					bonus = xp.matchNtFriendly;
-				else if(gameType == "matchLeague")
-					bonus = xp.matchNtLeague;
-			}	
-			var xp_gain = (bonus/90)*minutes;
-			
+			var getGameType = function(node){
+
+				var getBasicGameType = function(node){
+					var gametypeParent = node.getElementsByClassName("keyColumn")[0];
+					var gameTypeImage = gametypeParent.getElementsByClassName("iconMatchtype")[0].getElementsByTagName("img")[0];
+					return gameTypeImage.className;
+				}
+
+				var gameType = getBasicGameType(node);
+				var isNT = isNTmatch(node);
+				if(isNT){
+					if(gameType == "matchFriendly")
+						return "matchNtFriendly"
+					else if(gameType == "matchLeague")
+						return  "matchNtLeague"
+				} else
+					return gameType; 
+			}
+
+			var getXpGain = function(minutes, gametype){
+				return (xp[gameType]/90)*minutes;	
+			}
+
+			var ntMatch = isNTmatch( entry );
+			var minutes = getPlayedMinutes( entry );
+			var gameType = getGameType(entry);
+			var xp_gain = getXpGain(minutes, gameType);
+
 			if(xp_entry == lastGameXP){
-				xp_sub_max += xp_gain;
-				xp_sub_min += xp_gain;
-				if(!isNTmatch && gameType == "matchFriendly")
-					xp_sub_min -= 0.1;
+				if(!ntMatch){
+					 if(gameType == "matchFriendly"){
+					 	xp_sub_min += xp_gain/2;
+					 	xp_sub_max += xp_gain;	
+					 } else {
+					 	xp_sub_min += xp_gain;
+					 	xp_sub_max += xp_gain;	
+					 }
+				} else {
+					xp_sub_min += xp_gain;
+					xp_sub_max += xp_gain;	
+				}
 			} else {
 				hadXpSkillUp = true;
 			}
+
+			var ts_xp = doc.createElement("td");
 			ts_xp.textContent = xp_gain.toFixed(2);
-			entry.appendChild(ts_xp);
+			entry.insertBefore(ts_xp, entry.getElementsByTagName("td")[xp_column+1]);
 
 		}, stats_entries);
 
 
 		var div = doc.createElement("div");
-		var textNode = doc.createTextNode("Experience Sub is between " + (xp_sub_min/28.0).toFixed(2) + " and " + (xp_sub_max/28.0).toFixed(2));
+		var textNode = doc.createTextNode("Experience Sub is between " + (xp_sub_min/28.0).toFixed(2) + " (" + xp_sub_min.toFixed(2) +")" + " and " + (xp_sub_max/28.0).toFixed(2)  + " (" + xp_sub_max.toFixed(2) +"). ");
 		
 		if(!hadXpSkillUp)
-			textNode.textContent = textNode.textContent + ". Info might be incorrect because the player has no recorded experience-skillup or you díd not expand to all matches (Work in progress)";
+			textNode.textContent = textNode.textContent + "Info might be incorrect because the player has no recorded experience-skillup or you díd not expand to all matches (Work in progress)";
 
 		div.appendChild(textNode);
 		var navigation = doc.getElementById("ctl00_ctl00_CPContent_CPMain_pnlMatchHistorySlideToggle");
