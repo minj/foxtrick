9ecac5066a351aa8053d32ab0b59b5aa984ef4af
convincedd <convincedd@gmail.com>
2012-03-30 17:46:29 +0000

    ticker-coloring: pure css again (should work fine and needs one less frowned on DOMeventListener less)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9241 1e3807c3-0949-0410-b78e-337b627472c1

--- content/alert/ticker-coloring.js
+++ content/alert/ticker-coloring.js
@@ -11,37 +11,5 @@ Foxtrick.modules["TickerColoring"]={
 	CSS : Foxtrick.InternalPath + "resources/css/ticker-coloring.css",
 
 	run : function(doc) {
-		var ticker = doc.getElementById("ticker");
-
-		var update = function() {
-			var links = ticker.getElementsByTagName("a");
-			for (var i = 0; i < links.length; ++i) {
-				var link = links[i];
-				if (!Foxtrick.hasClass(link, "ft-ticker-link")) {
-					Foxtrick.addClass(link, "ft-ticker-link");
-					// need to use getAttribute to get relative path
-					var href = link.getAttribute("href");
-					if (href === "/MyHattrick/Dashboard.aspx")
-						Foxtrick.addClass(link, "ft-ticker-welcome");
-					else if (href.indexOf("/Club/Manager/?teamId=") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-supporter");
-					else if (href.indexOf("/Forum/") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-forum");
-					else if (href.indexOf("/Players/") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-transfer");
-					else if (href.indexOf("/Challenges/") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-challenge");
-					else if (href.indexOf("/Club/Manager/Guestbook.aspx?teamid=") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-guestbook");
-					else if (href.indexOf("/Inbox/") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-mail");
-					else if (href.indexOf("/Myhattrick/Dashboard.aspx?actionType") !== -1)
-						Foxtrick.addClass(link, "ft-ticker-myht");
-				}
-			}
-			ticker.addEventListener('DOMNodeInserted', update, false);
-		}
-
-		update();
 	}
 };
