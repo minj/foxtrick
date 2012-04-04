9cd7c29b19a6c465f123f8cbdc91f5b0073ea35c
CatzHoek <catzhoek@gmail.com>
2012-04-04 09:57:26 +0000

    dom.js : Added opportunity to create images instead of automatically appending when we want to use insertBefore or similar.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9276 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/dom.js
+++ content/util/dom.js
@@ -293,7 +293,7 @@ Foxtrick.GetDataURIText = function (filetext) {
 	return "data:text/plain;charset=utf-8,"+encodeURIComponent(filetext);
 }
 
-Foxtrick.addImage = function (doc, elem, features) {
+Foxtrick.createImage = function (doc, features) {
 	if (Foxtrick.platform == "Opera" && features.src.indexOf('resources') != -1) {
 		sandboxed.extension.sendRequest({ req : "getDataUrl", url:features.src},
 			function (data) {
@@ -301,15 +301,20 @@ Foxtrick.addImage = function (doc, elem, features) {
 				for (i in features) 
 					img.setAttribute(i, features[i]);
 				img.src = data.url;
-				elem.appendChild(img);
+				return img;
 		});
 	}
 	else {
 		var img = doc.createElement("img");
 		for (i in features)  
 			img.setAttribute(i, features[i]);
-		elem.appendChild(img);
+		return img;
 	}
+};
+
+Foxtrick.addImage = function (doc, elem, features) {
+	var image = Foxtrick.createImage(doc, features);
+	elem.appendChild( image );
 };
 
 Foxtrick.getImageFeatures = function (features, callback) {
