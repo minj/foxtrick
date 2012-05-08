8a65ce6cd2e45631dda0e3ae6cd26d3c533604a3
CatzHoek <catzhoek@gmail.com>
2012-05-08 21:07:28 +0000

    @093 loyalty-display.js : Copied from trunk ... ready to show bars on matchorder inface too
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.9.3@9544 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/loyalty-display.js
+++ content/presentation/loyalty-display.js
@@ -10,16 +10,11 @@ Foxtrick.modules["LoyaltyDisplay"]={
 	PAGES : ['players', 'playerdetail'],
 	CSS : Foxtrick.InternalPath + "resources/css/loyalty-display.css",
 	
-	run : function(doc) {
-	
-		//on players page, just run for the own team
-		if(Foxtrick.isPage('players', doc) && !Foxtrick.Pages.Players.isOwnPlayersPage(doc))
-			return;
+	replacePercentageImage : function(player, node) {
 
-		var str_mcb = Foxtrickl10n.getString('LoyaltyDisplay.motherClubBonus')
-		var str_lo = Foxtrickl10n.getString('LoyaltyDisplay.loyaltyBonus')
-			
-		var replaceBars = function(node, skillUp, appendix){
+		var replaceBars = function(node, skillUp, appendix) {
+			var str_mcb = Foxtrickl10n.getString('LoyaltyDisplay.motherClubBonus')
+			var str_lo = Foxtrickl10n.getString('LoyaltyDisplay.loyaltyBonus')
 			var str = (skillUp == 1.5)?str_mcb:str_lo;
 			var count = 0;
 			var bars = node.getElementsByTagName("img");
@@ -33,54 +28,57 @@ Foxtrick.modules["LoyaltyDisplay"]={
 				}
 				++count;
 			}
-		}
-		
-		var replacePercentageImage = function(player, node){
-			
-			var loyalty = player.Loyalty;
-			if(loyalty === undefined)
-				loyalty  = player.loyalty;
-					
-			var mcb = player.MotherClubBonus;
-			if(mcb === undefined)
-				mcb  = player.motherClubBonus;
+		};
 
-			if (mcb === undefined || mcb == 'False'){
-				if (loyalty !== undefined){
-					//formula
-					// loyalty = 1 + sqrt(days/336)*19
-					// bonus = sqrt(days/336)
-					// -> bonus = (loyalty - 1) / 19
-					var skillUp = ( loyalty - 1 ) / 19.0;
-					
-					var appendix;
-					//find correct style for this loyalty level
-					if(skillUp == 1)
-						appendix = "1000";
-					else if(skillUp >= 0.875)
-						appendix = "875";
-					else if(skillUp >= 0.75)
-						appendix = "750";
-					else if(skillUp >= 0.625)
-						appendix = "625";
-					else if(skillUp >= 0.50)
-						appendix = "500";
-					else if(skillUp >= 0.375)
-						appendix = "375";
-					else if(skillUp >= 0.25)
-						appendix = "250";
-					else if(skillUp >= 0.125)
-						appendix = "125";
-					else 
-						return;
+		var loyalty = player.Loyalty;
+		if(loyalty === undefined)
+			loyalty  = player.loyalty;
+				
+		var mcb = player.MotherClubBonus;
+		if(mcb === undefined)
+			mcb  = player.motherClubBonus;
 
-					replaceBars(node, skillUp, appendix);
-				}
-			} else {
-				//homegrown, skillUp should be 1.5
-				replaceBars(node, 1.5, 'homegrown');
+		if (mcb === undefined || mcb == 'False'){
+			if (loyalty !== undefined){
+				//formula
+				// loyalty = 1 + sqrt(days/336)*19
+				// bonus = sqrt(days/336)
+				// -> bonus = (loyalty - 1) / 19
+				var skillUp = ( loyalty - 1 ) / 19.0;
+				
+				var appendix;
+				//find correct style for this loyalty level
+				if(skillUp == 1)
+					appendix = "1000";
+				else if(skillUp >= 0.875)
+					appendix = "875";
+				else if(skillUp >= 0.75)
+					appendix = "750";
+				else if(skillUp >= 0.625)
+					appendix = "625";
+				else if(skillUp >= 0.50)
+					appendix = "500";
+				else if(skillUp >= 0.375)
+					appendix = "375";
+				else if(skillUp >= 0.25)
+					appendix = "250";
+				else if(skillUp >= 0.125)
+					appendix = "125";
+				else 
+					return;
+
+				replaceBars(node, skillUp, appendix);
 			}
+		} else {
+			//homegrown, skillUp should be 1.5
+			replaceBars(node, 1.5, 'homegrown');
 		}
+	},
+	run : function(doc) {
+	
+		//on players page, just run for the own team
+		if(Foxtrick.isPage('players', doc) && !Foxtrick.Pages.Players.isOwnPlayersPage(doc))
+			return;
 		
 		if( Foxtrick.Pages.Players.isOwnPlayersPage(doc) && Foxtrick.isPage('players', doc) ){		
 			var playersNode = doc.getElementsByClassName("playerInfo");
@@ -92,14 +90,14 @@ Foxtrick.modules["LoyaltyDisplay"]={
 					if (playerid) {
 						var thisPlayerInfo = Foxtrick.Pages.Players.getPlayerFromListById(playerInfo, playerid);
 						if (thisPlayerInfo) 
-							replacePercentageImage(thisPlayerInfo, playersNode[p]);
+							Foxtrick.modules["LoyaltyDisplay"].replacePercentageImage(thisPlayerInfo, playersNode[p]);
 					}
 				}	
 			});
 				
 		} else {
 			Foxtrick.Pages.Player.getPlayer(doc, Foxtrick.Pages.Player.getId(doc), function(player){
-				replacePercentageImage(player, doc.getElementById('mainBody'));
+				Foxtrick.modules["LoyaltyDisplay"].replacePercentageImage(player, doc.getElementById('mainBody'));
 			});
 		}
 	}
