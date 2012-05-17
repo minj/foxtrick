a46296293354e6e2561173061a13f163158c6d6c
CatzHoek <catzhoek@gmail.com>
2012-05-17 13:29:01 +0000

    staff-marker.js : Okay, uncompressed json for HY for now
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9649 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/staff-marker.js
+++ content/forum/staff-marker.js
@@ -54,8 +54,8 @@ Foxtrick.modules["StaffMarker"]={
 		];
 		
 		//add HY json hosted on google code
-		if (FoxtrickPrefs.isModuleOptionEnabled("StaffMarker","HT-Youthclub"))
-			uris.push(Foxtrick.DataPath + "staff/hy.json.zip");
+		//if (FoxtrickPrefs.isModuleOptionEnabled("StaffMarker","HT-Youthclub"))
+		//	uris.push(Foxtrick.DataPath + "staff/hy.json.zip");
 
 		// counter of URI remaining to fetch
 		var todo = uris.length;
@@ -81,26 +81,24 @@ Foxtrick.modules["StaffMarker"]={
 			}, 3);
 		}, uris);
 
-		// leaving this in because it's a perfect example for the future
-
 		// hty server delivers gzipped and the browser itself handles that
-		//var unzipped_uris = [];
-		// if (FoxtrickPrefs.isModuleOptionEnabled("StaffMarker","HT-Youthclub")) {
-		// 	unzipped_uris.push("http://static.hattrick-youthclub.org/resources/foxtrick/team.json");
-		// 	++todo;
-		// }
+		var unzipped_uris = [];
+		if (FoxtrickPrefs.isModuleOptionEnabled("StaffMarker","HT-Youthclub")) {
+			unzipped_uris.push(Foxtrick.DataPath + "staff/hy.json");
+			++todo;
+		}
 		// counter of URI remaining to fetch
-		// Foxtrick.map(function(uri) {
-		// 	Foxtrick.get(uri)("success", function(text) {
-		// 		Foxtrick.log('parse ', uri);
-		// 		parseMarkers(text);
-		// 		FoxtrickPrefs.setString("Markers."+uri, text);							
-		// 	})("failure", function(code) {
-		// 		Foxtrick.log("Failure loading file: " + uri, ". Using cached markers.");
-		// 		var text =  FoxtrickPrefs.getString("Markers."+uri);
-		// 		parseMarkers(text);
-		// 	});
-		// }, unzipped_uris);
+		Foxtrick.map(function(uri) {
+			Foxtrick.get(uri)("success", function(text) {
+				Foxtrick.log('parse ', uri);
+				parseMarkers(text);
+				FoxtrickPrefs.setString("Markers."+uri, text);							
+			})("failure", function(code) {
+				Foxtrick.log("Failure loading file: " + uri, ". Using cached markers.");
+				var text =  FoxtrickPrefs.getString("Markers."+uri);
+				parseMarkers(text);
+			});
+		}, unzipped_uris);
 	},
 
 	run : function(doc) {
