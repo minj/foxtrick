9c8435e7decb5f012f3bc12bc68a915cd1fa367c
CatzHoek <catzhoek@gmail.com>
2012-05-23 23:11:40 +0000

    match-income.js : Support for ticket price changes and related release notes.
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.9.3@9721 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/match-income.js
+++ content/matches/match-income.js
@@ -8,8 +8,8 @@
 Foxtrick.modules["MatchIncome"]={
 	MODULE_CATEGORY : Foxtrick.moduleCategories.MATCHES,
 	PAGES : ['match'],
-
 	run : function(doc) {
+
 		var sidebar = doc.getElementById('sidebar');
 		var sidebarBoxes = sidebar.getElementsByClassName('sidebarBox');
 		var isSoldSeats = function(n) {
@@ -30,13 +30,36 @@ Foxtrick.modules["MatchIncome"]={
 		var soldSeatBox = Foxtrick.filter(isSoldSeats, sidebarBoxes)[0];
 		if (!soldSeatBox) // cannot find sold seat boxes
 			return;
+		
+		//find correct price for match
+		var prices = [
+		{ from: "01.01.1990 00:00", until: "10.06.2012 23:59", terraces : 6.5, basicSeats : 9.5, seatsUnderRoof : 18, vip : 32.5 },
+		{ from: "11.06.2012 00:00", until: null, terraces : 7, basicSeats : 10, seatsUnderRoof : 19, vip : 35 },
+		];
 
+		var matchDate = Foxtrick.util.time.getDateFromText(doc.getElementsByClassName("date")[0].textContent);
+		//use last if we find nothing
+		var priceIdx = prices.length - 1;
+		for(var i = 0; i < prices.length; i++){
+			var from = Foxtrick.util.time.getDateFromText(prices[i].from);
+			var until = Foxtrick.util.time.getDateFromText(prices[i].until);
+			if(until != null){
+				var already = matchDate.getTime() - from.getTime();
+				var upcoming = until.getTime() - matchDate.getTime();
+				if(already >= 0 && upcoming >= 0){
+					priceIdx = i;
+					break;
+				}
+			}
+		}
+		var isCup = doc.getElementById("mainBody").getElementsByClassName("matchCup").length > 0?true:false;
+		
 		var table = soldSeatBox.getElementsByTagName('table')[0];
 		var tbody = table.getElementsByTagName('tbody')[0];
-		var sum = Foxtrick.trimnum(table.rows[0].cells[1].textContent)*6.5
-			+ Foxtrick.trimnum(table.rows[1].cells[1].textContent)*9.5
-			+ Foxtrick.trimnum(table.rows[2].cells[1].textContent)*18
-			+ Foxtrick.trimnum(table.rows[3].cells[1].textContent)*32.5;
+		var sum = Foxtrick.trimnum(table.rows[0].cells[1].textContent)	*	prices[priceIdx].terraces *(isCup?(2.0/3.0):1)
+			+ Foxtrick.trimnum(table.rows[1].cells[1].textContent)		*	prices[priceIdx].basicSeats *(isCup?(2.0/3.0):1)
+			+ Foxtrick.trimnum(table.rows[2].cells[1].textContent)		*	prices[priceIdx].seatsUnderRoof *(isCup?(2.0/3.0):1)
+			+ Foxtrick.trimnum(table.rows[3].cells[1].textContent)		*	prices[priceIdx].vip *(isCup?(2.0/3.0):1);
 
 		// convert to local currency
 		sum /= Foxtrick.util.currency.getRate();
