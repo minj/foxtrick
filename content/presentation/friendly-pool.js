1e97d44235a9823063c7a8f13a87ae7d48cc8bc2
convincedd <convincedd@gmail.com>
2012-05-26 23:44:36 +0000

    friendly-pool: missing parts and some tweaking (guess work)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9750 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/friendly-pool.js
+++ content/presentation/friendly-pool.js
@@ -11,6 +11,9 @@ Foxtrick.modules["FriendlyPool"]={
 	CSS : Foxtrick.InternalPath + "resources/css/friendly-pool.css",
 
 	run : function(doc) {
+		var leagueSelect = doc.getElementById("ctl00_ctl00_CPContent_CPMain_ddlPoolLeagues");
+		if (!leagueSelect)
+			return;
 		var ownteamid = Foxtrick.util.id.getOwnTeamId();
 		var owncountryid = Foxtrick.util.id.getOwnLeagueId();
 		var parameters = [
@@ -28,23 +31,34 @@ Foxtrick.modules["FriendlyPool"]={
 					//destCell.title = errorText;
 					return;
 				}
+				
 				var home = {};
 				var LeagueIds = xml.getElementsByTagName("HomeFlags")[0].getElementsByTagName("LeagueId");
 				for (var i=0; i<LeagueIds.length; ++i)
 					home[LeagueIds[i].textContent]=true;
+
 				var away = {};
 				var LeagueIds = xml.getElementsByTagName("AwayFlags")[0].getElementsByTagName("LeagueId");
 				for (var i=0; i<LeagueIds.length; ++i)
 					away[LeagueIds[i].textContent]=true;
-				var leagueSelect = doc.getElementById("ctl00_ctl00_CPContent_CPMain_ddlPoolLeagues");
+
 				var options = leagueSelect.getElementsByTagName("option");
 				for (var i=0;i<leagueSelect.length;++i) {
-					if (home[leagueSelect[i].getAttribute('value')])
+					if (home[leagueSelect[i].getAttribute('value')]
+						&& away[leagueSelect[i].getAttribute('value')]) {
+						Foxtrick.addClass(leagueSelect[i],'ft-home ft-away');
+						leagueSelect[i].setAttribute('title',Foxtrickl10n.getString('matches.playedHomeAway'));
+					}
+					else if (home[leagueSelect[i].getAttribute('value')]) {
 						Foxtrick.addClass(leagueSelect[i],'ft-home');
-					if (away[leagueSelect[i].getAttribute('value')])
+						leagueSelect[i].setAttribute('title',Foxtrickl10n.getString('matches.playedHome'));
+					}
+					else if (away[leagueSelect[i].getAttribute('value')]) {
 						Foxtrick.addClass(leagueSelect[i],'ft-away');
-					if (leagueSelect[i].getAttribute('value') == owncountryid)
-					Foxtrick.addClass(leagueSelect[i],'ft-own');
+						leagueSelect[i].setAttribute('title',Foxtrickl10n.getString('matches.playedAway'));
+					}
+					else if (leagueSelect[i].getAttribute('value') == owncountryid)
+						Foxtrick.addClass(leagueSelect[i],'ft-own');
 				}
 		});
 	}
