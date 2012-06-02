fcf847971bfd5c27396033acba276035fbf10df1
CatzHoek <catzhoek@gmail.com>
2012-06-02 23:03:25 +0000

    tabs-test.js : fix for newbs
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9825 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/tabs-test.js
+++ content/presentation/tabs-test.js
@@ -3,7 +3,7 @@ Foxtrick.modules["TabsTest"]={
 	PAGES : ['all'],
 	lorum : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
 	ipsum : "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
-	nightly: "You are a nighly user, found a bug? Report it please. If you don't know where to report you shouldn't use this version.",
+	nightly: "You are a nightly user, found a bug? Report it please. If you don't know where to report you shouldn't use this version.",
 	run : function(doc) {
 		if(!Foxtrick.util.tabs.hasTabSupport(doc))
 			return;
@@ -14,7 +14,7 @@ Foxtrick.modules["TabsTest"]={
 		Foxtrick.util.tabs.add(doc, "Main", { alt: "Main", title: "Main", src: "/Img/Icons/cross_small.png"}, "chpp-tab-main");
 		Foxtrick.util.tabs.add(doc, "Lorum" , { alt: "Lorum", title: "Lorum", src: "/Img/Icons/cross_small.png"}, "ft-tab-test");
 		Foxtrick.util.tabs.add(doc, "Ipsum", { alt: "Ipsum", title: "Ipsum", src: "/Img/Icons/cross_small.png"} , "ft-tab-test2");
-		Foxtrick.util.tabs.add(doc, "Nightly", { alt: "Ipsum", title: "Ipsum", src: "/Img/Icons/cross_small.png"} , "ft-tab-nightly");
+		Foxtrick.util.tabs.add(doc, "Read me", { alt: "Ipsum", title: "Ipsum", src: "/Img/Icons/cross_small.png"} , "ft-tab-nightly");
 
 		var div1 = doc.createElement("div");
 		div1.textContent = this.lorum;
