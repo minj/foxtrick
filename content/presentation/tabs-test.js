d8c32668fa3e4e9c60491a3b693c683c66a9556e
CatzHoek <catzhoek@gmail.com>
2012-06-03 14:57:48 +0000

    tabs.js : Changed to less intrusive approach. No more containers for hattrick content.
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9831 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/tabs-test.js
+++ content/presentation/tabs-test.js
@@ -4,34 +4,34 @@ Foxtrick.modules["TabsTest"]={
 	lorum : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
 	ipsum : "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 	nightly: "You are a Foxtrick nightly user, found a bug? Report it please. If you don't know where to report you shouldn't use this version.\n Known issues: Finances Page, MatchReport.",
+	NICE: -1,
 	run : function(doc) {
 		if(!Foxtrick.util.tabs.hasTabSupport(doc))
 			return;
 
-
 		Foxtrick.util.tabs.initialize(doc);
 
-		Foxtrick.util.tabs.add(doc, "Main", { alt: "Main", title: "Main", src: "/Img/Icons/cross_small.png"}, "chpp-tab-main");
+		Foxtrick.util.tabs.add(doc, "Main", { alt: "Main", title: "Main", src: "/Img/Icons/cross_small.png"}, "tab-main");
 		Foxtrick.util.tabs.add(doc, "Lorum" , { alt: "Lorum", title: "Lorum", src: "/Img/Icons/cross_small.png"}, "ft-tab-test");
 		Foxtrick.util.tabs.add(doc, "Ipsum", { alt: "Ipsum", title: "Ipsum", src: "/Img/Icons/cross_small.png"} , "ft-tab-test2");
 		Foxtrick.util.tabs.add(doc, "Read me", { alt: "Read me", title: "Read me", src: "/Img/Icons/cross_small.png"} , "ft-tab-nightly");
 
 		var div1 = doc.createElement("div");
 		div1.textContent = this.lorum;
-		div1.setAttribute("id", "ft-tab-test");
+		div1.setAttribute("tab", "ft-tab-test");
 
 		var div2 = doc.createElement("div");
 		div2.textContent = this.ipsum;
-		div2.setAttribute("id", "ft-tab-test2");
+		div2.setAttribute("tab", "ft-tab-test2");
 
 		var div3 = doc.createElement("div");
 		div3.textContent = this.nightly;
-		div3.setAttribute("id", "ft-tab-nightly");
+		div3.setAttribute("tab", "ft-tab-nightly");
 
 		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div1);
 		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div2);
 		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div3);
 
-		Foxtrick.util.tabs.show(doc, "chpp-tab-main");
+		Foxtrick.util.tabs.show(doc, "tab-main");
 	}
 }
--- content/util/tabs.js
+++ content/util/tabs.js
@@ -6,46 +6,51 @@ Foxtrick.util.tabs.initialize = function(doc){
 	if(!Foxtrick.util.tabs.hasTabSupport(doc))
 		return;
 
-	var header = doc.getElementsByTagName("h1")[0];
-	var tabs = doc.getElementById('tab');
-
-	//copy elements in our container
+	var header = doc.getElementsByTagName("h1")[0];	
 	var hasByLine = Foxtrick.hasClass(header, "hasByline");
 	var byline = doc.getElementsByClassName("byline")[0];
-	var start = header.nextSibling;
+	var contentNode = header.nextSibling;
 	if(hasByLine && byline)
-		start = byline.nextSibling;
-
-	var container = doc.createElement("div");
-	container.setAttribute('id','chpp-tab-main');
-	var next = start.nextSibling;
-	while(start){
-		next = start.nextSibling;
-		container.appendChild(start);
-		start = next;
-	}
+		contentNode = byline.nextSibling;
 
 	//create tab bar or pull existing one out of subNodes
+	var tabs = doc.getElementById('tab');
 	if(!tabs)
 		Foxtrick.util.tabs._create(doc);
 	else {
 		Foxtrick.log("Foxtrick.util.tabs.initialize : pulling out existing tab");
-		header.parentNode.appendChild(tabs);
+		header.parentNode.insertBefore(tabs, contentNode);
 	}
-	header.parentNode.appendChild(container);
+
+	//mark everything that's already there as main tab content
+	while(contentNode){
+		try{
+			var tab = contentNode.getAttribute("tab");
+			if(!tab)
+				contentNode.setAttribute("tab", "tab-main");
+		}
+		catch(e){
+			Foxtrick.log("TextNode:", contentNode);
+		}
+		contentNode = contentNode.nextSibling;
+	}
 }
 
 //private
 Foxtrick.util.tabs._create = function(doc){
 	var header = doc.getElementsByTagName("h1")[0];
-	var tabs = doc.getElementById('tab');
-
 	if(!header)
 		return;
-	
+
+	var hasByLine = Foxtrick.hasClass(header, "hasByline");
+	var byline = doc.getElementsByClassName("byline")[0];
+	var contentNode = header.nextSibling;
+	if(hasByLine && byline)
+		contentNode = byline.nextSibling;
+
 	var list = doc.createElement("ul");
 	list.setAttribute("id", "tab");
-	header.parentNode.appendChild(list);
+	header.parentNode.insertBefore(list, contentNode);
 }
 
 Foxtrick.util.tabs.hasTabSupport = function(doc) {
@@ -90,11 +95,14 @@ Foxtrick.util.tabs.show = function(doc, id){
 	//show
 	var content = tabs.nextSibling;
 	while(content){
-		if(content.getAttribute('id') == id)
-			content.setAttribute('style','display:block;');
-		else
-			content.setAttribute('style','display:none;');
-
+		try{
+			if(content.getAttribute('tab') == id)
+				Foxtrick.removeClass(content, "hidden");
+			else
+				Foxtrick.addClass(content, "hidden");
+		} catch(e){
+			Foxtrick.log("TextNode:", content);
+		}
 		content = content.nextSibling;
 	}
 }
