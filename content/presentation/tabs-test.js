6540a24bc3241adff8a6b188149385c39ba3142d
CatzHoek <catzhoek@gmail.com>
2012-06-04 13:06:03 +0000

    tabs : Some commit
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9837 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/tabs-test.js
+++ content/presentation/tabs-test.js
@@ -1,45 +1,32 @@
 Foxtrick.modules["TabsTest"]={
 	MODULE_CATEGORY : Foxtrick.moduleCategories.MATCHES,
 	PAGES : ['all'],
-	lorum : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-	ipsum : "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
-	nightly: "You are a Foxtrick nightly user, found a bug? Report it please. If you don't know where to report you shouldn't use this version.\n Known issues: Finances Page, MatchReport.",
-	NICE: -1,
+	OPTIONS: ['tabify'],
+	nightly: "This is a proof of concept module. Please report any mayor problems.",
+	NICE: 50,
 	run : function(doc) {
 		if(!Foxtrick.util.tabs.hasTabSupport(doc))
 			return;
-
 		Foxtrick.util.tabs.initialize(doc);
+		Foxtrick.util.tabs.addHandle(doc, "Main", { alt: "Main", title: "Main", src: "/Img/Icons/cross_small.png"}, "tab-main");
+		Foxtrick.util.tabs.addHandle(doc, "Read me", { alt: "Read me", title: "Read me", src: "/Img/Icons/cross_small.png"} , "ft-tab-readme");
 
-		Foxtrick.util.tabs.add(doc, "Main", { alt: "Main", title: "Main", src: "/Img/Icons/cross_small.png"}, "tab-main");
-		Foxtrick.util.tabs.add(doc, "Lorum" , { alt: "Lorum", title: "Lorum", src: "/Img/Icons/cross_small.png"}, "ft-tab-test");
-		Foxtrick.util.tabs.add(doc, "Ipsum", { alt: "Ipsum", title: "Ipsum", src: "/Img/Icons/cross_small.png"} , "ft-tab-test2");
-		Foxtrick.util.tabs.add(doc, "Read me", { alt: "Read me", title: "Read me", src: "/Img/Icons/cross_small.png"} , "ft-tab-nightly");
+		var div = doc.createElement("div");
+		div.textContent = this.nightly;
+		div.setAttribute("tab", "ft-tab-readme");
 
-		var div1 = doc.createElement("div");
-		div1.textContent = this.lorum;
-		div1.setAttribute("tab", "ft-tab-test");
-
-		var div2 = doc.createElement("div");
-		div2.textContent = this.ipsum;
-		div2.setAttribute("tab", "ft-tab-test2");
-
-		var div3 = doc.createElement("div");
-		div3.textContent = this.nightly;
-		div3.setAttribute("tab", "ft-tab-nightly");
-
-		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div1);
-		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div2);
-		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div3);
-
-		Foxtrick.util.tabs.show(doc, "tab-main");
+		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div);
+		Foxtrick.util.tabs.addElementToTab(doc, div, "ft-tab-readme");
 
 		var container = doc.getElementsByTagName("h1")[0].parentNode;
-
+		Foxtrick.log("addMutationEventListener");
 		//match report
 		Foxtrick.addMutationEventListener(container, "DOMNodeInserted", function(){
 			Foxtrick.util.tabs.initialize(doc);
-		}, false); 
+			Foxtrick.util.tabs.tabify(doc);
+		}, false);
 
+		Foxtrick.util.tabs.tabify(doc);
+		Foxtrick.util.tabs.show(doc, "tab-main");
 	}
 }
--- content/util/tabs.js
+++ content/util/tabs.js
@@ -18,19 +18,21 @@ Foxtrick.util.tabs.initialize = function(doc){
 	if(!tabs)
 		Foxtrick.util.tabs._create(doc);
 	else {
-		Foxtrick.log("Foxtrick.util.tabs.initialize : pulling out existing tab");
 		header.parentNode.insertBefore(tabs, contentNode);
 	}
 
+	contentNode = doc.getElementById("tab").nextSibling;
 	//mark everything that's already there as main tab content
 	while(contentNode){
-		try{
-			var tab = contentNode.getAttribute("tab");
-			if(!tab)
-				contentNode.setAttribute("tab", "tab-main");
-		}
-		catch(e){
-			//Foxtrick.log("TextNode:", contentNode);
+		if(contentNode.nodeType != Foxtrick.NodeTypes.TEXT_NODE){
+			try {
+				var tab = contentNode.getAttribute("tab");
+				if(!tab){
+					Foxtrick.util.tabs.addToAttribute(contentNode, "tab", "tab-main");
+					Foxtrick.addClass(contentNode, "tab-content");
+				}
+			}
+			catch(e){}
 		}
 		contentNode = contentNode.nextSibling;
 	}
@@ -57,15 +59,20 @@ Foxtrick.util.tabs.hasTabSupport = function(doc) {
 	return doc.getElementsByTagName("h1")[0];
 }
 
-Foxtrick.util.tabs.add = function(doc, title, icon, shows){
+Foxtrick.util.tabs.addHandle = function(doc, title, icon, shows){
 	if(!Foxtrick.util.tabs.hasTabSupport(doc))
 		return;
 
 	var tabs = doc.getElementById("tab");
 	var li = doc.createElement("li");
+	li.id = shows + "-handle";
 	var link = doc.createElement("a");
 	link.href = "javascript:void(0);";
 	link.setAttribute("disabled", "disabled");
+	var _shows = link.getAttribute("shows");
+	if(_shows)
+		shows = _shows + ',' + shows;
+
 	link.setAttribute("shows", shows);
 	if(icon){
 		Foxtrick.addImage(doc, link, icon); 
@@ -86,23 +93,145 @@ Foxtrick.util.tabs.show = function(doc, id){
 	var tabs = doc.getElementById("tab");
 	var links = tabs.getElementsByTagName("a");
 	for(var i = 0; i < links.length; i++){
-		if(links[i].getAttribute("shows") == id)
+		if(links[i].getAttribute("shows").search(id) > -1)
 			Foxtrick.addClass(links[i], "active");
 		else
 			Foxtrick.removeClass(links[i], "active");
 	}
-
 	//show
-	var content = tabs.nextSibling;
-	while(content){
-		try{
-			if(content.getAttribute('tab') == id)
-				Foxtrick.removeClass(content, "hidden");
-			else
-				Foxtrick.addClass(content, "hidden");
-		} catch(e){
-			//Foxtrick.log("TextNode:", content);
+	var tabContents = doc.getElementsByClassName("tab-content");
+	for(var i = 0; i < tabContents.length; ++i){
+		var tabs = tabContents[i].getAttribute("tab");
+
+		if(tabs.search(id) > -1){
+			Foxtrick.log("show");
+			Foxtrick.removeClass(tabContents[i], "hidden");
+		} else {
+			Foxtrick.log("hide");
+			Foxtrick.addClass(tabContents[i], "hidden");		
 		}
-		content = content.nextSibling;
+	}
+}
+Foxtrick.util.tabs.addToAttribute = function(elem, attrib, value){
+	var _attrib = elem.getAttribute(attrib);
+	if(_attrib  && _attrib.search(value) == -1)
+		elem.setAttribute(attrib,_attrib +"," + value);	
+	else
+		elem.setAttribute(attrib, value);	
+} 
+
+Foxtrick.util.tabs.addElementToTab = function(doc, elem, tab){
+	Foxtrick.addClass(elem, "tab-content");
+	Foxtrick.util.tabs.addToAttribute(elem, "tab", tab);
+	var parent = doc.getElementsByTagName("h1")[0].parentNode;
+	var elemParent = elem.parentNode;
+	while(elemParent != parent){
+		Foxtrick.util.tabs.addToAttribute(elemParent, "tab", tab);
+		elemParent = elemParent.parentNode;
+	}
+}
+
+Foxtrick.util.tabs.tabify = function(doc){
+	var parent = doc.getElementsByTagName("h1")[0].parentNode;
+	var h2s = parent.getElementsByTagName("h2");
+	for(var i = 0; i < h2s.length; ++i){
+		if(Foxtrick.hasClass(h2s[i], "info"))
+			continue;
+		
+		var label = h2s[i].textContent.replace(/ /g, "");
+		if(doc.getElementById("tab-" + label + "-handle"))
+			continue;
+
+		Foxtrick.util.tabs.addHandle(doc, h2s[i].textContent, null, "tab-" + label);
+		Foxtrick.util.tabs.addH2ToTab(doc, h2s[i], "tab-" + label);
+		Foxtrick.addClass(h2s[i], "tab-content");
+	}
+	Foxtrick.util.tabs.initialize(doc);
+	Foxtrick.util.tabs.show(doc, "tab-main");
+}
+
+Foxtrick.util.tabs.addH2ToTab = function(doc, h2, tab) {
+	var setupHeaderSiblings = function(el, tab) {
+
+		Foxtrick.addClass(el, "tab-content");
+
+		var parent = el.parentNode;
+		el = el.nextSibling;
+		var forumThreads = {}, numUnread = 0, idString='';
+		while ( el ) { 
+			// if text node, wrap in span on first encounter
+			if (el.nodeType ==  Foxtrick.NodeTypes.TEXT_NODE) {
+				if (Foxtrick.trim(el.nodeValue) != "") {
+					var target = el.nextSibling;
+					var span =  doc.createElement('span');
+					span.appendChild(el);
+					el = parent.insertBefore(span, target);
+					Foxtrick.log("textnode in span", el);
+				} else {
+					el = el.nextSibling;
+					continue;
+				}
+			}
+
+			// stop with next header or dedicated parentNode mainBox
+			if ( (el.className == 'mainBox' && el.getElementsByTagName('h2')[0] != undefined)
+				|| el.nodeName == 'H1'
+				|| (el.nodeName == 'H2' && !Foxtrick.hasClass(el, 'info'))
+				|| (el.getElementsByTagName('h2')[0] !== undefined && !Foxtrick.hasClass(el.getElementsByTagName('h2')[0],'info') )) {
+				break;
+			}
+
+			// // don't show which is hidden originally, eg ft-forum-preview-area 
+			// if (el.id == 'ft-forum-preview-area' && foxtrick.hasclass(el, 'hidden')) {
+			// 	el = el.nextsibling;
+			// 	continue;
+			// }
+			Foxtrick.addClass(el, "tab-content");
+			Foxtrick.util.tabs.addToAttribute(el, "tab", tab);
+
+			// count new forum postings
+			if ( Foxtrick.hasClass(el,'hidden') && el.getElementsByClassName('fplThreadInfo')[0] != undefined ) {
+				var rows = el.getElementsByClassName('fplThreadInfo');
+				Foxtrick.map(function(n) {
+					var unread = n.getElementsByClassName('highlight')[0];
+					if (unread !== undefined) {
+						var tid = unread.getAttribute('onclick').match(/'read\|(\d+)'/)[1];
+						if (!forumThreads[tid])
+							numUnread += Number(unread.textContent);
+						forumThreads[tid] = true;
+						if (idString)
+							idString += ',';
+						idString += tid;
+						
+						//idString += "__doPostBack('ctl00$ctl00$CPContent$CPMain$updLatestThreads','read|" + tid + "');\n"
+					}
+				}, rows);
+			}
+			el = el.nextSibling;
+		}
+		
+		// show new forum postings
+		if (numUnread && h2.getElementsByClassName('highlight')[0] == undefined) {
+			var page_num = 0;
+			var pages = h2.parentNode.getElementsByClassName('page');
+			for (var i=0; i<pages.length; ++i) {
+				if (pages[i].getAttribute('disabled')=='disabled') {
+					page_num = Number(pages[i].textContent)-1;
+				}
+			}
+			h2.appendChild(doc.createTextNode(' '));
+			var span = doc.createElement('span');
+			span.className = 'highlight ft-dummy';
+			span.textContent = '('+numUnread+')';
+			Foxtrick.makeFeaturedElement(span, Foxtrick.modules.HeaderToggle);
+			h2.appendChild(span);
+		}
+	};
+
+	Foxtrick.util.tabs.addElementToTab(doc, h2, tab);
+	setupHeaderSiblings(h2, tab);
+	if (h2.parentNode.nodeName=='TD') {
+		// in tables we also toggle sibling rows
+		setupHeaderSiblings(h2.parentNode.parentNode, tab);
 	}
 }
