a13eb6a9fe0bddf0d10eed2abb7923d3fab0477f
foxtrickdev <foxtrickdev@gmail.com>
2012-06-05 13:21:25 +0000

    *automated* resource zip update
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@9877 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/commit-resources.sh
+++ maintainer/commit-resources.sh
@@ -8,7 +8,7 @@ RES_FOLDER="../res/*.json ../res/staff/*.json"
 #zip resources
 for File in $RES_FOLDER; do
 	echo $File
-	zip -fv $File.zip $File
+	zip -f $File.zip $File
 done
 
 #commit res zips to svn
