54f7d28793ec713eec48602f4532e7c4336aa0e3
CatzHoek <catzhoek@gmail.com>
2012-06-06 14:18:15 +0000

    @0.10.0 : dom.js : It's supposed to crash, but that makes more sense.
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.10.0@9907 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/dom.js
+++ content/util/dom.js
@@ -7,14 +7,15 @@
 if (!Foxtrick) var Foxtrick = {};
 
 Foxtrick.createFeaturedElement = function(doc, module, type) {
-	var node = doc.createElement(type);
-	node.className = 'ft-dummy';
 	if(module && module.MODULE_NAME && module.MODULE_CATEGORY){
+		var node = doc.createElement(type);
+		node.className = 'ft-dummy';
 		if (FoxtrickPrefs.getBool("featureHighlight"))
 			node.title = module.MODULE_NAME + ' (' + Foxtrickl10n.getString("tab."+module.MODULE_CATEGORY) + '): ' + FoxtrickPrefs.getModuleDescription(module.MODULE_NAME);
 		return node;
 	} else {
 		Foxtrick.log("Incorrect usage of Foxtrick.createFeaturedElement. typeof(module) = ", typeof(module));
+		return null;
 	}
 };
 
