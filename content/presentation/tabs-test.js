a7a1363c274405a4710a6786dc7c3e287ef78341
CatzHoek <catzhoek@gmail.com>
2012-06-13 10:50:41 +0000

    @0100 tabs-test.js : happy r10k party too
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.10.0@10001 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/tabs-test.js
+++ content/presentation/tabs-test.js
@@ -1,8 +1,8 @@
 Foxtrick.modules["TabsTest"]={
-	MODULE_CATEGORY : Foxtrick.moduleCategories.MATCHES,
+	MODULE_CATEGORY : Foxtrick.moduleCategories.PRESENTATION,
 	PAGES : ['all'],
-	OPTIONS: ['tabify'],
-	nightly: "This is a proof of concept module by Foxtrick. Please report any problems or weird things to the Foxtrick developer.",
+	OPTIONS: ['Tabify'],
+	nightly: "Today we celebrate r10000!",
 	NICE: 50,
 	run : function(doc) {
 		if(!Foxtrick.util.tabs.hasTabSupport(doc))
@@ -10,16 +10,25 @@ Foxtrick.modules["TabsTest"]={
 		Foxtrick.util.tabs.initialize(doc);
 
 		var h1 = doc.getElementsByTagName("h1")[0].textContent;
+		
+		//Foxtrick.util.tabs.addHandle(doc, h1, { alt: h1, title: h1, src: "/Img/Icons/cross_small.png"}, "tab-main");
+		var main_handle = Foxtrick.util.tabs.addHandle(doc, h1, { alt: h1, title: h1, src: "http://www.hattrick.org/favicon.ico"}, "tab-main");
+		if(main_handle)
+			Foxtrick.addClass(main_handle, "tab-hattrick");
 
-		Foxtrick.util.tabs.addHandle(doc, h1, { alt: h1, title: h1, src: "/Img/Icons/cross_small.png"}, "tab-main");
-		Foxtrick.util.tabs.tabify(doc);
-		Foxtrick.util.tabs.addHandle(doc, "Read me", { alt: "Read me", title: "Read me", src: "/Img/Icons/cross_small.png"} , "ft-tab-readme");
+		if (FoxtrickPrefs.isModuleOptionEnabled("TabsTest", "Tabify"))
+			Foxtrick.util.tabs.tabify(doc);
+		var ft_handle = Foxtrick.util.tabs.addHandle(doc, "r10000!", { alt: h1, title: h1, src: Foxtrick.InternalPath + "resources/img/staff/foxtrick.png"} , "ft-tab-readme");
+		if(ft_handle)
+			Foxtrick.addClass(ft_handle, "ft-tab-foxtrick");
 
 		var div = doc.createElement("div");
-		div.textContent = this.nightly;
-		div.setAttribute("tab", "ft-tab-readme");
-		Foxtrick.addClass(div, "ft-tab-custom")
-
+		var r1000h2 = doc.createElement("h2");
+		r1000h2.textContent = "Today we celebrate r10000"
+		var t = doc.createTextNode("Have a beer at your expense ;) Thanks for all your testing, suggestions and for doing translations etc.!");
+		div.appendChild(r1000h2);
+		div.appendChild(t);
+		Foxtrick.addClass(div, "ft-tab-custom");
 		doc.getElementsByTagName("h1")[0].parentNode.appendChild(div);
 		Foxtrick.util.tabs.addElementToTab(doc, div, "ft-tab-readme");
 
@@ -28,11 +37,10 @@ Foxtrick.modules["TabsTest"]={
 		//match report
 		Foxtrick.addMutationEventListener(container, "DOMNodeInserted", function(){
 			Foxtrick.util.tabs.initialize(doc);
-			Foxtrick.util.tabs.tabify(doc);
+			if (FoxtrickPrefs.isModuleOptionEnabled("TabsTest", "Tabify"))
+				Foxtrick.util.tabs.tabify(doc);
 		}, false);
 
-		Foxtrick.log("fix display");
 		Foxtrick.util.tabs.show(doc, "tab-main");
-		
 	}
 }
