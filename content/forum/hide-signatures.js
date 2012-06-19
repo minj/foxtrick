baa49b273245f7fcfce2aa54b09b9df2126fa64f
CatzHoek <catzhoek@gmail.com>
2012-06-19 14:00:16 +0000

    hide-signatures.js : Fixed for truncated signatures
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10025 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/hide-signatures.js
+++ content/forum/hide-signatures.js
@@ -9,41 +9,59 @@ Foxtrick.modules["HideSignatures"]={
 	MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
 	PAGES : ['forumViewThread'],
 	run : function(doc) {
-		var elems = doc.getElementsByClassName("signature");
-		for(var i=0; i < elems.length; i++) {
-			//the signature
-			Foxtrick.addClass(elems[i], "hidden");
-			elems[i].setAttribute("id", "foxtrick-signature-" + i);
+		var hideSignatures = function (elems){
+			for(var i=0; i < elems.length; i++) {
+				//the signature
+				Foxtrick.addClass(elems[i], "hidden");
+				var sigId = elems[i].id;
+				if(!sigId){
+					elems[i].setAttribute("id", "foxtrick-signature-" + i);
+					sigId = i;
+				}
+				//the button
+				var showSigLink = Foxtrick.createFeaturedElement(doc, Foxtrick.modules["HideSignatures"], "a");
+				Foxtrick.addClass(showSigLink, "foxtrick-signaturetoggle");
+				showSigLink.setAttribute("style", "cursor: pointer;");
+				showSigLink.setAttribute("title", Foxtrickl10n.getString('HideSignatures.signaturetoggle') );
+				showSigLink.setAttribute("id","foxtrick-st-link-" + sigId);
+				var text = doc.createTextNode( Foxtrickl10n.getString('HideSignatures.signaturetoggle') );
+				showSigLink.appendChild( text );
 
-			//the button
-			var showSigLink = Foxtrick.createFeaturedElement(doc, this, "a");
-			Foxtrick.addClass(showSigLink, "foxtrick-signaturetoggle");
-			showSigLink.setAttribute("style", "cursor: pointer;");
-			showSigLink.setAttribute("title", Foxtrickl10n.getString('HideSignatures.signaturetoggle') );
-			showSigLink.setAttribute("id","foxtrick-st-link" + i);
-
-			var text = doc.createTextNode( Foxtrickl10n.getString('HideSignatures.signaturetoggle') );
-			showSigLink.appendChild( text );
-
-			try {
-				// append the show sig link to the right footer
-				var cfWrapper = elems[i].parentNode.parentNode.parentNode;
-				var cfFooter = cfWrapper.getElementsByClassName("cfFooter")[0];
-				var floatRight = cfFooter.getElementsByClassName("float_right")[0];
-				floatRight.appendChild(showSigLink);
-			} catch(e){
-				Foxtrick.dump('HideSignatures: Unexpected DOM Structure', e);
-			}
-			//toogle
-			Foxtrick.listen(showSigLink, 'click', function(ev) {
 				try {
-					var id = ev.target.getAttribute("id").match(/\d+/)[0];
-					var sig = doc.getElementById("foxtrick-signature-" + id);
-					Foxtrick.toggleClass(sig, "hidden");
+					// append the show sig link to the right footer
+					var cfWrapper = elems[i].parentNode.parentNode.parentNode;
+					var cfFooter = cfWrapper.getElementsByClassName("cfFooter")[0];
+					var floatRight = cfFooter.getElementsByClassName("float_right")[0];
+					floatRight.appendChild(showSigLink);
 				} catch(e){
-					Foxtrick.dump('HideSignatures click listener error. Id: ' + id + ' ' + sig + ' ' + e + '\n');	
-				}							
-			}, false);			
+					Foxtrick.dump('HideSignatures: Unexpected DOM Structure', e);
+				}
+
+				//toogle
+				Foxtrick.listen(showSigLink, 'click', function(ev) {
+					try {
+						var id = ev.target.getAttribute("id").replace("foxtrick-st-link-","");						
+						try {
+							var sig = doc.getElementById("foxtrick-signature-" + id);
+							Foxtrick.toggleClass(sig, "hidden");
+						} catch(e){
+							Foxtrick.log
+							var sig = doc.getElementById(id);
+							Foxtrick.toggleClass(sig, "hidden");
+						}
+					} catch(e){
+						Foxtrick.dump('HideSignatures click listener error. Id: ' + id + ' ' + sig + ' ' + e + '\n');	
+					}							
+				}, false);			
+			}
 		}
+
+		//ht setting full signatures
+		var signatures = doc.getElementsByClassName("signature");
+		hideSignatures(signatures);
+
+		//ht setting truncated signatures
+		var signatures_trunc = doc.getElementsByClassName("signature-trunc");		
+		hideSignatures(signatures_trunc);
 	}
 };
