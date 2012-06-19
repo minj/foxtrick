b253828d143088643bf0955b0a52047a3058358c
CatzHoek <catzhoek@gmail.com>
2012-06-19 14:02:16 +0000

    @0100 hide-signatures.js : Fixed for truncated signatures
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.10.0@10026 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/hide-signatures.js
+++ content/forum/hide-signatures.js
@@ -2,60 +2,66 @@
 /**
  * forumhidesignature.js
  * Script which hides signatures on the forums, but shows a 'Show sig' link
- * @author smates, larsw84
+ * @author smates, larsw84, CatzHoek
  */
 
 Foxtrick.modules["HideSignatures"]={
 	MODULE_CATEGORY : Foxtrick.moduleCategories.FORUM,
-	PAGES : new Array('forumViewThread'),
-
+	PAGES : ['forumViewThread'],
 	run : function(doc) {
-		//return;
-		var p = 0;
-		var elems = doc.getElementsByTagName("div");
-		for(var i=0; i < elems.length; i++) {
-			if( elems[i].className == "signature" ||
-				elems[i].className == "signature-trunc") {
-				p++;
-				if( !doc.getElementById( "foxtrick-st-link"+p ) ) {
-					try {
-						Foxtrick.addClass(elems[i], "hidden");
-						var sigId = elems[i].id;
-						if( !sigId ) {
-							sigId = "foxtrick-signature-"+p;
-							elems[i].id = sigId;
-						}
+		var hideSignatures = function (elems){
+			for(var i=0; i < elems.length; i++) {
+				//the signature
+				Foxtrick.addClass(elems[i], "hidden");
+				var sigId = elems[i].id;
+				if(!sigId){
+					elems[i].setAttribute("id", "foxtrick-signature-" + i);
+					sigId = i;
+				}
+				//the button
+				var showSigLink = Foxtrick.createFeaturedElement(doc, Foxtrick.modules["HideSignatures"], "a");
+				Foxtrick.addClass(showSigLink, "foxtrick-signaturetoggle");
+				showSigLink.setAttribute("style", "cursor: pointer;");
+				showSigLink.setAttribute("title", Foxtrickl10n.getString('HideSignatures.signaturetoggle') );
+				showSigLink.setAttribute("id","foxtrick-st-link-" + sigId);
+				var text = doc.createTextNode( Foxtrickl10n.getString('HideSignatures.signaturetoggle') );
+				showSigLink.appendChild( text );
 
-						var showSig = [];
-						showSig[p] = doc.createElement("a");
-						showSig[p].setAttribute("id","foxtrick-st-link"+p);
-						showSig[p].title = Foxtrickl10n.getString('HideSignatures.signaturetoggle');
-						showSig[p].className="foxtrick-signaturetoggle";
-						showSig[p].textContent = Foxtrickl10n.getString('HideSignatures.signaturetoggle');
-						showSig[p] = Foxtrick.makeFeaturedElement(showSig[p], this );
-						showSig[p].setAttribute("style", "cursor: pointer;");
-						
-						Foxtrick.onClick(showSig[p], function(ev) {
-							var id = ev.target.getAttribute("id").match(/\d+/)[0];
+				try {
+					// append the show sig link to the right footer
+					var cfWrapper = elems[i].parentNode.parentNode.parentNode;
+					var cfFooter = cfWrapper.getElementsByClassName("cfFooter")[0];
+					var floatRight = cfFooter.getElementsByClassName("float_right")[0];
+					floatRight.appendChild(showSigLink);
+				} catch(e){
+					Foxtrick.dump('HideSignatures: Unexpected DOM Structure', e);
+				}
+
+				//toogle
+				Foxtrick.listen(showSigLink, 'click', function(ev) {
+					try {
+						var id = ev.target.getAttribute("id").replace("foxtrick-st-link-","");						
+						try {
 							var sig = doc.getElementById("foxtrick-signature-" + id);
 							Foxtrick.toggleClass(sig, "hidden");
-						});
-
-						// append the show sig link to the right footer
-						var cfInnerWrapper = elems[i].parentNode.parentNode;
-						var cfFooter = cfInnerWrapper.nextSibling;
-						while( cfFooter.className != "cfFooter" ) {
-							cfFooter = cfFooter.nextSibling;
+						} catch(e){
+							Foxtrick.log
+							var sig = doc.getElementById(id);
+							Foxtrick.toggleClass(sig, "hidden");
 						}
-						var divsInFooter = cfFooter.getElementsByTagName("div");
-						for(var j = 0; j < divsInFooter.length; j++) {
-							if( divsInFooter[j].className == "float_right" ) {
-								divsInFooter[j].appendChild(showSig[p]);
-							}
-						}
-					} catch(e) {Foxtrick.dump('HideSignatures ERROR ' + e + '\n');}
-				}
+					} catch(e){
+						Foxtrick.dump('HideSignatures click listener error. Id: ' + id + ' ' + sig + ' ' + e + '\n');	
+					}							
+				}, false);			
 			}
 		}
+
+		//ht setting full signatures
+		var signatures = doc.getElementsByClassName("signature");
+		hideSignatures(signatures);
+
+		//ht setting truncated signatures
+		var signatures_trunc = doc.getElementsByClassName("signature-trunc");		
+		hideSignatures(signatures_trunc);
 	}
 };
