921c48ae35116e02c8e40eb18f3ef566186503f3
CatzHoek <catzhoek@gmail.com>
2012-06-23 13:44:20 +0000

    lineup faces, the js part for replaced and highlighted players
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10061 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/match-order.js
+++ content/matches/match-order.js
@@ -100,6 +100,12 @@ Foxtrick.modules["MatchOrderInterface"]={
 				playerdivs = target.getElementsByClassName('box_substitute');				
 				for (var k=0; k<playerdivs.length; ++k) 
 					add_image(playerdivs[k]);
+				playerdivs = target.getElementsByClassName('box_highlighted');				
+				for (var k=0; k<playerdivs.length; ++k) 
+					add_image(playerdivs[k]);
+				playerdivs = target.getElementsByClassName('box_replaced');				
+				for (var k=0; k<playerdivs.length; ++k) 
+					add_image(playerdivs[k]);
 			}
 		};
 
@@ -151,7 +157,9 @@ Foxtrick.modules["MatchOrderInterface"]={
 					return;
 				} 
 				check_Specialties(doc, doc.getElementsByClassName('field')[0], playerInfo, getID, 'box_lineup');	
-				check_Specialties(doc, doc.getElementsByClassName('field')[0], playerInfo, getID, 'box_substitute');	
+				check_Specialties(doc, doc.getElementsByClassName('field')[0], playerInfo, getID, 'box_substitute');
+				check_Specialties(doc, doc.getElementsByClassName('field')[0], playerInfo, getID, 'box_highlighted');	
+				check_Specialties(doc, doc.getElementsByClassName('field')[0], playerInfo, getID, 'box_replaced');	
 			}, {teamid:teamid, current_squad:true, includeMatchInfo:true} );
 
 
