8b5e363ebe9df5354cf093cfe0a344e837a9f8d5
convincedd <convincedd@gmail.com>
2012-06-25 15:56:26 +0000

    trunk&0100. fx detection for mobile fixed
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10103 1e3807c3-0949-0410-b78e-337b627472c1

--- content/env.js
+++ content/env.js
@@ -448,19 +448,23 @@ else if (typeof(chrome) == "object") {
 else {
 	Foxtrick.arch = "Gecko";
 	Foxtrick.InternalPath = Foxtrick.ResourcePath = "chrome://foxtrick/content/";
-
-	var Cc = Components.classes;
-	var Ci = Components.interfaces;
-	var Cu = Components.utils;
-	Cu.import("resource://gre/modules/Services.jsm");
-	var appInfoID = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo).ID;
-
-	if (appInfoID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}") 
-		Foxtrick.platform = "Android";
-	else if (appInfoID == "{a23983c0-fd0e-11dc-95ff-0800200c9a66}")
+		
+	try {
+		var Cc = Components.classes;
+		var Ci = Components.interfaces;
+		var Cu = Components.utils;
+		Cu.import("resource://gre/modules/Services.jsm");
+		var appInfoID = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo).ID;
+		if (appInfoID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}") 
+			Foxtrick.platform = "Android";
+		else if (appInfoID == "{a23983c0-fd0e-11dc-95ff-0800200c9a66}")
+			Foxtrick.platform = "Mobile";
+		else 
+			Foxtrick.platform = "Firefox";  // includes SeaMonkey here
+	} catch(e) {
+		// above not working in mobile content. so it's that 
 		Foxtrick.platform = "Mobile";
-	else 
-		Foxtrick.platform = "Firefox";  // includes SeaMonkey here
+	}
 
 	if (Foxtrick.platform == "Mobile" || Foxtrick.platform == "Android") {
 		Foxtrick.chromeContext = function() {
@@ -488,7 +492,7 @@ else {
 		}
 		else // happens for fennec prefs. not needed thus ignored or it would mess up above
 			var addListener = function(name, handler) {}
-
+log(Foxtrick.platform+Foxtrick.chromeContext())
 		// fennec adapter. adapted from adblockplus for safari
 		var sandboxed = {
 			// Track tabs that make requests to the global page, assigning them
