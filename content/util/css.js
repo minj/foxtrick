7502f4dbd8a9dfdcbd2e880f85cff67d9e715060
CatzHoek <catzhoek@gmail.com>
2012-08-12 22:53:37 +0000

    @011 options_css, fix for null supplied as value
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.11.0@10419 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/css.js
+++ content/util/css.js
@@ -178,8 +178,10 @@ Foxtrick.util.css.collect_module_css = function() {
 							if (FoxtrickPrefs.isModuleOptionEnabled(module.MODULE_NAME, options[i]) && css[i]){
 								Foxtrick.cssFiles.push(css[i]);
 							}
+						} else if (css[i] == null){
+							//
 						} else {
-							alert("OPTIONS_CSS not matching OPTIONS structure")
+							alert("OPTIONS_CSS not matching OPTIONS structure: " + typeof(css[i]) + " " + module.MODULE_NAME + " " + options[i] + " " + css[i]);
 						}
 					}
 				}
