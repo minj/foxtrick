1ba700d9aa26e848dff870103ffbc258ef509d97
LA-MJ <4mr.minj@gmail.com>
2012-08-23 18:54:42 +0000

    @0110 & trunk branched glossary upload to another file
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.11.0@10480 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/crowdin-upload.sh
+++ maintainer/crowdin-upload.sh
@@ -77,21 +77,5 @@ if [ false ]; then
 	done
 fi
 	
-# get module names from foxtrick.properties file
-cat ../content/foxtrick.properties | egrep -o "^module\.[^\.]+\.desc" > module-names
-sed -i -r 's|module.||' module-names
-sed -i -r 's|.desc||' module-names
-
-# make tbx glossary from module names
-perl crowdin-module-glossary-update.pl
-
-#upload glossary
-re="$(curl \
-	-F "file=@crowdin-glossary.tbx" \
-	"$CROWDIN_URL"/upload-glossary?key="$CROWDIN_KEY" | grep -c success)"
-if [ $re -ne 1 ]; then
-  echo "crowdin-glossary failed"
-else
-  echo "crowdin-glossary ok"
-fi
+./crowdin-glossary-upload.sh
 
