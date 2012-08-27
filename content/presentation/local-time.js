500e9cf1634075a34dcc3b791caf4955555917fb
LA-MJ <4mr.minj@gmail.com>
2012-08-27 21:27:01 +0000

    @all fix local-time in firefox
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.11.0@10515 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/local-time.js
+++ content/presentation/local-time.js
@@ -25,7 +25,8 @@ Foxtrick.modules["LocalTime"]={
 		var updateTime = function() {
 			localTime.textContent = Foxtrick.util.time.buildDate(null, true, true);
 		};
-		time.addEventListener('DOMCharacterDataModified',updateTime, false);
+		time.addEventListener('DOMCharacterDataModified',updateTime, false); // Chrome changes text node (optimised)
+		time.addEventListener('DOMNodeInserted',updateTime, false); // FF creates a new node (correct behaviour)
 
 		localTime.title = Foxtrickl10n.getString("LocalTime.local.title");
 		localTime = Foxtrick.makeFeaturedElement(localTime, this);
