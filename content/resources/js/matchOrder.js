3596a7c100a0d75840895a35cb5076bb6e859c3c
LA-MJ <4mr.minj@gmail.com>
2012-10-02 14:48:18 +0000

    lintify time for injects
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10635 1e3807c3-0949-0410-b78e-337b627472c1

--- content/resources/js/matchOrder.js
+++ content/resources/js/matchOrder.js
@@ -1,35 +1,35 @@
 
 /* fixing ht function to raise and custom event after init of interface */
 ht.orders.handleDataOrg = ht.orders.handleData;
-ht.orders.handleData = function(arg){
+ht.orders.handleData = function(arg) {
 	// all org
-	ht.orders.handleDataOrg.apply(this,arguments);
+	ht.orders.handleDataOrg.apply(this, arguments);
 	// raise event after
 	var evt = document.createEvent('Event');
 	evt.initEvent('interface_ready', true, true);
-	document.getElementsByTagName("body")[0].dispatchEvent(evt);
+	document.getElementsByTagName('body')[0].dispatchEvent(evt);
 	// restore old
 	ht.orders.handleData = ht.orders.handleDataOrg;
-}
+};
 
 /* function to swap players left to right including their orientation */
 function ft_swap_positions() {
 
 	// position ids to swap
-	var swaps = [ 
-		[1,5],  // right/left WB
-		[2,4],  // right/left CD
-		[6,10], // right/left WI
-		[7,9],  // right/left IM
-		[11,13] // right/left FW
+	var swaps = [
+		[1, 5],  // right/left WB
+		[2, 4],  // right/left CD
+		[6, 10], // right/left WI
+		[7, 9],  // right/left IM
+		[11, 13] // right/left FW
 	];
-	
-	for (var i=0; i<swaps.length; ++i) {
-		var pos1 = ht.field.positions[ swaps[i][0] ];
-		var pos2 = ht.field.positions[ swaps[i][1] ];
 
-		var player1=null, player2=null, tactic1, tactic2;
-		
+	for (var i = 0; i < swaps.length; ++i) {
+		var pos1 = ht.field.positions[swaps[i][0]];
+		var pos2 = ht.field.positions[swaps[i][1]];
+
+		var player1 = null, player2 = null, tactic1, tactic2;
+
 		// get player and their orientation
 		if (pos1.player != null) {
 			player1 = ht.playerManager.getPlayer(pos1.player.id);
@@ -44,51 +44,52 @@ function ft_swap_positions() {
 		// there's no ht function to remove a behaviour (stored in the class). so we do that ourself
 		if (player1) {
 			pos2.changePlayer(player1);
-			ht.$("#" + pos2.id).removeClass(pos2.getTactic());
+			ht.$('#' + pos2.id).removeClass(pos2.getTactic());
 			pos2.tacticInt = tactic1;
 			pos2.initTactic();
 		}
 		if (player2) {
 			pos1.changePlayer(player2);
-			ht.$("#" + pos1.id).removeClass(pos1.getTactic());
+			ht.$('#' + pos1.id).removeClass(pos1.getTactic());
 			pos1.tacticInt = tactic2;
 			pos1.initTactic();
 		}
 	}
 }
-function ft_clear_penalty_takers(){
+function ft_clear_penalty_takers() {
 	ht.$.each(ht.field.positions, function(i, item) {
 		if (i > 20)
 			item.reset();
 	});
 	ht.field.updateFormation();
 }
-function ft_fix_penalty_takers(){
-// based on matchorder_1_0_7.js:1691	
+function ft_fix_penalty_takers() {
+// based on matchorder_1_0_7.js:1691
 	if (ht.field.ft_kickersFixed) return;
-	ht.$.each(ht.field.positions, function(i, p){
+	ht.$.each(ht.field.positions, function(i, p) {
 		p.ft_handleKickerOld = p.handleKicker; // saving function
 		p.handleKicker = function() {
-			try{
+			try {
 				// saving data
 				var player = arguments[0];
 				var playerHere = this.player;
 				var draggedFrom = player.getKickerPosition();
 				var draggedFromPlayer = (draggedFrom && draggedFrom.player !== null);
 			}
-			catch(e){}
+			catch (e) {}
 			p.ft_handleKickerOld.apply(this, arguments); // redirecting to saved
-			try{	
+			try {
 				if (draggedFromPlayer && draggedFrom.player === null) { // HTs messed up: let's fix
-					if(playerHere) draggedFrom.changePlayer(playerHere);
+					if (playerHere)
+						draggedFrom.changePlayer(playerHere);
 				}
 			}
-			catch(e){}
+			catch (e) {}
 		};
 	});
 	ht.field.ft_kickersFixed = true;
 }
 
-function ft_stay_on_page() { 
+function ft_stay_on_page() {
 	ht.orders.doneURL = document.location.href;
 }
--- content/resources/js/matchSimulator.js
+++ content/resources/js/matchSimulator.js
@@ -6,17 +6,17 @@ var ft_updatePlayers = function() {
 	//console.log('ft_updatePlayers stamina');
 	var fieldplayers = document.getElementById('fieldplayers');
 	var playerdivs = fieldplayers.getElementsByClassName('position');
-	for (var position=0; position<14; ++position) {	
+	for (var position = 0; position < 14; ++position) {
 		var player = ht.field.positions[position].player;
-		
-		if (player != null ) {
+
+		if (player != null) {
 			playerdivs[position].setAttribute('stamina', player.stamina);
 		}
 		else {
 			playerdivs[position].removeAttribute('stamina');
 		}
 	}
-}
+};
 
 // run when all changes have passed
 var ft_checkStamina = function() {
@@ -24,17 +24,17 @@ var ft_checkStamina = function() {
 	if (--ft_queued > 0)
 		return;
 	ft_updatePlayers();
-	fieldOverlay.addEventListener( "DOMNodeInserted", ft_queueChange, false);
+	fieldOverlay.addEventListener('DOMNodeInserted', ft_queueChange, false);
 };
 
 // queue DOMchanges and run only once after all have passed
 var ft_queued = 0;
 var ft_queueChange = function(ev) {
 	var fieldOverlay = document.getElementById('fieldplayers');
-	fieldOverlay.removeEventListener( "DOMNodeInserted", ft_queueChange, false);
+	fieldOverlay.removeEventListener('DOMNodeInserted', ft_queueChange, false);
 	++ft_queued;
 	window.setTimeout(ft_checkStamina, 0);
 };
 
 // listen to field player changes
-document.getElementById('fieldplayers').addEventListener( "DOMNodeInserted", ft_queueChange, false);
+document.getElementById('fieldplayers').addEventListener('DOMNodeInserted', ft_queueChange, false);
