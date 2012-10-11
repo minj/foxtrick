f13010e6777d51cf3e18e86cf635b0cfc3006d77
LA-MJ <4mr.minj@gmail.com>
2012-10-11 15:41:02 +0000

    reformat the whole thing and branch into MatchLineupFixes
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10714 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ content/matches/match-lineup-fixes.js
@@ -0,0 +1,441 @@
+'use strict';
+/**
+ * match-lineup-fixes.js
+ * Fixes for the new style match lineup
+ * @author LA-MJ
+ */
+
+Foxtrick.modules['MatchLineupFixes'] = {
+	MODULE_CATEGORY: Foxtrick.moduleCategories.MATCHES,
+	PAGES: ['match'],
+	OPTIONS: [
+
+	],
+	//CSS: Foxtrick.InternalPath + 'resources/css/match-lineup-fixes.css',
+	run: function(doc) {
+		// START PREPARATION STAGE
+
+		// this is where we fix HTs shit
+		// we need to traverse the hidden input fields in the timeline
+		// that are used as data sources by HTs to assemble the field on each click.
+		// first, each minute/event has input.hidden[id$="_time"][value="min.sec"]
+		var timeline = Foxtrick.map(function(el) {
+			var time = el.value;
+			return { min: Number(time.match(/^\d+/)), sec: Number(time.match(/\d+$/)) };
+		}, doc.querySelectorAll('input[id$="_time"]'));
+
+		//doc.querySelectorAll('input[id$="_playerRatings'"]);
+		// each minute has input.hidden[id$="_playerRatingsHome"][value="jsonArray"]
+		// where jsonArray is an array of Player objects
+		// Player = {
+		//	Cards: 0,
+		//	FromMin: -1,
+		//	InjuryLevel: 0,
+		//	IsCaptain: false,
+		//	IsKicker: false,
+		//	PlayerId: 360991810,
+		//	PositionBehaviour: 0,
+		//	PositionID: 100,
+		//	Stamina: 1,
+		//	Stars: 3,
+		//	ToMin: 90,
+		//};
+
+		var initTeamRatings = function(isHome) {
+			var playerRatings = doc.querySelectorAll('input[id$="_playerRatings' +
+													 (isHome ? 'Home' : 'Away') + '"]');
+			var playerRatingsByEvent = Foxtrick.map(function(ratings) {
+				return { players: JSON.parse(ratings.value), source: ratings };
+			}, playerRatings);
+			// keep playerRatingsByEvent[i].source as a pointer to the input
+			// so that we know where to save
+			return playerRatingsByEvent;
+		};
+		var playerRatingsHome = initTeamRatings(true);
+		var playerRatingsAway = initTeamRatings(false);
+
+		var saveRatings = function(playerRatingsByEvent) {
+			// save modifications
+			for (var i = 0; i < playerRatingsByEvent.length; ++i) {
+				playerRatingsByEvent[i].source.value = JSON.stringify(playerRatingsByEvent[i].players);
+			}
+		};
+
+		var resetPlayers = function(playerRatingsByEvent, eventIdx, sourceIdx, attributesToReset) {
+			if (typeof (attributesToReset) == 'undefined')
+				attributesToReset = [
+					'IsCaptain',
+					'IsKicker',
+					'PositionBehaviour',
+					'PositionID',
+					'Stars',
+					// stamina is not position dependent + it changes in time
+				];
+			var players = playerRatingsByEvent[eventIdx].players;
+			var correctPlayers = playerRatingsByEvent[sourceIdx].players;
+			for (var l = 0; l < players.length; l++) {
+				for (var m = 0, attr; attr = attributesToReset[m]; m++) {
+					players[l][attr] = correctPlayers[l][attr];
+				}
+			}
+		};
+
+		// more stuff in each event:
+		//doc.querySelectorAll('input[id$="_matchEventTypeId"]')
+		// leaves field events: 91-97; not 94; 350-352; 512-514
+		// other player movement events: 360-262; 370-372;
+
+		//doc.querySelectorAll('input[id$="_eventIndex"]') points to the report tab!
+		var eventIndices = doc.querySelectorAll('input[id$="_eventIndex"]');
+		var eventIndexByEvent = [];
+		for (var i = 0; i < eventIndices.length; i++) {
+			eventIndexByEvent.push({ eventIdx: Number(eventIndices[i].value), idx: i });
+		}
+
+		//doc.querySelectorAll('input[id$="_timelineEventType"]')
+		var TIMELINE_EVENT_TYPES = {
+			YELLOW_CARD: 6,
+			SECOND_YELLOW_CARD: 7, 	// TODO: bug? unused!
+			RED_CARD: 8,			// also second yellow now
+			GOAL: 9,
+			SUB: 10, 				// also swap right now
+			NEW_BEHAVIOR: 11,
+			SWAP: 12, 				// bug? unused!
+			MINUTE: 14,
+			PULLBACK: 15,
+			CONFUSION: 16,
+			INJURY: 18,
+			BRUISED: 19,
+			NERVES: 20,
+			RAIN: 21,
+			SUN: 22,
+		};
+		var subEventTypes = [
+			TIMELINE_EVENT_TYPES.SUB,
+			TIMELINE_EVENT_TYPES.NEW_BEHAVIOR,
+			TIMELINE_EVENT_TYPES.SWAP // TODO: currently not in use!
+		];
+		var redCardEventTypes = [
+			TIMELINE_EVENT_TYPES.SECOND_YELLOW_CARD, // TODO: currently not in use!
+			TIMELINE_EVENT_TYPES.RED_CARD,
+		];
+		var lineupEventTypes = Foxtrick.union(redCardEventTypes, subEventTypes);
+
+		var timelineEvents = doc.querySelectorAll('input[id$="_timelineEventType"]');
+		var tEventTypeByEvent = [];
+		for (var i = 0; i < timelineEvents.length; i++) {
+			tEventTypeByEvent.push({ type: Number(timelineEvents[i].value), idx: i });
+		}
+
+		var lineupEvents = Foxtrick.filter(function(event) {
+			return Foxtrick.member(event.type, lineupEventTypes);
+		}, tEventTypeByEvent);
+
+
+		// info for CHPP
+		var SourceSystem = 'Hattrick';
+		var isYouth = Foxtrick.Pages.Match.isYouth(doc);
+		var isHTOIntegrated = Foxtrick.Pages.Match.isHTOIntegrated(doc);
+		if (isYouth)
+			SourceSystem = 'Youth';
+		if (isHTOIntegrated)
+			SourceSystem = 'HTOIntegrated';
+		var matchId = Foxtrick.Pages.Match.getId(doc);
+		// add locale as argument to prevent using old cache after
+		// language changed
+		var locale = FoxtrickPrefs.getString('htLanguage');
+		var detailsArgs = [
+			['file', 'matchdetails'],
+			['matchEvents', 'true'],
+			['matchID', matchId],
+			['SourceSystem', SourceSystem],
+			['version', '2.3'],
+			['lang', locale]
+		];
+
+
+		// END PREPARATION STAGE
+
+
+
+		// here comes the actual stuff
+
+		var fixWeatherSEs = function() {
+			Foxtrick.util.api.retrieve(doc, detailsArgs, { cache_lifetime: 'session' },
+			  function(xml) {
+				var events = xml.getElementsByTagName('Event');
+				Foxtrick.map(function(evt) {
+					var evtMarkup = evt.getElementsByTagName('EventText')[0]
+						.textContent.replace(RegExp('<br\\s*/?>', 'g'), '');
+					var evtType = evt.getElementsByTagName('EventTypeID')[0]
+						.textContent;
+
+					if (evtType > 300 && evtType < 310) {
+						//whether events
+						var temp = doc.createElement('div');
+						Foxtrick.util.sanitize.addHTML(doc, evtMarkup, temp);
+						//trusted source
+						var link = temp.getElementsByTagName('a')[0];
+						var table = doc.querySelector('table.tblHighlights');
+						var row = table.insertRow(-1);
+						Foxtrick.addClass(row, 'hidden');
+						var cell = row.insertCell(-1);
+						cell.id = 'matchEventIndex_' +
+							evt.attributes.getNamedItem('Index').textContent;
+						Foxtrick.addClass(cell, 'undefined');
+						cell.appendChild(link);
+					}
+				}, events);
+			});
+		};
+
+		// add stars for players that leave the field
+		var addStarsToSubs = function(playerRatingsByEvent) {
+			// filter players that have not played: { FromMin: 0, ToMin: 0 }
+			// these have
+			var played = [];
+			for (var i = 0; i < playerRatingsByEvent[0].players.length; ++i) {
+				var player = playerRatingsByEvent[0].players[i];
+				if (!(player.FromMin == 0 && player.ToMin == 0)) {
+					player.ftIdx = i; // saving the index in the original array
+					played.push(player);
+				}
+			}
+			// WARNING: those are object references, not clones, modify with care!
+			// perhaps we should clone instead?
+
+			// let's make some player groups
+
+			// players who play till the end: { ToMin: LastMinuteInTheGame }
+			// these don't
+			var leavesField = Foxtrick.filter(function(player) {
+				return player.ToMin != timeline[timeline.length - 1].min;
+			}, played);
+
+			//// players who start the game: { FromMin: -1}
+			//// these don't
+			//var entersField = Foxtrick.filter(function(player) {
+			//	return player.FromMin != -1;
+			//}, played);
+			//
+			// these players both entersField and leavesField later
+			//var comesAndGoes = Foxtrick.intersect(leavesField, entersField);
+			//
+			// some exlusive groups
+			//var leavesFieldOnly = Foxtrick.filter(function(player) {
+			//	return !Foxtrick.member(player, comesAndGoes);
+			//}, leavesField);
+			//var entersFieldOnly = Foxtrick.filter(function(player) {
+			//	return !Foxtrick.member(player, comesAndGoes);
+			//}, entersField);
+
+			for (var i = 0; i < leavesField.length; ++i) {
+				var player = leavesField[i];
+				var subMin = player.ToMin;
+				var idx = player.ftIdx;
+				// index in the original players array
+
+				// there are multiple events per minute
+				// so we need to find the correct substitution event first
+				// players on the bench: { Stars: -1 }
+				for (var j = 0; j < timeline.length; ++j) {
+					if (timeline[j].min == subMin) {
+						// reached the sub minute
+						while (playerRatingsByEvent[j].players[idx].Stars != -1)
+							++j;
+						// reached the sub second because stars = -1
+
+						var starsLast = playerRatingsByEvent[j - 1].players[idx].Stars;
+						var subSec = timeline[j].sec;
+						// save stamina for later?
+						// player.ftLastStamina = ratings.players[idx].Stamina;
+
+						// add stars for all further events
+						while (timeline[j]) {
+							playerRatingsByEvent[j].players[idx].Stars = starsLast;
+							++j;
+						}
+						// add stars for all events at the same second only
+						//while (timeline[j].min == subMin && timeline[j].sec == subSec) {
+						//	playerRatingsByEvent[j].players[idx].Stars = starsLast;
+						//	++j;
+						//}
+
+						// timeline parsed: let's have a break =)
+						break;
+					}
+				}
+			}
+
+			saveRatings(playerRatingsByEvent);
+		};
+
+
+		var undoPreviousEvents = function() {
+
+			for (var i = 0; i < lineupEvents.length; i++) {
+				var idx = lineupEvents[i].idx;
+				var j = idx;
+				// trace back and search for events at the same time
+				var eventMin = timeline[j].min;
+				var eventSec = timeline[j].sec;
+				var found = false;
+				--j;
+				while (j > -1 && timeline[j].min == eventMin && timeline[j].sec == eventSec) {
+					if (Foxtrick.member(tEventTypeByEvent[j].type, lineupEventTypes))
+						// multiple lineup events happen at the same time
+						// will be fixed in fixMulipleSubs()
+						break;
+					found = true;
+					--j;
+				}
+				if (j < 0 || !found)
+					continue;
+
+				// j now points to an event before these events
+				// while idx is still our lineup event
+				// let's go forward and reset players one event at a time
+				for (var k = j + 1; k < idx ; k++) {
+					resetPlayers(playerRatingsHome, k, j);
+					resetPlayers(playerRatingsAway, k, j);
+				}
+			}
+			saveRatings(playerRatingsHome);
+			saveRatings(playerRatingsAway);
+		};
+
+		// this will fix red carded players going for the bench straight away
+		// red card subs don't need fixing
+		var undoRedCards = function() {
+			var redCardEvents = Foxtrick.filter(function(event) {
+				return Foxtrick.member(event.type, redCardEventTypes);
+			}, tEventTypeByEvent);
+			if (!redCardEvents.length)
+				return;
+
+			for (var i = 0; i < redCardEvents.length; i++) {
+				var idx = redCardEvents[i].idx;
+				var j = idx - 1;
+				if (j < 0)
+					continue;
+
+				// j now points to an event before the red card
+				// while idx is still our red card event
+				// let's reset players
+				resetPlayers(playerRatingsHome, idx, j);
+				resetPlayers(playerRatingsAway, idx, j);
+			}
+			saveRatings(playerRatingsHome);
+			saveRatings(playerRatingsAway);
+		};
+
+		// unfortunately there is not enough info available
+		// to parse multiple subs that happen at the same time
+		// so we will have to use chpp
+		var fixMultipleSubs = function() {
+			var subEvents = Foxtrick.filter(function(event) {
+				return Foxtrick.member(event.type, subEventTypes);
+			}, tEventTypeByEvent);
+			if (!subEvents.length)
+				return;
+
+			var subTimes = {}, multiple = false;
+			for (var i = 0; i < subEvents.length; i++) {
+				var idx = subEvents[i].idx;
+				var eventMin = timeline[idx].min;
+				var eventSec = timeline[idx].sec;
+				var eventTime = Number(eventMin) * 100 + Number(eventSec);
+				var matchEventIdx = eventIndexByEvent[idx].eventIdx;
+				var eventDesc = doc.querySelector('#matchEventIndex_' +
+												  matchEventIdx);
+				if (!eventDesc)
+					continue;
+				var isHomeEvent;
+				if (Foxtrick.hasClass(eventDesc, 'highlightHome'))
+					isHomeEvent = true;
+				else if (Foxtrick.hasClass(eventDesc, 'highlightAway'))
+					isHomeEvent = false;
+				else
+					continue;
+
+				if (!subTimes.hasOwnProperty(eventTime)) {
+					subTimes[eventTime] = [];
+				}
+				else
+					multiple = true;
+
+				subTimes[eventTime].push({
+					matchEventIdx: matchEventIdx, idx: idx, isHome: isHomeEvent
+				});
+			}
+			if (!multiple)
+				return;
+
+			var multipleSubs = [];
+			for (var time in subTimes) {
+				if (subTimes[time].length > 1) {
+					multipleSubs.push(subTimes[time]);
+				}
+			}
+			// OK, so at this point in multipleSubs
+			// we have groups of subs that happen at the same time
+
+			Foxtrick.util.api.retrieve(doc, detailsArgs, { cache_lifetime: 'session' },
+			  function(xml) {
+				var homeId = xml.getElementsByTagName('HomeTeamID')[0].textContent;
+				var awayId = xml.getElementsByTagName('AwayTeamID')[0].textContent;
+				var homeName = xml.getElementsByTagName('HomeTeamName')[0].textContent;
+				var awayName = xml.getElementsByTagName('AwayTeamName')[0].textContent;
+
+				var homeArgs = [
+					['file', 'matchlineup'],
+					['matchID', matchId],
+					['teamID', homeId],
+					['SourceSystem', SourceSystem],
+					['version', '1.8']
+				];
+				var awayArgs = [
+					['file', 'matchlineup'],
+					['matchID', matchId],
+					['teamID', awayId],
+					['SourceSystem', SourceSystem],
+					['version', '1.8']
+				];
+
+				Foxtrick.util.api.retrieve(doc, homeArgs, { cache_lifetime: 'session' },
+				  function(homeXml) {
+					Foxtrick.util.api.retrieve(doc, awayArgs, { cache_lifetime: 'session' },
+					  function(awayXml) {
+						var events = xml.getElementsByTagName('Event');
+						Foxtrick.log('showtime');
+					});
+				});
+			});
+		};
+
+
+
+		// DO STUFF
+		if (Foxtrick.Pages.Match.isPrematch(doc)
+			|| Foxtrick.Pages.Match.inProgress(doc))
+			return;
+
+		if (!Foxtrick.Pages.Match.hasNewRatings(doc))
+			return;
+
+		if (playerRatingsHome[0].players[0].hasOwnProperty('ftIdx'))
+			return; // FF is executing twice, wtf?
+
+		fixWeatherSEs();
+		addStarsToSubs(playerRatingsHome);
+		addStarsToSubs(playerRatingsAway);
+		if (lineupEvents.length) {
+			undoPreviousEvents();
+			undoRedCards();
+			fixMultipleSubs();
+			// we should be good now unless something weird happens
+			// e.g. red card that _follows_ a sub (same second, obviously)
+		}
+
+	},
+};
