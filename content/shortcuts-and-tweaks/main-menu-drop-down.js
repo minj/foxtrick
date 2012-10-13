714d6747f21534b981a20596d09aab43e67cc891
CatzHoek <catzhoek@gmail.com>
2012-10-13 04:21:53 +0000

    main-menu-drop-down.js : Added update time tracking for possible invalidation above a certain time or smthng. tmp. visible as last li
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10751 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/main-menu-drop-down.js
+++ content/shortcuts-and-tweaks/main-menu-drop-down.js
@@ -47,23 +47,26 @@ Foxtrick.modules['MainMenuDropDown']={
 			return (matches && matches[1] && matches[2])?{color:matches[1], backgroundColor:matches[2]}:null;
 		}
 
+		//get css
 		var normal_bgc = getMenuBackgroundColor(css);
 		var normal_tc = getMenuTextColor(css);
-
 		var hoverColors = getHoverColors(css);
-		if(hoverColors){
-			var hover_bgc = hoverColors.backgroundColor;
-			var hover_tc = hoverColors.color;
-		}
+
 		
 		if(normal_bgc && normal_tc){
 			Foxtrick.util.inject.css(doc, '.ft-drop-down-submenu hr { border: 0; height: 1px; background-color: '+ normal_tc + ' !important;} .ft-drop-down-submenu h3, .ft-drop-down-submenu a, #ft-drop-down-menu > li > a { color: '+ normal_tc + ' !important;} .ft-drop-down-submenu, .ft-drop-down-submenu li { background-color: ' + normal_bgc + ' !important;}');
 		}
 
+		//play arround with s and v in hsv color-space to find a suitable hover color
 		if(hoverColors){
+
+			var hover_bgc = hoverColors.backgroundColor;
+			var hover_tc = hoverColors.color;
+
 			var rgb = Foxtrick.util.color.hexToRgb(hover_bgc);
 			var hsv = Foxtrick.util.color.rgbToHsv(rgb[0], rgb[1], rgb[2]);
 
+			//this is not perfect yet. when s ([1]) and v ([2]) are both quite close to 80% and one of them is below 80% the computed hover color is barely noticable
 			if(1-hsv[2] > 0.20)
 				hsv[2] = hsv[2] + (1-hsv[2])/2;
 			else if(1-hsv[1] > 0.20)
@@ -75,12 +78,12 @@ Foxtrick.modules['MainMenuDropDown']={
 
 			Foxtrick.util.inject.css(doc, '#menu > a:hover, #ft-drop-down-menu > li > a:hover, .ft-drop-down-submenu li:hover { color: ' + hover_tc + ' !important; background-color: rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ') !important; }');	
 		}
-//end of css hacks
+//end of css hacks to support custom styles (meaning ht supporter feature)
 
 		var activeLanguage = FoxtrickPrefs.getString('htLanguage');
 
 		var learnCurrentPage = function(menuStructure){
-			Foxtrick.log('MainMenuDropDown Updating: ' + doc.location.pathname)
+			Foxtrick.log('MainMenuDropDown Updating: ' + doc.location.pathname);
 			var subMenuContent = doc.querySelectorAll('.subMenu > .subMenuBox > .boxBody')[0];
 			
 			if(!Foxtrick.util.layout.isStandard(doc))
@@ -119,7 +122,12 @@ Foxtrick.modules['MainMenuDropDown']={
 			if(menuStructure[activeLanguage] === undefined)
 				menuStructure[activeLanguage] = {};
 
-			menuStructure[activeLanguage][doc.location.pathname] = entries;
+			//save date too, some menus might get unreliable and we might invalidate them after weeks or so
+			var pageEntry = {
+				updated: (new Date()).getTime(),
+				content: entries
+			}
+			menuStructure[activeLanguage][doc.location.pathname] = pageEntry;
 			Foxtrick.localSet('htMenuStructure.' + Foxtrick.modules['Core'].getSelfTeamInfo().teamId, menuStructure);
 		};
 
@@ -146,8 +154,9 @@ Foxtrick.modules['MainMenuDropDown']={
 					return;
 
 				if(menuStructure[activeLanguage] !== undefined 
-					&& menuStructure[activeLanguage][item.href.replace(/^.*\/\/[^\/]+/, '')] !== undefined){
-					
+					&& menuStructure[activeLanguage][item.href.replace(/^.*\/\/[^\/]+/, '')] !== undefined					 
+					&& menuStructure[activeLanguage][item.href.replace(/^.*\/\/[^\/]+/, '')].content !== undefined){
+
 					var li = doc.createElement('li');
 
 					var subMenuList = doc.createElement('ul');
@@ -164,7 +173,8 @@ Foxtrick.modules['MainMenuDropDown']={
 						if(entry.tag == 'a'){
 							var link_li = doc.createElement('li');
 							var link_link = doc.createElement('a');
-							link_link.textContent = entry.text;
+							link_link.textContent = Foxtrick.trim(entry.text);
+							//link_link.setAttribute('title',  Foxtrick.trim(entry.text));
 							link_link.href = entry.link;
 							link_li.appendChild(link_link);
 							subMenuList.appendChild(link_li);
@@ -185,13 +195,30 @@ Foxtrick.modules['MainMenuDropDown']={
 							hr_li.appendChild(hr);
 							subMenuList.appendChild(hr_li);
 						} 
-					}, menuStructure[activeLanguage][item.href.replace(/^.*\/\/[^\/]+/, '')]);
+					}, menuStructure[activeLanguage][item.href.replace(/^.*\/\/[^\/]+/, '')].content);
+
+					//append last updated time
+					var now = (new Date()).getTime();
+					var diff = Number(now) - Number(menuStructure[activeLanguage][item.href.replace(/^.*\/\/[^\/]+/, '')].updated);
+					var days = diff/(1000*60*60*24);
+
+					var hr_li = doc.createElement('li');
+					var hr = doc.createElement('hr');
+					hr_li.appendChild(hr);
+					subMenuList.appendChild(hr_li);
+
+					var update_li = doc.createElement('li');
+					var update_link = item.cloneNode(true);
+					update_link.textContent = 'Updated ' + days.toFixed(2) + ' days ago';
+					update_link.setAttribute('title',  'This might eventually serve as place where a warning could be placed when a menu gets old because the landing page is never used (room for improvement here) and an update is required or something');
+					update_li.appendChild(update_link);
+					subMenuList.appendChild(update_li);
 
 					li.appendChild(item); 			//reposition original link from the #menu
 					li.appendChild(subMenuList); 	//the popup
 					nav.appendChild(li); 			//attach to #menu
 				} else {
-
+					Foxtrick.log("No records found");
 					//no records for this language and/or page are present, just attach as is, without popup
 					var li = doc.createElement('li');
 					li.appendChild(item);
