c3b51b32b1f31c16a7eb100d3a8b6d45964cdd4f
CatzHoek <catzhoek@gmail.com>
2012-10-21 00:37:34 +0000

    auto-post-specs.js: First commit
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10866 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ content/forum/auto-post-specs.js
@@ -0,0 +1,50 @@
+'use strict';
+/**
+ * auto-post-specs.js
+ * Automatically adds specs about system. browser, stage, ht language etc. when writing posts in Foxtrick forums
+ * @author CatzHoek
+ */
+
+Foxtrick.modules['AutoPostSpecs'] = {
+	CORE_MODULE: true, 
+	MODULE_CATEGORY: Foxtrick.moduleCategories.FORUM,
+	PAGES: ['forumWritePost'],
+	run: function(doc) {
+
+		var setCaretPosition = function(ctrl, pos){
+			if(ctrl.setSelectionRange)
+			{
+				ctrl.focus();
+				ctrl.setSelectionRange(pos,pos);
+			}
+			else if (ctrl.createTextRange) {
+				var range = ctrl.createTextRange();
+				range.collapse(true);
+				range.moveEnd('character', pos);
+				range.moveStart('character', pos);
+				range.select();
+			}
+		}
+
+		var breadCrumbs = doc.querySelectorAll('#mainWrapper h2 > span');
+		if(breadCrumbs && breadCrumbs[1]){
+			var link = breadCrumbs[1].querySelector('a');
+			if(!link)
+				return;
+
+			//only on Foxtrick Forums
+			if(!link.href.match('f=173635'))
+				return;
+
+			var textarea = doc.getElementById('ctl00_ctl00_CPContent_CPMain_ucHattrickMLEditor_txtBody');
+			var txt = textarea.getAttribute('value');
+			txt = txt?txt:'' + '\n[hr][spoiler]\n' + 'FoxTrick ' + Foxtrick.version() + ' ' + Foxtrick.branch() + ' - Stage: ' + Foxtrick.isStage(doc) + ' - Skin: ' + (Foxtrick.util.layout.isStandard(doc)?'normal':'simple') + ' - ' + FoxtrickPrefs.getString('htLanguage') + ' - '  + window.screen.availWidth + ' x ' + window.screen.availHeight + ' - ' + navigator.userAgent + ' - ' + navigator.platform + '[/spoiler]';
+			textarea.value = txt;
+			setCaretPosition(textarea, 0);
+		}
+	},
+
+	change: function(doc) {
+		
+	}
+};
