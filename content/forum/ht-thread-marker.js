bf7483d0743fd044fea12e9253e7ed086754ca6c
CatzHoek <catzhoek@gmail.com>
2012-11-17 17:31:39 +0000

    ht-thread-marker.js: Fixed, simplified, rewritten, pick one
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@10954 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/ht-thread-marker.js
+++ content/forum/ht-thread-marker.js
@@ -2,7 +2,7 @@
 /**
  * ht-thread-marker.js
  * Marks threads created by HTs.
- * @author Mod-PaV
+ * @author CatzHoek (original version by Mod-PaV)
  */
 
 Foxtrick.modules['HTThreadMarker'] = {
@@ -11,65 +11,19 @@ Foxtrick.modules['HTThreadMarker'] = {
 	CSS: Foxtrick.InternalPath + 'resources/css/ht-thread.css',
 
 	run: function(doc) {
-		this.ColorLatest(doc, 'threadContent', 'folderitem');
+		var threadLinks = doc.querySelectorAll('.threadItem > td:nth-child(2) > .url > a' 
+			+ ', .folderitem > td:nth-child(2) > .fplLongThreadName > a');
 
-		var myForums = doc.getElementsByClassName('subMenuConf')[0];
-
-		var folders = myForums.getElementsByClassName('forumFolder');
-		for (var j = 0; j < folders.length; ++j) {
-			var folderName = folders[j].getElementsByClassName('folderName')[0]
-				.getElementsByTagName('a')[0].textContent;
-			var divs = folders[j].getElementsByTagName('div');
-			var cname;
-
-			var i = 0, div;
-			while (div = divs[++i]) {
-				cname = div.getAttribute('class');
-				if (cname == 'url') {
-					var inner = div.childNodes[0];
-					var strong = div.getElementsByTagName('strong');
-					if (strong != null && strong[0] != null) {
-						inner = strong[0];
-					}
-					inner = inner.firstChild.data;
-					var title = div.childNodes[0].getAttribute('title').replace(inner, '');
-					var poster = title.replace(folderName, '');
-
-					if (poster.match(/ HT-\S+/)) {
-						Foxtrick.addClass(div, 'ft-ht-thread');
-					}
-				}
+		Foxtrick.map( function(threadLink){
+			var title = threadLink.getAttribute('title');
+			if(title.match(/ HT-\S+/))	{
+				Foxtrick.log('Matched', title);
+				Foxtrick.addClass(threadLink.parentNode, 'ft-ht-thread');
 			}
-		}
+		},threadLinks);
 	},
 
 	change: function(doc) {
 		this.run(doc);
-	},
-
-	ColorLatest: function(doc, id, classname) {
-		var myForums = doc.getElementById(id);
-		if (myForums) {
-			var links = myForums.getElementsByTagName('table');
-
-			for (var j = 0; j < links.length; j++) {
-				var cname;
-
-				if (links[j].rows.length == 0
-					|| links[j].rows[0].cells.length == 0
-					|| links[j].rows[0].cells[0].className.search('date') == -1)
-					continue;
-
-				for (var i = 0; i < links[j].rows.length; ++i) {
-					if (links[j].rows[i].cells.length <= 1)
-						continue;
-					var node = links[j].rows[i].cells[1].childNodes[1];
-					var title = node.getElementsByTagName('a')[0].title;
-					if (title.match(/.* HT-[^\s]*$/i)) {
-						Foxtrick.addClass(node, 'ft-ht-thread');
-					}
-				}
-			}
-		}
 	}
 };
