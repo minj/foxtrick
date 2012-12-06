11ce0488db3ebc69ce57e90546b80dbad5b6f093
CatzHoek <catzhoek@gmail.com>
2012-12-06 14:13:15 +0000

    @011: match-order.js : player faces fixed for non supporters
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.11.0@11099 1e3807c3-0949-0410-b78e-337b627472c1

--- content/matches/match-order.js
+++ content/matches/match-order.js
@@ -36,8 +36,10 @@ Foxtrick.modules["MatchOrderInterface"]={
 					var kiturl = shirt.getAttribute("kiturl");
 					if(!kiturl && !isYouth){
 						var shirtstyle = shirt.getAttribute("style");
-						var kiturl = shirtstyle.match(/http:\/\/res.hattrick.org\/kits\/\d+\/\d+\/\d+\/\d+\//)[0];
-						shirt.setAttribute("kiturl", kiturl);
+						if(shirtstyle){
+							var kiturl = shirtstyle.match(/http:\/\/res.hattrick.org\/kits\/\d+\/\d+\/\d+\/\d+\//)[0];
+							shirt.setAttribute("kiturl", kiturl);
+						}
 					}
 				} else {
 					var outer = doc.createElement('div');
