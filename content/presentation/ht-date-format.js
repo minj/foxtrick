e38971ee40d6952c80824f57d93f472b84ad8f0a
LA-MJ <4mr.minj@gmail.com>
2012-12-07 13:18:21 +0000

    @11&trunk date format in tournament
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@11112 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/ht-date-format.js
+++ content/presentation/ht-date-format.js
@@ -14,11 +14,14 @@ Foxtrick.modules['HTDateFormat'] = {
 	OPTION_EDITS_DISABLED_LIST: [true, false],
 
 	run: function(doc) {
-		var pages = ['transfersTeam', 'transfersPlayer', 'transfer',
+		var pages = [
+			'transfersTeam', 'transfersPlayer', 'transfer',
 			'transferCompare', 'match', 'matches', 'matchesArchive',
 			'teamPageGeneral', 'achievements', 'playerEvents',
 			'teamEvents', 'history', 'arena', 'country', 'hallOfFame',
-			'statsMatchesHeadToHead', 'seriesHistory', 'playerStats'];
+			'statsMatchesHeadToHead', 'seriesHistory', 'playerStats',
+			'tournamentsGeneral',
+		];
 		// don't show on where not needed and cluttering
 		if (!Foxtrick.any(function(page) {
 				return Foxtrick.isPage(doc, page);
