3270f611497d8b3e50a2ab341fc19d92df070ee6
foxtrickdev <foxtrickdev@gmail.com>
2013-03-14 16:48:16 +0000

    release 0.12.0
    git-svn-id: https://foxtrick.googlecode.com/svn/build@11392 1e3807c3-0949-0410-b78e-337b627472c1

--- foxtrick-release.sh
+++ foxtrick-release.sh
@@ -23,7 +23,7 @@ cd ..
 cd $RELEASE || log "Cannot cd to $RELEASE"
 git stash
 git svn rebase || log "Cannot git-svn rebase"
-./version.sh 0.12
+./version.sh 0.12.0
 ./version.sh
 cd maintainer || log "Cannot cd to maintainer"
 #./crowdin-upload.sh || echo "Cannot upload external translations"
