769e4cc230f904c83cffd663004cee2c90dae72e
LA-MJ <4mr.minj@gmail.com>
2013-06-22 16:10:48 +0000

    mmdd: fix coding style
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@11622 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/main-menu-drop-down.js
+++ content/shortcuts-and-tweaks/main-menu-drop-down.js
@@ -5,38 +5,39 @@
  * @author CatzHoek
  */
 
-Foxtrick.modules['MainMenuDropDown']={
-	MODULE_CATEGORY : Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	PAGES : ['all'],
-	OPTIONS : ['DisregardFirstHeader'],
-	CSS : [Foxtrick.InternalPath + 'resources/css/main-menu-drop-down.css'],
+Foxtrick.modules['MainMenuDropDown'] = {
+	MODULE_CATEGORY: Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
+	PAGES: ['all'],
+	OPTIONS: ['DisregardFirstHeader'],
+	CSS: [Foxtrick.InternalPath + 'resources/css/main-menu-drop-down.css'],
 
-	convertMainMenuToUnorderedList : function(doc){
+	convertMainMenuToUnorderedList: function(doc) {
 		var menuLinks = doc.querySelectorAll('#menu > a');
 		var list = Foxtrick.createFeaturedElement(doc, Foxtrick.modules.MainMenuDropDown, 'ul');
 		list.id = 'ft-drop-down-menu';
-		Foxtrick.map(function(link){
-			if(link.id == 'ctl00_ctl00_CPHeader_ucMenu_hypLogout')
+		Foxtrick.map(function(link) {
+			if (link.id == 'ctl00_ctl00_CPHeader_ucMenu_hypLogout')
 				return;
 			var listItem = doc.createElement('li');
 			listItem.appendChild(link);
 			list.appendChild(listItem);
 		}, menuLinks);
-		doc.getElementById('menu').insertBefore(list, doc.getElementById('ctl00_ctl00_CPHeader_ucMenu_hypLogout'));
+		var logout = doc.getElementById('ctl00_ctl00_CPHeader_ucMenu_hypLogout');
+		doc.getElementById('menu').insertBefore(list, logout);
 	},
 	
-	addMenusToListItem : function(doc, node, menus, className){
-		if(!menus.length)
+	addMenusToListItem: function(doc, node, menus, className) {
+		if (!menus.length)
 			return;
 
-		var addSeperator = function(list, text){
+		var addSeperator = function(list, text) {
 			var li = doc.createElement('li');
 			var hr = doc.createElement('hr');
 			li.appendChild(hr);
 			list.appendChild(li);
 
 			li = doc.createElement('li');
-			if(text){
+			if (text) {
 				var h3 = doc.createElement('h3');
 				h3.textContent = text;
 				li.appendChild(h3);
@@ -51,15 +52,15 @@ Foxtrick.modules['MainMenuDropDown']={
 		var firstHeader = true;
 		var list = doc.createElement('ul');
 		Foxtrick.addClass(list, className);
-		Foxtrick.map(function(menu){
-			if(firstHeader){
-				if(!FoxtrickPrefs.isModuleOptionEnabled('MainMenuDropDown', 'DisregardFirstHeader'))
+		Foxtrick.map(function(menu) {
+			if (firstHeader) {
+				if (!FoxtrickPrefs.isModuleOptionEnabled('MainMenuDropDown', 'DisregardFirstHeader'))
 					addSeperator(list, menu.name);
 				firstHeader = false;
 			} else {
 				addSeperator(list, menu.name);	
 			}
-			Foxtrick.map(function(entry){
+			Foxtrick.map(function(entry) {
 				var li = doc.createElement('li');
 				var anchor = doc.createElement('a');
 				anchor.textContent = entry.text;
@@ -72,34 +73,40 @@ Foxtrick.modules['MainMenuDropDown']={
 
 		node.appendChild(list);
 	},
-	run : function(doc){
+	run: function(doc) {
+
+		var module = this;
 		
-		//put #menu > a in #menu > ul > li', logout is left as is
+		// put #menu > a in #menu > ul > li', logout is left as is
 		this.convertMainMenuToUnorderedList(doc);
 
-		//current page
-		// var isOnMainMenuPage = function(){
-		// 	var menuLinks = doc.querySelectorAll('#menu a');
-		// 	var ret = false;
-		// 	Foxtrick.map(function(menuLink){
-		// 		if(Foxtrick.isPageHref(menuLink.href.replace(/^.*\/\/[^\/]+/, '') +'$', doc.location.href))
-		// 			ret = true;
-		// 	}, menuLinks);	
-		// 	return ret;
-		// }
+		// current page
+		//var isOnMainMenuPage = function() {
+		//	var menuLinks = doc.querySelectorAll('#menu a');
+		//	var ret = false;
+		//	Foxtrick.map(function(menuLink) {
+		//		if (Foxtrick.isPageHref(menuLink.href.replace(/^.*\/\/[^\/]+/, '') +'$',
+		//								doc.location.href))
+		//			ret = true;
+		//	}, menuLinks);
+		//	return ret;
+		//}
 
 
-		var NavigationStructure = function(){
+		var NavigationStructure = function() {
 			this.menus = {
-				primary : [],
-				secondary : []
+				primary: [],
+				secondary: []
 			}
-			this.save = function(){
-				Foxtrick.localSet('Menu.v4.' + Foxtrick.modules['Core'].getSelfTeamInfo().teamId + '.' + FoxtrickPrefs.getString('htLanguage'), { menus : this.menus });
+			this.save = function() {
+				Foxtrick.localSet('Menu.v4.' + Foxtrick.modules['Core'].getSelfTeamInfo().teamId +
+								  '.' + FoxtrickPrefs.getString('htLanguage'), { menus: this.menus });
 			}
-			this.load = function(func){
-				Foxtrick.localGet('Menu.v4.'  + Foxtrick.modules['Core'].getSelfTeamInfo().teamId + '.' + FoxtrickPrefs.getString('htLanguage'), function(menu){
-					if(!menu){
+			this.load = function(func) {
+				Foxtrick.localGet('Menu.v4.' + Foxtrick.modules['Core'].getSelfTeamInfo().teamId +
+								  '.' + FoxtrickPrefs.getString('htLanguage'),
+				  function(menu) {
+					if (!menu) {
 						func(new NavigationStructure());
 					} else {
 						var nav = new NavigationStructure();
@@ -108,32 +115,32 @@ Foxtrick.modules['MainMenuDropDown']={
 					}
 				});
 			}
-			this.getMenusForUrl = function(url, source){
+			this.getMenusForUrl = function(url, source) {
 				var properUrl = url.replace(/^.*\/\/[^\/]+/, '').replace(/Default\.aspx/i, '');
-				var primaries = Foxtrick.filter(function(entry){
-					if(entry.url.toLowerCase() == properUrl.toLowerCase())
+				var primaries = Foxtrick.filter(function(entry) {
+					if (entry.url.toLowerCase() == properUrl.toLowerCase())
 						return true;
 					else
 						return false;
 				}, source);
 				return primaries;	
 			}
-			this.getPrimaryMenusForUrl = function(url){
+			this.getPrimaryMenusForUrl = function(url) {
 				return this.getMenusForUrl(url, navi.menus.primary);
 			}
-			this.getSecondaryMenusForUrl = function(url){
+			this.getSecondaryMenusForUrl = function(url) {
 				return this.getMenusForUrl(url, navi.menus.secondary);
 			}
-			this.update = function(menu, target){
-				for(var existingMenu in target){
+			this.update = function(menu, target) {
+				for (var existingMenu in target) {
 					Foxtrick.log(target[existingMenu].name, target[existingMenu].name);
 				}
 			}
-			this.concat = function(menus, target){
+			this.concat = function(menus, target) {
 				var secondary = this.menus[target];
-				Foxtrick.map(function(newMenu){
-					Foxtrick.any(function(menu){
-						if(menu.url.toLowerCase() === newMenu.url.toLowerCase() &&
+				Foxtrick.map(function(newMenu) {
+					Foxtrick.any(function(menu) {
+						if (menu.url.toLowerCase() === newMenu.url.toLowerCase() &&
 						   menu.name === newMenu.name) {
 							secondary = Foxtrick.remove(secondary, menu);
 							return true; // stops on first match
@@ -144,31 +151,33 @@ Foxtrick.modules['MainMenuDropDown']={
 				}, menus);
 				this.menus[target] = secondary;
 			}
-			this.learn = function(doc){
-				//learns secondary menus from current document
-				var getSecondaryMenus = function(doc){
+			this.learn = function(doc) {
+				// learns secondary menus from current document
+				var getSecondaryMenus = function(doc) {
 					var boxBodies = doc.querySelectorAll('.sidebarBox > .boxBody');
 					var menuslist = [];
-					Foxtrick.map(function(boxBody){
-						//Foxtrick.log(boxBody);
-						//only accept sidebar thingies that have the structure .boxbody > a
-						//but allow <br> and empty textnode
-						var isEmptyTextNode = function(node){
+					Foxtrick.map(function(boxBody) {
+						// Foxtrick.log(boxBody);
+						// only accept sidebar thingies that have the structure .boxbody > a
+						// but allow <br> and empty textnode
+						var isEmptyTextNode = function(node) {
 							var nodeType = node.nodeType;
-							if(Foxtrick.NodeTypes.TEXT_NODE == nodeType && node.textContent.replace(/\s*/gi, '') == '')
+							if (Foxtrick.NodeTypes.TEXT_NODE == nodeType &&
+							   node.textContent.replace(/\s*/gi, '') == '')
 								return true;
 
 							return false;
 						};
 						var linkOnlyBox = true;
-						for(var c = 0; c < boxBody.childNodes.length; c++) {
+						for (var c = 0; c < boxBody.childNodes.length; c++) {
 							var child = boxBody.childNodes[c];
-							//ignore stupid empty textnodes or line break
+							// ignore stupid empty textnodes or line break
 							if (isEmptyTextNode(child) || child.tagName == 'BR' ) {
-								//that's okay
+								// that's okay
 							}
-							else if(child.tagName != 'A') {
-								//hack out those anchor tags that are wrapped in paragraphs for some reason
+							else if (child.tagName != 'A') {
+								// hack out those anchor tags
+								// that are wrapped in paragraphs for some reason
 								if (child.tagName == 'P' || child.tagName == 'SPAN') {
 									var ok = true;
 									Foxtrick.map(function(node) {
@@ -185,7 +194,8 @@ Foxtrick.modules['MainMenuDropDown']={
 									}
 								}
 								else if (child.tagName == 'DIV') {
-									if (child.getAttribute('style') && child.getAttribute('style').match(/clear:both;/)){
+									if (child.getAttribute('style') &&
+										child.getAttribute('style').match(/clear:both;/)) {
 										Foxtrick.log('Hacked arround clear both div');
 										continue;
 									}
@@ -197,8 +207,8 @@ Foxtrick.modules['MainMenuDropDown']={
 								linkOnlyBox = false;
 							}
 						}
-						//only contains links (and <br> and empty text nodes) -> build object
-						if(linkOnlyBox){
+						// only contains links (and <br> and empty text nodes) -> build object
+						if (linkOnlyBox) {
 							var header = boxBody.parentNode.querySelector('h2');
 							var links = boxBody.parentNode.querySelectorAll('a');
 
@@ -209,15 +219,15 @@ Foxtrick.modules['MainMenuDropDown']={
 							menu.entries = [];
 							menu.timestamp = (new Date()).getTime();
 
-							Foxtrick.map(function(link){
-								//no empty shit, lile
-								if(link.textContent.replace(/\s*/gi, '') == '')
+							Foxtrick.map(function(link) {
+								// no empty shit, like
+								if (link.textContent.replace(/\s*/gi, '') == '')
 									return;
 								
-								if(Foxtrick.getHref(doc) + '#' == link.href)
+								if (Foxtrick.getHref(doc) + '#' == link.href)
 									return;
 
-								if(link.href.match(/javascript\:/))
+								if (link.href.match(/javascript\:/))
 									return;
 
 								var entry = {};
@@ -227,18 +237,18 @@ Foxtrick.modules['MainMenuDropDown']={
 								menu.entries.push(entry);
 							}, links);
 
-							if(menu.entries.length > 0)
+							if (menu.entries.length > 0)
 								menuslist.push(menu);
 						} 
 					}, boxBodies);
 					return menuslist;
 				}
-				//learns primary menus from current document
-				var getPrimaryMenus = function(doc){
+				// learns primary menus from current document
+				var getPrimaryMenus = function(doc) {
 					var subMenuContent = doc.querySelectorAll('.subMenu > .subMenuBox > .boxBody')[0];
 					
-					//no navigation sidebar, like forums
-					if(subMenuContent  === undefined)
+					// no navigation sidebar, like forums
+					if (subMenuContent === undefined)
 						return [];
 
 					// map all teamPages to /Club/ primary
@@ -249,8 +259,8 @@ Foxtrick.modules['MainMenuDropDown']={
 							.replace(/Default\.aspx/i, '');
 
 					var menulist = [];
-					Foxtrick.map(function(node){
-						if(node.tagName === 'H3'){
+					Foxtrick.map(function(node) {
+						if (node.tagName === 'H3') {
 							menu = {};
 							menu.name = Foxtrick.trim(node.textContent);
 
@@ -259,10 +269,10 @@ Foxtrick.modules['MainMenuDropDown']={
 
 							menu.url = primaryUrl;
 						}
-						if(node.tagName === 'UL'){
+						if (node.tagName === 'UL') {
 							var links = node.getElementsByTagName('a');
 
-							Foxtrick.map(function(link){
+							Foxtrick.map(function(link) {
 								var entry = {};
 								entry.text = Foxtrick.trim(link.textContent);
 								entry.link = !Foxtrick.isHtUrl(link.href) ? link.href :
@@ -289,30 +299,33 @@ Foxtrick.modules['MainMenuDropDown']={
 			}
 		}
 
-		//that shit saves and loads from localstore and stuff
+		// that shit saves and loads from localstore and stuff
 		var navi = new NavigationStructure();
-		navi.load(function(loaded){
+		navi.load(function(loaded) {
 			navi = loaded;
-			navi.learn(doc); //learn current page
-			navi.save();	 //save to localstore
+			navi.learn(doc); // learn current page
+			navi.save();	 // save to localstore
 
-			//build the menu
+			// build the menu
 			var links = doc.querySelectorAll('#menu > ul > li > a');
-			for(var l = 0; l < links.length; l++){
+			for (var l = 0; l < links.length; l++) {
 				var primaryUrl = links[l].href;
 				var primaries = navi.getPrimaryMenusForUrl(primaryUrl);
 				var secondaries = navi.getSecondaryMenusForUrl(primaryUrl);
-				Foxtrick.modules['MainMenuDropDown'].addMenusToListItem(doc, links[l].parentNode, Foxtrick.union(primaries, secondaries), "ft-drop-down-submenu");
+				module.addMenusToListItem(doc, links[l].parentNode,
+										  Foxtrick.union(primaries, secondaries),
+										  'ft-drop-down-submenu');
 				
-				//secondary links
+				// secondary links
 				var secondaryLinks = links[l].parentNode.querySelectorAll('a');
-				for(var sl = 0; sl < secondaryLinks.length; sl++){
-					if(sl == 0) //skip first entry, that's cover already
+				for (var sl = 0; sl < secondaryLinks.length; sl++) {
+					if (sl == 0) // skip first entry, that's cover already
 						continue;
 					var secondaryUrl = secondaryLinks[sl].href;
 					var secondaries = navi.getSecondaryMenusForUrl(secondaryUrl);
-					Foxtrick.modules['MainMenuDropDown'].addMenusToListItem(doc, secondaryLinks[sl].parentNode, secondaries, "ft-drop-down-submenu ft-drop-down-submenu-2");
-					if(secondaries.length){
+					module.addMenusToListItem(doc, secondaryLinks[sl].parentNode, secondaries,
+											  'ft-drop-down-submenu ft-drop-down-submenu-2');
+					if (secondaries.length) {
 						var ul = secondaryLinks[sl].parentNode.getElementsByTagName('ul')[0];
 						var span = doc.createElement('span');
 						if (!Foxtrick.util.layout.isRtl(doc))
@@ -324,73 +337,78 @@ Foxtrick.modules['MainMenuDropDown']={
 				}
 			}
 
-			//css adjustments for custom ht designs ()
-			var getCustomCss = function(doc){
+			// css adjustments for custom ht designs ()
+			var getCustomCss = function(doc) {
 				var inlinestyleNodes = doc.getElementsByTagName('style');
 				var inlineStyle = '';
-				Foxtrick.map(function(styleNode){
-					if(styleNode.id != 'ft-module-css' && styleNode.id != 'dynamic-mmmd-style')
+				Foxtrick.map(function(styleNode) {
+					if (styleNode.id != 'ft-module-css' && styleNode.id != 'dynamic-mmmd-style')
 						inlineStyle = inlineStyle + styleNode.textContent + '\n';
 				}, inlinestyleNodes);
 				return inlineStyle;
 			};
 
 			var css = getCustomCss(doc);
-			var newcss = css.replace(/#menu\s*{/gi, "#menu h3, #menu ul, #menu {");
-			var newcss = newcss.replace(/#menu\s*a\s*{/gi, "#menu h3, #menu a {");
-			newcss = newcss.replace(/#menu\s*a\s*:\s*hover\s*{/gi, "#menu li:hover, #menu a:hover {");
-			if(newcss != css) {
+			var newcss = css.replace(/#menu\s*{/gi, '#menu h3, #menu ul, #menu {');
+			var newcss = newcss.replace(/#menu\s*a\s*{/gi, '#menu h3, #menu a {');
+			newcss = newcss.replace(/#menu\s*a\s*:\s*hover\s*{/gi, '#menu li:hover, #menu a:hover {');
+			if (newcss != css) {
 				Foxtrick.util.inject.css(doc, newcss, 'modified-ht-style');
 			}
 		});
 
-		function hoverBgColor(text){
+		function hoverBgColor(text) {
 			var re = new RegExp('#menu\\s*a\\s*:\\s*hover\\s*{.*background-color:([^;]+);', 'i');
 			var matches = text.match(re);
-			if(matches)
+			if (matches)
 				return matches[1];
 			return null;
 		}
-		function hoverColor(text){
+		function hoverColor(text) {
 			var re = new RegExp('#menu\\s*a\\s*:\\s*hover\\s*{.*\\s*color:([^;]+);', 'i');
 			var matches = text.match(re);
-			if(matches)
+			if (matches)
 				return matches[1];
 			return null;
 		}
-		function bgColor(text){
+		function bgColor(text) {
 			var re = new RegExp('#menu\\s*a\\s*{.*background-color:([^;]+);', 'i');
 			var matches = text.match(re);
-			if(matches)
+			if (matches)
 				return matches[1];
 			return null;
 		}
-		function textColor(text){
+		function textColor(text) {
 			var re = new RegExp('#menu\\s*a\\s*{.*\\s*color:([^;]+);', 'i');
 			var matches = text.match(re);
-			if(matches)
+			if (matches)
 				return matches[1];
 			return null;
 		}
 		
-		function getMenuTextColor(){
+		function getMenuTextColor() {
 			var tcolor;
-			Foxtrick.map(function(styleSheet){
+			Foxtrick.map(function(styleSheet) {
 				try {
 					// opera throws security exception when accessing CSS from different domain
-					if(styleSheet.cssRules)
-						Foxtrick.map(function(rule){
-							// var hbc = hoverBgColor(rule.cssText);
-							// var hc = hoverColor(rule.cssText);
-							// var bc = bgColor(rule.cssText);
+					if (styleSheet.cssRules) {
+						Foxtrick.map(function(rule) {
+							//var hbc = hoverBgColor(rule.cssText);
+							//var hc = hoverColor(rule.cssText);
+							//var bc = bgColor(rule.cssText);
 							var c = textColor(rule.cssText);
 
-							// if(hbc) Foxtrick.log("hover bg color",hbc);
-							// if(bc) Foxtrick.log("text bg c",bc);
-							// if(hc) Foxtrick.log("text hover",hc);
-							if(c) tcolor = c;
+							//if (hbc)
+							//	Foxtrick.log('hover bg color',hbc);
+							//if (bc)
+							//	Foxtrick.log('text bg c',bc);
+							//if (hc)
+							//	Foxtrick.log('text hover',hc);
+							if (c)
+								tcolor = c;
 
 						}, styleSheet.cssRules);
+					}
 				}
 				catch (e) {};
 			}, doc.styleSheets);	
@@ -398,7 +416,8 @@ Foxtrick.modules['MainMenuDropDown']={
 		}
 		var tc = getMenuTextColor();
 	
-		var hrstyle = '#menu hr { background-color:' + tc + ' !important;} .ft-drop-down-submenu li span, #menu h3 {color:' + tc + ' !important;}';
+		var hrstyle = '#menu hr { background-color:' + tc +
+			' !important;} .ft-drop-down-submenu li span, #menu h3 {color:' + tc + ' !important;}';
 		Foxtrick.util.inject.css(doc, hrstyle, 'dynamic-mmmd-style');
 	}
 }
