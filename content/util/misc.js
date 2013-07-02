d11b13bb0ebd6e73ce034568f62d3e580d5b52ba
LA-MJ <4mr.minj@gmail.com>
2013-07-02 10:00:34 +0000

    @12.1&trunk unmerge last commit
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@11714 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/misc.js
+++ content/util/misc.js
@@ -62,19 +62,19 @@ Foxtrick.playSound = function(url, doc) {
 		else
 			type = url.match(/.+\.([^\.]+)$/)[1];
 		Foxtrick.log('play: ' + url.substring(0, 100));
-		//if (Foxtrick.arch === 'Gecko' && url.indexOf('chrome') === 0) {
-		//	try {
-		//		Foxtrick.log('using ff soundService for chrome url ', url);
-		//		var soundService = Components.classes['@mozilla.org/sound;1']
-		//			.getService(Components.interfaces.nsISound);
-		//		var ioService = Components.classes['@mozilla.org/network/io-service;1']
-		//			.getService(Components.interfaces.nsIIOService);
-		//		soundService.play(ioService.newURI(url, null, null));
-		//		return;
-		//	} catch (e) {
-		//		Foxtrick.log("ff soundService can't play ", url);
-		//	}
-		//}
+		if (Foxtrick.arch === 'Gecko' && url.indexOf('chrome') === 0) {
+			try {
+				Foxtrick.log('using ff soundService for chrome url ', url);
+				var soundService = Components.classes['@mozilla.org/sound;1']
+					.getService(Components.interfaces.nsISound);
+				var ioService = Components.classes['@mozilla.org/network/io-service;1']
+					.getService(Components.interfaces.nsIIOService);
+				soundService.play(ioService.newURI(url, null, null));
+				return;
+			} catch (e) {
+				Foxtrick.log("ff soundService can't play ", url);
+			}
+		}
 		try {
 			var music = new Audio();
 			var canPlay = music.canPlayType('audio/' + type);
