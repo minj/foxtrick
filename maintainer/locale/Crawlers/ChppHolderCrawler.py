6e0a76835118ab294421e9981008e090c9bdaff2
CatzHoek <catzhoek@gmail.com>
2013-09-18 16:15:20 +0000

    oops, wrong dir name
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@11939 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/locale/Crawlers/ChppHolderCrawler.py
+++ maintainer/locale/Crawlers/ChppHolderCrawler.py
@@ -39,7 +39,7 @@ def run(username, password):
 	editors = getList(username, password);
 	editors = sorted(editors, key=lambda x: x["name"])
 	if len(editors):
-		saveJson(editors, '/foxtrick/trunk/res/stuff/editor.json')
+		saveJson(editors, '/home/foxtrick/trunk/res/stuff/editor.json')
 
 if __name__ == "__main__":
 	user = raw_input("Login:");
