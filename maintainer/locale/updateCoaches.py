509216fa84597ae100d1d4593a6a44fb049935cd
CatzHoek <catzhoek@gmail.com>
2013-09-18 14:48:34 +0000

    updateCoaches.py Made it usable from anywhere
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@11937 1e3807c3-0949-0410-b78e-337b627472c1

--- maintainer/locale/updateCoaches.py
+++ maintainer/locale/updateCoaches.py
@@ -1,57 +1,58 @@
 #!/usr/bin/env python
 
-if __name__ == '__main__':
-	from Hattrick.CHPP import Client
-	from Hattrick.CHPP import Credentials
-	from Hattrick.CHPP import AccessToken
 
-	import xml.etree.ElementTree as ET
+from Hattrick.CHPP import Client
+from Hattrick.CHPP import Credentials
+from Hattrick.CHPP import AccessToken
 
-	CONSUMER_KEY = Credentials.KEY
-	CONSUMER_SECRET = Credentials.SECRET
+import xml.etree.ElementTree as ET
 
-	ACCESS_TOKEN_KEY = AccessToken.KEY
-	ACCESS_TOKEN_SECRET = AccessToken.SECRET
+CONSUMER_KEY = Credentials.KEY
+CONSUMER_SECRET = Credentials.SECRET
 
-	chpp = Client.ChppClient(CONSUMER_KEY, CONSUMER_SECRET)
-	chpp.setAccessToken((AccessToken.KEY, AccessToken.SECRET))
-	session = chpp.getSession()
+ACCESS_TOKEN_KEY = AccessToken.KEY
+ACCESS_TOKEN_SECRET = AccessToken.SECRET
 
-	def getCoaches(id):
-		#no get started
-		response = chpp.getFile('nationalteams', params={'version':'1.5', 'LeagueOfficeTypeID': id })
+chpp = Client.ChppClient(CONSUMER_KEY, CONSUMER_SECRET)
+chpp.setAccessToken((AccessToken.KEY, AccessToken.SECRET))
+session = chpp.getSession()
+
+def getCoaches(id):
+	#no get started
+	response = chpp.getFile('nationalteams', params={'version':'1.5', 'LeagueOfficeTypeID': id })
+	dom = ET.fromstring(response.content)
+
+	teams = []
+	iTeams =  dom.iter("NationalTeam")
+	for iTeam in iTeams:
+		team = {}
+		team['TeamId'] = int(iTeam.find('NationalTeamID').text)
+		team['LeagueId'] = int(iTeam.find('LeagueId').text)
+		team['TeamName'] = iTeam.find('NationalTeamName').text
+		teams.append(team)
+
+	for t in teams:
+		response = chpp.getFile('nationalteamdetails', params={'version':'1.8', 'teamID':t['TeamId']})
 		dom = ET.fromstring(response.content)
+		iCoaches =  dom.iter("NationalCoach")
+		for iCoach in iCoaches:
+			t['CoachId'] = int(iCoach.find('NationalCoachUserID').text)
+			t['CoachName'] = iCoach.find('NationalCoachLoginname').text
 
-		teams = []
-		iTeams =  dom.iter("NationalTeam")
-		for iTeam in iTeams:
-			team = {}
-			team['TeamId'] = int(iTeam.find('NationalTeamID').text)
-			team['LeagueId'] = int(iTeam.find('LeagueId').text)
-			team['TeamName'] = iTeam.find('NationalTeamName').text
-			teams.append(team)
+	return teams
 
-		for t in teams:
-			response = chpp.getFile('nationalteamdetails', params={'version':'1.8', 'teamID':t['TeamId']})
-			dom = ET.fromstring(response.content)
-			iCoaches =  dom.iter("NationalCoach")
-			for iCoach in iCoaches:
-				t['CoachId'] = int(iCoach.find('NationalCoachUserID').text)
-				t['CoachName'] = iCoach.find('NationalCoachLoginname').text
+def saveCoaches(coaches, filename):
+	file = open( filename, "w")
+	file.write('{\n')
+	file.write('\t"type": "%s",\n' % "coach")
+	file.write('\t"internal": "true",\n')
+	file.write('\t"list": [\n')
+	file.write('\t\t' + ',\n\t\t'.join(map(lambda a: '{ "id": %d, "name": "%s", "LeagueId": %d, "TeamId": %d, "TeamName": "%s" }' % (a["CoachId"], a["CoachName"].encode('utf-8'), a["LeagueId"], a["TeamId"], a["TeamName"].encode('utf-8')), coaches)))
+	file.write('\n\t]\n}')
+	file.close()
+	print filename, 'written'
 
-		return teams
-
-	def saveCoaches(coaches, filename):
-		file = open( filename, "w")
-		file.write('{\n')
-		file.write('\t"type": "%s",\n' % "coach")
-		file.write('\t"internal": "true",\n')
-		file.write('\t"list": [\n')
-		file.write('\t\t' + ',\n\t\t'.join(map(lambda a: '{ "id": %d, "name": "%s", "LeagueId": %d, "TeamId": %d, "TeamName": "%s" }' % (a["CoachId"], a["CoachName"].encode('utf-8'), a["LeagueId"], a["TeamId"], a["TeamName"].encode('utf-8')), coaches)))
-		file.write('\n\t]\n}')
-		file.close()
-		print filename, 'written'
-
+def run():
 	u20 = getCoaches(4);
 	nt = getCoaches(2);
 
@@ -60,3 +61,6 @@ if __name__ == '__main__':
 
 	saveCoaches(u20, '/home/foxtrick/trunk/res/staff/u20coaches.json')
 	saveCoaches(nt, '/home/foxtrick/trunk/res/staff/ntcoaches.json')
+
+if __name__ == '__main__':
+	run();
