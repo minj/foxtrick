2b3903dc7a0de67b014399ee038aa6e9abaa31f9
CatzHoek <catzhoek@gmail.com>
2013-10-06 20:34:54 +0000

    pastebin/get.js Add the actual functionality
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@12087 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ content/api/pastebin/get.js
@@ -0,0 +1,24 @@
+if (!Foxtrick)
+	var Foxtrick = {};
+if (!Foxtrick.api)
+	Foxtrick.api = {};
+if (!Foxtrick.api.pastebin)
+	Foxtrick.api.pastebin = {};
+
+/**
+ * Retrieve pastebin pasted text as raw string
+ * This isn't technically an API function, just a simple get request
+ * Calls _generic and executes callback(response);
+ * In case of success response is a the raw text of the paste
+ * failure(response, status) is called if the request fails
+ * finalize(response, status) is always called
+ * @param	{String}		api			api name
+ * @param	{function}		callback	function to execute
+ * @param	{String}		paste_key	the pastebin paste_key
+ * @param	{[Function]}	failure		function to execute (optional)
+ * @param	{[Function]}	finalize	function to execute (optional)
+ */
+Foxtrick.api.pastebin.get = function(callback, paste_key, failure, finalize) {
+	var url = 'http://pastebin.com/raw.php?i=' + paste_key;
+	Foxtrick.api.pastebin._generic('get', url, callback, null, failure, finalize);
+};
