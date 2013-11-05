c353f4e9993701a68538226228c2aabecfed9ada
LA-MJ <4mr.minj@gmail.com>
2013-11-05 02:35:27 +0000

    fix android again...
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@12429 1e3807c3-0949-0410-b78e-337b627472c1

--- content/entry.js
+++ content/entry.js
@@ -87,7 +87,8 @@ Foxtrick.entry.contentScriptInit = function(data) {
 		}
 		else {
 			// fennec can access them from context, but they still need to get initilized
-			var coreModules = [Foxtrick.Prefs, Foxtrick.L10n, Foxtrick.XMLData];
+			// xmldata has nothing to init only fetch
+			var coreModules = [Foxtrick.Prefs, Foxtrick.L10n];
 			for (var i = 0; i < coreModules.length; ++i) {
 				if (typeof(coreModules[i].init) == 'function')
 					coreModules[i].init();
