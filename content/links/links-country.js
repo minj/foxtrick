03f89498a59f40d05d1b8ee35f3d6170486e2c26
LA-MJ <4mr.minj@gmail.com>
2014-01-07 23:17:37 +0000

    add trackernationalteamlink to country pages
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@12571 1e3807c3-0949-0410-b78e-337b627472c1

--- content/links/links-country.js
+++ content/links/links-country.js
@@ -26,25 +26,47 @@ Foxtrick.modules['LinksCountry'] = {
 		// get English name
 		var nameShort = Foxtrick.XMLData.League[leagueId].ShortName;
 
+		var ownBoxBody = Foxtrick.createFeaturedElement(doc, this, 'div');
+		var header = Foxtrick.L10n.getString('links.boxheader');
+		var ownBoxBodyId = 'foxtrick_links_content';
+		ownBoxBody.setAttribute('id', ownBoxBodyId);
+
+		var added = 0;
+
 		var links = Foxtrick.modules['Links'].getLinks('countrylink', {
-				'countryid': leagueId,
-				'english_name': nameShort
-			}, doc, this);
+			'countryid': leagueId,
+			'english_name': nameShort
+		}, doc, this);
 
 		if (links.length > 0) {
-			var ownBoxBody = Foxtrick.createFeaturedElement(doc, this, 'div');
-			var header = Foxtrick.L10n.getString('links.boxheader');
-			var ownBoxBodyId = 'foxtrick_links_content';
-			ownBoxBody.setAttribute('id', ownBoxBodyId);
-
 			for (var k = 0; k < links.length; k++) {
 				links[k].link.className = 'inner';
 				ownBoxBody.appendChild(links[k].link);
+				added++;
 			}
+		}
 
+		if (Foxtrick.Prefs.isModuleEnabled('LinksTracker')) {
+			var links2 = Foxtrick.modules['Links'].getLinks('trackernationalteamlink', {
+				'countryid': leagueId,
+			}, doc, Foxtrick.modules['LinksTracker']);
+			for (var k = 0; k < links2.length; k++) {
+				links2[k].link.className = 'flag inner';
+				var img = links2[k].link.getElementsByTagName('img')[0];
+				var style = 'vertical-align:top; margin-top:1px; background: ' +
+					'transparent url(/Img/Flags/flags.gif) no-repeat scroll ' + (-20) * leagueId +
+					'px 0pt; -moz-background-clip: -moz-initial; -moz-background-origin: ' +
+					'-moz-initial; -moz-background-inline-policy: -moz-initial;';
+				img.setAttribute('style', style);
+				img.src = '/Img/Icons/transparent.gif';
+				ownBoxBody.appendChild(links2[k].link);
+				++added;
+			}
+		}
+		if (added) {
 			var box = Foxtrick.addBoxToSidebar(doc, header, ownBoxBody, -20);
 			box.id = 'ft-links-box';
+			Foxtrick.util.links.add(doc, ownBoxBody, this.MODULE_NAME, { 'countryid': leagueId });
 		}
-		Foxtrick.util.links.add(doc, ownBoxBody, this.MODULE_NAME, { 'countryid': leagueId });
 	}
 };
