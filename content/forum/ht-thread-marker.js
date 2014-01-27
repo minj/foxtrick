79ad0b1ecd3fdc57f2f2880c706232dd5ae0268b
LA-MJ <4mr.minj@gmail.com>
2014-01-27 21:46:08 +0000

    @13 ht-thread-marker: use data-author attribute pure CSS
    git-svn-id: https://foxtrick.googlecode.com/svn/branches/0.13.0@12693 1e3807c3-0949-0410-b78e-337b627472c1

--- content/forum/ht-thread-marker.js
+++ content/forum/ht-thread-marker.js
@@ -8,32 +8,5 @@
 Foxtrick.modules['HTThreadMarker'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.FORUM,
 	PAGES: ['forum'],
-	CSS: Foxtrick.InternalPath + 'resources/css/ht-thread.css',
-
-	run: function(doc) {
-
-		var markThreads = function(threads){
-			Foxtrick.map( function(threadLink){
-				var title = threadLink.getAttribute('title');
-				var prefixInThreadName = threadLink.textContent.match(/ HT-\S+/);
-				var prefixInTitle = title.match(/ HT-\S+/);
-
-				if(!prefixInThreadName && prefixInTitle)
-					Foxtrick.addClass(threadLink.parentNode, 'ft-ht-thread');
-			}, threads);	
-		}
-
-		//threadList
-		var threadLinks = doc.querySelectorAll('.threadItem > td:nth-child(2) > .url > a' 
-			+ ', .folderitem > td:nth-child(2) > .fplLongThreadName > a');
-		markThreads(threadLinks);
-
-		//hotlinks
-		threadLinks = doc.querySelectorAll('#ctl00_ctl00_CPContent_CPMain_updHotThreads a');
-		markThreads(threadLinks);
-	},
-
-	change: function(doc) {
-		this.run(doc);
-	}
+	CSS: Foxtrick.InternalPath + 'resources/css/ht-thread.css'
 };
