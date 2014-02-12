e1985a7d08c62d644c843e1b3e2f6ed9cfabfda6
LA-MJ <4mr.minj@gmail.com>
2014-02-12 19:08:26 +0000

    lintify series-transfers
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@12868 1e3807c3-0949-0410-b78e-337b627472c1

--- content/information-aggregation/series-transfers.js
+++ content/information-aggregation/series-transfers.js
@@ -11,229 +11,239 @@ Foxtrick.modules['SeriesTransfers'] = {
 
 	run: function(doc) {
 		var leagueTable = doc.getElementById('mainBody').getElementsByTagName('table')[0];
-		
+
 		// checks whether a team is ownerless
-		var isNotOwnerless = function(link) { return !Foxtrick.hasClass(link, 'shy') && Foxtrick.getParameterFromUrl(link.getAttribute('href'), 'teamid')};
-		
+		var isNotOwnerless = function(link) {
+			return !Foxtrick.hasClass(link, 'shy') &&
+				Foxtrick.getParameterFromUrl(link.getAttribute('href'), 'teamid');
+		};
+
 		// get bots/ownerless
 		var teams = leagueTable.getElementsByTagName('a');
 		var teamLinks = Foxtrick.filter(isNotOwnerless, teams);
-		
-		//get teamdIds
-		var teamIds = []
-		for(var i in teamLinks){
+
+		// get teamdIds
+		var teamIds = [];
+		for (var i in teamLinks) {
 			var teamId = Foxtrick.getParameterFromUrl(teamLinks[i].getAttribute('href'), 'teamid');
-			teamIds.push( teamId );
+			teamIds.push(teamId);
 		}
 
-		//build batchArgs
-		var batchArgs = []
+		// build batchArgs
+		var batchArgs = [];
 		Foxtrick.map(function(n) {
-				var args = { 'teamid': n, 'file': 'players'};
-				batchArgs.push(args);
-			}, teamIds);
+			var args = { 'teamid': n, 'file': 'players' };
+			batchArgs.push(args);
+		}, teamIds);
 
-		//create html
+		// create html
 		var div = Foxtrick.createFeaturedElement(doc, this, 'div');
 		var mainBody = doc.getElementById('mainBody');
-		var before = doc.getElementById('ctl00_ctl00_CPContent_CPMain_ucForumSneakpeek_updSneakpeek');
+		var before =
+			doc.getElementById('ctl00_ctl00_CPContent_CPMain_ucForumSneakpeek_updSneakpeek');
 		mainBody.insertBefore(div, before);
 
 		var mainBox = doc.createElement('div');
 		Foxtrick.addClass(mainBox, 'mainBox');
 
 		var h2 = doc.createElement('h2');
-		h2.textContent = Foxtrick.L10n.getString("SeriesTransfers.header");
+		h2.textContent = Foxtrick.L10n.getString('SeriesTransfers.header');
 
 		mainBox.appendChild(h2);
 		div.appendChild(mainBox);
 
-		var createRowElement = function (type, textContent){
-			var elem = doc.createElement(type);	
+		var createRowElement = function(type, textContent) {
+			var elem = doc.createElement(type);
 			elem.textContent = textContent;
 			return elem;
-		}
+		};
 
 		var table = doc.createElement('table');
-		Foxtrick.addClass(table, "hidden");
+		Foxtrick.addClass(table, 'hidden');
 		table.setAttribute('id', 'ft-players-for-sale');
 
-		//thead
-		var columns =	[{text:'Nationality.abbr', title:'Nationality'},						 
-						{text:'Player', title:'Player'}, 
-						{text:'Team', title:'Team'},
-						{text:'Speciality.abbr', title:'Speciality'}, 
-						{text:'Age.abbr', title:'Age'}, 
-						{text:'Experience.abbr', title:'Experience'}, 
-						{text:'Form.abbr', title:'Form'}, 
-						{text:'TSI.abbr', title:'TSI'}, 
-						{text:'Salary.abbr', title:'Salary'}, 
-						{text:'Injured.abbr', title:'Injured'}];
+		// thead
+		var columns = [
+			{ text: 'Nationality.abbr', title: 'Nationality' },
+			{ text: 'Player', title: 'Player' },
+			{ text: 'Team', title: 'Team' },
+			{ text: 'Speciality.abbr', title: 'Speciality' },
+			{ text: 'Age.abbr', title: 'Age' },
+			{ text: 'Experience.abbr', title: 'Experience' },
+			{ text: 'Form.abbr', title: 'Form' },
+			{ text: 'TSI.abbr', title: 'TSI' },
+			{ text: 'Salary.abbr', title: 'Salary' },
+			{ text: 'Injured.abbr', title: 'Injured' },
+		];
 
 		var thead = doc.createElement('thead');
 		var thead_tr = doc.createElement('tr');
-		for(var i = 0; i < columns.length; i++)
-		{
+		for (var i = 0; i < columns.length; i++) {
 			var localized_text = Foxtrick.L10n.getString(columns[i].text);
 			var localized_alt_n_title = Foxtrick.L10n.getString(columns[i].title);
 			var th = createRowElement('th', localized_text);
 			th.setAttribute('title', localized_alt_n_title);
 			th.setAttribute('alt', localized_alt_n_title);
-			thead_tr.appendChild(th);					
+			thead_tr.appendChild(th);
 		}
 		thead.appendChild(thead_tr);
 
-		//tbody
+		// tbody
 		var tbody = doc.createElement('tbody');
 		table.appendChild(thead);
 		table.appendChild(tbody);
 		mainBox.appendChild(table);
 
-		//loading note
+		// loading note
 		if (Foxtrick.Prefs.getBool('xmlLoad')) {
 			var loading = Foxtrick.util.note.createLoading(doc);
 			mainBox.appendChild(loading);
 		}
 
-		//retrieve currency rate
-		Foxtrick.util.currency.establish(doc, function(){
+		// retrieve currency rate
+		Foxtrick.util.currency.establish(doc, function() {
 			var currencyRate = Foxtrick.util.currency.getRate(doc);
-			//batch retrieve
+			// batch retrieve
 			Foxtrick.util.api.batchRetrieve(doc, batchArgs, { cache_lifetime: 'session' },
-				function(xmls, errors) {
-					if (xmls) {
-						var hasListedPlayers = false;
-						for (var i = 0; i < xmls.length; ++i) {
-							var xml = xmls[i];
-							var errorText = errors[i];
-							if (!xml || errorText) {
-								Foxtrick.log('No XML in batchRetrieve', batchArgs[i], errorText);
-								continue;
-							}
-							var tid = Number(xml.getElementsByTagName('TeamID')[0].textContent);
-							var teamName = xml.getElementsByTagName('TeamName')[0].textContent;
-							var players = xml.getElementsByTagName('Player');
-							for(var p = 0; p < players.length; p++){
-								var player = players[p];
-								var playerID = Number(player.getElementsByTagName('PlayerID')[0].textContent);
-								var isTransferListed = Number(player.getElementsByTagName('TransferListed')[0].textContent);
-								
-								if(isTransferListed){
-									hasListedPlayers = true;
+			  function(xmls, errors) {
+				if (xmls)
+					processXMLs(xmls, errors, currencyRate);
+			});
+		});
 
-									var playerName = player.getElementsByTagName('PlayerName')[0].textContent;
-									var playerTsi = Number(player.getElementsByTagName('TSI')[0].textContent);
-
-									var specialty 		= Number( player.getElementsByTagName('Specialty')[0].textContent );
-									var experience 		= Number( player.getElementsByTagName('Experience')[0].textContent );
-									var age 			= Number( player.getElementsByTagName('Age')[0].textContent );
-									var ageDays 		= Number( player.getElementsByTagName('AgeDays')[0].textContent );
-									var form 			= Number( player.getElementsByTagName('PlayerForm')[0].textContent );
-									var salary 			= Math.floor(Number( player.getElementsByTagName('Salary')[0].textContent ) / (10 * currencyRate));
-									var agreeability 	= Number( player.getElementsByTagName('Agreeability')[0].textContent );
-									var aggressivness 	= Number( player.getElementsByTagName('Aggressiveness')[0].textContent );
-									var honesty 		= Number( player.getElementsByTagName('Honesty')[0].textContent );
-									var leadership 		= Number( player.getElementsByTagName('Leadership')[0].textContent );
-									var cards 			= Number( player.getElementsByTagName('Cards')[0].textContent );
-									var countryID 		= Number( player.getElementsByTagName('CountryID')[0].textContent );
-									var specialty 		= Number( player.getElementsByTagName('Specialty')[0].textContent );
-									var injuryLevel 	= Number( player.getElementsByTagName('InjuryLevel')[0].textContent );
-
-									var tr = doc.createElement('tr');
-
-									//country + image
-									var countryTd = doc.createElement('td');
-									var flag = Foxtrick.util.id.createFlagFromCountryId(doc, countryID);
-									countryTd.appendChild(flag);
-									tr.appendChild( countryTd );
-
-									//link to player
-									var playerLinkTD = doc.createElement('td');
-									var playerLink = createRowElement('a', playerName);
-									playerLink.setAttribute('href', '/Club/Players/Player.aspx?PlayerID=' + playerID);
-									playerLink.setAttribute('alt', playerName);
-									playerLink.setAttribute('title', playerName);
-									playerLinkTD.appendChild( playerLink );
-									tr.appendChild( playerLinkTD );
-
-									//link to team
-									var teamLinkTD = doc.createElement('td');
-									var teamLink = createRowElement('a', teamName);
-									teamLink.setAttribute('href', '/Club/?TeamID=' + tid);
-									teamLink.setAttribute('alt', teamName);
-									teamLink.setAttribute('title', teamName);
-									teamLinkTD.appendChild( teamLink );
-									tr.appendChild( teamLinkTD );
-
-									//specialty + respective icon
-									var specialtyTD = doc.createElement('td');
-									var specialityFunc = function(cell, spec) {
-										if(spec){
-											var icon_suffix = '';
-											var specialtyName = Foxtrick.L10n.getSpecialityFromNumber(spec);
-											var specialtyUrl = Foxtrick.getSpecialtyImagePathFromNumber(spec);
-											Foxtrick.addImage(doc, cell, {
-												alt: specialtyName,
-												title: specialtyName,
-												src: specialtyUrl
-											});
-										}
-										cell.setAttribute('index', spec);
-									};
-									specialityFunc(specialtyTD, specialty);
-									tr.appendChild( specialtyTD );
-									
-									//rest
-									tr.appendChild( createRowElement('td', age + '.' + ageDays) );
-									tr.appendChild( createRowElement('td', experience) );
-									tr.appendChild( createRowElement('td', form) );
-									tr.appendChild( createRowElement('td', Foxtrick.formatNumber(playerTsi, '\u00a0') ) );
-									tr.appendChild( createRowElement('td', Foxtrick.formatNumber(salary, '\u00a0')) );
-																	
-									//injurytd
-									var injuryTD = doc.createElement('td');
-									var injuryFunc = function(cell, injury){
-										if(injury > -1){
-											if (injury == 0) { //bruised
-												var img = doc.createElement('img');
-												img.src = '/Img/Icons/bruised.gif';
-												img.alt = Foxtrick.L10n.getString('Bruised.abbr');
-												img.title = Foxtrick.L10n.getString('Bruised');
-												cell.appendChild(img);
-											} else {
-												var img = doc.createElement('img');
-												img.src = '/Img/Icons/injured.gif';
-												img.alt = Foxtrick.L10n.getString('Injured.abbr');
-												img.title = Foxtrick.L10n.getString('Injured');
-												cell.appendChild(img);
-												// player.injured is number from players page,
-												// or boolean from transfer result page.
-												if (typeof(injury) == 'number' && injury > 1) {
-													cell.appendChild(doc.createTextNode(injury));
-												}
-											}
-										}
-									}
-									injuryFunc(injuryTD, injuryLevel);
-									tr.appendChild( injuryTD );
-
-									tbody.appendChild( tr );
-								}
-							}
+		var processXMLs = function(xmls, errors, currencyRate) {
+			var getNumFromXML = function(field) {
+				return Number(player.getElementsByTagName(field)[0].textContent);
+			};
+			var injuryFunc = function(cell, injury) {
+				if (injury > -1) {
+					var img;
+					if (injury === 0) { //bruised
+						img = doc.createElement('img');
+						img.src = '/Img/Icons/bruised.gif';
+						img.alt = Foxtrick.L10n.getString('Bruised.abbr');
+						img.title = Foxtrick.L10n.getString('Bruised');
+						cell.appendChild(img);
+					}
+					else {
+						img = doc.createElement('img');
+						img.src = '/Img/Icons/injured.gif';
+						img.alt = Foxtrick.L10n.getString('Injured.abbr');
+						img.title = Foxtrick.L10n.getString('Injured');
+						cell.appendChild(img);
+						// player.injured is number from players page,
+						// or boolean from transfer result page.
+						if (typeof(injury) == 'number' && injury > 1) {
+							cell.appendChild(doc.createTextNode(injury));
 						}
-						Foxtrick.removeClass(table, "hidden");
-						loading.parentNode.removeChild(loading);
-						if(!hasListedPlayers){
-							table.parentNode.removeChild(table);
-							var span = doc.createElement('span');
-							span.textContent = Foxtrick.L10n.getString("SeriesTransfers.notransfers");
-							mainBox.appendChild(span);
-						} 
-					} else {
-						//chpp retrieved shizzle
 					}
 				}
-			);
-		});
+			};
+			var specialityFunc = function(cell, spec) {
+				if (spec) {
+					var icon_suffix = '';
+					var specialtyName = Foxtrick.L10n.getSpecialityFromNumber(spec);
+					var specialtyUrl = Foxtrick.getSpecialtyImagePathFromNumber(spec);
+					Foxtrick.addImage(doc, cell, {
+						alt: specialtyName,
+						title: specialtyName,
+						src: specialtyUrl
+					});
+				}
+				cell.setAttribute('index', spec);
+			};
+			var hasListedPlayers = false;
+			for (var i = 0; i < xmls.length; ++i) {
+				var xml = xmls[i];
+				var errorText = errors[i];
+				if (!xml || errorText) {
+					Foxtrick.log('No XML in batchRetrieve', batchArgs[i], errorText);
+					continue;
+				}
+				var tid = Number(xml.getElementsByTagName('TeamID')[0].textContent);
+				var teamName = xml.getElementsByTagName('TeamName')[0].textContent;
+				var players = xml.getElementsByTagName('Player');
+				for (var p = 0; p < players.length; p++) {
+					var player = players[p];
+					var playerID = getNumFromXML('PlayerID');
+					var isTransferListed = getNumFromXML('TransferListed');
+
+					if (isTransferListed) {
+						hasListedPlayers = true;
+
+						var playerName = player.getElementsByTagName('PlayerName')[0].textContent;
+						var playerTsi = getNumFromXML('TSI');
+						var specialty = getNumFromXML('Specialty');
+						var experience = getNumFromXML('Experience');
+						var age = getNumFromXML('Age');
+						var ageDays = getNumFromXML('AgeDays');
+						var form = getNumFromXML('PlayerForm');
+						var salary = Math.floor(getNumFromXML('Salary') / (10 * currencyRate));
+						var agreeability = getNumFromXML('Agreeability');
+						var aggressivness = getNumFromXML('Aggressiveness');
+						var honesty = getNumFromXML('Honesty');
+						var leadership = getNumFromXML('Leadership');
+						var cards = getNumFromXML('Cards');
+						var countryID = getNumFromXML('CountryID');
+						var injuryLevel = getNumFromXML('InjuryLevel');
+
+						var tr = doc.createElement('tr');
+
+						// country + image
+						var countryTd = doc.createElement('td');
+						var flag = Foxtrick.util.id.createFlagFromCountryId(doc, countryID);
+						countryTd.appendChild(flag);
+						tr.appendChild(countryTd);
+
+						// link to player
+						var playerLinkTD = doc.createElement('td');
+						var playerLink = createRowElement('a', playerName);
+						playerLink.href = '/Club/Players/Player.aspx?PlayerID=' + playerID;
+						playerLink.setAttribute('alt', playerName);
+						playerLink.setAttribute('title', playerName);
+						playerLinkTD.appendChild(playerLink);
+						tr.appendChild(playerLinkTD);
+
+						// link to team
+						var teamLinkTD = doc.createElement('td');
+						var teamLink = createRowElement('a', teamName);
+						teamLink.setAttribute('href', '/Club/?TeamID=' + tid);
+						teamLink.setAttribute('alt', teamName);
+						teamLink.setAttribute('title', teamName);
+						teamLinkTD.appendChild(teamLink);
+						tr.appendChild(teamLinkTD);
+
+						// specialty + respective icon
+						var specialtyTD = doc.createElement('td');
+						specialityFunc(specialtyTD, specialty);
+						tr.appendChild(specialtyTD);
+
+						// rest
+						tr.appendChild(createRowElement('td', age + '.' + ageDays));
+						tr.appendChild(createRowElement('td', experience));
+						tr.appendChild(createRowElement('td', form));
+						var tsi = Foxtrick.formatNumber(playerTsi, '\u00a0');
+						tr.appendChild(createRowElement('td', tsi));
+						var wage = Foxtrick.formatNumber(salary, '\u00a0');
+						tr.appendChild(createRowElement('td', wage));
+
+						// injurytd
+						var injuryTD = doc.createElement('td');
+						injuryFunc(injuryTD, injuryLevel);
+						tr.appendChild(injuryTD);
+
+						tbody.appendChild(tr);
+					}
+				}
+			}
+			Foxtrick.removeClass(table, 'hidden');
+			loading.parentNode.removeChild(loading);
+			if (!hasListedPlayers) {
+				table.parentNode.removeChild(table);
+				var span = doc.createElement('span');
+				span.textContent = Foxtrick.L10n.getString('SeriesTransfers.notransfers');
+				mainBox.appendChild(span);
+			}
+		};
 	}
 };
