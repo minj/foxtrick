073353b5a3cc978bb16281c8f1b0ab846148cec3
LA-MJ <4mr.minj@gmail.com>
2014-02-27 08:07:52 +0000

    lintify log.js
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@12985 1e3807c3-0949-0410-b78e-337b627472c1

--- content/util/log.js
+++ content/util/log.js
@@ -13,7 +13,7 @@ if (!Foxtrick)
  * @param  {string} err String to log. Will be converted into string if otherwise.
  */
 Foxtrick.error = function(err) {
-	Foxtrick.log(new Error('' + err));
+	Foxtrick.log(new Error(String(err)));
 };
 
 
@@ -24,38 +24,34 @@ Foxtrick.log = function() {
 		return;
 
 	// compile everything into a single string for trivial logging contexts
-	var i, hasError = false, concated = '';
+	var i, content, hasError = false, concated = '', item = '';
 	for (i = 0; i < arguments.length; ++i) {
-		var content = arguments[i];
-		var item = '';
+		content = arguments[i];
 		if (content instanceof Error) {
 			// exception
 			hasError = true;
 			if (Foxtrick.arch == 'Gecko') {
 				// there also would be Components.stack if someone finds a nice way to display it
-				item = content.message + '\n'
-					+ content.fileName + ': ' + content.lineNumber + '\n'
-					+ 'Stack trace:\n'
-					+ content.stack.replace(/^.*@/g, ''); // strip function names
+				item = content.message + '\n' +
+					content.fileName + ': ' + content.lineNumber + '\n' +
+					'Stack trace:\n' +
+					content.stack.replace(/^.*@/g, ''); // strip function names
 			}
 			else if (Foxtrick.arch == 'Sandboxed') {
 				item = content.message;
-				if (typeof (content.stack) != 'undefined')
+				if (typeof content.stack !== 'undefined')
 					item += '\n' + content.stack;
 			}
 		}
-		else if (typeof(content) != 'string') {
+		else if (typeof content !== 'string') {
 			try {
 				item = JSON.stringify(content);
-				if (item == '{}' && typeof(content) == 'object') {
-					// stringify didn't work as intented
-					var j;
-					for (j in content)
-						item += j + ':' + content[j] + '\n';
-				}
 			}
 			catch (e) {
 				item = String(content);
+				var j;
+				for (j in content)
+					item += j + ':' + content[j] + '\n';
 			}
 		}
 		else {
@@ -63,15 +59,16 @@ Foxtrick.log = function() {
 		}
 		concated += ' ' + item;
 	}
+	concated += '\n';
 	// add the compiled string to HTML log container
-	Foxtrick.log.cache += concated + '\n';
+	Foxtrick.log.cache += concated;
 	Foxtrick.log.flush();
 
 	// store in debug storage (retrieved with forum debug log icon)
 	if (Foxtrick.chromeContext() == 'content')
-		Foxtrick.SB.extension.sendRequest({ req: 'addDebugLog', log: concated + '\n' });
+		Foxtrick.SB.extension.sendRequest({ req: 'addDebugLog', log: concated });
 	else {
-		Foxtrick.addToDebugLogStorage(concated + '\n');
+		Foxtrick.addToDebugLogStorage(concated);
 	}
 
 	// live logging in browser for devs
@@ -81,13 +78,12 @@ Foxtrick.log = function() {
 	}
 	catch (e) {}
 
-	if (typeof(Firebug) != 'undefined'
-		&& typeof(Firebug.Console) != 'undefined'
-		&& typeof(Firebug.Console.log) == 'function') {
+	if (typeof Firebug !== 'undefined' && typeof Firebug.Console !== 'undefined' &&
+	    typeof Firebug.Console.log === 'function') {
 		// if Firebug.Console.log is available, make use of it
 		Firebug.Console.log(arguments);
 	}
-	if (typeof console != 'undefined' && typeof console.log == 'function') {
+	if (typeof console !== 'undefined' && typeof console.log === 'function') {
 		// if console.log is available, make use of it
 		// for firefox it's in the webconsole (ctrl+shift+K) in preferences.html
 		// and logging in the browser console (ctrl+shift+J)
@@ -96,16 +92,16 @@ Foxtrick.log = function() {
 			// print a nice stack trace for exceptions in the console
 			var stackDumped = false;
 			for (i = 0; i < arguments.length; ++i) {
-				var content = arguments[i];
-				if (content instanceof Error && typeof (content.stack) != 'undefined') {
-					if (typeof console.error != 'undefined')
+				content = arguments[i];
+				if (content instanceof Error && typeof content.stack !== 'undefined') {
+					if (typeof console.error !== 'undefined')
 						console.error(content.stack);
 					else
 						console.log(content.stack);
 					stackDumped = true;
 				}
 			}
-			if (!stackDumped && typeof(console.trace) == 'function')
+			if (!stackDumped && typeof console.trace === 'function')
 				console.trace();
 		}
 	}
@@ -117,33 +113,34 @@ Foxtrick.log = function() {
 		else
 			Services.console.logStringMessage('FoxTrick: ' + concated);
 	}
-	if (typeof(dump) == 'function' && Foxtrick.Prefs.getBool('dump')) {
+	if (typeof dump === 'function' && Foxtrick.Prefs.getBool('dump')) {
 		// window.dump, a Gecko extension
 		if (Foxtrick.chromeContext() === 'background') {
 			var lines = concated.split(/\n/);
-			lines = Foxtrick.map(function(l) {
+			Foxtrick.forEach(function(l) {
 				dump('FT: ' + l.substr(0, 500) + '\n');
 			}, lines);
 		}
 		else {
 			// send it via background since fennec doesn't show all dumps from content side
-			Foxtrick.SB.extension.sendRequest({ req: 'log', log: concated + '\n' });
+			Foxtrick.SB.extension.sendRequest({ req: 'log', log: concated });
 		}
 	}
 };
 
 // environment info shown in log as header
 Foxtrick.log.header = function(doc) {
-	var headString = 'Version %1, %2 platform, %3 locale, %4 layout, %5 direction'
-				.replace(/%1/, Foxtrick.version() + ' ' + Foxtrick.branch())
-				.replace(/%2/, Foxtrick.arch + ' ' + Foxtrick.platform)
-				.replace(/%3/, Foxtrick.Prefs.getString('htLanguage'))
-				.replace(/%4/, Foxtrick.util.layout.isStandard(doc) ? 'standard' : 'simple')
-				.replace(/%5/, Foxtrick.util.layout.isRtl(doc) ? 'RTL' : 'LTR');
+	var headString = 'Version %1, %2 platform, %3 locale, %4 layout, %5 direction'.
+		replace(/%1/, Foxtrick.version() + ' ' + Foxtrick.branch()).
+		replace(/%2/, Foxtrick.arch + ' ' + Foxtrick.platform).
+		replace(/%3/, Foxtrick.Prefs.getString('htLanguage')).
+		replace(/%4/, Foxtrick.util.layout.isStandard(doc) ? 'standard' : 'simple').
+		replace(/%5/, Foxtrick.util.layout.isRtl(doc) ? 'RTL' : 'LTR');
+
 	if (Foxtrick.isStage(doc))
 		headString += ', Stage';
-	headString += '\n';
-	return headString;
+
+	return headString + '\n';
 };
 
 // cache log contents, will be flushed to page after calling
