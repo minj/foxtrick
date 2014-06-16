d8904e453522b23345d0cccf0c94819ab01169e9
tasosventouris <tasosventouris@gmail.com>
2014-06-16 12:16:42 +0000

    #1204: information-aggregation/top-players-owner.js (changes upon request in comments)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13202 1e3807c3-0949-0410-b78e-337b627472c1

--- content/information-aggregation/top-players-owner.js
+++ content/information-aggregation/top-players-owner.js
@@ -7,42 +7,39 @@
 Foxtrick.modules['TopPlayersOwner'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.INFORMATION_AGGREGATION,
 	PAGES: ['statsTopPlayers'],
+	CSS: Foxtrick.InternalPath + 'resources/css/top-players-owner.css',
 
 	run: function(doc) {
 
-			var teamId = Foxtrick.modules.Core.getSelfTeamInfo().teamId;
+		var teamId = Foxtrick.modules.Core.getSelfTeamInfo().teamId;
+		var ids = [];
 
-			var ids = [];
+		Foxtrick.util.api.retrieve(doc, [
+			['file', 'players'],
+			['version', '2.2'],
+			['teamId', teamId]
+		],
+		{ cache_lifetime: 'session' },
 
-
-			Foxtrick.util.api.retrieve(doc, [
-                        ['file', 'players'],
-                        ['version', '2.3'],
-                        ['teamID', teamId],
-						['actionType', "view"]
-					],
-                  { cache_lifetime: 'session' },
-
-                  function(xml, errorText) {
-                        if (!xml || errorText) {
-                                Foxtrick.log(errorText);
-                                return;
-                        }
-                        
-                        var all = xml.getElementsByTagName('PlayerID');
-                        for (var i = 0; i < all.length; i++)
-                                ids.push(all[i].textContent);
-                       
-						var allPlayers = doc.getElementsByTagName('a');
-
-						for (var i = 0; i<allPlayers.length; ++i) {
-							var re = /playerId=([0-9]+)/g;
-							var player = re.exec(allPlayers[i].href)
-							if (player && ids.indexOf(player[1]) >= 0) {
-								allPlayers[i].setAttribute('style', 'background-color:yellow;')
-							}			
-						}
-					}
-				);
+		function(xml, errorText) {
+			if (!xml || errorText) {
+				Foxtrick.log(errorText);
+				return;
 			}
-		}
+                        
+			var all = xml.getElementsByTagName('PlayerID');
+			for (var i = 0; i < all.length; i++)
+				ids.push(all[i].textContent);
+                       
+			var allPlayers = doc.getElementsByClassName('mainBox')[0].getElementsByTagName('a');
+			for (var i = 0; i<allPlayers.length; ++i) {
+				var player = Foxtrick.getParameterFromUrl(allPlayers[i].href, 'playerId')
+				if (player && ids.indexOf(player) >= 0) {
+					var row = allPlayers[i].parentNode.parentNode
+					Foxtrick.addClass(row, 'ft-top-players-owner');
+					Foxtrick.makeFeaturedElement(row, Foxtrick.modules.TopPlayersOwner)
+				}			
+			}
+		});
+	}
+}
