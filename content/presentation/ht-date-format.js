0a0a7a2f175ad31a4733a84d96b80d18cbe0c767
LA-MJ <4mr.minj@gmail.com>
2014-06-25 15:30:00 +0000

    ht-date-format: run on global week [post=16412248.173]
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13264 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/ht-date-format.js
+++ content/presentation/ht-date-format.js
@@ -7,6 +7,7 @@
 
 Foxtrick.modules['HTDateFormat'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.PRESENTATION,
+	OUTSIDE_MAINBODY: true,
 	PAGES: ['all'],
 	NICE: 20, // after anthing that adds dates
 	OPTIONS: ['LocalSeason', 'FirstDayOfWeekOffset'],
@@ -14,6 +15,50 @@ Foxtrick.modules['HTDateFormat'] = {
 	OPTION_EDITS_DISABLED_LIST: [true, false],
 
 	run: function(doc) {
+		var module = this;
+
+		var useLocal = Foxtrick.Prefs.isModuleOptionEnabled('HTDateFormat', 'LocalSeason');
+		var weekOffset = Foxtrick.Prefs.getString('module.' + this.MODULE_NAME +
+		                                         '.FirstDayOfWeekOffset_text');
+		// set up function for separating date and week/season,
+		var separate = function(node) {
+			node.appendChild(doc.createTextNode(' '));
+		};
+
+		var modifyDate = function(node) {
+			if (Foxtrick.hasClass(node, 'ft-date') ||
+			    (Foxtrick.hasClass(node, 'matchdate') && !Foxtrick.isPage(doc, 'playerStats')))
+				return;
+
+			var date;
+			if (node.hasAttribute('x-ht-date')) {
+				// attribute x-ht-date set by LocalTime, while inner
+				// text is not HT date but local date
+				date = new Date();
+				date.setTime(node.getAttribute('x-ht-date'));
+			}
+			else
+				date = Foxtrick.util.time.getDateFromText(node.textContent);
+
+			if (date) {
+				var htDate = Foxtrick.util.time.gregorianToHT(date, weekOffset, useLocal);
+				Foxtrick.addClass(node, 'ft-date');
+				separate(node);
+				var wsDate = Foxtrick.createFeaturedElement(doc, module, 'span');
+				wsDate.textContent = '(w/s)'.replace(/w/, htDate.week).replace(/s/, htDate.season);
+				node.appendChild(wsDate);
+			}
+		};
+
+		var now = Foxtrick.util.time.getHtDate(doc);
+		var htDate = Foxtrick.util.time.gregorianToHT(now, weekOffset, useLocal);
+		var online = doc.getElementById('online');
+		online.textContent = online.textContent.trim().replace(/\d+$/, htDate.week);
+
+		var mainBody = doc.getElementById('mainBody');
+		if (!mainBody)
+			return;
+
 		var pages = [
 			'transfersTeam', 'transfersPlayer', 'transfer',
 			'transferCompare', 'match', 'matches', 'matchesArchive',
@@ -28,43 +73,6 @@ Foxtrick.modules['HTDateFormat'] = {
 			}, pages)) {
 			return;
 		}
-
-		var mainBody = doc.getElementById('mainBody');
-		if (!mainBody) return;
-
-		var useLocal = Foxtrick.Prefs.isModuleOptionEnabled('HTDateFormat', 'LocalSeason');
-		var weekOffset = Foxtrick.Prefs.getString('module.' + this.MODULE_NAME +
-		                                         '.FirstDayOfWeekOffset_text');
-		// set up function for separating date and week/season,
-		var separate = function(node) {
-			node.appendChild(doc.createTextNode(' '));
-		};
-
-		var modifyDate = function(node) {
-			if (Foxtrick.hasClass(node, 'ft-date') || (Foxtrick.hasClass(node, 'matchdate')
-			    && !Foxtrick.isPage(doc, 'playerStats')))
-				return;
-			if (node.hasAttribute('x-ht-date')) {
-				// attribute x-ht-date set by LocalTime, while inner
-				// text is not HT date but local date
-				var date = new Date();
-				date.setTime(node.getAttribute('x-ht-date'));
-			}
-			else
-				var date = Foxtrick.util.time.getDateFromText(node.textContent);
-
-			if (date) {
-				var htDate = Foxtrick.util.time.gregorianToHT(date, weekOffset, useLocal);
-				Foxtrick.addClass(node, 'ft-date');
-				separate(node);
-				var wsDate = Foxtrick.createFeaturedElement(doc,
-				                                            Foxtrick.modules.HTDateFormat, 'span');
-				wsDate.textContent = '(w/s)'
-					.replace(/w/, htDate.week)
-					.replace(/s/, htDate.season);
-				node.appendChild(wsDate);
-			}
-		};
 
 		var dates = mainBody.getElementsByClassName('date');
 		Foxtrick.map(modifyDate, dates);
