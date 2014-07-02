73a465347184a48e58fa9cb0350a1801ecaf2cae
LA-MJ <4mr.minj@gmail.com>
2014-07-02 00:30:19 +0000

    add nationality and tracker support in youth player details
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13284 1e3807c3-0949-0410-b78e-337b627472c1

--- content/links/links-youth.js
+++ content/links/links-youth.js
@@ -90,8 +90,8 @@ Foxtrick.modules['LinksYouthPlayerDetail'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.LINKS,
 	PAGES: ['youthPlayerDetails'],
 	OPTION_FUNC: function(doc, callback) {
-		return Foxtrick.modules['Links']
-			.getOptionsHtml(doc, 'LinksYouthPlayerDetail', 'youthplayerdetaillink', callback);
+		return Foxtrick.modules['Links'].getOptionsHtml(doc, 'LinksYouthPlayerDetail',
+		                                                'youthplayerdetaillink', callback);
 	},
 
 	run: function(doc) {
@@ -106,7 +106,7 @@ Foxtrick.modules['LinksYouthPlayerDetail'] = {
 		if (boxleft == null)
 			return;
 		var teamid = Foxtrick.util.id.findTeamId(boxleft);
-		if (teamid == '')
+		if (teamid === '')
 			return;
 
 		var main = doc.getElementById('ctl00_ctl00_CPContent_divStartMain');
@@ -116,7 +116,7 @@ Foxtrick.modules['LinksYouthPlayerDetail'] = {
 		var server = Foxtrick.Prefs.getBool('hty-stage') ? 'stage' : 'www';
 
 		var thisdiv = main.getElementsByTagName('div')[0];
-		var nationality = Foxtrick.util.id.findLeagueId(doc);
+		var countryid = Foxtrick.util.id.findLeagueId(main);
 		var playerid = Foxtrick.util.id.findPlayerId(thisdiv);
 		var playername = thisdiv.getElementsByTagName('a')[1].textContent;
 
@@ -126,31 +126,56 @@ Foxtrick.modules['LinksYouthPlayerDetail'] = {
 		var days = age.days;
 
 		//addExternalLinksToYouthdetail
-		var ownBoxBody = null;
+		var added = 0;
+		var header = Foxtrick.L10n.getString('links.boxheader');
+		var ownBoxBody = Foxtrick.createFeaturedElement(doc, this, 'div');
+		var ownBoxBodyId = 'foxtrick_links_content';
+		ownBoxBody.id = ownBoxBodyId;
+
 		var links = Foxtrick.modules['Links'].getLinks('youthplayerdetaillink', {
 			'ownteamid': ownteamid,
 			'teamid': teamid,
 			'youthteamid': youthteamid,
 			'playerid': playerid,
+			'playername': playername,
 			'age': years,
 			'age_days': days,
 			'owncountryid': owncountryid,
+			'nationality': countryid,
 			'server': server
 		}, doc, this);
 		if (links.length > 0) {
-			ownBoxBody = Foxtrick.createFeaturedElement(doc, this, 'div');
-			var header = Foxtrick.L10n.getString('links.boxheader');
-			var ownBoxBodyId = 'foxtrick_links_content';
-			ownBoxBody.setAttribute('id', ownBoxBodyId);
-
 			for (var k = 0; k < links.length; k++) {
 				links[k].link.className = 'inner';
 				ownBoxBody.appendChild(links[k].link);
+				added++;
 			}
+		}
+		if (Foxtrick.Prefs.isModuleEnabled('LinksTracker')) {
+			var links2 = Foxtrick.modules['Links'].getLinks('trackeryouthlink', {
+				'countryid': countryid,
+			}, doc, Foxtrick.modules['LinksTracker']);
+			if (links2.length > 0) {
+				for (var i = 0; i < links2.length; ++i) {
+					links2[i].link.className = 'flag inner';
+					var img = links2[i].link.getElementsByTagName('img')[0];
+					var style = 'vertical-align:top; margin-top:1px; background: transparent ' +
+						'url(/Img/Flags/flags.gif) no-repeat scroll ' + (-20) * countryid +
+						'px 0pt; background-clip: -moz-initial; background-origin: ' +
+						'-moz-initial; background-inline-policy: -moz-initial;';
+					img.setAttribute('style', style);
+					img.src = '/Img/Icons/transparent.gif';
+					ownBoxBody.appendChild(links2[i].link);
+					++added;
+				}
+			}
+		}
+		if (added) {
 			var box = Foxtrick.addBoxToSidebar(doc, header, ownBoxBody, -20);
 			box.id = 'ft-links-box';
+			Foxtrick.util.links.add(doc, ownBoxBody, this.MODULE_NAME, {});
 		}
-		Foxtrick.util.links.add(doc, ownBoxBody, this.MODULE_NAME, {});
+
 	}
 };
 
