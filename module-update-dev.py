cb776972d5f5d537a81f22448465bacce0f88d70
tasosventouris <tasosventouris@gmail.com>
2014-07-04 17:04:33 +0000

    New version of module-update-dev.py [testing]
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13312 1e3807c3-0949-0410-b78e-337b627472c1

--- module-update-dev.py
+++ module-update-dev.py
@@ -1,80 +1,117 @@
-# Update module listing in manifest files
-#
-# Author: Anastasios Ventouris <tasosventouris@gmail.com>
-
 import re
+import argparse
+import os
 
-targets = [
+
+def update(sourcefile, excludefile, dirfile):
+    targets = [
 	{
 		"file" : "manifest.json",
 		"from" : "//<!-- categorized modules -->",
 		"to" : "//<!-- end categorized modules -->",
 		"prefix" : "\t\t\t\"content/",
-		"suffix" : "\",\n"
+		"suffix" : "\","
 	},
 	{
 		"file" : "Info.plist",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t\t\t\t<string>content/",
-		"suffix" : "</string>\n"
+		"suffix" : "</string>"
 	},
 	{
 		"file" : "content/scripts-fennec.js",
 		"from" : "//<!-- categorized modules -->",
 		"to" : "//<!-- end categorized modules -->",
 		"prefix" : "\t\t'",
-		"suffix" : "',\n"
+		"suffix" : "',"
 	},
 	{
 		"file" : "content/bootstrap-firefox.js",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t\t'",
-		"suffix" : "',\n"
+		"suffix" : "',"
 	},
 	{
 		"file" : "content/preferences.html",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t<script type=\"application/x-javascript\" src=\"./",
-		"suffix" : "\"></script>\n"
+		"suffix" : "\"></script>"
 	},
 	{
 		"file" : "content/background.html",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t<script type=\"application/x-javascript\" src=\"./",
-		"suffix" : "\"></script>\n"
+		"suffix" : "\"></script>"
 	}
-]
+    ]
 
-#get module file list from file *modules*
-modules = open("modules", "r").read()
-modules = modules.split("\n")
+    #if exclude file in fuction, read the file
+    if excludefile:
+        ignorelist = open(excludefile, "r").read()
+        ignorelist = ignorelist.split("\n")
+        
+        #delete files from ignorelist in the build
+        for mod in ignorelist:
+            path = dirfile + mod
+            path = path.replace("/",'\\')
+            if os.path.isfile(path):
+                os.remove(path)
+            else:
+                print path
+    else:
+        ignorelist = []
 
-#remove the last empty entry due to *newline character* in file
-del modules[-1]
+    #get module file list from file *modules*
+    modules = open("modules", "r").read()
+    modules = modules.split("\n")
 
-#iterate through targets
-for tar in targets:
-    #open the file and copy the content to a variable
-    fh = file(tar['file'], 'r')
-    originaltext = fh.read()
-    fh.close()
+    #remove the last empty entry due to *newline character* in file
+    del modules[-1]
 
-    #create the text with the modules list
-    modstring = "\n"
-    for mod in modules:
-        line = tar['prefix'] + mod + tar['suffix']
-        modstring = modstring + line
+    #iterate through targets
+    for tar in targets:
+        #open the file and copy the content to a list variable
+        fh = file(tar['file'], 'r')
+        originaltext = fh.read().splitlines()
+        fh.close()
 
-    #find and replace the content between *from* and *to*
-    reg = "(?<=%s).*?(?=\t*%s)" % (tar['from'],tar['to'])
-    r = re.compile(reg,re.DOTALL)
-    result = r.sub(modstring, originaltext)
+        #find the index of *from* and *to*
+        start = 0
+        end = 0
+        for step in originaltext:
+            if tar['from'] in step:
+                start = originaltext.index(step)
+            elif tar['to'] in step:
+                end = originaltext.index(step)
+                
+        #create the text with the modules list
+        modstring = []
+        for mod in modules:
+            line = tar['prefix'] + mod + tar['suffix']
+            modstring.append(line)
 
-    #write the new file
-    f_out = file(tar['file'], 'w')
-    f_out.write(result)
-    f_out.close()    
+        #replace the content of the file   
+        originaltext[start+1:end] = modstring
+
+        #join the list to a text file
+        result = '\n'.join(originaltext)
+
+        #write the new file
+        f_out = file(tar['file'], 'w')
+        f_out.write(result)
+        f_out.close()    
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Module-Update.py')
+    parser.add_argument('-source','--sourcefile', help='The name of the source file. Default value = modules', required=False, default="modules")
+    parser.add_argument('-exclude','--excludefile', help='The name of the source file to ignore. Default value = None', required=False)
+    parser.add_argument('-dir','--dirfile', help='????. Default value = None', required=False)
+
+    args = parser.parse_args()
+    update(args.sourcefile,args.excludefile,args.dirfile)
