c6d997ec6b96d7af2552a6234a4ef622bb268dc4
LA-MJ <4mr.minj@gmail.com>
2014-07-05 09:11:08 +0000

    module-update.py: deel with line separators consistently finally produces no diff
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13314 1e3807c3-0949-0410-b78e-337b627472c1

--- module-update.py
+++ module-update.py
@@ -13,56 +13,56 @@ def update(sourcefile, excludefile, dirfile):
 		"from" : "//<!-- categorized modules -->",
 		"to" : "//<!-- end categorized modules -->",
 		"prefix" : "\t\t\t\"content/",
-		"suffix" : "\","
+		"suffix" : "\",\n"
 	},
 	{
 		"file" : "Info.plist",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t\t\t\t<string>content/",
-		"suffix" : "</string>"
+		"suffix" : "</string>\n"
 	},
 	{
 		"file" : "content/scripts-fennec.js",
 		"from" : "//<!-- categorized modules -->",
 		"to" : "//<!-- end categorized modules -->",
 		"prefix" : "\t\t'",
-		"suffix" : "',"
+		"suffix" : "',\n"
 	},
 	{
 		"file" : "content/bootstrap-firefox.js",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t\t'",
-		"suffix" : "',"
+		"suffix" : "',\n"
 	},
 	{
 		"file" : "content/preferences.html",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t<script type=\"application/x-javascript\" src=\"./",
-		"suffix" : "\"></script>"
+		"suffix" : "\"></script>\n"
 	},
 	{
 		"file" : "content/background.html",
 		"from" : "<!-- categorized modules -->",
 		"to" : "<!-- end categorized modules -->",
 		"prefix" : "\t<script type=\"application/x-javascript\" src=\"./",
-		"suffix" : "\"></script>"
+		"suffix" : "\"></script>\n"
 	}
     ]
 
     path = dirfile+"/"
-    
+
     #if exclude file in fuction, read the file
     if excludefile:
         ignorelist = open(path+excludefile, "r").read()
-        ignorelist = ignorelist.split("\n")
-        
+        ignorelist = ignorelist.splitlines()
+
         #delete files from ignorelist in the build
         for mod in ignorelist:
             pathfile = dirfile + '/content/' + mod
-            #replace the backslash with a slash 
+            #replace the backslash with a slash
             #pathfile = pathfile.replace("/",'\\')
             if os.path.isfile(pathfile):
                 os.remove(pathfile)
@@ -71,43 +71,38 @@ def update(sourcefile, excludefile, dirfile):
 
     #get module file list from file *modules*
     modules = open(path+sourcefile, "r").read()
-    modules = modules.split("\n")
-
-    #remove the last empty entry due to *newline character* in file
-    del modules[-1]
+    modules = modules.splitlines()
 
     #iterate through targets
     for tar in targets:
         #open the file and copy the content to a list variable
         fh = file(path+tar['file'], 'r')
-        originaltext = fh.read().splitlines()
+        #keep \n
+        lines = fh.read().splitlines(True)
         fh.close()
 
         #find the index of *from* and *to*
         start = 0
         end = 0
-        for step in originaltext:
+        for step in lines:
             if tar['from'] in step:
-                start = originaltext.index(step)
+                start = lines.index(step)
             elif tar['to'] in step:
-                end = originaltext.index(step)
-                
+                end = lines.index(step)
+
         #create the text with the modules list
-        modstring = []
+        modlines = []
         for mod in modules:
             line = tar['prefix'] + mod + tar['suffix']
-            modstring.append(line)
+            modlines.append(line)
 
-        #replace the content of the file   
-        originaltext[start+1:end] = modstring
-
-        #join the list to a text file
-        result = '\n'.join(originaltext)
+        #replace the content of the file
+        lines[start+1:end] = modlines
 
         #write the new file
         f_out = file(path+tar['file'], 'w')
-        f_out.write(result)
-        f_out.close()    
+        f_out.writelines(lines)
+        f_out.close()
 
 
 if __name__ == '__main__':
