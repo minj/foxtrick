00ce080439d59d0f5fe854d47b46795744924eeb
LA-MJ <4mr.minj@gmail.com>
2014-07-08 00:30:34 +0000

    add-class.js: cleanup
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13345 1e3807c3-0949-0410-b78e-337b627472c1

--- content/add-class.js
+++ content/add-class.js
@@ -7,7 +7,14 @@
 
 Foxtrick.modules.AddClass = {
 	CORE_MODULE: true,
-	PAGES: ['playerDetails', 'search', 'bookmarks', 'match', 'transferCompare', 'playerHistory'],
+	PAGES: [
+		'playerDetails',
+		'playerHistory',
+		'search',
+		'transferCompare',
+		'bookmarks',
+		'match',
+	],
 	NICE: -20, // place before all date-related modules
 
 	run: function(doc) {
@@ -15,9 +22,10 @@ Foxtrick.modules.AddClass = {
 			this.addDateForTl(doc);
 		else if (Foxtrick.isPage(doc, 'playerHistory'))
 			this.addDateForFeed(doc);
-		else if (Foxtrick.isPage(doc, 'search'))
-			this.addDateForTable(doc, doc.getElementById('ctl00_ctl00_CPContent_CPMain' +
-			                     '_grdYouthSeries_ctl00'));
+		else if (Foxtrick.isPage(doc, 'search')) {
+			var table = doc.getElementById('ctl00_ctl00_CPContent_CPMain_grdYouthSeries_ctl00');
+			this.addDateForTable(doc, table);
+		}
 		else if (Foxtrick.isPage(doc, 'transferCompare'))
 			this.addDateForTable(doc, doc.querySelector('#mainBody table'));
 		else if (Foxtrick.isPage(doc, 'bookmarks'))
@@ -33,8 +41,8 @@ Foxtrick.modules.AddClass = {
 	replaceInNode: function(doc, parent, timeRe) {
 		var node = parent.firstChild;
 		while (node) {
-			if (node.parentNode.getElementsByClassName('date').length == 0
-				&& node.nodeType == Foxtrick.NodeTypes.TEXT_NODE) {
+			if (!parent.getElementsByClassName('date').length &&
+			    node.nodeType == Foxtrick.NodeTypes.TEXT_NODE) {
 
 				var texts = node.textContent.match(timeRe);
 				if (texts) {
@@ -42,14 +50,17 @@ Foxtrick.modules.AddClass = {
 					var span = doc.createElement('span');
 					span.textContent = texts[2];
 					span.className = 'date';
-					node.parentNode.insertBefore(span, node.nextSibling);
-					node.parentNode.insertBefore(doc.createTextNode(texts[3]), span.nextSibling);
+					parent.insertBefore(span, node.nextSibling);
+					parent.insertBefore(doc.createTextNode(texts[3]), span.nextSibling);
 					break;
 				}
 			}
 			node = node.nextSibling;
 		}
 	},
+
+	timeRe: /(^|.*?\D+?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+\d{1,2}\D\d{1,2})(\D+?.*?|$)/,
+	timeReNoTime: /(^|.*?\D+?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+)(\D+?.*?|$)/,
 
 	// add date class for match
 	addDateForMatch: function(doc) {
@@ -58,15 +69,15 @@ Foxtrick.modules.AddClass = {
 			return;
 
 		// start time
-		var timeRe = /(.*?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+\d{1,2}\D\d{1,2})(.*?)/;
+		var timeRe = this.timeRe;
 		var parent = mainBody.getElementsByClassName('byline')[0];
 		this.replaceInNode(doc, parent, timeRe);
 	},
 	addDateForFeed: function(doc) {
 		var feeds = doc.getElementsByClassName('feed');
-		for (var i = 0, feed; i < feeds.length && (feed = feeds[i]); i++) {
+		Foxtrick.forEach(function(feed) {
 			Foxtrick.addClass(feed, 'date');
-		}
+		}, feeds);
 	},
 	// add date class for bookmark
 	addDateForBookmarks: function(doc) {
@@ -74,7 +85,7 @@ Foxtrick.modules.AddClass = {
 		if (!mainBody)
 			return;
 
-		var timeRe = /(.+?\D+?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+\d{1,2}\D\d{1,2})(\D+?.+?)/;
+		var timeRe = this.timeRe;
 		var cells = mainBody.getElementsByTagName('td');
 		Foxtrick.map(function(cell) {
 			Foxtrick.modules.AddClass.replaceInNode(doc, cell, timeRe);
@@ -86,14 +97,14 @@ Foxtrick.modules.AddClass = {
 		if (!table)
 			return;
 
-		var timeReFull = /(.+?\D+?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+\d{1,2}\D\d{1,2})(\D+?.+?)/;
-		var timeReShort = /(.+?\D+?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+)(\D+?.+?)/;
+		var timeReFull = this.timeRe;
+		var timeReShort = this.timeReNoTime;
 
 		// start time
 		var cells = table.getElementsByTagName('td');
 		Foxtrick.map(function(cell) {
-			if (cell.getElementsByClassName('date').length == 0) {
-				if (cell.textContent.search(timeReFull) != -1)
+			if (!cell.getElementsByClassName('date').length) {
+				if (timeReFull.test(cell.textContent))
 					Foxtrick.modules.AddClass.replaceInNode(doc, cell, timeReFull);
 				else
 					Foxtrick.modules.AddClass.replaceInNode(doc, cell, timeReShort);
@@ -107,32 +118,18 @@ Foxtrick.modules.AddClass = {
 		if (!transferList)
 			return;
 
-		var timeRe = /(.+?\D+?)(\d{1,4}\D\d{1,2}\D\d{1,4}\D?\s+\d{1,2}\D\d{1,2})(\D+?.+?)/;
+		var timeRe = this.timeRe;
 
 		// deadline time
 		var dlPar = transferList.getElementsByTagName('p')[0];
-		if (!dlPar) return;
-		if (dlPar.getElementsByClassName('date').length == 0)
+		if (!dlPar)
+			return;
+		if (!dlPar.getElementsByClassName('date').length)
 			this.replaceInNode(doc, dlPar, timeRe);
 
 		// reload time
 		var firstLine = transferList.getElementsByClassName('float_left')[0];
-		if (firstLine.getElementsByClassName('date').length == 0) {
-			var timeNode = firstLine.lastChild;
-			if (timeNode.textContent.search(timeRe) >= 0) {
-				var time = timeNode.textContent.match(timeRe)[1];
-				var timePos = timeNode.textContent.search(timeRe);
-				var before = timeNode.textContent.substr(0, timePos);
-				var after = timeNode.textContent.substr(timePos + time.length);
-
-				var span = doc.createElement('span');
-				span.className = 'date';
-				span.textContent = time;
-				timeNode.parentNode.appendChild(doc.createTextNode(before));
-				timeNode.parentNode.appendChild(span);
-				timeNode.parentNode.appendChild(doc.createTextNode(after));
-				timeNode.parentNode.removeChild(timeNode);
-			}
-		}
+		if (firstLine)
+			this.replaceInNode(doc, firstLine, timeRe);
 	}
 };
