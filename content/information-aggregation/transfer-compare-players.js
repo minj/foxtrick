2f28a7e09d12a1f8a42bc7ac36544ca3819ac9c8
tasosventouris <tasosventouris@gmail.com>
2014-07-11 22:30:31 +0000

    Add percentage of increase-decrease in players transfers history. (#526)
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13398 1e3807c3-0949-0410-b78e-337b627472c1

--- content/information-aggregation/transfer-compare-players.js
+++ content/information-aggregation/transfer-compare-players.js
@@ -8,48 +8,52 @@
 
 Foxtrick.modules['TransferComparePlayers'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.INFORMATION_AGGREGATION,
-	PAGES: ['transferCompare'],
+	PAGES: ['transferCompare','transfersPlayer'],
 	// CSS: Foxtrick.InternalPath + 'resources/css/transfercompareplayers.css',
 
 	run: function(doc) {
-		var table = doc.querySelectorAll('#mainBody > table')[0];
-		if (!table)
-			return;
-		if (table.rows[0].cells.length < 5)
-			return;
+		var isCompare = Foxtrick.isPage(doc, 'transferCompare');
+		var isHistory = Foxtrick.isPage(doc, 'transfersPlayer');
 
-		var link = Foxtrick.createFeaturedElement(doc, this, 'a');
-		Foxtrick.addClass(link, 'ft-transfer-compare-players ft-link');
+		if (isCompare) {
+			var table = doc.querySelectorAll('#mainBody > table')[0];
+			if (!table)
+				return;
+			if (table.rows[0].cells.length < 5)
+				return;
 
-		var title = Foxtrick.L10n.getString('TransferComparePlayers.fetch');
-		Foxtrick.addImage(doc, link, {
-			alt: title,
-			title: title,
-			height: 16,
-			width: 16,
-			src: Foxtrick.InternalPath + 'resources/img/Aiga_downarrow_yellow.png',
-		}, null, function(button) {
-			Foxtrick.addClass(button, 'ft-transfer-compare-players');
-		});
+			var link = Foxtrick.createFeaturedElement(doc, this, 'a');
+			Foxtrick.addClass(link, 'ft-transfer-compare-players ft-link');
 
-		var div = doc.getElementsByClassName('speedBrowser')[0].parentNode;
-		div.appendChild(doc.createTextNode(' '));
-		div.appendChild(link);
+			var title = Foxtrick.L10n.getString('TransferComparePlayers.fetch');
+			Foxtrick.addImage(doc, link, {
+				alt: title,
+				title: title,
+				height: 16,
+				width: 16,
+				src: Foxtrick.InternalPath + 'resources/img/Aiga_downarrow_yellow.png',
+			}, null, function(button) {
+				Foxtrick.addClass(button, 'ft-transfer-compare-players');
+			});
 
-		Foxtrick.onClick(link, function() {
-			var count = table.rows.length;
-			var argsPlayers = [];
-			for (var i = 5; i < count; i++) {
-				var player = table.rows[i].cells[0].getElementsByTagName('a')[0];
-				if (player) {
-					var playerid = Foxtrick.getParameterFromUrl(player.href, 'playerId');
-					argsPlayers.push([['file', 'playerdetails'],['version', '2.1'], ['playerId', parseInt(playerid,10)]]);
-				};
-			}
-			Foxtrick.util.api.batchRetrieve(doc, argsPlayers, { cache_lifetime: 'session' },
-				  function(xmls, errors) {
-					if (xmls)
-						for (i = 0; i < xmls.length; ++i) {
+			var div = doc.getElementsByClassName('speedBrowser')[0].parentNode;
+			div.appendChild(doc.createTextNode(' '));
+			div.appendChild(link);
+
+			Foxtrick.onClick(link, function() {
+				var count = table.rows.length;
+				var argsPlayers = [];
+				for (var i = 5; i < count; i++) {
+					var player = table.rows[i].cells[0].getElementsByTagName('a')[0];
+					if (player) {
+						var playerid = Foxtrick.getParameterFromUrl(player.href, 'playerId');
+						argsPlayers.push([['file', 'playerdetails'],['version', '2.1'], ['playerId', parseInt(playerid,10)]]);
+					};
+				}
+				Foxtrick.util.api.batchRetrieve(doc, argsPlayers, { cache_lifetime: 'session' },
+					function(xmls, errors) {
+						if (xmls)
+							for (i = 0; i < xmls.length; ++i) {
 								if (xmls[i] && !errors[i]) {
 									var age = xmls[i].getElementsByTagName('Age')[0].innerHTML;
 									var days = xmls[i].getElementsByTagName('AgeDays')[0].innerHTML;
@@ -73,87 +77,125 @@ Foxtrick.modules['TransferComparePlayers'] = {
 								else
 									Foxtrick.log('No XML in batchRetrieve', argsPlayers[i], errors[i]);
 							}
-				});
-		});
+					});
+			});
 
-		var ownPlayer = table.rows[1].cells[0].getElementsByTagName('a')[0];
-		var ownPlayerId = Foxtrick.getParameterFromUrl(ownPlayer.href, 'playerId');
+			var ownPlayer = table.rows[1].cells[0].getElementsByTagName('a')[0];
+			var ownPlayerId = Foxtrick.getParameterFromUrl(ownPlayer.href, 'playerId');
 
-		var count = table.rows.length;
-		var priceArray = [];
-		var tsiArray = [];
-		for (var i = 5; i < count; i++) {
-			var thisPlayer = table.rows[i].cells[0].getElementsByTagName('a')[0];
-			var thisPlayerId = Foxtrick.getParameterFromUrl(thisPlayer.href, 'playerId');
-			if (thisPlayerId === ownPlayerId) {
-				Foxtrick.addClass(table.rows[i], 'ft-tableHighlight');
+			var count = table.rows.length;
+			var priceArray = [];
+			var tsiArray = [];
+			for (var i = 5; i < count; i++) {
+				var thisPlayer = table.rows[i].cells[0].getElementsByTagName('a')[0];
+				var thisPlayerId = Foxtrick.getParameterFromUrl(thisPlayer.href, 'playerId');
+				if (thisPlayerId === ownPlayerId) {
+					Foxtrick.addClass(table.rows[i], 'ft-tableHighlight');
+				}
+				if (table.rows[i].cells[3].textContent.search(/\d/) != -1) {
+					var thisPrice = Foxtrick.trimnum(table.rows[i].cells[3].textContent);
+					priceArray.push(thisPrice);
+				}
+				if (table.rows[i].cells[2].textContent.search(/\d/) != -1) {
+					var thisTsi = Foxtrick.trimnum(table.rows[i].cells[2].textContent);
+					tsiArray.push(thisTsi);
+				}
 			}
-			if (table.rows[i].cells[3].textContent.search(/\d/) != -1) {
-				var thisPrice = Foxtrick.trimnum(table.rows[i].cells[3].textContent);
-				priceArray.push(thisPrice);
+
+			priceArray.sort(function(a, b) { return a - b; });
+			tsiArray.sort(function(a, b) { return a - b; });
+
+			var medianprice = 0;
+			var avgprice = 0;
+			var mediantsi = 0;
+			var avgtsi = 0;
+
+			var len = priceArray.length;
+			for (var i = 0; i < priceArray.length; ++i) {
+				avgprice = avgprice + priceArray[i];
+				avgtsi = avgtsi + tsiArray[i];
 			}
-			if (table.rows[i].cells[2].textContent.search(/\d/) != -1) {
-				var thisTsi = Foxtrick.trimnum(table.rows[i].cells[2].textContent);
-				tsiArray.push(thisTsi);
+			avgprice = Math.round(avgprice / len);
+			avgtsi = Math.round(avgtsi / len);
+
+			if (len % 2 == 1) {
+				medianprice = Math.round(priceArray[(len - 1) / 2]);
+				mediantsi = Math.round(tsiArray[(len - 1) / 2]);
 			}
+			else {
+				medianprice = Math.round((priceArray[(len / 2) - 1] + priceArray[len / 2]) / 2);
+				mediantsi = Math.round((tsiArray[(len / 2) - 1] + tsiArray[len / 2]) / 2);
+			}
+
+			if (count > 0) {
+				var currency = table.rows[5].cells[3].textContent.match(/\D+$/)[0].trim();
+				var row = Foxtrick.insertFeaturedRow(table, this, table.rows.length);
+				var cell = row.insertCell(0);
+				cell.className = 'left bold';
+				cell.colSpan = 2;
+				cell.textContent = Foxtrick.L10n.getString('TransferComparePlayers.median');
+				cell = row.insertCell(1);
+				cell.className = 'right bold nowrap';
+				cell.colSpan = 1;
+				cell.textContent = Foxtrick.formatNumber(mediantsi, '\u00a0');
+				cell = row.insertCell(2);
+				cell.className = 'right bold nowrap';
+				cell.textContent = Foxtrick.formatNumber(medianprice, '\u00a0') + ' ' + currency;
+				cell = row.insertCell(3);
+				cell.colSpan = 2;
+
+				row = Foxtrick.insertFeaturedRow(table, this, table.rows.length);
+				cell = row.insertCell(0);
+				cell.className = 'left bold';
+				cell.colSpan = 2;
+				cell.textContent = Foxtrick.L10n.getString('TransferComparePlayers.average');
+				cell = row.insertCell(1);
+				cell.className = 'right bold nowrap';
+				cell.colSpan = 1;
+				cell.textContent = Foxtrick.formatNumber(avgtsi, '\u00a0');
+				cell = row.insertCell(2);
+				cell.className = 'right bold nowrap';
+				cell.textContent = Foxtrick.formatNumber(avgprice, '\u00a0') + ' ' + currency;
+				cell = row.insertCell(3);
+				cell.colSpan = 2;
+			}
+		} else if (isHistory) {
+			var table = doc.querySelectorAll('#mainBody > table')[0];
+			if (!table)
+				return;
+			if (table.rows[0].cells.length < 2)
+				return;
+
+			var count = table.rows.length;
+			var th = doc.createElement('th');
+			th.textContent = Foxtrick.L10n.getString('TransferComparePlayers.difference');
+			table.rows[0].insertBefore(th,table.rows[0].cells[4]);
+
+			for (var i = 1; i < count-1; i++) {
+				var next = Foxtrick.trimnum(table.rows[i].cells[3].textContent);
+				var last = Foxtrick.trimnum(table.rows[i+1].cells[3].textContent);
+				var percentage = doc.createElement('span');
+
+				if (next > last) {
+					var dif = (next-last)/last;
+					Foxtrick.addClass(percentage, 'ft-player-transfer-history positive');
+					percentage.textContent = "(+" + Math.round(dif*100) + "%)";
+				} else {
+					var dif = (last-next)/next;
+					Foxtrick.addClass(percentage, 'ft-player-transfer-history negative');
+					percentage.textContent = "(-" + Math.round(dif*100) + "%)";
+				}
+
+				table.rows[i].insertCell(4);
+				table.rows[i].cells[4].appendChild(percentage);
+
+			};
+
+			var td = doc.createElement('td');
+			td.textContent = " ";
+			table.rows[i].insertBefore(td, table.rows[i].cells[4]);
+			}
+
 		}
 
-		priceArray.sort(function(a, b) { return a - b; });
-		tsiArray.sort(function(a, b) { return a - b; });
-
-		var medianprice = 0;
-		var avgprice = 0;
-		var mediantsi = 0;
-		var avgtsi = 0;
-
-		var len = priceArray.length;
-		for (var i = 0; i < priceArray.length; ++i) {
-			avgprice = avgprice + priceArray[i];
-			avgtsi = avgtsi + tsiArray[i];
-		}
-		avgprice = Math.round(avgprice / len);
-		avgtsi = Math.round(avgtsi / len);
-
-		if (len % 2 == 1) {
-			medianprice = Math.round(priceArray[(len - 1) / 2]);
-			mediantsi = Math.round(tsiArray[(len - 1) / 2]);
-		}
-		else {
-			medianprice = Math.round((priceArray[(len / 2) - 1] + priceArray[len / 2]) / 2);
-			mediantsi = Math.round((tsiArray[(len / 2) - 1] + tsiArray[len / 2]) / 2);
-		}
-
-		if (count > 0) {
-			var currency = table.rows[5].cells[3].textContent.match(/\D+$/)[0].trim();
-			var row = Foxtrick.insertFeaturedRow(table, this, table.rows.length);
-			var cell = row.insertCell(0);
-			cell.className = 'left bold';
-			cell.colSpan = 2;
-			cell.textContent = Foxtrick.L10n.getString('TransferComparePlayers.median');
-			cell = row.insertCell(1);
-			cell.className = 'right bold nowrap';
-			cell.colSpan = 1;
-			cell.textContent = Foxtrick.formatNumber(mediantsi, '\u00a0');
-			cell = row.insertCell(2);
-			cell.className = 'right bold nowrap';
-			cell.textContent = Foxtrick.formatNumber(medianprice, '\u00a0') + ' ' + currency;
-			cell = row.insertCell(3);
-			cell.colSpan = 2;
-
-			row = Foxtrick.insertFeaturedRow(table, this, table.rows.length);
-			cell = row.insertCell(0);
-			cell.className = 'left bold';
-			cell.colSpan = 2;
-			cell.textContent = Foxtrick.L10n.getString('TransferComparePlayers.average');
-			cell = row.insertCell(1);
-			cell.className = 'right bold nowrap';
-			cell.colSpan = 1;
-			cell.textContent = Foxtrick.formatNumber(avgtsi, '\u00a0');
-			cell = row.insertCell(2);
-			cell.className = 'right bold nowrap';
-			cell.textContent = Foxtrick.formatNumber(avgprice, '\u00a0') + ' ' + currency;
-			cell = row.insertCell(3);
-			cell.colSpan = 2;
-		}
-	}
-};
+	};
