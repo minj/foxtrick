670f9963a993d54b0c6991b390395d3af0fea7fb
LA-MJ <4mr.minj@gmail.com>
2014-07-14 10:25:52 +0000

    add stack to low-level FF errors
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13429 1e3807c3-0949-0410-b78e-337b627472c1

--- content/bootstrap-firefox.js
+++ content/bootstrap-firefox.js
@@ -265,7 +265,7 @@ FoxtrickFirefox.prototype = {
 			}
 		}
 		catch (e) {
-			e.message = 'Foxtrick ERROR: ' + e.message;
+			e.message = 'Foxtrick ERROR: ' + e.message + '\n' + e.stack + '\n';
 			Services.console.logStringMessage(e);
 		}
 		// loading Foxtrick into window.Foxtrick
@@ -274,7 +274,7 @@ FoxtrickFirefox.prototype = {
 				Services.scriptloader.loadSubScript(PATH + this.scripts[i], this.owner, 'UTF-8');
 			}
 			catch (e) {
-				e.message = 'Foxtrick ERROR: ' + e.message;
+				e.message = 'Foxtrick ERROR: ' + e.message + '\n' + e.stack + '\n';
 				Services.console.logStringMessage(e);
 			}
 		}
@@ -447,8 +447,10 @@ function loadIntoWindow(window) {
 		window.Foxtrick.init();
 	}
 	catch (e) {
-		dump('FoxTrick error: ' + e + '\n');
-		Cu.reportError('FoxTrick error: ' + e);
+		let msg = 'FoxTrick error: ' + e + '\n' + e.stack + '\n';
+		dump(msg);
+		Services.console.logStringMessage(msg);
+		Cu.reportError(msg);
 	}
 }
 
