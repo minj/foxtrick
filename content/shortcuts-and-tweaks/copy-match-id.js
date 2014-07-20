e6906f116f82f05a50caa8bad95abf6d972de8f9
LA-MJ <4mr.minj@gmail.com>
2014-07-20 11:02:42 +0000

    rewrite copy-matchid.js should work on worldMatches now
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13521 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/copy-match-id.js
+++ content/shortcuts-and-tweaks/copy-match-id.js
@@ -7,14 +7,17 @@
 
 Foxtrick.modules['CopyMatchID'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	PAGES: ['matches', 'matchesArchive', 'matchesHistory',
-					'matchesLatest', 'arena'],
+	PAGES: [
+		'matches', 'worldMatches', 'matchesArchive',
+		'matchesHistory', 'matchesLatest', 'arena'
+	],
 	//CSS: Foxtrick.InternalPath + 'resources/css/copy-match-id.css',
 
 	run: function(doc) {
-		var copyId = function(ev) {
+		var module = this;
+		var copyId = function() {
 			try {
-				var matchid = ev.target.getAttribute('matchid');
+				var matchid = this.getAttribute('data-matchid');
 				Foxtrick.copyStringToClipboard(matchid);
 				Foxtrick.util.note.add(doc, Foxtrick.L10n.getString('copy.matchid.copied'),
 				                       'ft-match-id-copy-note');
@@ -24,51 +27,33 @@ Foxtrick.modules['CopyMatchID'] = {
 			}
 		};
 
+		var mainBody = doc.getElementById('mainBody');
+		var matches = '.' + Foxtrick.Pages.Matches.Types.join(', .');
 		var count = 0;
 
-		if (Foxtrick.isPage(doc, 'matchesArchive')
-			|| Foxtrick.isPage(doc, 'matches')
-			|| Foxtrick.isPage(doc, 'matchesHistory')
-			|| Foxtrick.isPage(doc, 'matchesLatest')
-			|| Foxtrick.isPage(doc, 'arena')) {
-			var cells = doc.getElementById('mainBody').getElementsByTagName('td');
-			for (var i = 0; i < cells.length; i++) {
-				// nested table check
-				var td = cells[i].getElementsByTagName('td').length;
-				if (td != 0)
-					continue;
+		var images = mainBody.querySelectorAll(matches);
+		Foxtrick.forEach(function(image) {
+			var parent = image.parentNode;
+			if (parent.nodeName !== 'TD')
+				// only support tables for now
+				return;
+			var row = parent.parentNode;
+			var matchLink = row.querySelector('a[href*="/Club/Matches/Match.aspx"]');
+			if (!matchLink)
+				return;
 
-				var images = cells[i].getElementsByTagName('img');
-				for (var j = 0; j < images.length; j++) {
-					var image = images[j];
-					if (image.className.search(new RegExp('matchLeague|matchFriendly' +
-					    '|matchMasters|matchCup|matchQualification|matchTournament' +
-						'|matchSingleMatch')) == -1)
-						continue;
+			var href = matchLink.href;
+			var matchid = href.replace(/.+matchID=/i, '').match(/^\d+/)[0];
 
-					var href = '';
-					var links = cells[i + 1].getElementsByTagName('a');
-					for (var j = 0; j < links.length; j++) {
-						if (links[j].href.match(/Club\/Matches\/Match\.aspx/i)) {
-							href = links[j].href;
-							break;
-						}
-					}
-					if (href == '')
-						continue;
-					var matchid = href.replace(/.+matchID=/i, '').match(/^\d+/)[0];
+			image.setAttribute('title', image.title + ': ' +
+			                   Foxtrick.L10n.getString('copy.matchid'));
+			image = Foxtrick.makeFeaturedElement(image, module);
+			image.setAttribute('data-matchid', matchid);
+			image.id = 'ft_' + module.MODULE_NAME + count;
+			Foxtrick.onClick(image, copyId);
+			Foxtrick.addClass(image, 'ft-link');
 
-					image.setAttribute('title', image.title + ': ' +
-					                   Foxtrick.L10n.getString('copy.matchid'));
-					image = Foxtrick.makeFeaturedElement(image, this);
-					image.setAttribute('matchid', matchid);
-					image.setAttribute('id', '_' + this.MODULE_NAME + count);
-					Foxtrick.onClick(image, copyId);
-					Foxtrick.addClass(image, 'ft-link');
-
-					count++;
-				}
-			}
-		}
+			count++;
+		}, images);
 	}
 };
