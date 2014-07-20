f0a93ea41dfee831f3b36e5c53627896faebf36a
LA-MJ <4mr.minj@gmail.com>
2014-07-20 11:02:53 +0000

    rewrite relive-links.js should be more smarter: no more hard-coded cell indexes enables on worldMatches
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13523 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/relive-links.js
+++ content/shortcuts-and-tweaks/relive-links.js
@@ -7,7 +7,10 @@
 
 Foxtrick.modules['ReLiveLinks'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	PAGES: ['matches', 'matchesArchive', 'cupMatches', 'fixtures', 'youthFixtures', 'series'],
+	PAGES: [
+		'matches', 'worldMatches', 'matchesArchive', 'cupMatches',
+		'fixtures', 'youthFixtures', 'series'
+	],
 	NICE: -1, // before any modules that might change row count
 	run: function(doc) {
 		// don't run on live table
@@ -16,21 +19,35 @@ Foxtrick.modules['ReLiveLinks'] = {
 			return;
 		}
 
+		var matchSelector = 'a[href*="/Club/Matches/Match.aspx"]';
+		var liveSelector = 'a[href*="/Club/Matches/Live.aspx"]';
+		var findMatchTdIdx = function(rows) {
+			var tbody = rows[0].parentNode;
+			var matchLink = tbody.querySelector(matchSelector);
+			if (!matchLink)
+				return -1;
+			var cell = matchLink.parentNode;
+			if (cell.nodeName !== 'TD')
+				// unknown structure
+				return -1;
+			var row = cell.parentNode;
+			if (row.parentNode !== tbody)
+				// unknown structure
+				return -1;
+			return Foxtrick.indexOf(row.cells, cell);
+		};
+
 		var img = doc.createElement('img');
 		img.src = '/Img/Icons/transparent.gif';
 		img.alt = img.title = 'HT Re-Live';
 		img.className = 'matchHTReLive';
 
-		var rows, row, liveTdIdx = 4, cloneUrlIdx = 5, tdCount = 7, scoreIdx = 3;
-		var addAllLink, matches;
+		// var rows, row, liveTdIdx = 4, matchTdIdx = 5, tdCount = 7, scoreIdx = 3;
+		var rows, addAllLink, matches, insertCells = false, insertHeader = false;
 
 		if (Foxtrick.isPage(doc, 'series')) {
 
 			matches = [];
-			tdCount = 2;//3;
-			cloneUrlIdx = 0;
-			liveTdIdx = 2;
-			scoreIdx = 1;
 			rows = doc.querySelectorAll('table.indent.left.thin > tbody > tr');
 
 			if (!rows)
@@ -57,71 +74,80 @@ Foxtrick.modules['ReLiveLinks'] = {
 				rows[0].cells[0].appendChild(addAllSpan);
 		}
 		else if (Foxtrick.isPage(doc, 'fixtures') || Foxtrick.isPage(doc, 'youthFixtures')) {
-			tdCount = 4;
-			cloneUrlIdx = 1;
-			liveTdIdx = 3;
-			scoreIdx = 2;
 			rows = doc.querySelectorAll('#ctl00_ctl00_CPContent_CPMain_repFixtures tr');
+
 			if (Foxtrick.isPage(doc, 'youthFixtures')) {
-				for (var i = 0, m = rows.length; i < m; ++i) {
-					row = rows[i];
-					Foxtrick.insertFeaturedCell(row, this, liveTdIdx);
-				}
+				insertCells = true;
 			}
 		}
 		else if (Foxtrick.isPage(doc, 'cupMatches')) {
-			tdCount = 6;
-			cloneUrlIdx = 2;
 			rows = doc.querySelectorAll('table.indent > tbody > tr');
-			for (var i = 0, m = rows.length; i < m; ++i) {
-				row = rows[i];
-				if (i)
-					Foxtrick.insertFeaturedCell(row, this, liveTdIdx);
-				else {
-					var header = Foxtrick.createFeaturedElement(doc, this, 'th');
-					row.appendChild(header);
-				}
-			}
+			insertCells = true;
+			insertHeader = true;
 		}
 		else if (Foxtrick.isPage(doc, 'matchesArchive')) {
 			rows = doc.querySelectorAll('table.indent > tbody > tr');
-			for (var i = 0, m = rows.length; i < m; ++i) {
-				row = rows[i];
-				Foxtrick.insertFeaturedCell(row, this, liveTdIdx);
+			insertCells = true;
+		}
+		else {
+			rows = doc.querySelectorAll('table.naked > tbody > tr');
+			if (Foxtrick.isPage(doc, 'worldMatches')) {
+				insertCells = true;
 			}
 		}
-		else
-			rows = doc.querySelectorAll('table.naked > tbody > tr');
 
-		var tds, liveTd, address, id, source, links, link;
+		var matchTdIdx = findMatchTdIdx(rows);
+		if (matchTdIdx === -1)
+			return;
+		var scoreIdx = matchTdIdx + 1;
+		var liveTdIdx = matchTdIdx + 2;
 
-		for (var i = 0, m = rows.length; i < m; ++i) {
-			row = rows[i];
-			tds = row.getElementsByTagName('td');
-			if (tds.length != tdCount)
+		var i = 0;
+		if (insertCells) {
+			var j = 0;
+			if (insertHeader) {
+				var header = Foxtrick.createFeaturedElement(doc, this, 'th');
+				var lastCell = rows[0].cells.length - 1;
+				if (liveTdIdx < lastCell)
+					rows[0].insertBefore(header, rows[0].cells[liveTdIdx]);
+				else
+					rows[0].appendChild(header);
+				i = j = 1;
+			}
+			for (var m = rows.length; j < m; ++j) {
+				Foxtrick.insertFeaturedCell(rows[j], this, liveTdIdx);
+			}
+		}
+
+		for (var m = rows.length; i < m; ++i) {
+			var row = rows[i];
+			var tds = row.cells;
+			var scoreTd = tds[scoreIdx];
+			if (!scoreTd || !/^\d+\D+\d+$/.test(scoreTd.textContent.trim()))
 				continue;
-			if (!/^\d/.test(tds[scoreIdx].textContent.trim()))
+
+			var matchLink = row.querySelector(matchSelector);
+			if (!matchLink)
 				continue;
-			liveTd = tds[liveTdIdx];
-			if (liveTd && liveTd.getElementsByTagName('a').length)
+
+			var liveLink = row.querySelector(liveSelector);
+			if (liveLink)
 				continue;
-			links = tds[cloneUrlIdx].getElementsByTagName('a');
-			if (!links.length)
-				continue;
-			address = links[0].href;
-			id = Foxtrick.util.id.getMatchIdFromUrl(address);
-			source = Foxtrick.getParameterFromUrl(address, 'SourceSystem');
+
+			var url = matchLink.href;
+			var id = Foxtrick.util.id.getMatchIdFromUrl(url);
+			var source = Foxtrick.getParameterFromUrl(url, 'SourceSystem');
 			if (matches) {
 				matches.push(id);
 				continue;
-				//don't run on series
+				// don't run on series
 			}
-			address = '/Club/Matches/Live.aspx?matchID=' + id +
+			url = '/Club/Matches/Live.aspx?matchID=' + id +
 				'&actionType=addMatch&SourceSystem=' + source;
-			link = Foxtrick.createFeaturedElement(doc, this, 'a');
-			link.href = address;
-			link.appendChild(img.cloneNode(true));
-			liveTd.appendChild(link);
+			liveLink = Foxtrick.createFeaturedElement(doc, this, 'a');
+			liveLink.href = url;
+			liveLink.appendChild(img.cloneNode(true));
+			tds[liveTdIdx].appendChild(liveLink);
 		}
 		if (addAllLink)
 			addAllLink.href = '/Club/Matches/Live.aspx?matchID=' + matches.join(',') +
