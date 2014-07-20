0a09b3f21b9335260b94395c9d336190d24494b9
LA-MJ <4mr.minj@gmail.com>
2014-07-20 11:02:47 +0000

    clean relive-links.js
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13522 1e3807c3-0949-0410-b78e-337b627472c1

--- content/shortcuts-and-tweaks/relive-links.js
+++ content/shortcuts-and-tweaks/relive-links.js
@@ -7,15 +7,12 @@
 
 Foxtrick.modules['ReLiveLinks'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.SHORTCUTS_AND_TWEAKS,
-	PAGES: ['matches', 'matchesArchive', 'cupMatches',
-			 'fixtures', 'youthFixtures', 'series'],
-	NICE: -1, //before any modules that might change row count
+	PAGES: ['matches', 'matchesArchive', 'cupMatches', 'fixtures', 'youthFixtures', 'series'],
+	NICE: -1, // before any modules that might change row count
 	run: function(doc) {
-		//don't run on live table
+		// don't run on live table
 		var liveSeriesLink = doc.getElementById('ctl00_ctl00_CPContent_CPMain_hlLive');
 		if (liveSeriesLink && liveSeriesLink.hasAttribute('disabled')) {
-			this.CSS = null;
-			Foxtrick.util.css.reload_module_css(doc);
 			return;
 		}
 
@@ -25,9 +22,11 @@ Foxtrick.modules['ReLiveLinks'] = {
 		img.className = 'matchHTReLive';
 
 		var rows, row, liveTdIdx = 4, cloneUrlIdx = 5, tdCount = 7, scoreIdx = 3;
+		var addAllLink, matches;
 
 		if (Foxtrick.isPage(doc, 'series')) {
 
+			matches = [];
 			tdCount = 2;//3;
 			cloneUrlIdx = 0;
 			liveTdIdx = 2;
@@ -47,40 +46,22 @@ Foxtrick.modules['ReLiveLinks'] = {
 			addAll.alt = addAll.title =
 				Foxtrick.L10n.getString('ReLiveLinks.addRound');
 
-/*			var addAll = doc.createDocumentFragment();
-			Foxtrick.addImage(doc, addAll, {
-				src: Foxtrick.InternalPath+'resources/img/relive-small.png',
-				alt: doc.getElementById('ctl00_ctl00_CPContent_CPMain_imgAddRound').alt,
-				title: doc.getElementById('ctl00_ctl00_CPContent_CPMain_imgAddRound').alt });*/
-
-			var addAllLink = doc.createElement('a');
+			addAllLink = doc.createElement('a');
 			addAllLink.appendChild(addAll);
 			var addAllSpan = Foxtrick.createFeaturedElement(doc, this, 'span');
 			Foxtrick.addClass(addAllSpan, 'float_right');
 			addAllSpan.appendChild(addAllLink);
 
-			var matches = [];
-
-			for (var i = 0, m = rows.length; i < m; ++i) {
-				row = rows[i];
-				if (i);//Foxtrick.insertFeaturedCell(row, this, -1);
-				else {
-/*					var header = Foxtrick.createFeaturedElement(doc, this, 'th');
-					header.appendChild(addAllSpan);
-					row.appendChild(header);*/
-					if (/\d[^\d]+\d/.test(rows[1].cells[1].textContent))
-						// don't add before the first round of the season
-						row.cells[0].appendChild(addAllSpan);
-				}
-			}
+			if (/\d[^\d]+\d/.test(rows[1].cells[1].textContent))
+				// don't add before the first round of the season
+				rows[0].cells[0].appendChild(addAllSpan);
 		}
 		else if (Foxtrick.isPage(doc, 'fixtures') || Foxtrick.isPage(doc, 'youthFixtures')) {
 			tdCount = 4;
 			cloneUrlIdx = 1;
 			liveTdIdx = 3;
 			scoreIdx = 2;
-			rows = doc.querySelectorAll('#ctl00_ctl00_CPContent_CPMain_repFixtures > table ' +
-			                            '> tbody > tr');
+			rows = doc.querySelectorAll('#ctl00_ctl00_CPContent_CPMain_repFixtures tr');
 			if (Foxtrick.isPage(doc, 'youthFixtures')) {
 				for (var i = 0, m = rows.length; i < m; ++i) {
 					row = rows[i];
@@ -109,20 +90,24 @@ Foxtrick.modules['ReLiveLinks'] = {
 				Foxtrick.insertFeaturedCell(row, this, liveTdIdx);
 			}
 		}
-		else rows = doc.querySelectorAll('table.naked > tbody > tr');
+		else
+			rows = doc.querySelectorAll('table.naked > tbody > tr');
 
 		var tds, liveTd, address, id, source, links, link;
 
 		for (var i = 0, m = rows.length; i < m; ++i) {
 			row = rows[i];
 			tds = row.getElementsByTagName('td');
-			if (tds.length != tdCount) continue;
-			if (!tds[scoreIdx].textContent.trim().match(/^\d/))
+			if (tds.length != tdCount)
+				continue;
+			if (!/^\d/.test(tds[scoreIdx].textContent.trim()))
 				continue;
 			liveTd = tds[liveTdIdx];
-			if (liveTd && liveTd.getElementsByTagName('a').length) continue;
+			if (liveTd && liveTd.getElementsByTagName('a').length)
+				continue;
 			links = tds[cloneUrlIdx].getElementsByTagName('a');
-			if (!links.length) continue;
+			if (!links.length)
+				continue;
 			address = links[0].href;
 			id = Foxtrick.util.id.getMatchIdFromUrl(address);
 			source = Foxtrick.getParameterFromUrl(address, 'SourceSystem');
@@ -131,14 +116,15 @@ Foxtrick.modules['ReLiveLinks'] = {
 				continue;
 				//don't run on series
 			}
-			address = '/Club/Matches/Live.aspx?matchID=' + id + '&actionType=addMatch&SourceSystem='
-				+ source;
+			address = '/Club/Matches/Live.aspx?matchID=' + id +
+				'&actionType=addMatch&SourceSystem=' + source;
 			link = Foxtrick.createFeaturedElement(doc, this, 'a');
 			link.href = address;
 			link.appendChild(img.cloneNode(true));
 			liveTd.appendChild(link);
 		}
 		if (addAllLink)
-			addAllLink.href = '/Club/Matches/Live.aspx?matchID=' + matches.join(',') + '&actionType=addMatch&SourceSystem=' + source;
+			addAllLink.href = '/Club/Matches/Live.aspx?matchID=' + matches.join(',') +
+				'&actionType=addMatch&SourceSystem=' + source;
 		}
 };
