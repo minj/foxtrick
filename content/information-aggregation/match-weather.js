089d0f0c682e98fff3540570ac523db55c386f1a
LA-MJ <4mr.minj@gmail.com>
2014-08-20 13:55:00 +0000

    match-weather: simplify selectors, fix featured els, use xml utils, cleanup
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13695 1e3807c3-0949-0410-b78e-337b627472c1

--- content/information-aggregation/match-weather.js
+++ content/information-aggregation/match-weather.js
@@ -12,14 +12,12 @@ Foxtrick.modules['MatchWeather'] = {
 	CSS: Foxtrick.InternalPath + 'resources/css/match-weather.css',
 
 	run: function(doc) {
-		var me = this;
-		var imgW = doc.querySelector('#ctl00_ctl00_CPContent_CPMain_pnlPreMatch > ' +
-									 'div:not(.float_left) a[href*="ArenaID="]');
-		if (!imgW)
-			return; // no arena id, match played or ongoing, no need to have tomorrow weather
+		var module = this;
+		if (!Foxtrick.Pages.Match.isPrematch(doc))
+			return;
 
-		var arenaId = Foxtrick.getParameterFromUrl(imgW.href, 'arenaId');
-
+		var arenaLink = doc.querySelector('#mainBody a[href*="ArenaID="]');
+		var arenaId = Foxtrick.getParameterFromUrl(arenaLink.href, 'arenaId');
 		var parameters = [
 			['file', 'arenadetails'],
 			['version', '1.5'],
@@ -32,55 +30,51 @@ Foxtrick.modules['MatchWeather'] = {
 				return;
 			}
 
-			var regionID = xml.getElementsByTagName('RegionID')[0].textContent;
+			var regionID = xml.num('RegionID');
 			Foxtrick.sessionGet('weather.region.' + regionID,
 			  function(data) {
 				if (!data) {
-					me.fetchRegion(doc, regionID);
+					module.fetchRegion(doc, regionID);
 				}
 				else {
-					me.showWeather(doc, data);
+					module.showWeather(doc, data);
 				}
 			});
 		});
 	},
 
 	showWeather: function(doc, data) {
-		var div =
-			doc.querySelector('#ctl00_ctl00_CPContent_CPMain_pnlPreMatch > div:not(.float_left)' +
-							  ':not(#ctl00_ctl00_CPContent_CPMain_pnlTeamInfo)');
-		if (!div)
-			return;
-
 		var expectedText = Foxtrick.L10n.getString('matchWeather.expected');
 		var todayText = Foxtrick.L10n.getString('matchWeather.today');
 		var tomorrowText = Foxtrick.L10n.getString('matchWeather.tomorrow');
 		var irlNowText = Foxtrick.L10n.getString('matchWeather.irltoday');
 		var irlTomorrowText = Foxtrick.L10n.getString('matchWeather.irltomorrow');
 
+		var div = doc.querySelector('#ctl00_ctl00_CPContent_CPMain_pnlPreMatch > div');
 		var img = div.querySelector('p:last-child img');
 		if (img)
 			img.parentNode.appendChild(doc.createTextNode(' ' + expectedText));
-		var pN = doc.createElement('p');
+
+		var pN = Foxtrick.createFeaturedElement(doc, this, 'p');
 		Foxtrick.addImage(doc, pN, {
 			src: 'Img.axd?res=Weather&img=weather' + data.weatherNow + '.png'
 		}, false, function() {
 			pN.appendChild(doc.createTextNode(' ' + todayText));
 		});
 		div.appendChild(pN);
-		var pT = doc.createElement('p');
+
+		var pT = Foxtrick.createFeaturedElement(doc, this, 'p');
 		Foxtrick.addImage(doc, pT, {
 			src: 'Img.axd?res=Weather&img=weather' + data.weatherTomorrow + '.png'
 		}, false, function() {
 			pT.appendChild(doc.createTextNode(' ' + tomorrowText));
 		});
 		div.appendChild(pT);
-		Foxtrick.makeFeaturedElement(pT, 'MatchWeather');
 
 		if (Foxtrick.Prefs.isModuleOptionEnabled('MatchWeather', 'Irl')) {
 			if (typeof data.irlNow !== 'undefined') {
 				Foxtrick.addClass(pN, 'ft-match-weather');
-				var iN = doc.createElement('p');
+				var iN = Foxtrick.createFeaturedElement(doc, this, 'p');
 				Foxtrick.addImage(doc, iN, {
 					src: 'Img.axd?res=Weather&img=weather' + data.irlNow + '.png'
 				}, false, function() {
@@ -88,28 +82,26 @@ Foxtrick.modules['MatchWeather'] = {
 				});
 
 				div.insertBefore(iN, pT);
-				Foxtrick.makeFeaturedElement(iN, 'MatchWeather');
 			}
 			if (typeof data.irlTomorrow !== 'undefined') {
 				Foxtrick.addClass(pT, 'ft-match-weather');
-				var iT = doc.createElement('p');
+				var iT = Foxtrick.createFeaturedElement(doc, this, 'p');
 				Foxtrick.addImage(doc, iT, {
 					src: 'Img.axd?res=Weather&img=weather' + data.irlTomorrow + '.png'
 				}, false, function() {
 					iT.appendChild(doc.createTextNode(' ' + irlTomorrowText));
 				});
 				div.appendChild(iT);
-				Foxtrick.makeFeaturedElement(iT, 'MatchWeather');
 			}
 		}
 	},
 
-	fetchRegion: function(doc, regionID) {
-		var me = this;
+	fetchRegion: function(doc, regionId) {
+		var module = this;
 		var parameters = [
 			['file', 'regiondetails'],
 			['version', '1.2'],
-			['regionId', parseInt(regionID, 10)]
+			['regionId', regionId]
 		];
 		Foxtrick.util.api.retrieve(doc, parameters, { cache_lifetime: 'session' },
 		  function(xml, errorText) {
@@ -118,36 +110,47 @@ Foxtrick.modules['MatchWeather'] = {
 				return;
 			}
 			var data = {};
-			data.weatherNow = xml.getElementsByTagName('WeatherID')[0].textContent;
-			data.weatherTomorrow = xml.getElementsByTagName('TomorrowWeatherID')[0].textContent;
-			data.regionName = xml.getElementsByTagName('RegionName')[0].textContent
-				.replace("'", '').replace(' ', '-').replace(',', '-');
-			data.country = xml.getElementsByTagName('LeagueName')[0].textContent;
+			data.weatherNow = xml.num('WeatherID');
+			data.weatherTomorrow = xml.num('TomorrowWeatherID');
+			data.regionName = xml.text('RegionName').replace("'", '').replace(/ ,/g, '-');
+			data.country = xml.text('LeagueName');
 
 			if (Foxtrick.Prefs.isModuleOptionEnabled('MatchWeather', 'Irl')) {
 				var uri = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' +
 					encodeURIComponent(data.regionName) + ',' + data.country +
 					'&mode=json&units=metric&cnt=2';
+				var weather = { 1: 3, 2: 2, 3: 2, 4: 1, 9: 0, 10: 1, 11: 0, 13: 0, 50: 1 };
+
 				Foxtrick.log('Fetching JSON data from', uri);
 				Foxtrick.util.load.get(uri)('success', function(response) {
 					if (response !== '') {
-						var json = JSON.parse(response);
+						var json;
+						try {
+							json = JSON.parse(response);
+						}
+						catch (e) {
+							Foxtrick.error(e);
+							return;
+						}
+
 						if (json.cod == 200 && json.list.length == 2) {
-							var matching = {1:3, 2:2, 3:2, 4:1, 9:0, 10:1, 11:0, 13:0, 50:1};
-							data.irlNow = matching[Foxtrick.trimnum(json.list[0].weather[0].icon)];
-							data.irlTomorrow = matching[Foxtrick.trimnum(json.list[1].weather[0].icon)];
+							var now = json.list[0].weather[0].icon;
+							var tomorrow = json.list[1].weather[0].icon;
+							data.irlNow = weather[Foxtrick.trimnum(now)];
+							data.irlTomorrow = weather[Foxtrick.trimnum(tomorrow)];
 						}
 					}
-					Foxtrick.sessionSet('weather.region.' + regionID, data);
-					me.showWeather(doc, data);
+					Foxtrick.sessionSet('weather.region.' + regionId, data);
+					module.showWeather(doc, data);
 				})('failure', function(code) {
 					Foxtrick.log('Fail loading weather:' + code);
-					Foxtrick.sessionSet('weather.region.' + regionID, data);
-					me.showWeather(doc, data);
+					Foxtrick.sessionSet('weather.region.' + regionId, data);
+					module.showWeather(doc, data);
 				});
-			} else {
-				Foxtrick.sessionSet('weather.region.' + regionID, data);
-				me.showWeather(doc, data);
+			}
+			else {
+				Foxtrick.sessionSet('weather.region.' + regionId, data);
+				module.showWeather(doc, data);
 			}
 		});
 	}
