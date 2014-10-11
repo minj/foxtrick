f7dd9760d72266307ae00370bfa4497646b39640
LA-MJ <4mr.minj@gmail.com>
2014-10-11 11:07:55 +0000

    clean fans.js
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13767 1e3807c3-0949-0410-b78e-337b627472c1

--- content/presentation/fans.js
+++ content/presentation/fans.js
@@ -13,44 +13,50 @@ Foxtrick.modules['Fans'] = {
 		'ShowSumFans'
 	],
 
-    run: function(doc) {
+	run: function(doc) {
+		var module = this;
 
-        if (Foxtrick.Prefs.isModuleOptionEnabled('Fans', 'AddLiveLink')) {
-            var reM = new RegExp('matchID=([0-9]*)', 'i');
-            var reS = new RegExp('SourceSystem=([A-Za-z]*)', 'i');
-            var links = doc.querySelectorAll('#upcoming a[href*="matchID"]');
-            for(var i=0; i<links.length; i++) {
-                var mM = reM.exec(links[i].getAttribute('href'));
-                var mS = reS.exec(links[i].getAttribute('href'));
-                var node = Foxtrick.createFeaturedElement(doc, Foxtrick.modules.Fans, 'a');
-                node.setAttribute('href', '/Club/Matches/Live.aspx?matchID='+mM[1]+'&actionType=addMatch&SourceSystem='+mS[1]);
-                Foxtrick.addImage(doc, node, {alt:Foxtrick.L10n.getString('MyMonitor.htLive'), title:Foxtrick.L10n.getString('MyMonitor.htLive'), src:'/Img/icons/transparent.gif', class:'matchHTLive'});
-                links[i].parentNode.parentNode.previousElementSibling.appendChild(node);
-            }
-        }
+		if (Foxtrick.Prefs.isModuleOptionEnabled('Fans', 'AddLiveLink')) {
+			var links = doc.querySelectorAll('#upcoming a[href*="matchID"]');
+			Foxtrick.forEach(function(link) {
+				var node = Foxtrick.createFeaturedElement(doc, module, 'a');
+				node.href = link.href.replace('/Club/Matches/Match.aspx?',
+				                              '/Club/Matches/Live.aspx?actionType=addMatch');
+				var liveText = Foxtrick.L10n.getString('MyMonitor.htLive');
+				Foxtrick.addImage(doc, node, {
+					alt: liveText,
+					title: liveText,
+					src: '/Img/icons/transparent.gif',
+					class: 'matchHTLive',
+				});
+				link.parentNode.parentNode.previousElementSibling.appendChild(node);
+			}, links);
+		}
 
-        if (Foxtrick.Prefs.isModuleOptionEnabled('Fans', 'ShowSumFans')) {
-            var nums = doc.querySelectorAll('#sidebar #members .inc, #sidebar #members .dec');
-            var total = 0;
-            for(var i=0; i<nums.length; i++) {
-                var n = nums[i].textContent;
-                if(n.match(/%/)) return; // we may have "-10%" as value, then we can't calculate total
-                total += parseInt(n);
-            }
+		if (Foxtrick.Prefs.isModuleOptionEnabled('Fans', 'ShowSumFans')) {
+			var nums = doc.querySelectorAll('#members .inc, #members .dec');
+			var total = 0;
+			Foxtrick.forEach(function(num) {
+				var text = num.textContent;
+				var n = parseInt(text, 10);
+				if (/%/.test(text))
+					return; // we may have "-10%" as value, then we can't calculate total
+				total += n;
+			}, nums);
 
-            var table = doc.querySelector("#sidebar #members .boxBody .thin");
-            var row = Foxtrick.insertFeaturedRow(table, Foxtrick.modules.Fans, -1);
-            row.setAttribute('class', 'ft-bordertop');
-            var td1 = doc.createElement('td');
-            td1.setAttribute('class', 'date bold');
-            td1.textContent = Foxtrick.L10n.getString('TeamStats.Total');
-            var td2 = doc.createElement('td');
-            td2.setAttribute('class', (total>0?'inc':'dec'));
-            td2.textContent = (total>0?'+'+total:total);
-            row.appendChild(td1);
-            row.appendChild(td2);
-        }
+			var table = doc.querySelector('#members .thin');
+			var row = Foxtrick.insertFeaturedRow(table, module, -1);
+			Foxtrick.addClass(row, 'ft-bordertop');
+			var td1 = doc.createElement('td');
+			td1.className = 'date bold';
+			td1.textContent = Foxtrick.L10n.getString('TeamStats.Total');
+			var td2 = doc.createElement('td');
+			td2.className = total > 0 ? 'inc' : 'dec';
+			td2.textContent = total > 0 ? '+' + total : total;
+			row.appendChild(td1);
+			row.appendChild(td2);
+		}
 
 	}
 
-}
+};
