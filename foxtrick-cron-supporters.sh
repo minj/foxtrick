0574c804163d26f5ae8f726d431a123cabbe79d9
LA-MJ <4mr.minj@gmail.com>
2015-01-04 15:50:01 +0000

    add build/foxtrick-cron-supporters.sh
    git-svn-id: https://foxtrick.googlecode.com/svn/build@13900 1e3807c3-0949-0410-b78e-337b627472c1

---
+++ foxtrick-cron-supporters.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR=$(cd $(dirname $0); pwd)
+LOG_FILE="$DIR"/foxtrick-cron.log
+ERROR_FILE="$DIR"/error
+VERBATIM_FILE="$DIR"/verbatim
+
+function log {
+	echo "[`date`] $1" >> $LOG_FILE
+	echo "################ [`date`] $1 ###############" >> $ERROR_FILE
+	echo "################ [`date`] $1 ###############" >> $VERBATIM_FILE
+	exit 1
+}
+
+. ~/.bashrc
+LANG=en_US.utf-8
+LC_ALL=en_US.utf-8
+
+cd "$DIR"
+. cron-config.sh
+cd ..
+
+echo 'update crowdin'
+cd $BETA || log "Cannot cd to $BETA"
+git stash
+git svn rebase || log "Cannot git-svn rebase"
+cd maintainer/locate || log "Cannot cd to maintainer/locale"
+python updateFTStaff.py
+git commit -a -m "*automated* supporter update"
+git svn dcommit
+log "Success supporter update."
