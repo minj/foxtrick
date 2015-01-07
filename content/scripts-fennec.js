7059ac9b42e562a3361f3fcbb5dde210afddd43c
tasosventouris <tasosventouris@gmail.com>
2015-01-07 09:49:25 +0000

    Update modules for HTPress
    git-svn-id: https://foxtrick.googlecode.com/svn/trunk@13915 1e3807c3-0949-0410-b78e-337b627472c1

--- content/scripts-fennec.js
+++ content/scripts-fennec.js
@@ -193,6 +193,7 @@ Foxtrick.loader.background.contentScriptManager = {
 		'presentation/country-list.js',
 		'presentation/currency-converter.js',
 		'presentation/custom-medals.js',
+		'presentation/embed-media-htpress.js',
 		'presentation/fans.js',
 		'presentation/fix-css-problems.js',
 		'presentation/friendly-interface.js',
