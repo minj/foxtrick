e50e4b7321935761f8f54eab20afb5c3e330aca8
LA-MJ <4mr.minj@gmail.com>
2015-03-25 23:25:16 +0200

    fix variable name collision in stats.sh

--- stats/stats.sh
+++ stats/stats.sh
@@ -8,18 +8,18 @@ JSONDATE=$(date --date="1 day ago" -u +%Y%m%d)
 echo "do $JSONDATE"
 echo "updating JSON"
 # update the json logs from ftp
-for log in stats-archive-update stats-archive-foxtrick
+for logfile in stats-archive-update stats-archive-foxtrick
 do
 	cp access-ftp-tmpl access-ftp-down
 	sed -i "s|{HOST}|${HOST}|" access-ftp-down
 	sed -i "s|{USER}|${USER}|" access-ftp-down
 	sed -i "s|{PSWD}|${PASSWORD}|" access-ftp-down
 	sed -i "s|{DIR}|htdocs|" access-ftp-down
-	sed -i "s|{FILE}|${log}|" access-ftp-down
+	sed -i "s|{FILE}|${logfile}|" access-ftp-down
 	lftp -f access-ftp-down || exit 3
 	rm access-ftp-down
 	# exit if date already exists
-	grep -c $JSONDATE $log && echo "$JSONDATE already exists" && exit
+	grep -c $JSONDATE $logfile && echo "$JSONDATE already exists" && exit
 done
 
 # get yesterday info and add it to stats archive
