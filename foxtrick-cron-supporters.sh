ed21f0df01c49ddfacc8f3941604d3319cf1598e
LA-MJ <4mr.minj@gmail.com>
2015-03-26 00:01:44 +0200

    use a dedicated cdn directory
    prevents confusion/mess with branch switching

--- foxtrick-cron-supporters.sh
+++ foxtrick-cron-supporters.sh
@@ -4,12 +4,12 @@ echo 'update supporters'
 DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/include.sh || (echo "==============ERROR=========== include.sh" && exit -1)
 . "$DIR"/cron-config.sh || (echo "==============ERROR=========== cron-config.sh" && exit -1)
-cd "$DIR/../$BETA" || log "Cannot cd to $BETA"
+cd "$DIR/../cdn" || log "Cannot cd to cdn"
 
 git stash
-git svn rebase || log "Cannot git-svn rebase"
+git pull --rebase || log "Cannot git pull rebase"
 cd maintainer/locale || log "Cannot cd to maintainer/locale"
-python updateFTStaff.py
+python updateFTStaff.py || log "Cannot update staff"
 git commit -a -m "*automated* supporter update"
-git svn dcommit
+git push origin cdn || log "Cannot push cdn"
 log "Success supporter update."
--- rotate-cdn-tags.sh
+++ rotate-cdn-tags.sh
@@ -7,18 +7,13 @@ DIR=$(cd $(dirname $0); pwd)
 ago=$(date -d '2 days ago' +'%y%m%d')
 tomorrow=$(date -d 'tomorrow' +'%y%m%d')
 
-cd "$DIR" || log "Cannot cd to $DIR"
-
-branch=$(git-branch)
+cd "$DIR../cdn" || log "Cannot cd to cdn"
 
 git stash
 git fetch --prune origin "+refs/tags/*:refs/tags/*" || log "Cannot update tags"
-git fetch origin || log "Cannot fetch origin"
-git checkout cdn || log "Cannot checkout cdn"
-git reset --hard origin/cdn || (git checkout "$branch" && log "Cannot reset to origin/cdn")
-git checkout "$branch" || log "Cannot checkout $branch"
+git pull --rebase || log "Cannot pull rebase"
 git tag cdn/$tomorrow cdn || log "Cannot create cdn/$tomorrow"
 git tag --delete cdn/$ago || log "Cannot delete cdn/$ago"
-git push --tags origin :cdn/$ago || log "Cannot push tags to origin"
+git push --tags origin cdn :cdn/$ago || log "Cannot push tags to origin"
 
 log 'success rotate cdn tags'
