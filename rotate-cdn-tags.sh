99d7ef0ad54fc8fe33290a201848141dcfcddf37
LA-MJ <4mr.minj@gmail.com>
2015-03-25 20:35:41 +0200

    update repo before rotating tags
    local changes are stashed
    local tags are replaced/pruned
    cdn changes are pulled with reset

--- rotate-cdn-tags.sh
+++ rotate-cdn-tags.sh
@@ -6,6 +6,15 @@ ago=$(date -d '2 days ago' +'%y%m%d')
 tomorrow=$(date -d 'tomorrow' +'%y%m%d')
 
 cd "$DIR" || log "Cannot cd to $DIR"
+
+branch=$(git-branch)
+
+git stash
+git fetch --prune origin "+refs/tags/*:refs/tags/*" || log "Cannot update tags"
+git fetch origin || log "Cannot fetch origin"
+git checkout cdn || log "Cannot checkout cdn"
+git reset --hard origin/cdn || (git checkout "$branch" && log "Cannot reset to origin/cdn")
+git checkout "$branch" || log "Cannot checkout $branch"
 git tag cdn/$tomorrow cdn || log "Cannot create cdn/$tomorrow"
 git tag --delete cdn/$ago || log "Cannot delete cdn/$ago"
 git push --tags origin :cdn/$ago || log "Cannot push tags to origin"
