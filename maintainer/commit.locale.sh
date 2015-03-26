2ef7478695234f56068b09186281421caf6d24cf
LA-MJ <4mr.minj@gmail.com>
2015-03-26 02:41:07 +0200

    clean commit.locale.sh

--- maintainer/commit.locale.sh
+++ maintainer/commit.locale.sh
@@ -2,19 +2,9 @@
 
 SVN_FILES=../content
 EXTERNAL_LOCALE_FOLDER=crowdin/locale
-#space seperated blacklist of locales which only use svn 
-IGNORELIST=""
 
 # load configuration, probably overwriting defaults above
 . ./upload.conf.sh
-
-#remove blacklisted from crowdin folder
-for LOC in $IGNORELIST
-do
-   echo "Skipping ignorelist"
-   #echo "rm -rf $EXTERNAL_LOCALE_FOLDER/$LOC"
-   #rm -rfv "$EXTERNAL_LOCALE_FOLDER/$LOC"
-done
 
 #copy crowdin locale to svn folder
 cp -rf "$EXTERNAL_LOCALE_FOLDER" "$SVN_FILES"
