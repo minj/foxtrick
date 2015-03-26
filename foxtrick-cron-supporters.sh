9847257861e325a9f6c6bfa03a223478b835ef76
LA-MJ <4mr.minj@gmail.com>
2015-03-26 15:23:22 +0200

    also rebase nightly in foxtrick-cron-supporters.sh
    otherwise stale updateFTStaff.py is used

--- foxtrick-cron-supporters.sh
+++ foxtrick-cron-supporters.sh
@@ -4,10 +4,16 @@ echo 'update supporters'
 DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/include.sh || (echo "==============ERROR=========== include.sh" && exit -1)
 . "$DIR"/cron-config.sh || (echo "==============ERROR=========== cron-config.sh" && exit -1)
+
+cd "$DIR/../$NIGHTLY" || log "Cannot cd to $NIGHTLY"
+
+git stash
+git pull --rebase || log "Cannot git pull rebase $NIGHTLY"
+
 cd "$DIR/../cdn" || log "Cannot cd to cdn"
 
 git stash
-git pull --rebase || log "Cannot git pull rebase"
+git pull --rebase || log "Cannot git pull rebase cdn"
 python "$DIR/../$NIGHTLY/maintainer/locale/updateFTStaff.py" 'res/staff/foxtrick.json' || log "Cannot update staff"
 git commit -a -m "*automated* supporter update"
 git push origin cdn || log "Cannot push cdn"
