0a5561a728d85e7b096085fefa650788088677bb
LA-MJ <4mr.minj@gmail.com>
2015-04-20 18:50:52 +0300

    make pre tag work on announcements and teamPage (guestbook)

--- content/forum/forum-change-posts-modules.js
+++ content/forum/forum-change-posts-modules.js
@@ -8,7 +8,7 @@
 Foxtrick.modules['FormatPostingText'] = {
 	MODULE_CATEGORY: Foxtrick.moduleCategories.FORUM,
 	PAGES: [
-		'forumWritePost', 'messageWritePost', 'guestbook', 'announcements',
+		'forumWritePost', 'messageWritePost', 'guestbook', 'announcements', 'teamPage',
 		'newsLetter', 'mailNewsLetter', 'ntNewsLetter',
 		'forumModWritePost'
 	],
@@ -18,15 +18,20 @@ Foxtrick.modules['FormatPostingText'] = {
 	run: function(doc) {
 		var format = this.format;
 		var reformat = this.reformat;
-		//format view
-		if (Foxtrick.isPage(doc, 'messageWritePost')
-			|| Foxtrick.isPage(doc, 'guestbook')
-			|| Foxtrick.isPage(doc, 'forumWritePost')) {
+		// format view
+		var VIEW_PAGES = [
+			'forumWritePost', 'messageWritePost', 'guestbook', // view + edit
+			'announcements', 'teamPage' // view-only
+		];
+		if (Foxtrick.any(function(page) { return Foxtrick.isPage(doc, page); }, VIEW_PAGES)) {
 			try {
+				var messages;
 				if (Foxtrick.isPage(doc, 'forumWritePost'))
-					var messages = doc.getElementsByClassName('message');
+					messages = doc.getElementsByClassName('message');
+				else if (Foxtrick.isPage(doc, 'teamPage'))
+					messages = doc.getElementsByClassName('mainBox');
 				else
-					var messages = doc.getElementsByClassName('feedItem');
+					messages = doc.getElementsByClassName('feedItem');
 				for (var i = 0; i < messages.length; i++) {
 					var count_pre = Foxtrick.substr_count(messages[i].textContent, '[pre');
 					var org = [
