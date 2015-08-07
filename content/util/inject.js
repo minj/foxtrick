c053192d2f7fabb7a9c107b3e50cd90763ad938c
LA-MJ <4mr.minj@gmail.com>
2015-08-07 21:16:49 +0300

    use scriptloader to inject scripts in Gecko
    removes the possibility to inject strings
    which is frowned upon by mozilla

--- content/util/inject.js
+++ content/util/inject.js
@@ -50,20 +50,25 @@ Foxtrick.util.inject.css = function(doc, css, id) {
 // attaches a JavaScript file to the page
 // dynamically injected from chrome only
 Foxtrick.util.inject.jsLink = function(doc, url) {
-	var id = url.match(/([^\/]+)\.js$/)[1];
-	Foxtrick.util.load.get(url)('success', function(text) {
-		Foxtrick.util.inject.js(doc, text, id);
-	});
-};
+	if (Foxtrick.arch == 'Gecko') {
+		Services.scriptloader.loadSubScript(url, doc.defaultView, 'UTF-8');
+	}
+	else {
+		var inject = function(doc, js, id) {
+			var sourceName = 'ft.' + id + '.js';
+			var head = doc.getElementsByTagName('head')[0];
 
-// attaches a JavaScript snippet to the page
-Foxtrick.util.inject.js = function(doc, js, id) {
-	var sourceName = 'ft.' + id + '.js';
-	var head = doc.getElementsByTagName('head')[0];
-	var script = doc.createElement('script'); // dynamically injected from chrome only
-	script.setAttribute('type', 'text/javascript');
-	script.id = id;
-	script.textContent = js + '\n\n//# sourceURL=' + sourceName + '\n';
-	head.appendChild(script);
-	return script;
+			var script = doc.createElement('script'); // does not run in FF: see above
+			script.setAttribute('type', 'text/javascript');
+			script.id = id;
+			script.textContent = js + '\n\n//# sourceURL=' + sourceName + '\n';
+			head.appendChild(script);
+			return script;
+		};
+
+		var id = url.match(/([^\/]+)\.js$/)[1];
+		Foxtrick.util.load.get(url)('success', function(text) {
+			inject(doc, text, id);
+		});
+	}
 };
