8abec6e4fd7dc31e17127812c34f3275f4113e19
LA-MJ <4mr.minj@gmail.com>
2015-08-31 14:36:12 +0300

    cleanup Language.py

--- maintainer/locale/Hattrick/Language.py
+++ maintainer/locale/Hattrick/Language.py
@@ -1,72 +1,72 @@
 Codes = {}
-Codes[1]='sv-SE';	#swedish
-Codes[2]='en-GB';	#english
-Codes[3]='de';		#german
-Codes[4]='it';		#italian
-Codes[5]='fr';		#french
-Codes[6]='es-ES';	#spanish
-Codes[7]='no';		#Norsk bokmal
-Codes[8]='da';		#danish
-Codes[9]='fi';		#finnish
-Codes[10]='nl';		#Dutch
-Codes[11]='pt-PT';	#Portuguese
-Codes[12]='ja';		#japan
-Codes[13]='pl';		#Polish
-Codes[14]='ru';		#Russian
-Codes[15]='zh-CN';	#chinese simplified
-Codes[17]='ko';		#korean
-Codes[19]='tr';		#Turkish
-Codes[22]='ar'; 	#arabic
-Codes[23]='ro';		#romanian
-Codes[25]='is';		#island
-Codes[32]='sr';		#serbian
-Codes[33]='hu';		#hungarian
-Codes[34]='el';		#greek
-Codes[35]='cs';		#czech
-Codes[36]='et';		#Eesti
-Codes[37]='lv';		#Latvian, Lettish
-Codes[39]='hr';		#croatian
-Codes[40]='he';		#hebrew
-Codes[43]='bg';		#bulgarian
-Codes[45]='sl';		#slovenian
-Codes[50]='pt-BR';	#Portuguese Brasil
-Codes[51]='es-AR';	#spanish, south-america, using sub-language es-AR for crowdin support
-Codes[53]='sk';		#Slovak
-Codes[55]='vi';		#vietnamese
-Codes[56]='lt';		#Lithuanian
-Codes[57]='uk';		#ukranian
-Codes[58]='bs';		#Bosanski
-Codes[65]='vls-BE';	#Vlaams, netherland
-Codes[66]='ca';		#Catalan
-Codes[74]='gl';		#Galician
-Codes[75]='fa';		#farsi, iranian
-Codes[83]='mk';		#Macedonian
-Codes[84]='be';		#belarus
-Codes[85]='sq';		#Albanian
-Codes[87]='mt';		#Maltese, Malta
-Codes[90]='ka';		#georgian
-Codes[100]='az';	#Azerbaijani
-Codes[103]='es-CR';	#spanish, central america, using sub-language es-MX for crowdin support
-Codes[109]='fy-NL';	#Frisian, east-netherland/north germany
-Codes[110]='eu';	#Euskara, basque
-Codes[111]='lb';	#Letzebuergesch
-Codes[113]='fur-IT';	#Furlan, northitaly
-Codes[136]='nn-NO';	#Norsk nynorsk
-Codes[151]='en-US'; #English (US)
+Codes[1] = 'sv-SE'    # Swedish
+Codes[2] = 'en-GB'    # English
+Codes[3] = 'de'       # German
+Codes[4] = 'it'       # Italian
+Codes[5] = 'fr'       # French
+Codes[6] = 'es-ES'    # Spanish
+Codes[7] = 'no'       # Norsk bokmal
+Codes[8] = 'da'       # Danish
+Codes[9] = 'fi'       # Finnish
+Codes[10] = 'nl'      # Dutch
+Codes[11] = 'pt-PT'   # Portuguese
+Codes[12] = 'ja'      # Japan
+Codes[13] = 'pl'      # Polish
+Codes[14] = 'ru'      # Russian
+Codes[15] = 'zh-CN'   # Chinese simplified
+Codes[17] = 'ko'      # Korean
+Codes[19] = 'tr'      # TurkishCodes = {}
+Codes[22] = 'ar'      # Arabic
+Codes[23] = 'ro'      # Romanian
+Codes[25] = 'is'      # Island
+Codes[32] = 'sr'      # Serbian
+Codes[33] = 'hu'      # Hungarian
+Codes[34] = 'el'      # Greek
+Codes[35] = 'cs'      # Czech
+Codes[36] = 'et'      # Eesti
+Codes[37] = 'lv'      # Latvian, Lettish
+Codes[39] = 'hr'      # Croatian
+Codes[40] = 'he'      # Hebrew
+Codes[43] = 'bg'      # Bulgarian
+Codes[45] = 'sl'      # Slovenian
+Codes[50] = 'pt-BR'   # Portuguese Brasil
+Codes[51] = 'es-AR'   # Spanish, south-america, using sub-language es-AR for crowdin support
+Codes[53] = 'sk'      # Slovak
+Codes[55] = 'vi'      # Vietnamese
+Codes[56] = 'lt'      # Lithuanian
+Codes[57] = 'uk'      # Ukranian
+Codes[58] = 'bs'      # Bosanski
+Codes[65] = 'vls-BE'  # Vlaams, netherland
+Codes[66] = 'ca'      # Catalan
+Codes[74] = 'gl'      # Galician
+Codes[75] = 'fa'      # Farsi, iranian
+Codes[83] = 'mk'      # Macedonian
+Codes[84] = 'be'      # Belarus
+Codes[85] = 'sq'      # Albanian
+Codes[87] = 'mt'      # Maltese, Malta
+Codes[90] = 'ka'      # Georgian
+Codes[100] = 'az'     # Azerbaijani
+Codes[103] = 'es-CR'  # Spanish, central america, using sub-language es-MX for crowdin support
+Codes[109] = 'fy-NL'  # Frisian, east-netherland/north germany
+Codes[110] = 'eu'     # Euskara, basque
+Codes[111] = 'lb'     # Letzebuergesch
+Codes[113] = 'fur-IT'    #Furlan, northitaly
+Codes[136] = 'nn-NO'  # Norsk nynorsk
+Codes[151] = 'en-US'  # English (US)
 
 def getAll():
-	return Codes
+    return Codes
 
 def getIdByLanguage(language):
-	for key in Codes:
-		if Codes[key].lower() == language.lower():
-			return key
+    for key in Codes:
+        if Codes[key].lower() == language.lower():
+            return key
 
-	return None
+    return None
 
-def getLanguageById(id):
-	for key in Codes:
-		if key == id:
-			return Codes[id]
+def getLanguageById(lang_id):
+    for key in Codes:
+        if key == lang_id:
+            return Codes[lang_id]
 
-	return None
+    return None
