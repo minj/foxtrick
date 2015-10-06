23e6eb062cdaefb5888b1d8f26001f5d6c2d026c
LA-MJ <4mr.minj@gmail.com>
2015-10-06 15:18:23 +0300

    don't try to calculate HTMS/Psico for youth

--- content/pages/players.js
+++ content/pages/players.js
@@ -377,7 +377,7 @@ Foxtrick.Pages.Players.getPlayerList = function(doc, callback, options) {
 					}
 				}
 
-				if (newSkillInfo) {
+				if (newSkillInfo && !isYouth) {
 					var htmsInput = {
 						years: player.age.years,
 						days: player.age.days,
