82e0f1aaa0a5e4addef0f5e6e20e2306208b8554
LA-MJ <4mr.minj@gmail.com>
2015-11-17 14:25:34 +0200

    rewrite LastLogin

--- content/information-aggregation/last-login.js
+++ content/information-aggregation/last-login.js
@@ -10,41 +10,37 @@ Foxtrick.modules['LastLogin'] = {
 
 	change: function(doc) {
 		var div = doc.getElementById('pnlLogin');
-		if (!div || div.hasAttribute('processed')) {
+		if (!div || div.dataset.done) {
 			// if the last login div isn't present or has been processed,
 			// return.
 			return;
 		}
-		var httime = doc.getElementById('time').textContent;
-		var HT_date = Foxtrick.util.time.getDateFromText(httime);
-		if (!HT_date) return;
+		var now = Foxtrick.util.time.getHtTimeStamp(doc);
+		if (!now)
+			return;
 
-		var login_elm = div.childNodes;
-		for (var i = 0; i < login_elm.length; i++) {
-			if (login_elm[i].nodeType != Foxtrick.NodeTypes.TEXT_NODE)
-				continue;
-			var elm = login_elm[i].textContent;
-			if (elm.search(/\*\*\*\.\*\*\*/) != -1) {
+		Foxtrick.forEach(function(el) {
+			if (el.nodeType != Foxtrick.NodeTypes.TEXT_NODE)
+				return;
+
+			var text = el.textContent;
+			if (/\*\*\*\.\*\*\*/.test(text)) {
 				var last = doc.createElement('span');
-				var ST_date = Foxtrick.util.time.getDateFromText(elm);
-
-				var _s = Math.floor((HT_date.getTime() - ST_date.getTime()) / 1000); // sec
-				var diffEl = Foxtrick.util.time.timeDiffToSpan(doc, _s, { useSWD: true });
-				var DiffText = diffEl.textContent;
-				if (DiffText.search('NaN') == -1) {
-					last.textContent = '(' + DiffText + ')';
+				var loginDate = Foxtrick.util.time.getDateFromText(text);
+				var secs = Math.floor((now - loginDate.getTime()) / 1000); // sec
+				var diffEl = Foxtrick.util.time.timeDiffToSpan(doc, secs, { useSWD: true });
+				var result = diffEl.textContent;
+				if (!/NaN/.test(result)) {
+					last.textContent = '(' + result + ')';
 					last.className = 'date smallText ft-last-login-diff nowrap';
 				}
 				else
-					Foxtrick.log('Could not create timediff (NaN)');
-				if (i !== login_elm.length - 1) {
-					div.insertBefore(last, login_elm[i].nextSibling);
-				}
-				else
-					div.appendChild(last);
-			}
+					Foxtrick.log('Could not create timeDiff (NaN)');
 
-		}
-		div.setAttribute('processed', 'processed');
-	}
+				Foxtrick.insertAfter(last, el);
+			}
+		}, div.childNodes);
+
+		div.dataset.done = 1;
+	},
 };
