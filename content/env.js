438d6c946471c228b05f5bc1574f5d3560497ac0
LA-MJ <4mr.minj@gmail.com>
2015-12-09 05:22:29 +0200

    move irrelevant stuff out of env.js

--- content/env.js
+++ content/env.js
@@ -734,35 +734,6 @@ Foxtrick.lazyProp = function(obj, prop, calc) {
 		}
 	}
 
-
-	// Node not available for all browsers. let's just use our own
-	Foxtrick.NodeTypes = {
-		ELEMENT_NODE: 1,
-		ATTRIBUTE_NODE: 2,
-		TEXT_NODE: 3,
-		CDATA_SECTION_NODE: 4,
-		ENTITY_REFERENCE_NODE: 5,
-		ENTITY_NODE: 6,
-		PROCESSING_INSTRUCTION_NODE: 7,
-		COMMENT_NODE: 8,
-		DOCUMENT_NODE: 9,
-		DOCUMENT_TYPE_NODE: 10,
-		DOCUMENT_FRAGMENT_NODE: 11,
-		NOTATION_NODE: 12,
-	};
-
-
-	// List of categories
-	Foxtrick.moduleCategories = {
-		INFORMATION_AGGREGATION: 'informationAggregation',
-		SHORTCUTS_AND_TWEAKS: 'shortcutsAndTweaks',
-		PRESENTATION: 'presentation',
-		MATCHES: 'matches',
-		FORUM: 'forum',
-		LINKS: 'links',
-		ALERT: 'alert',
-		ACCESSIBILITY: 'accessibility',
-	};
 })();
 
 if (Foxtrick.arch === 'Gecko') {
