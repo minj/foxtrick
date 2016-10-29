db42d3682c4265220b36de6cf48d2c2397ec4470
LA-MJ <4mr.minj@gmail.com>
2016-10-29 20:17:40 +0300

    readd old HT-Live support to LiveAlert
    apparently HTs made that optional

--- content/alert/live-alert.js
+++ content/alert/live-alert.js
@@ -17,9 +17,15 @@ Foxtrick.modules['LiveAlert'] = {
 	store: {},
 
 	run: function(doc) {
-		var results = doc.querySelector('#ngLive .htbox-content');
+		var isNewLive = Foxtrick.Pages.Match.isNewLive(doc);
+		var results = isNewLive ? doc.querySelector('#ngLive .htbox-content') :
+			Foxtrick.getMBElement(doc, 'UpdatePanelPopupMessages');
+
 		var opts = { childList: true, characterData: true, subtree: true };
 		Foxtrick.onChange(results, this.runTabs.bind(this), opts);
+
+		if (!isNewLive)
+			return;
 
 		// add overlay pre-announce support
 		var container = doc.querySelector('.live-left-container');
@@ -78,15 +84,24 @@ Foxtrick.modules['LiveAlert'] = {
 	 * @return {array}       <Array.<number>>
 	 */
 	getScoreFromTab: function(tab) {
-		var ret = null;
+		var goals = null;
 
-		var scores = tab.querySelectorAll('.live-matchlist-item-score');
-		var goals = Foxtrick.map(function(score) {
-			return score.textContent;
-		}, scores);
-		ret = goals.map(function(s) { return parseInt(s, 10); });
+		if (Foxtrick.Pages.Match.isNewLive(tab.ownerDocument)) {
+			var scores = tab.querySelectorAll('.live-matchlist-item-score');
+			goals = Foxtrick.map(function(score) {
+				return score.textContent;
+			}, scores);
+		}
+		else {
+			var score = tab.querySelector('.liveTabScore');
+			var match = score.textContent.trim().match(/^(\d+) - (\d+)$/);
+			if (match)
+				goals = Foxtrick.toArray(match).slice(1);
+		}
 
-		return ret;
+		goals = goals.map(function(s) { return parseInt(s, 10); });
+
+		return goals;
 	},
 
 	/**
@@ -98,12 +113,23 @@ Foxtrick.modules['LiveAlert'] = {
 	 * @return {array}       {Array.<HTMLAnchorElement|HTMLSpanElement}
 	 */
 	getTeamsFromTab: function(tab) {
-		return Foxtrick.toArray(tab.querySelectorAll('.ellipsis'));
+		var ret = null;
+
+		if (Foxtrick.Pages.Match.isNewLive(tab.ownerDocument))
+			ret = Foxtrick.toArray(tab.querySelectorAll('.ellipsis'));
+		else
+			ret = [tab.querySelector('.hometeam'), tab.querySelector('.awayteam')];
+
+		return ret;
 	},
 
 	runTabs: function(doc, results) {
-		var tabs = results.querySelectorAll('li');
-		// skip first tab = overview
+		var isNewLive = Foxtrick.Pages.Match.isNewLive(doc);
+
+		var tabs = isNewLive ? results.querySelectorAll('li') :
+			doc.querySelectorAll('.liveTabText');
+
+		// skip first tab = overview/header
 		for (var tab of Foxtrick.toArray(tabs).slice(1)) {
 			if (!tab.childElementCount)
 				continue;
@@ -117,8 +143,10 @@ Foxtrick.modules['LiveAlert'] = {
 			var info = {
 				homeGoals: score[0],
 				homeName: teams[0].textContent,
+				homeTitle: teams[0].title,
 				awayGoals: score[1],
 				awayName: teams[1].textContent,
+				awayTitle: teams[1].title,
 			};
 			info.teamsText = info.homeName + '-' + info.awayName; // used as index
 
@@ -151,17 +179,18 @@ Foxtrick.modules['LiveAlert'] = {
 		// README: during HT-Live games own.full == own.abbr!!!
 		// FIXME: no way to recognize own goal during Live?!
 		var isHomeOwn = own.abbr == info.homeName || // HT-Live
-			// own.full == info.homeLong || // Re-Live
+			own.full == info.homeTitle || // Re-Live
 			own.full == info.homeName; // HT-Live fallback (if shortTeamName failed)
 
 		var isAwayOwn = own.abbr == info.awayName || // HT-Live
-			// own.full == info.awayLong || // Re-Live
+			own.full == info.awayTitle || // Re-Live
 			own.full == info.awayName; // HT-Live fallback (if shortTeamName failed)
 
 		var ownScored = isHomeOwn && homeScored || isAwayOwn && awayScored;
 		var opScored = isHomeOwn && awayScored || isAwayOwn && homeScored;
 
 		store.score = score;
+
 		// show notification
 		var txt = Foxtrick.format(ALERT_TMPL, info);
 		var url = doc.location.href;
