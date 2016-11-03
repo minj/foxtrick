18c663919d86eafde0be8cb13057b33542658ff3
LA-MJ <4mr.minj@gmail.com>
2016-11-03 23:11:48 +0200

    lint friendly-pool.js

--- content/presentation/friendly-pool.js
+++ content/presentation/friendly-pool.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*
  * show-friendly-booked.js
  * Show whether a team has booked friendly on series page
@@ -16,60 +17,58 @@ Foxtrick.modules['FriendlyPool'] = {
 		if (!leagueSelect)
 			return;
 
-		if (Foxtrick.Prefs.isModuleOptionEnabled('FriendlyPool', 'ExpandCountrySelection')) {
+		if (Foxtrick.Prefs.isModuleOptionEnabled('FriendlyPool', 'ExpandCountrySelection'))
 			leagueSelect.size = leagueSelect.querySelectorAll('option').length;
-		}
 
-		var ownteamid = Foxtrick.util.id.getOwnTeamId();
-		var owncountryid = Foxtrick.util.id.getOwnLeagueId();
+		var ownTeamId = Foxtrick.util.id.getOwnTeamId();
+		var ownLeagueId = Foxtrick.util.id.getOwnLeagueId();
 		var parameters = [
 			['file', 'teamdetails'],
 			['version', '2.6'],
-			['teamId', ownteamid],
-			['includeFlags', 'true']
+			['teamId', ownTeamId],
+			['includeFlags', 'true'],
 		];
 		Foxtrick.util.api.retrieve(doc, parameters, { cache_lifetime: 'default' },
 		  function(xml, errorText) {
 			if (!xml || errorText) {
 				Foxtrick.log(errorText);
-				//destCell.textContent = Foxtrick.L10n.getString('status.error.abbr');
-				//destCell.title = errorText;
+
+				// destCell.textContent = Foxtrick.L10n.getString('status.error.abbr');
+				// destCell.title = errorText;
+
 				return;
 			}
 
 			var home = {};
-			var LeagueIds = xml.getElementsByTagName('HomeFlags')[0]
-				.getElementsByTagName('LeagueId');
-			for (var i = 0; i < LeagueIds.length; ++i)
-				home[LeagueIds[i].textContent] = true;
+			var homeIds = xml.node('HomeFlags').getElementsByTagName('LeagueId');
+			for (var homeId of Foxtrick.toArray(homeIds))
+				home[homeId.textContent] = true;
 
 			var away = {};
-			var LeagueIds = xml.getElementsByTagName('AwayFlags')[0]
-				.getElementsByTagName('LeagueId');
-			for (var i = 0; i < LeagueIds.length; ++i)
-				away[LeagueIds[i].textContent] = true;
+			var awayIds = xml.node('AwayFlags').getElementsByTagName('LeagueId');
+			for (var awayId of Foxtrick.toArray(awayIds))
+				away[awayId.textContent] = true;
 
 			var options = leagueSelect.getElementsByTagName('option');
-			for (var i = 0; i < leagueSelect.length; ++i) {
-				if (home[leagueSelect[i].getAttribute('value')]
-					&& away[leagueSelect[i].getAttribute('value')]) {
-					Foxtrick.addClass(leagueSelect[i], 'ft-home ft-away');
-					leagueSelect[i].setAttribute('title', Foxtrick.L10n
-					                             .getString('matches.playedHomeAway'));
+			for (var option of Foxtrick.toArray(options)) {
+				if (home[option.getAttribute('value')] && away[option.getAttribute('value')]) {
+					Foxtrick.addClass(option, 'ft-home ft-away');
+					option.title = Foxtrick.L10n.getString('matches.playedHomeAway');
 				}
-				else if (home[leagueSelect[i].getAttribute('value')]) {
-					Foxtrick.addClass(leagueSelect[i], 'ft-home');
-					leagueSelect[i].setAttribute('title',
-					                             Foxtrick.L10n.getString('matches.playedHome'));
+				else if (home[option.getAttribute('value')]) {
+					Foxtrick.addClass(option, 'ft-home');
+					option.title = Foxtrick.L10n.getString('matches.playedHome');
 				}
-				else if (away[leagueSelect[i].getAttribute('value')]) {
-					Foxtrick.addClass(leagueSelect[i], 'ft-away');
-					leagueSelect[i].setAttribute('title',
-					                             Foxtrick.L10n.getString('matches.playedAway'));
+				else if (away[option.getAttribute('value')]) {
+					Foxtrick.addClass(option, 'ft-away');
+					option.title = Foxtrick.L10n.getString('matches.playedAway');
 				}
-				else if (leagueSelect[i].getAttribute('value') == owncountryid)
-					Foxtrick.addClass(leagueSelect[i], 'ft-own');
+				else if (option.getAttribute('value') == ownLeagueId) {
+					Foxtrick.addClass(option, 'ft-own');
+				}
 			}
+
 		});
-	}
+
+	},
 };
