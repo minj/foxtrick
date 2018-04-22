79fa29ed16cfdc3ab6ccc0cd25b4d8e40b64767c
LA-MJ <4mr.minj@gmail.com>
2018-04-22 08:25:29 +0200

    fix crowdin download

--- maintainer/crowdin-download.sh
+++ maintainer/crowdin-download.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 CROWDIN_KEY=
-CROWDIN_URL=http://api.crowdin.net/api/project/foxtrick
+CROWDIN_URL=https://api.crowdin.com/api/project/foxtrick
 SVN_FILES=../content/locale/*
 CROWDIN_FOLDER=crowdin/locale
 
 # load configuration, probably overwriting defaults above
 . ./upload.conf.sh
-  
+
 #export/pack latest translations
 for i in {1..10}
 do
 	echo "order crowdin to repack.."
-	re="$(curl -s \
+	re="$(curl -sfL \
 	  "$CROWDIN_URL"/export?key="$CROWDIN_KEY" | grep -c success)"
 	  if [ $re -ne 1 ]; then
-		echo "try again in 1 min"
+		echo "try again in 2 min"
 		sleep 120
 	  else
 		echo "ok"
@@ -23,22 +23,24 @@ do
 	  fi
 done
 
-  
+
 if [ $re -eq 1 ]; then
 #Download all translations as a single ZIP archive.
 	echo "download zip.."
 	wget -O langs.zip "$CROWDIN_URL"/download/all.zip?key="$CROWDIN_KEY"
 	  if [ $? -ne 0 ]; then
 		echo "failed"
+		exit 1
 	  else
 		echo "ok"
 		mkdir -p "$CROWDIN_FOLDER"
 		unzip -q -o -a langs.zip -d "$CROWDIN_FOLDER"
 		  if [ $? -ne 0 ]; then
 			echo "Unzip failed"
+			exit 1
 		  else
 			echo "Unzipped to $CROWDIN_FOLDER"
-#get translation status  
+#get translation status
 			curl -s \
 			  "$CROWDIN_URL"/status?key="$CROWDIN_KEY" > status.xml
 			locale/XMLToJSON.py status.xml
@@ -50,8 +52,9 @@ if [ $re -eq 1 ]; then
 	rm -rf langs.zip
 else
 	echo "export failed"
+	exit 1
 fi
- 
+
 
 #Download French translations.
 #wget http://api.crowdin.net/api/project/{project-identifier}/download/fr.zip?key={project-key}
