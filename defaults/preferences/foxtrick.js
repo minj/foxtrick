ecde09610ffc76e640a674376863cec558aed566
Ricardo <rferro@hexacta.com>
2018-04-22 16:33:05 -0300

    Setting U20 LastMatch module enabled by default

--- defaults/preferences/foxtrick.js
+++ defaults/preferences/foxtrick.js
@@ -738,6 +738,7 @@ pref("extensions.foxtrick.prefs.module.YouthSkillHideUnknown.HideUnknown.enabled
 pref("extensions.foxtrick.prefs.module.YouthSkills.enabled", true);
 pref("extensions.foxtrick.prefs.module.YouthTwins.enabled", true);
 pref("extensions.foxtrick.prefs.module.YouthTwins.HideInfoLink.enabled", false);
+pref("extensions.foxtrick.prefs.module.U20LastMatch.enabled", true);
 pref("extensions.foxtrick.prefs.module.U20LastMatch.YouthPlayers.enabled", true);
 pref("extensions.foxtrick.prefs.module.U20LastMatch.SeniorPlayers.enabled", true);
 pref("extensions.foxtrick.prefs.module.U20LastMatch.AllPlayers.enabled", true);
