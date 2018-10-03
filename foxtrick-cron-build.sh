41d48d81a545bee7b389946abb7f1f8fcee56af6
foxtrickdev <foxtrickdev@gmail.com>
2018-10-03 19:48:14 +0000

    update

--- foxtrick-cron-build.sh
+++ foxtrick-cron-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo 'update build scripts'
 
+set -e
+
 DIR=$(cd $(dirname $0); pwd)
-. "$DIR"/include.sh || (echo "==============ERROR=========== include.sh" && exit -1)
-. "$DIR"/cron-config.sh || (echo "==============ERROR=========== cron-config.sh" && exit -1)
-cd "$DIR" || log "Cannot cd to $DIR"
+. "$DIR"/include.sh || (log "==============ERROR=========== include.sh" && false)
+. "$DIR"/cron-config.sh || (log "==============ERROR=========== cron-config.sh" && false)
+cd "$DIR" || (log "Cannot cd to $DIR" && false)
 
 git stash
-git pull --rebase || log "Cannot git pull rebase"
+git pull --rebase || (log "Cannot git pull rebase" && false)
 log "Success build script update."
