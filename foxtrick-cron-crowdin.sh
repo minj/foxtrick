0871990cc5b440d51d2ddc0854abe7f6abd02b4a
LA-MJ <4mr.minj@gmail.com>
2018-10-05 22:18:14 +0200

    adjust crowdin to use multiple repos

--- foxtrick-cron-crowdin.sh
+++ foxtrick-cron-crowdin.sh
@@ -5,10 +5,10 @@ set -e
 DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/include.sh || logf "==============ERROR=========== include.sh"
 . "$DIR"/cron-config.sh || logf "==============ERROR=========== cron-config.sh"
-cd "$DIR/../$BETA" || logf "Cannot cd to $BETA"
 
 unset GIT_DIR GIT_WORK_TREE
 
+cd "$DIR/../l10" || logf "Cannot cd to l10n"
 git stash
 git checkout l10n || logf "Cannot checkout l10n"
 git pull --rebase origin l10n || logf "Cannot git pull rebase l10n"
@@ -17,6 +17,9 @@ cd maintainer || logf "Cannot cd to maintainer"
 ./crowdin-download.sh || logf "Cannot download external translations"
 ./commit.locale.sh || logf "Cannot commit locale"
 git push origin l10n || logf "Cannot push l10n"
+
+cd "$DIR/../$BETA" || logf "Cannot cd to $BETA"
+git stash
 git checkout master || logf "Cannot checkout master"
 git pull --rebase origin master || logf "Cannot git pull rebase master"
 git merge --no-ff --no-edit l10n || logf "Cannot merge l10n"
