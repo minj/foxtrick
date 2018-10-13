3b1514919edf8dec9db7aab572ec796c5dccd44a
LA-MJ <4mr.minj@gmail.com>
2018-10-13 19:47:32 +0200

    adapt beta build script

--- foxtrick-cron-beta.sh
+++ foxtrick-cron-beta.sh
@@ -4,15 +4,17 @@ echo 'upload beta'
 DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/include.sh || (echo "==============ERROR=========== include.sh" && exit -1)
 . "$DIR"/cron-config.sh || (echo "==============ERROR=========== cron-config.sh" && exit -1)
-cd "$DIR/../$BETA" || log "Cannot cd to $BETA"
+
+unset GIT_DIR GIT_WORK_TREE
+cd "$DIR/../$BETA" || logf "Cannot cd to $BETA"
 
 git stash
-git checkout master || log "Cannot checkout master"
-git pull --rebase || log "Cannot git pull rebase"
-cd maintainer || log "Cannot cd to maintainer"
+git checkout master || logf "Cannot checkout master"
+git pull --rebase || logf "Cannot git pull rebase"
+cd maintainer || logf "Cannot cd to maintainer"
 #./crowdin-upload.sh || echo "Cannot upload external translations"
 #./crowdin-download.sh || echo "Cannot download external translations"
 #./commit.locale.sh || echo "Cannot commit locale"
-./upload-nightly.sh -c upload.beta.conf.sh BRANCH=beta XAR=/usr/local/bin/xar || log "Cannot upload beta"
+./upload-nightly.sh -c upload.beta.conf.sh BRANCH=beta XAR=/usr/local/bin/xar || logf "Cannot upload beta"
 
 log "Success beta upload."
