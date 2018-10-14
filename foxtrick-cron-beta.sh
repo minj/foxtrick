6e0dc97b4c50a73930caa3afd7a2c73876b15b86
LA-MJ <4mr.minj@gmail.com>
2018-10-14 17:59:09 +0200

    handle upload-nightly signals

--- foxtrick-cron-beta.sh
+++ foxtrick-cron-beta.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo 'upload beta'
 
+set -e
+
 DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/include.sh || (echo "==============ERROR=========== include.sh" && exit -1)
 . "$DIR"/cron-config.sh || (echo "==============ERROR=========== cron-config.sh" && exit -1)
@@ -12,9 +14,16 @@ git stash
 git checkout master || logf "Cannot checkout master"
 git pull --rebase || logf "Cannot git pull rebase"
 cd maintainer || logf "Cannot cd to maintainer"
+
 #./crowdin-upload.sh || echo "Cannot upload external translations"
 #./crowdin-download.sh || echo "Cannot download external translations"
 #./commit.locale.sh || echo "Cannot commit locale"
-./upload-nightly.sh -c upload.beta.conf.sh BRANCH=beta XAR=/usr/local/bin/xar || logf "Cannot upload beta"
+
+if ! ./upload-nightly.sh -c upload.beta.conf.sh BRANCH=beta XAR=/usr/local/bin/xar; then
+	if [ $? -ne 1 ]; then
+		logf "Cannot upload beta"
+	fi
+	log "Success: no new build was needed." && exit 0
+fi
 
 log "Success beta upload."
