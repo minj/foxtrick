0171affa9045d3008386b294630eb4dacafc18be
LA-MJ <4mr.minj@gmail.com>
2018-10-28 21:48:34 +0100

    rewrite transfer-search-results

--- content/pages/transfer-search-results.js
+++ content/pages/transfer-search-results.js
@@ -1,15 +1,20 @@
-'use strict';
-/* transfer-search-results.js
+/**
+ * transfer-search-results.js
  * Utilities on transfer search results page
  * @author convincedd, ryanli
  */
 
+'use strict';
+
+/* eslint-disable */
 if (!Foxtrick)
 	var Foxtrick = {};
 if (!Foxtrick.Pages)
 	Foxtrick.Pages = {};
+/* eslint-enable */
 
 Foxtrick.Pages.TransferSearchResults = {};
+
 /**
  * Test whether it's transfer search results
  * @param  {document}  doc
@@ -21,203 +26,220 @@ Foxtrick.Pages.TransferSearchResults.isPage = function(doc) {
 
 /**
  * Get a list of player objects with the information in the result page.
+ *
  * Keep in mind that not all info might be available (e.g. after deadline).
- * @param  {document} doc
- * @return {array}        Array.<object>
+ *
+ * @param  {document}      doc
+ * @return {Array<Player>}     Array<Player>
  */
 Foxtrick.Pages.TransferSearchResults.getPlayerList = function(doc) {
-	var players = doc.getElementsByClassName('transferPlayerInfo');
+	let parseSkills = (player, pNode) => {
+		const SKILLS = [
+			'stamina',
+			'keeper',
+			'playmaking',
+			'passing',
+			'winger',
+			'defending',
+			'scoring',
+			'setPieces',
+		];
 
-	return Foxtrick.map(function(playerInfo) {
-		var player = { playerNode: playerInfo };
+		// right skill table - skills
+		let skillTable = pNode.querySelector('.transferPlayerSkills table');
+		let skillLinks = Foxtrick.toArray(skillTable.getElementsByTagName('a'));
+		if (skillTable.querySelector('.findSimilarPlayers')) {
+			// similar player feature link
+			skillLinks.shift();
+		}
+		player.skills = {};
+		for (let [idx, skill] of SKILLS.entries()) {
+			player[skill] = Foxtrick.util.id.getSkillLevelFromLink(skillLinks[idx]);
+			player.skills[skill] = player[skill];
+		}
 
-		// return player whenever we encounter a problem
+		let contributions = Foxtrick.Pages.Player.getContributions(player.skills, player);
+		for (let name in contributions)
+			player[name] = contributions[name];
+
+		let bestPos = Foxtrick.Pages.Player.getBestPosition(contributions);
+		player.bestPosition = Foxtrick.L10n.getString(bestPos.position + 'Position.abbr');
+		player.bestPositionLong = Foxtrick.L10n.getString(bestPos.position + 'Position');
+		player.bestPositionValue = bestPos.value;
+
+		let htms = pNode.querySelector('.ft-htms-points');
+		if (htms) {
+			player.htmsAbility = parseInt(htms.getAttribute('data-htms-ability'), 10);
+			player.htmsPotential = parseInt(htms.getAttribute('data-htms-potential'), 10);
+		}
+
+		let psico = pNode.querySelector('.ft-psico');
+		if (psico) {
+			let psicoTSI = psico.getAttribute('data-psico-avg');
+			let psicoTitle = psico.getAttribute('data-psico-skill');
+			player.psicoTSI = psicoTSI;
+			player.psicoTitle = psicoTitle;
+		}
+	};
+
+	let addLinks = (player, pNode, urlTmpl) => {
+		let bookmarkLink = pNode.querySelector('.bookmarkSmall');
+		if (bookmarkLink) {
+			player.bookmarkLink = bookmarkLink.cloneNode(true);
+			player.bookmarkLink.target = '_blank';
+		}
+
+		let hotlistLink = pNode.querySelector('a[href*="hotList"]');
+		if (hotlistLink) {
+			player.hotlistLink = hotlistLink.cloneNode(true);
+			player.hotlistLink.target = '_blank';
+		}
+
+		// playerstats
+		let ps = doc.createElement('a');
+		player.performanceHistory = ps;
+		ps.target = '_blank';
+		ps.title = Foxtrick.L10n.getString('PerformanceHistory');
+		let pH = urlTmpl.replace('/Club/Players/Player.aspx', '/Club/Players/PlayerStats.aspx') +
+			'&ShowAll=true';
+		ps.href = pH;
+
+		Foxtrick.addImage(doc, ps, {
+			src: Foxtrick.InternalPath + 'resources/img/shortcuts/stats.png',
+			alt: Foxtrick.L10n.getString('PerformanceHistory.abbr'),
+			height: 16,
+			'aria-label': ps.title,
+		});
+
+		let tc = doc.createElement('a');
+		player.transferCompare = tc;
+		tc.target = '_blank';
+		tc.title = Foxtrick.L10n.getString('TransferCompare');
+		tc.href =
+			urlTmpl.replace('/Club/Players/Player.aspx', '/Club/Transfers/TransferCompare.aspx');
+
+		let tcImg = doc.createElement('img');
+		tcImg.height = 16;
+		tcImg.src = '/App_Themes/Standard/images/ActionIcons/sell.png';
+		tcImg.alt = Foxtrick.L10n.getString('TransferCompare.abbr');
+		tcImg.setAttribute('aria-label', tc.title);
+		tc.appendChild(tcImg);
+	};
+
+	let parseBidInfo = (player, bidContainer) => {
+		let items = [...bidContainer.getElementsByClassName('transferPlayerInfoItems')].reverse();
+		let bid = items[2];
+		player.currentBid = Foxtrick.trimnum(bid.textContent);
+
+		let bidder = items[1];
+		let bidderLink = bidder.querySelector('a');
+		if (!bidderLink)
+			return;
+
+		let bidDesc = Foxtrick.L10n.getString('CurrentBidder');
+		let link = bidderLink.cloneNode(true);
+		link.target = '_blank';
+
+		let shortBidder = link.cloneNode(true);
+		shortBidder.textContent = '';
+		shortBidder.title = bidDesc + ': ' + shortBidder.title;
+		player.currentBidderLinkShort = shortBidder;
+
+		link.title = bidDesc;
+		player.currentBidderLink = link;
+
+		let bidderImg = doc.createElement('img');
+		bidderImg.height = 16;
+		bidderImg.src = '/Img/Icons/dollar.gif';
+		bidderImg.alt = link.textContent;
+		bidderImg.setAttribute('aria-label', bidDesc);
+		shortBidder.appendChild(bidderImg);
+	};
+
+	let parseStatus = (player, statusContainer) => {
+		player.redCard = 0;
+		player.yellowCard = 0;
+		player.bruised = false;
+		player.injured = false;
+
+		let imgs = statusContainer.getElementsByTagName('img');
+		Foxtrick.forEach(function(img) {
+			if (img.className == 'cardsOne') {
+				if (/red_card/i.test(img.src))
+					player.redCard = 1;
+				else
+					player.yellowCard = 1;
+			}
+			else if (img.className == 'cardsTwo') {
+				player.yellowCard = 2;
+			}
+			else if (img.className == 'injuryBruised') {
+				player.bruised = true;
+			}
+			else if (img.className == 'injuryInjured') {
+				player.injured = true;
+			}
+		}, imgs);
+	};
+
+	let parseAttributes = (player, pNode, attrContainer) => {
+		// characteristics row - experience, leadership, form
+		let links = attrContainer.getElementsByTagName('a');
+		const ATTRIBUTES = ['experience', 'leadership', 'form'];
+		for (let [idx, attr] of ATTRIBUTES.entries())
+			player[attr] = Foxtrick.util.id.getSkillLevelFromLink(links[idx]);
+
+		// left info table - owner, age, TSI, specialty, deadline
+		let infoTable = pNode.querySelector('.transferPlayerInformation table');
+		let ownerCell = infoTable.rows[0].cells[1];
+		player.currentClubLink = ownerCell.querySelector('a').cloneNode(true);
+		player.ageText = infoTable.rows[1].cells[1].textContent.trim();
+		let ageMatch = player.ageText.match(/(\d+)/g);
+		player.age = { years: parseInt(ageMatch[0], 10), days: parseInt(ageMatch[1], 10) };
+		player.ageYears = player.age.years;
+		player.tsi = Foxtrick.trimnum(infoTable.rows[2].cells[1].textContent);
+		let specialty = infoTable.rows[3].cells[1].textContent.trim();
+		player.specialty = specialty == '-' ? '' : specialty;
+		player.specialtyNumber = Foxtrick.L10n.getNumberFromSpecialty(player.specialty);
+		player.deadline = infoTable.rows[4].cells[1].cloneNode(true);
+	};
+
+	let parsePlayer = (playerNode) => {
+		var p = { playerNode };
+
 		try {
-			var divs = playerInfo.getElementsByTagName('div');
-
 			// first row - country, name, ID
-			var flag = playerInfo.querySelector('.flag');
-			var leagueId = Foxtrick.getParameterFromUrl(flag.href, 'leagueId');
-			player.countryId = Foxtrick.XMLData.getCountryIdByLeagueId(leagueId);
-			var nameLink = playerInfo.querySelector('.transfer_search_playername a');
-			player.nameLink = nameLink.cloneNode(true);
-			player.nameLink.target = '_blank';
-			player.id = Foxtrick.getParameterFromUrl(player.nameLink.href, 'playerId');
-			var bookmarkLink = playerInfo.querySelector('.bookmarkSmall');
-			if (bookmarkLink) {
-				player.bookmarkLink = bookmarkLink.cloneNode(true);
-				player.bookmarkLink.target = '_blank';
-			}
-			var hotlistLink = playerInfo.querySelector('a[href*="hotList"]');
-			if (hotlistLink) {
-				player.hotlistLink = hotlistLink.cloneNode(true);
-				player.hotlistLink.target = '_blank';
-			}
+			let nameLink = playerNode.querySelector('.transfer_search_playername a');
+			p.id = Foxtrick.getParameterFromUrl(nameLink.href, 'playerId');
+			p.nameLink = nameLink.cloneNode(true);
+			p.nameLink.target = '_blank';
+			addLinks(p, playerNode, nameLink.href);
 
-			var htms = playerInfo.querySelector('.ft-htms-points');
-			if (htms) {
-				player.htmsAbility = parseInt(htms.getAttribute('data-htms-ability'), 10);
-				player.htmsPotential = parseInt(htms.getAttribute('data-htms-potential'), 10);
-			}
+			let flag = playerNode.querySelector('.flag');
+			let leagueId = Foxtrick.getParameterFromUrl(flag.href, 'leagueId');
+			p.countryId = Foxtrick.XMLData.getCountryIdByLeagueId(leagueId);
 
-			player.redCard = 0;
-			player.yellowCard = 0;
-			player.bruised = false;
-			player.injured = false;
-
-			var imgs = divs[0].getElementsByTagName('img');
-			Foxtrick.forEach(function(img) {
-				if (img.className == 'cardsOne') {
-					if (img.src.search(/red_card/i) != -1) {
-						player.redCard = 1;
-					}
-					else {
-						player.yellowCard = 1;
-					}
-				}
-				else if (img.className == 'cardsTwo') {
-					player.yellowCard = 2;
-				}
-				else if (img.className == 'injuryBruised') {
-					player.bruised = true;
-				}
-				else if (img.className == 'injuryInjured') {
-					player.injured = true;
-				}
-			}, imgs);
-
-			var items = divs[0].getElementsByClassName('transferPlayerInfoItems');
-			var bid = items[items.length - 3];
-			player.currentBid = Foxtrick.trimnum(bid.textContent);
-			var bidder = items[items.length - 2];
-			var bidderLink = bidder.querySelector('a');
-			if (bidderLink) {
-				var bidDesc = Foxtrick.L10n.getString('CurrentBidder');
-				bidderLink = bidderLink.cloneNode(true);
-				bidderLink.target = '_blank';
-
-				var shortBidder = bidderLink.cloneNode(true);
-				shortBidder.textContent = '';
-				shortBidder.title = bidDesc + ': ' + shortBidder.title;
-				player.currentBidderLinkShort = shortBidder;
-
-				bidderLink.title = bidDesc;
-				player.currentBidderLink = bidderLink;
-
-				var bidderImg = doc.createElement('img');
-				bidderImg.height = 16;
-				bidderImg.src = '/Img/Icons/dollar.gif';
-				bidderImg.alt = bidderLink.textContent;
-				bidderImg.setAttribute('aria-label', bidDesc);
-				shortBidder.appendChild(bidderImg);
-			}
+			var [container] = playerNode.getElementsByTagName('div');
+			parseBidInfo(p, container);
+			parseStatus(p, container);
 
 			// check if the player is sold, if he is, then following info
-			// are not available and we go to the finally block with
-			// return
-			var characteristics = playerInfo.querySelector('.transferPlayerCharacteristics');
-			if (!characteristics)
-				return;
-
-			// characteristics row - experience, leadership, form
-			var links = characteristics.getElementsByTagName('a');
-			var order = ['experience', 'leadership', 'form'];
-			for (var i = 0; i < order.length; ++i) {
-				player[order[i]] = Foxtrick.util.id.getSkillLevelFromLink(links[i]);
+			// are not available and we return
+			let attrDiv = playerNode.querySelector('.transferPlayerCharacteristics');
+			if (attrDiv) {
+				parseAttributes(p, playerNode, attrDiv);
+				parseSkills(p, playerNode);
 			}
-
-			// left info table - owner, age, TSI, specialty, deadline
-			var infoTable = playerInfo.querySelector('.transferPlayerInformation table');
-			var ownerCell = infoTable.rows[0].cells[1];
-			player.currentClubLink = ownerCell.querySelector('a').cloneNode(true);
-			player.ageText = infoTable.rows[1].cells[1].textContent.trim();
-			var ageMatch = player.ageText.match(/(\d+)/g);
-			player.age = { years: parseInt(ageMatch[0], 10), days: parseInt(ageMatch[1], 10) };
-			player.ageYears = player.age.years;
-			player.tsi = Foxtrick.trimnum(infoTable.rows[2].cells[1].textContent);
-			var specialty = infoTable.rows[3].cells[1].textContent.trim();
-			player.specialty = (specialty == '-') ? '' : specialty;
-			player.specialtyNumber = Foxtrick.L10n.getNumberFromSpecialty(player.specialty);
-			player.deadline = infoTable.rows[4].cells[1].cloneNode(true);
-
-			var tc = doc.createElement('a');
-			tc.target = '_blank';
-			tc.title = Foxtrick.L10n.getString('TransferCompare');
-			tc.href = player.nameLink.href.replace('/Club/Players/Player.aspx',
-			                                       '/Club/Transfers/TransferCompare.aspx');
-			player.transferCompare = tc;
-			var tcImg = doc.createElement('img');
-			tcImg.height = 16;
-			tcImg.src = '/App_Themes/Standard/images/ActionIcons/sell.png';
-			tcImg.alt = Foxtrick.L10n.getString('TransferCompare.abbr');
-			tcImg.setAttribute('aria-label', tc.title);
-			tc.appendChild(tcImg);
-
-			// playerstats
-			var ps = doc.createElement('a');
-			ps.target = '_blank';
-			ps.title = Foxtrick.L10n.getString('PerformanceHistory');
-			var psUrl = player.nameLink.href;
-			psUrl = psUrl.replace('/Club/Players/Player.aspx',
-			                      '/Club/Players/PlayerStats.aspx') + '&ShowAll=true';
-			ps.href = psUrl;
-			player.performanceHistory = ps;
-			Foxtrick.addImage(doc, ps, {
-				src: Foxtrick.InternalPath + 'resources/img/shortcuts/stats.png',
-				alt: Foxtrick.L10n.getString('PerformanceHistory.abbr'),
-				height: 16,
-				'aria-label': ps.title,
-			});
-
-			// right skill table - skills
-			var skillTable = playerInfo.querySelector('.transferPlayerSkills table');
-			var skillLinks = Foxtrick.toArray(skillTable.getElementsByTagName('a'));
-			if (skillTable.querySelector('.findSimilarPlayers'))
-				// similar player feature link
-				skillLinks.shift();
-
-			var skillOrder = [
-				'stamina',
-				'keeper',
-				'playmaking',
-				'passing',
-				'winger',
-				'defending',
-				'scoring',
-				'setPieces'
-			];
-			var skills = {};
-			for (var i = 0; i < skillOrder.length; ++i) {
-				player[skillOrder[i]] = Foxtrick.util.id.getSkillLevelFromLink(skillLinks[i]);
-				skills[skillOrder[i]] = player[skillOrder[i]];
-			}
-			player.skills = skills;
-
-			var psico = playerInfo.querySelector('.ft-psico');
-			if (psico) {
-				var psicoTSI = psico.getAttribute('data-psico-avg');
-				var psicoTitle = psico.getAttribute('data-psico-skill');
-				player.psicoTSI = psicoTSI;
-				player.psicoTitle = psicoTitle;
-			}
-
-			var contributions = Foxtrick.Pages.Player.getContributions(skills, player);
-			for (var name in contributions)
-				player[name] = contributions[name];
-
-			var bestPosition = Foxtrick.Pages.Player.getBestPosition(contributions);
-			player.bestPosition =
-				Foxtrick.L10n.getString(bestPosition.position + 'Position.abbr');
-			player.bestPositionLong =
-				Foxtrick.L10n.getString(bestPosition.position + 'Position');
-			player.bestPositionValue = bestPosition.value;
-
 		}
 		catch (e) {
 			Foxtrick.log(e);
 		}
-		finally {
-			return player;
-		}
-	}, players);
+
+		// return player even if we've encountered a problem
+		return p;
+	};
+
+	let playerNodes = [...doc.querySelectorAll('.transferPlayerInfo')];
+	return playerNodes.map(parsePlayer);
+
 };
