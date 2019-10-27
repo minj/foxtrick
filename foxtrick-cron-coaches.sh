5348c6f81d1cc96cd8cfa3d49f97d7a69edd290a
LA-MJ <4mr.minj@gmail.com>
2019-10-27 13:54:10 +0100

    add foxtrick-cron-coaches.sh

---
+++ foxtrick-cron-coaches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo 'update coaches'
+
+set -e
+
+DIR=$(cd $(dirname $0); pwd)
+. "$DIR"/include.sh || logf "==============ERROR=========== include.sh"
+
+unset GIT_DIR GIT_WORK_TREE
+
+cd "$DIR/../cdn" || logf "Cannot cd to cdn"
+
+git stash
+python "$DIR/../master/maintainer/locale/updateCoaches.py" ./res/staff || logf "Cannot build"
+git commit -am updateCoaches.py || logf "Cannot commit updateCoaches.py"
+git push origin cdn || logf "Cannot push cdn to origin"
+
+log 'success update coaches'
