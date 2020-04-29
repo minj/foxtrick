932efba698f81f6e544510ff90a8e75c0ea6a546
LA-MJ <4mr.minj@gmail.com>
2020-04-29 20:27:53 +0200

    parametrize TARGET_REPO

--- foxtrick-cron-coaches.sh
+++ foxtrick-cron-coaches.sh
@@ -7,12 +7,13 @@ DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/include.sh || logf "==============ERROR=========== include.sh"
 
 unset GIT_DIR GIT_WORK_TREE
+TARGET_REPO=cdn
 
-cd "$DIR/../cdn" || logf "Cannot cd to cdn"
+cd "$DIR/../$TARGET_REPO" || logf "Cannot cd to $TARGET_REPO"
 
 git stash
 python "$DIR/../master/maintainer/locale/updateCoaches.py" ./res/staff || logf "Cannot build"
 git commit -am updateCoaches.py || true
-git push origin cdn || logf "Cannot push cdn to origin"
+git push origin $TARGET_REPO || logf "Cannot push $TARGET_REPO to origin"
 
 log 'success update coaches'
--- foxtrick-cron-crowdin.sh
+++ foxtrick-cron-crowdin.sh
@@ -7,23 +7,24 @@ DIR=$(cd $(dirname $0); pwd)
 . "$DIR"/cron-config.sh || logf "==============ERROR=========== cron-config.sh"
 
 unset GIT_DIR GIT_WORK_TREE
+TARGET_REPO=l10n
 
-cd "$DIR/../l10n" || logf "Cannot cd to l10n"
+cd "$DIR/../$TARGET_REPO" || logf "Cannot cd to $TARGET_REPO"
 git stash
-git checkout l10n || logf "Cannot checkout l10n"
-git pull --rebase origin l10n || logf "Cannot git pull rebase l10n"
+git checkout $TARGET_REPO || logf "Cannot checkout $TARGET_REPO"
+git pull --rebase origin $TARGET_REPO || logf "Cannot git pull rebase $TARGET_REPO"
 cd maintainer || logf "Cannot cd to maintainer"
 #./crowdin-upload.sh || echo "Cannot upload external translations"
 ./crowdin-download.sh || logf "Cannot download external translations"
 ./commit.locale.sh || logf "Cannot commit locale"
-git push origin l10n || logf "Cannot push l10n"
+git push origin $TARGET_REPO || logf "Cannot push $TARGET_REPO"
 
 cd "$DIR/../$BETA" || logf "Cannot cd to $BETA"
 git stash
 git checkout master || logf "Cannot checkout master"
 git fetch --all || logf "Cannot fetch"
 git rebase origin/master || logf "Cannot rebase master"
-git merge --no-ff --no-edit origin/l10n || logf "Cannot merge origin/l10n"
-git push origin master || logf "Cannot push l10n merge"
+git merge --no-ff --no-edit origin/$TARGET_REPO || logf "Cannot merge origin/$TARGET_REPO"
+git push origin master || logf "Cannot push $TARGET_REPO merge"
 
 log "Success crowdin update."
