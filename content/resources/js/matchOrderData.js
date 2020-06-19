4885329c85fb98a760d7a746baaa06bad47b6e7f
LA-MJ <4mr.minj@gmail.com>
2020-06-19 13:03:35 +0200

    cleanup

--- content/resources/js/matchOrderData.js
+++ content/resources/js/matchOrderData.js
@@ -1,9 +1,13 @@
+/* eslint-disable strict */
 /* global ht */
 (function() {
 	'use strict';
+
+	const BRUISED_HEALTH = 0.5;
+
 	// save player objects onto player strips
 	var playerlist = document.getElementById('list');
-	var ft_updatePlayers = function() {
+	var ftUpdatePlayers = function() {
 		var strips = document.querySelectorAll('#players .player');
 		if (!strips.length)
 			return;
@@ -11,24 +15,27 @@
 		ht.playerManager.players.forEach(function(player) {
 			// clone standardise for FT use
 			var clone = JSON.parse(JSON.stringify(player));
+
 			// FIXME: no motherClubBonus data
 			clone.transferListed = clone.isTransferlisted;
 			clone.skills.setPieces = clone.skills.setpieces;
 			delete clone.skills.setpieces;
-			clone.bruised = clone.health == 0.5;
+			clone.bruised = clone.health == BRUISED_HEALTH;
 
 			var id = clone.id;
+
 			// HTs use the same ID for elements in '#players' and in '.position'
+			/** @type {HTMLElement} */
 			var playerStrip = document.querySelector('#players #list_playerID' + id);
 			if (playerStrip && !playerStrip.dataset.json)
 				playerStrip.setAttribute('data-json', JSON.stringify(clone));
 		});
 	};
 
-	var MO = window.MutationObserver || window.WebKitMutationObserver;
 	var opts = { childList: true, subtree: true };
-	var observer = new MO(function() {
-		ft_updatePlayers();
+	var observer = new window.MutationObserver(function() {
+		ftUpdatePlayers();
 	});
 	observer.observe(playerlist, opts);
+
 })();
